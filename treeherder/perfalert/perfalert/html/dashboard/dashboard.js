9a9c1c66e658fbbd4218937f8ba8c737cb08b586
Ben Hearsum <bhearsum@mozilla.com>
2009-08-18 11:42:46 -0400

    bug 511116: Firefox tree should be default on dashboard, not Firefox3.5. r=me, patch=johnath
    --HG--
    branch : 1.0

--- treeherder/perfalert/perfalert/html/dashboard/dashboard.js
+++ treeherder/perfalert/perfalert/html/dashboard/dashboard.js
@@ -1,5 +1,5 @@
 // vim: set sw=2 ts=2 sts=2 tw=78 expandtab :
-DEFAULT_TREE = "Firefox3.5";
+DEFAULT_TREE = "Firefox";
 gDataUrlBase = "http://graphs.mozilla.org/server/getdata.cgi?";
 
 // We use Math.floor all the time
