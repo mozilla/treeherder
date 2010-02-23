75b9de51d5fa9d5483b2de85334fe9fb3603c260
Chris AtLee <catlee@mozilla.com>
2010-02-23 17:12:00 -0500

    Bug 547876 - Track only talos-r3 and talos-rev2-tiger machines. r=alice
    --HG--
    branch : 1.0

--- treeherder/perfalert/perfalert/analyze_db.py
+++ treeherder/perfalert/perfalert/analyze_db.py
@@ -20,7 +20,7 @@ def getTestData(series, start_time):
         db.test_runs.machine_id == db.machines.id,
         db.test_runs.build_id == db.builds.id,
         db.test_runs.date_run > start_time,
-        sa.not_(db.machines.name.like('talos-r3%')),
+        sa.or_(db.machines.name.like('talos-r3%'), db.machines.name.like('talos-rev2-tiger%')),
         sa.not_(db.machines.name.like("%stage%")),
         ))
 
@@ -49,7 +49,7 @@ def getTestSeries(branches, start_date, test_names):
                 db.tests.id == db.test_runs.test_id,
                 db.test_runs.date_run > start_date,
                 db.branches.name.in_(branches),
-                sa.not_(db.machines.name.like('talos-r3%')),
+                sa.or_(db.machines.name.like('talos-r3%'), db.machines.name.like('talos-rev2-tiger%')),
                 sa.not_(db.machines.name.like('%stage%')),
                 sa.not_(db.tests.pretty_name.like("%NoChrome%")),
                 sa.not_(db.tests.pretty_name.like("%Fast Cycle%")),
@@ -76,7 +76,7 @@ def getMachinesForTest(series):
         db.tests.id == db.test_runs.test_id,
         db.tests.id == series.test_id,
         db.machines.os_id == series.os_id,
-        sa.not_(db.machines.name.like('talos-r3%')),
+        sa.or_(db.machines.name.like('talos-r3%'), db.machines.name.like('talos-rev2-tiger%')),
         sa.not_(db.machines.name.like('%stage%')),
         )).distinct()
     result = q.execute()
