904237b6c229618b2bdd21e8f60b7604fc0ef787
Robert Helmer <rhelmer@rhelmer.org>
2012-01-17 12:21:54 -0800

    use MPL2 license

--- treeherder/perfalert/perfalert/analyze_talos.py
+++ treeherder/perfalert/perfalert/analyze_talos.py
@@ -1,3 +1,6 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
 import time, urllib, urllib2, re, os, sys
 import logging as log
 import cPickle as pickle
