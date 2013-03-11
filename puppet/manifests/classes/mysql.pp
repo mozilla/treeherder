6882628ba2a77b9e001026f6ecbf4a5994f6635a
mdoglio <mauro.doglio@gmail.com>
2013-03-11 12:06:20 +0000

    fix db user without password. move db and user creation to mysql.pp

--- puppet/manifests/classes/mysql.pp
+++ puppet/manifests/classes/mysql.pp
@@ -13,4 +13,16 @@ class mysql {
         enable => true,
         require => Package['mysql-server'];
     }
+
+    exec { "create-${DB_NAME}-db":
+        unless => "mysql -uroot ${DB_NAME}",
+        command => "mysql -uroot -e \"create database ${DB_NAME};\"",
+        require => Service["mysql"],
+    }
+
+    exec { "grant-${name}-db":
+        unless => "mysql -u${DB_USER} -p${DB_PASS} ${DB_NAME}",
+        command => "mysql -uroot -e \"grant all on ${DB_NAME}.* to ${DB_USER}@localhost identified by '${DB_PASS}';\"",
+        require => [Service["mysql"], Exec["create-${DB_NAME}-db"]]
+    }
 }
--- puppet/manifests/classes/treeherder.pp
+++ puppet/manifests/classes/treeherder.pp
@@ -1,16 +1,4 @@
-# playdoh-specific commands that get playdoh all going so you don't
-# have to.
-
-# TODO: Make this rely on things that are not straight-up exec.
+#any additional stuff goes heere
 class treeherder {
-    exec { "create_mysql_database":
-        command => "mysql -uroot -B -e'CREATE DATABASE $DB_NAME CHARACTER SET utf8;'",
-        unless  => "mysql -uroot -B --skip-column-names -e 'show databases' | /bin/grep '$DB_NAME'",
-    }
 
-    exec { "grant_mysql_database":
-        command => "mysql -uroot -B -e'GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@localhost # IDENTIFIED BY \"$DB_PASS\"'",
-        unless  => "mysql -uroot -B --skip-column-names mysql -e 'select user from user' | grep '$DB_USER'",
-        require => Exec["create_mysql_database"];
-    }
 }
