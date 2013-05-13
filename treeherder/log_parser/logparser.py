a57a380dae8e12996333dbbd0b876716b2de14e0
Cameron Dawson <cdawson@mozilla.com>
2013-05-13 16:11:18 -0700

    parsing headers

--- treeherder/log_parser/logparser.py
+++ treeherder/log_parser/logparser.py
@@ -1,6 +1,7 @@
 import urllib2
 import gzip
-from StringIO import StringIO
+import datetime
+import re
 
 
 class LogParseManager(object):
@@ -72,6 +73,10 @@ class LogParseManager(object):
                 for parser in parsers:
                     parser.parse_line(line)
 
+            # let the parsers know we're done with all the lines
+            for parser in parsers:
+                parser.finalize()
+
             gz_file.close()
 
 
@@ -79,22 +84,64 @@ class LogParserBase(object):
     """
     Base class for all log parsers.
 
-    Given a log, parse it and generate a meta-data object as a
-    Table of Contents for the log.
+    The child class will know how to parse a particular type of log and
+    will keep track of state for started, finished, failed, etc.
+
+    This class is called for each line of the log file, so it has no
+    knowledge of the log file itself, as a whole.
+
+    Once finalize() is done, the generated metadata object should be
+    ready as a Table of Contents (of sorts) for the log.
+
+    @@@ Question: This class presumes started/finished regex's based on
+        buildbot results.  Do we need to support other types?
+        If so, we may want to make the patterns more modular than this.
+        Or perhaps have a different base class for other types of parsers.
 
     """
 
+    # state constants
+
+    # while still reading the initial header section
+    ST_HEADER = 'header'
+    # after having started any section
+    ST_STARTED = "started"
+    # after having finished any section
+    ST_FINISHED = "finished"
+
+    # regex patterns for started and finished sections
+    pattern = ' (.*?) \(results: \d+, elapsed: (?:\d+ mins, )?\d+ secs\) \(at (\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+)\) ={9}'
+    re_start = re.compile('={9} Started' + pattern)
+    re_finish = re.compile('={9} Finished' + pattern)
+    re_property = re.compile('(\w*): (.*)')
+    date_format = '%Y-%m-%d %H:%M:%S.%f'
+
+    def parsetime(self, match):
+        return datetime.datetime.strptime(match, self.date_format)
+
     def __init__(self):
-        # the url of the log to be parsed
-        self.metadata = {}
-        self.state = "started"
+        self.metadata = {"header": {}}
+        self.state = self.ST_HEADER
 
     def get_metadata(self):
-        """Return the metadata object"""
+        """Return the collected metadata object"""
         return self.metadata
+
+    def parse_header(self, line):
+        """Parse out a value in the header"""
+        header = line.split(": ", 1)
+        if len(header == 2):
+            self.metadata["header"][header[0]] = header[1]
 
     def parse_line(self, line):
         """Parse a single line of the log"""
+        if self.state == self.ST_HEADER:
+            self.parse_header(line)
+        else:
+            self.parse_content(line)
+
+    def parse_content(self, line):
+        """Child class implements to handle parsing of sections"""
         raise NotImplementedError
 
     def finalize(self):
