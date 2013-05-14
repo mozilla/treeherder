dc80946e220f7602a53b1eaa1886a6a5d085a357
Cameron Dawson <cdawson@mozilla.com>
2013-05-13 17:19:45 -0700

    added more comments

--- treeherder/log_parser/logparser.py
+++ treeherder/log_parser/logparser.py
@@ -42,12 +42,20 @@ class LogParseManager(object):
         """
         Returns a list of ``LogParser`` child objects specific to the
         type of log that needs parsing.
+
+        @@@ - not clear if I'll need to map logs to a list of parsers
+              for them.  Need to figure out if there will be different
+              types of logs for a single job.  There ARE logs within logs
+              (like Mochitest) but that would be handled by the Parser
+              knowing it needs a sub-parser.
+              Still working this out...
+
         """
 
         # we always have a SummaryParser
         parser_list = [SummaryParser()]
 
-        # @@@ todo: Figure out the type for the log
+        # @@@ todo: Figure out the type for the log.
         #parser_list.append(FooParser())
         return parser_list
 
@@ -95,12 +103,18 @@ class LogParserBase(object):
 
     @@@ Question: This class presumes started/finished regex's based on
         buildbot results.  Do we need to support other types?
-        If so, we may want to make the patterns more modular than this.
+        If so, we will need to make the patterns more modular than this.
         Or perhaps have a different base class for other types of parsers.
 
     """
 
+    #################
     # state constants
+    #################
+    # @@@ My thought here is that if/when the child class has sub states within
+    #     ``started`` (or other states) they could concatenate that state
+    #     string to it, thereby making it a sub-state.  You could check for
+    #     the greater ``started`` by using ``startswith`` on the state.
 
     # while still reading the initial header section
     ST_HEADER = 'header'
@@ -109,26 +123,40 @@ class LogParserBase(object):
     # after having finished any section
     ST_FINISHED = "finished"
 
+    #################
     # regex patterns for started and finished sections
+    #################
     pattern = ' (.*?) \(results: \d+, elapsed: (?:\d+ mins, )?\d+ secs\) \(at (\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+)\) ={9}'
     re_start = re.compile('={9} Started' + pattern)
     re_finish = re.compile('={9} Finished' + pattern)
     re_property = re.compile('(\w*): (.*)')
     date_format = '%Y-%m-%d %H:%M:%S.%f'
 
-    def parsetime(self, match):
-        return datetime.datetime.strptime(match, self.date_format)
-
     def __init__(self):
         self.metadata = {"header": {}}
         self.state = self.ST_HEADER
+
+    def parsetime(self, match):
+        return datetime.datetime.strptime(match, self.date_format)
 
     def get_metadata(self):
         """Return the collected metadata object"""
         return self.metadata
 
     def parse_header(self, line):
-        """Parse out a value in the header"""
+        """
+        Parse out a value in the header
+
+        The header values in the log look like this:
+            builder: mozilla-central_ubuntu32_vm_test-crashtest-ipc
+            slave: tst-linux32-ec2-137
+            starttime: 1368466076.01
+            results: success (0)
+            buildid: 20130513091541
+            builduid: acddb5f7043c4d5b9f66619f9433cab0
+            revision: c80dc6ffe865
+
+        """
         header = line.split(": ", 1)
         if len(header == 2):
             self.metadata["header"][header[0]] = header[1]
@@ -145,7 +173,12 @@ class LogParserBase(object):
         raise NotImplementedError
 
     def finalize(self):
-        """Wrap-up of this parser, if needed."""
+        """
+        Wrap-up of this parser, if needed.
+
+        This can be handy to get the "final duration" based on the
+        last finished section.
+        """
         pass
 
 
@@ -156,7 +189,9 @@ class SummaryParser(LogParserBase):
     This parser gathers the data that shows on the bottom panel of the main
     TBPL page.
 
-    Maintains its own state
+    Maintains its own state.
+
+    @@@ probably should go in a different file, too?
     """
     def parseline(self, line):
         """Parse a single line of the log"""
