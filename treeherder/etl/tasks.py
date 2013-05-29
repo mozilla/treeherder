4deb7863a83b5e09c66f74df5e77ee61c0bcd6c3
mdoglio <mauro.doglio@gmail.com>
2013-05-29 14:45:52 +0100

    add fetch_pending_jobs task

--- treeherder/etl/tasks.py
+++ treeherder/etl/tasks.py
@@ -1,14 +1,13 @@
 import celery
-from treeherder.buildapi_consumer import BuildApiConsumer
+from treeherder.etl.buildapi import TreeherderBuildapiAdapter
 
 
 @celery.task(name='fetch-pending-jobs')
 def fetch_pending_jobs():
-    consumer = BuildApiConsumer()
-    consumer.fetch_pending_jobs()
+    adpter = TreeherderBuildapiAdapter()
+    adapter.process_pending_jobs()
 
-
-@celery.task(name='fetch-running-jobs')
-def fetch_running_jobs():
-    consumer = BuildApiConsumer()
-    consumer.fetch_running_jobs()
+# @celery.task(name='fetch-running-jobs')
+# def fetch_running_jobs():
+#     consumer = BuildApiConsumer()
+#     consumer.fetch_running_jobs()
