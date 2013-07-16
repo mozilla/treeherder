bf691b26d100d18dbb7dc25a587acee467c214ec
mdoglio <mauro.doglio@gmail.com>
2013-07-16 15:04:24 +0100

    add init_datasources command

--- treeherder/model/management/commands/load_initial_data.py
+++ treeherder/model/management/commands/load_initial_data.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
-
+from treeherder.model.models import Datasource, Repository
 
 class Command(BaseCommand):
     help = "Load initial data into the master db"
