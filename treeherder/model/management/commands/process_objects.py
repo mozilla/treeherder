8a1dc6dcdcd8f42ae76344476d824292bc38ffc4
mdoglio <mauro.doglio@gmail.com>
2013-07-24 14:20:16 +0100

    add process_object command and celery task

--- treeherder/model/management/commands/process_objects.py
+++ treeherder/model/management/commands/process_objects.py
@@ -1,1 +1,17 @@
-__author__ = 'mdoglio'
+from optparse import make_option
+from django.core.management.base import BaseCommand
+from treeherder.model.tasks import process_objects
+
+class Command(BaseCommand):
+    help = """Process a number of objects with status 'ready' in the objectstore"""
+
+    option_list = BaseCommand.option_list + (
+        make_option('--limit',
+            action='store',
+            dest='limit',
+            default=None,
+            help='Limit the number of objects to process'),
+    )
+
+    def handle(self, *args, **options):
+        process_objects.delay(limit=options['limit'])
