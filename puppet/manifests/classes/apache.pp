871882ded6d70538a29c84b5a259df842fa64487
mdoglio <mauro.doglio@gmail.com>
2013-08-01 12:58:10 +0100

    update puppet modules for amazon linux compatibility

--- puppet/manifests/classes/apache.pp
+++ puppet/manifests/classes/apache.pp
@@ -1,44 +1,54 @@
+$apache_devel = $operatingsystem ? {
+  ubuntu => "apache2-dev",
+  default => "httpd-devel",
+}
+
+$apache_vhost_path = $operatingsystem ? {
+  ubuntu => "/etc/apache2/sites-enabled",
+  default => "/etc/httpd/conf.d",
+}
+
+$apache_service = $operatingsystem ? {
+  ubuntu => "apache2",
+  default => "httpd",
+}
+
 class apache {
-            package { "apache2":
-                ensure => present,
-                before => File['/etc/apache2/sites-enabled/treeherder-service.conf'];
-            }
+  package { $apache_devel:
+    ensure => present
+  }
 
-            package { "apache2-dev":
-                ensure => present,
-                before => [
-                    Exec['a2enmod rewrite'],
-                    Exec['a2enmod proxy'],
-                    Exec['a2enmod proxy_http'],   
-                ];
-            }
+  package { $apache_service:
+    ensure => present
+  }
 
-            file { "/etc/apache2/sites-enabled/treeherder-service.conf":
-                source => "$PROJ_DIR/puppet/files/apache/treeherder-service.conf",
-                owner => "root", group => "root", mode => 0644,
-                notify => Service["apache2"],
-                require => [
-                    Package['apache2']
-                ];
-            }
+  file { "${apache_vhost_path}/treeherder-service.conf":
+    content => template("${PROJ_DIR}/puppet/files/apache/treeherder-service.conf"),
+    owner => "root", group => "root", mode => 0644,
+    require => [Package[$apache_devel]],
+    notify => Service[$apache_service],
+  }
 
-            exec {
-                'a2enmod rewrite':
-                    onlyif => 'test ! -e /etc/apache2/mods-enabled/rewrite.load';
-                'a2enmod proxy':
-                    onlyif => 'test ! -e /etc/apache2/mods-enabled/proxy.load';
-                'a2enmod proxy_http':
-                    onlyif => 'test ! -e /etc/apache2/mods-enabled/proxy_http.load';
+  service { $apache_service:
+    ensure => running,
+    enable => true,
+    require => [
+      File["${apache_vhost_path}/treeherder-service.conf"]
+    ],
+  }
 
-            }
-
-            service { "apache2":
-                ensure => running,
-                enable => true,
-                require => [
-                    Package['apache2'],
-                    File['/etc/apache2/sites-enabled/treeherder-service.conf']
-                ];
-            }
-
-        }
+  if $operatingsystem == 'ubuntu'{
+    /*by default ubuntu doesn't have these modules enabled*/
+    exec {
+      'a2enmod rewrite':
+        onlyif => 'test ! -e /etc/apache2/mods-enabled/rewrite.load',
+        before => Service[$apache_service];
+      'a2enmod proxy':
+        onlyif => 'test ! -e /etc/apache2/mods-enabled/proxy.load',
+        before => Service[$apache_service];
+      'a2enmod proxy_http':
+        onlyif => 'test ! -e /etc/apache2/mods-enabled/proxy_http.load',
+        before => Service[$apache_service];
+    }
+  }
+}
--- puppet/manifests/classes/python.pp
+++ puppet/manifests/classes/python.pp
@@ -1,53 +1,78 @@
 # Install python and compiled modules for project
+
+$python_devel = $operatingsystem ? {
+    ubuntu => "python-dev",
+    default => "python-devel",
+}
+
+$libxml2 = $operatingsystem ? {
+    ubuntu => "libxml2-dev",
+    default => "libxml2-devel",
+}
+
+$site_packages = $operatingsystem ? {
+    ubuntu => "lib/python2.7/site-packages",
+    default => "lib64/python2.6/site-packages",
+}
+
 class python {
-    package {
-        ["python2.7-dev", "python2.7", "python-pip", "python-virtualenv", "git"]:
-            ensure => installed;
-    }
 
-    exec { "create-virtualenv":
-        command => "virtualenv /home/vagrant/venv/",
-        creates => "/home/vagrant/venv/",
-        require => [
-            Package['python-virtualenv'],
-        ],
-        user => "vagrant"
-    }
+  package{[$python_devel,
+           "gcc",
+           "python-setuptools",
+           "python-pip",
+           "python-virtualenv",
+           $libxml2]:
+    ensure => installed;
+  }
 
-    exec { "update-distribute":
-        command => "/home/vagrant/venv/bin/pip install --upgrade distribute",
+  exec {
+    "create-virtualenv":
+    cwd => "/home/${APP_USER}",
+    user => "${APP_USER}",
+    command => "virtualenv --no-site-packages ${VENV_DIR}",
+    creates => "${VENV_DIR}",
+    require => Package["python-virtualenv"],
+  }
+
+  exec {"activate-venv-on-login":
+    unless => "cat /home/${APP_USER}/.bashrc | grep 'source venv/bin/activate'",
+    command => "echo 'source venv/bin/activate' >> /home/${APP_USER}/.bashrc",
+    require => Exec["create-virtualenv"],
+    user => "${APP_USER}",
+  }
+
+  exec{"pip-install-compiled":
+    require => [
+      Exec['create-virtualenv'],
+      File[ "/home/${APP_USER}/pip_cache"],
+      Exec['update-distribute']
+    ],
+    user => "${APP_USER}",
+    cwd => '/tmp',
+    command => "${VENV_DIR}/bin/pip install --download-cache=/home/${APP_USER}/pip_cache -r ${PROJ_DIR}/requirements/compiled.txt",
+    timeout => 1800,
+  }
+
+  file {"/home/${APP_USER}/pip_cache":
+    ensure => directory,
+    owner  => "${APP_USER}",
+    group  => "${APP_GROUP}",
+  }
+
+  file { "vendor.pth":
+    path => "${VENV_DIR}/${site_packages}/vendor.pth",
+    ensure => present,
+    content => "$PROJ_DIR/vendor/",
+    require => Exec["create-virtualenv"],
+  }
+
+  exec { "update-distribute":
+        command => "${VENV_DIR}/bin/pip install --upgrade distribute",
         require => [
             Package['python-pip'],
-            Exec['create-virtualenv'],
+            Exec['create-virtualenv']
         ]
     }
 
-    file { "vendor.pth":
-        path => "/home/vagrant/venv/lib/python2.7/site-packages/vendor.pth",
-        ensure => present,
-        content => "$PROJ_DIR/vendor/",
-        require => Exec["create-virtualenv"],
-    }
-
-    exec { "pip-install-requirements":
-        command => "/home/vagrant/venv/bin/pip install \
-        -r $PROJ_DIR/requirements/compiled.txt -r $PROJ_DIR/requirements/dev.txt \
-        -r $PROJ_DIR/requirements/pure.txt",
-        require => [
-            Package['python-pip'],
-            Package['git'],
-            Package['python2.7-dev'],
-            Exec['update-distribute'],
-            Exec['create-virtualenv'],
-        ],
-        user => "vagrant",
-        logoutput => "on_failure",
-    }
-
-    exec {"activate-venv-on-login":
-        unless => "cat /home/vagrant/.bashrc | grep 'source venv/bin/activate'",
-        command => "echo 'source venv/bin/activate' >> /home/vagrant/.bashrc",
-        require => Exec["create-virtualenv"],
-        user => vagrant
-    }
 }
