fe0cf6d70598d3ee1609b0436a0f9b41be3a683f
Cameron Dawson <cdawson@mozilla.com>
2013-08-08 09:18:02 -0700

    local settings file shouldn't be persisted to repo

--- ui/js/config/local.conf.js
+++
@@ -1,5 +0,0 @@
-// mozilla hosted service
-//window.thServiceDomain = "http://dev.treeherder.mozilla.org";
-
-// local vagrant instance of service
-window.thServiceDomain = "http://local.treeherder.mozilla.org";
