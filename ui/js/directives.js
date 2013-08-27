4330fb89d2e2e8b96ec7cdb31d4f0fbd9b5cf4d6
Cameron Dawson <cdawson@mozilla.com>
2013-08-26 18:03:23 -0700

    code cleanup.

--- ui/js/directives.js
+++ ui/js/directives.js
@@ -1,14 +1,3 @@
 'use strict';
 
 /* Directives */
-treeherder.directive('ngRightClick', function($parse) {
-    return function(scope, element, attrs) {
-        var fn = $parse(attrs.ngRightClick);
-        element.bind('contextmenu', function(event) {
-            scope.$apply(function() {
-                event.preventDefault();
-                fn(scope, {$event:event});
-            });
-        });
-    };
-});
