701914f27e19c5d68d3cd0b168adcd49be467b55
Cameron Dawson <cdawson@mozilla.com>
2013-11-08 10:46:29 -0800

    attempt to fix travis test run

--- ui/js/config/sample.local.conf.js
+++ ui/js/config/sample.local.conf.js
@@ -3,3 +3,8 @@
 
 // local vagrant instance of service
 window.thServiceDomain = "http://local.treeherder.mozilla.org";
+
+// dummy values required to use the library at: https://tbpl.mozilla.org/js/Config.js
+// for the platform name conversion
+window.BuildbotDBUser = "Treeherder";
+window.PushlogJSONParser = "None";
