111cd1f134404931fd3857a3edc1824c67ae2936
Cameron Dawson <cdawson@mozilla.com>
2013-11-08 09:50:35 -0800

    fix to unit tests

--- tests/ui/config/karma.conf.js
+++ tests/ui/config/karma.conf.js
@@ -17,6 +17,7 @@ module.exports = function (config) {
             'test/vendor/angular/angular-mocks.js',
             'test/vendor/jasmine-jquery.js',
             'test/unit/**/*.js',
+            'https://tbpl.mozilla.org/js/Config.js',
 
             // fixtures
             {pattern: 'test/mock/*.json', watched: true, served: true, included: false}
