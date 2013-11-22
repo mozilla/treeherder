30d36571eaaa24bc4a741bc1813bb28e9ed0f3b1
Cameron Dawson <cdawson@mozilla.com>
2013-11-22 11:02:03 -0800

    dang, accidentally checked in my script...

--- camtrans.py
+++
@@ -1,9 +0,0 @@
-from treeherder.etl import buildbot
-import pprint
-
-result = {}
-for group in buildbot.GROUP_NAMES:
-    for name in buildbot.GROUP_NAMES[group]:
-        result[name] = group
-
-pprint.pprint(result)
