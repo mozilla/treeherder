538a01fb219eef75ef220c32171c7dc31a82a1e1
Cameron Dawson <cdawson@mozilla.com>
2013-12-02 16:44:51 -0800

    fix and consolidate button class (color) logic

--- ui/js/providers.js
+++ ui/js/providers.js
@@ -34,3 +34,78 @@ treeherder.provider('thStarTypes', function() {
         };
     };
 });
+
+treeherder.provider('thResultStatusInfo', function() {
+    this.$get = function() {
+        return function(resultState) {
+            // default if there is no match, used for pending
+            var resultStatusInfo = {
+                btnClass: "btn-default",
+                showButtonIcon: "glyphicon glyphicon-time",
+                jobButtonIcon: ""
+            };
+
+            switch (resultState) {
+                case "busted":
+                    resultStatusInfo = {
+                        btnClass: "btn-danger",
+                        showButtonIcon: "glyphicon glyphicon-fire",
+                        jobButtonIcon: "glyphicon glyphicon-fire"
+                    };
+                    break;
+                case "exception":
+                    resultStatusInfo = {
+                        btnClass: "btn-purple",
+                        showButtonIcon: "glyphicon glyphicon-fire",
+                        jobButtonIcon: "glyphicon glyphicon-fire"
+                    };
+                    break;
+                case "running":
+                    resultStatusInfo = {
+                        btnClass: "btn-ltgray",
+                        showButtonIcon: "glyphicon glyphicon-time",
+                        jobButtonIcon: ""
+                    };
+                    break;
+                case "retry":
+                    resultStatusInfo = {
+                        btnClass: "btn-primary",
+                        showButtonIcon: "glyphicon glyphicon-time",
+                        jobButtonIcon: ""
+                    };
+                    break;
+                case "success":
+                    resultStatusInfo = {
+                        btnClass: "btn-success",
+                        showButtonIcon: "glyphicon glyphicon-ok",
+                        jobButtonIcon: ""
+                    };
+                    break;
+                case "testfailed":
+                    resultStatusInfo = {
+                        btnClass: "btn-warning",
+                        showButtonIcon: "glyphicon glyphicon-warning-sign",
+                        jobButtonIcon: "glyphicon glyphicon-warning-sign"
+                    };
+                    break;
+                case "usercancel":
+                    resultStatusInfo = {
+                        btnClass: "btn-pink",
+                        showButtonIcon: "glyphicon glyphicon-stop",
+                        jobButtonIcon: ""
+                    };
+                    break;
+                case "unknown":
+                    resultStatusInfo = {
+                        btnClass: "btn-black",
+                        showButtonIcon: "glyphicon glyphicon-time",
+                        jobButtonIcon: ""
+                    };
+                    break;
+            }
+
+            return resultStatusInfo;
+        };
+
+    };
+});
