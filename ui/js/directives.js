7e15faaa9bdaae1af85299d2b5d13df7532b454d
Cameron Dawson <cdawson@mozilla.com>
2014-03-04 17:33:33 -0800

    fix to share-able links

--- ui/js/directives.js
+++ ui/js/directives.js
@@ -191,8 +191,8 @@ treeherder.directive('thCloneJobs', function(
 
                 revision = resultset.revisions[i];
 
-                element.scope.revisionRepoUrl = $rootScope.currentRepo.url + "/rev/" + revision;
-//                element.scope.revisionRepoUrl = element.scope.urlBasePath + "/rev/" + revision;
+                revision['urlBasePath'] = $rootScope.urlBasePath;
+                revision['currentRepo'] = $rootScope.currentRepo;
 
                 userTokens = revision.author.split(/[<>]+/);
                 if (userTokens.length > 1) {
