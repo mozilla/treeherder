2cbd66db2bf504dadb858fc416fbe57c3483c19b
Cameron Dawson <cdawson@mozilla.com>
2014-03-12 16:10:08 -0700

    not logged in errors should be "danger"

--- ui/plugins/pinboard.js
+++ ui/plugins/pinboard.js
@@ -40,7 +40,7 @@ treeherder.controller('PinboardCtrl',
                 $scope.classification.who = $scope.user.email;
                 thPinboard.save($scope.classification);
             } else {
-                thNotify.send("must be logged in to classify jobs");
+                thNotify.send("must be logged in to classify jobs", "danger");
             }
         };
 
@@ -49,7 +49,7 @@ treeherder.controller('PinboardCtrl',
                 $scope.classification.who = $scope.user.email;
                 thPinboard.saveClassificationOnly($scope.classification);
             } else {
-                thNotify.send("must be logged in to classify jobs");
+                thNotify.send("must be logged in to classify jobs", "danger");
             }
         };
 
@@ -57,7 +57,7 @@ treeherder.controller('PinboardCtrl',
             if ($scope.user.loggedin) {
                 thPinboard.saveBugsOnly();
             } else {
-                thNotify.send("must be logged in to classify jobs");
+                thNotify.send("must be logged in to classify jobs", "danger");
             }
         };
 
