2a604b47a01b10a8522150ca936343c06019bf46
Cameron Dawson <cdawson@mozilla.com>
2014-03-14 10:09:26 -0700

    classification delete partially working

--- ui/plugins/annotations/controller.js
+++ ui/plugins/annotations/controller.js
@@ -1,12 +1,33 @@
 "use strict";
 
 treeherder.controller('AnnotationsPluginCtrl',
-    function AnnotationsPluginCtrl($scope, $log) {
+    function AnnotationsPluginCtrl($scope, $log, ThJobClassificationModel, thNotify) {
         $log.debug("annotations plugin initialized");
 
         $scope.$watch('classifications', function(newValue, oldValue){
 
             $scope.tabs.annotations.num_items = newValue ? $scope.classifications.length : 0;
         }, true);
+
+        $scope.deleteClassification = function(classification) {
+            var jcModel = new ThJobClassificationModel(classification);
+            jcModel.delete()
+                .then(
+                    function(){
+                        thNotify.send({
+                            message: "Classification successfully deleted",
+                            severity: "success",
+                            sticky: false
+                        });
+                    },
+                    function(){
+                        thNotify.send({
+                            message: "Classification deletion failed",
+                            severity: "danger",
+                            sticky: true
+                        });
+                    }
+                );
+        };
     }
 );
