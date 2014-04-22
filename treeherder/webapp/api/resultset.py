604c6fd0ae13716e8fc33c19e4a080a5b763c39c
Cameron Dawson <cdawson@mozilla.com>
2014-04-22 11:31:25 -0700

    adding count limiters to list endpoints.  return revisions with resultsets.

--- treeherder/webapp/api/resultset.py
+++ treeherder/webapp/api/resultset.py
@@ -120,6 +120,7 @@ class ResultSetViewSet(viewsets.ViewSet):
             # all rs should have the revisions_uri, so add it here
             rs["revisions_uri"] = reverse("resultset-revisions",
                 kwargs={"project": jm.project, "pk": rs["id"]})
+            rs["revisions"] = jm.get_resultset_revisions_list(str(rs["id"]))
 
         jobs_ungrouped = jm.get_result_set_job_list(
             rs_map.keys(),
