1560b80dc30f3cb08a883ef8dd3fcff964eb5f36
Jonathan Eads <superjeads@gmail.com>
2014-04-23 13:34:15 -0700

    added grunt build for js/css

---
+++ Gruntfile.js
@@ -0,0 +1,70 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        useminPrepare:{
+            html:'webapp/app/index.html',
+            options:{
+                dest:'dist'
+            }
+        },
+        usemin:{ html:['dist/index.html'] },
+        copy:{
+
+            main: {
+
+                files: [
+
+                    { src:'webapp/app/index.html', dest:'dist/index.html' },
+
+                    { src:'webapp/app/help.html', dest:'dist/help.html' },
+
+                    { src:'webapp/app/logviewer.html', dest:'dist/logviewer.html' }
+                ],
+            },
+            // Copy img dir
+            img:{
+                expand: true,
+                src: 'webapp/app/img/*',
+                dest: 'dist/img/',
+                flatten: true
+                },
+            partials:{
+                expand: true,
+                src: 'webapp/app/partials/*',
+                dest: 'dist/partials/',
+                flatten: true
+                },
+            fonts:{
+                expand: true,
+                src: 'webapp/app/fonts/*',
+                dest: 'dist/fonts/',
+                flatten: true
+                },
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-usemin');
+
+    // Default tasks
+    grunt.registerTask('build', [
+
+        'copy:main',
+        'copy:img',
+        'copy:partials',
+        'copy:fonts',
+        'useminPrepare',
+        'concat',
+        'cssmin',
+        'uglify',
+        'usemin'
+        ]);
+
+
+};
