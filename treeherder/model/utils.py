395931e9424ec7fa8112d6d691a8cf549ddafdc8
Cameron Dawson <cdawson@mozilla.com>
2014-05-06 14:35:12 -0700

    returning excluded count of failures

--- treeherder/model/utils.py
+++ treeherder/model/utils.py
@@ -1,4 +1,5 @@
 import time
+import json
 import datetime
 import sys
 
@@ -10,3 +11,55 @@ def get_now_timestamp():
     This is useful because it can be mocked out in unit tests.
     """
     return int(time.time())
+
+
+def where_wolf(project, flat_exclusions):
+    """
+    Return the AND portion of a WHERE clause based on all the combinations of
+    ``flat_exclusions``
+
+    project - The repo/branch/project name to be used.  Ignore all others.
+    flat_exclusions - looks like::
+
+        {
+
+            "mozilla-inbound": {
+                "osx-10-9": {
+                    "Talos chrome": {
+                        "debug": 1,
+                        "opt": 1,
+                        "asan": 1,
+                        "pgo": 1
+                    },
+                    "Talos": {
+                        "debug": 1,
+                        "opt": 1,
+                        "asan": 1,
+                        "pgo": 1
+                    },
+                    "Talos xperf": { ... },
+                }
+            }
+        }
+
+    """
+
+    condition_list = []
+
+    for exclusion in flat_exclusions:
+        repos = json.loads(exclusion["flat_exclusion"])
+        for repo, platforms in repos.items():
+            if repo == project:
+                for platform, jobs in platforms.items():
+                    for job, opts in jobs.items():
+                        for opt, v in opts.items():
+                            condition_list.append(
+                                (" (mp.platform = '{0}' AND "
+                                "jt.name = '{1}' AND "
+                                "opt.name = '{2}')").format(platform, job, opt)
+                            )
+
+    if condition_list:
+        return "AND ({0})".format(" OR ".join(condition_list))
+    else:
+        return None
