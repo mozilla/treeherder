96771b3c573bb2e840977464d1e37e901fbfd04d
Jonathan Eads <superjeads@gmail.com>
2014-05-13 21:36:53 -0700

    new manage commands

---
+++ treeherder/model/management/commands/cycle_data.py
@@ -0,0 +1,30 @@
+from optparse import make_option
+from django.core.management.base import BaseCommand
+from treeherder.model.models import Repository
+from treeherder.model.derived import JobsModel
+from treeherder.model.tasks import cycle_data
+
+class Command(BaseCommand):
+    help = """Cycle data that exceeds the time constraint limit"""
+
+    option_list = BaseCommand.option_list + (
+
+        make_option('--debug',
+            action='store_true',
+            dest='debug',
+            default=None,
+            help='Write debug messages to stdout'),
+
+        make_option('--iterations',
+            action='store',
+            dest='iterations',
+            default=5,
+            help='Number of data cycle iterations to execute in a single run'),
+    )
+
+    def handle(self, *args, **options):
+
+        debug = options.get("debug", None)
+        max_iterations = int(options.get("iterations"))
+
+        cycle_data(max_iterations, debug)
