1a7a9e5c64a71f8aade8a46a773b9350687002ff
pnispel <pnispel@gmail.com>
2014-05-22 14:24:37 -0700

    changed directory for log cache from /var/tmp to webapp/log_cache

--- treeherder/settings/base.py
+++ treeherder/settings/base.py
@@ -260,7 +260,7 @@ CACHES = {
     },
     "filesystem": {
         "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
-        "LOCATION": "/var/tmp/django_cache",
+        "LOCATION": path("webapp", "log_cache"),
 
         "VERSION": 1
     }
