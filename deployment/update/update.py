08c77a340f536eb78a461b2b511e92983a66abfe
Jonathan Eads <superjeads@gmail.com>
2014-05-27 13:29:17 -0700

    made changes

--- deployment/update/update.py
+++ deployment/update/update.py
@@ -25,11 +25,14 @@ def update_code(ctx, tag):
         ctx.local('git submodule update --init --recursive')
         ctx.local("find . -type f -name '*.pyc' -delete")
 
-@task
 def update_assets(ctx):
-    with ctx.lcd(settings.SRC_DIR):
-        ctx.local("python2.6 manage.py collectstatic --noinput")
+    ctx.remote("{0} grunt build".format(settings.WEB_DIR))
 
+def update_oauth_credentials(ctx):
+
+    ctx.local("python2.6 manage.py export_project_credentials")
+
+    # Need to scp the credentials from admin node to workers and web service nodes
 
 @task
 def update_db(ctx):
@@ -44,14 +47,26 @@ def checkin_changes(ctx):
     """Use the local, IT-written deploy script to check in changes."""
     ctx.local(settings.DEPLOY_SCRIPT)
 
+def deploy_admin_node(ctx):
+    ctx.local('/usr/bin/supervisorctl restart celerybeat')
+    ctx.local('/usr/bin/supervisorctl restart celerymon')
+    ctx.local('/usr/bin/supervisorctl restart celery')
+
 @hostgroups(settings.WEB_HOSTGROUP, remote_kwargs={'ssh_key': settings.SSH_KEY})
-def deploy_app(ctx):
+def deploy_web_app(ctx):
     """Call the remote update script to push changes to webheads."""
     ctx.remote(settings.REMOTE_UPDATE_SCRIPT)
-    ctx.remote('/usr/bin/supervisorctl gunicorn restart')
+
+    # Make sure web assets are rebuilt when code is updated
+    update_assets(ctx)
+
+    ctx.remote('/usr/bin/supervisorctl restart gunicorn')
+    ctx.remote('/usr/bin/supervisorctl restart socketio-server')
 
 @hostgroups(settings.CELERY_HOSTGROUP, remote_kwargs={'ssh_key': settings.SSH_KEY})
-def deploy_app(ctx):
+def deploy_workers(ctx):
+    """Call the remote update script to push changes to workers."""
+    ctx.remote(settings.REMOTE_UPDATE_SCRIPT)
     ctx.remote('/usr/bin/supervisorctl restart celery')
 
 @task
@@ -81,10 +96,10 @@ def update(ctx):
 
 @task
 def deploy(ctx):
-    install_cron()
     checkin_changes()
-    deploy_app()
-    #update_celery()
+    deploy_admin_node()
+    deploy_web_app()
+    deploy_workers()
     update_info()
 
 
