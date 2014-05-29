3d2de3719a525bf2a4bd6dc3a7af40df37eacea9
Jonathan Eads <superjeads@gmail.com>
2014-05-28 18:19:20 -0700

    fixed stuff

--- deployment/update/update.py
+++ deployment/update/update.py
@@ -14,10 +14,20 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from commander.deploy import task, hostgroups
 import commander_settings as settings
 
+th_service_src = "{0}/treeherder-service/".format(settings.SRC_DIR)
+th_ui_src = "{0}/treeherder-ui/".format(settings.SRC_DIR)
+
 @task
 def update_code(ctx, tag):
     """Update the code to a specific git reference (tag/sha/etc)."""
-    with ctx.lcd(settings.SRC_DIR):
+    with ctx.lcd(th_service_src):
+        ctx.local('git checkout %s' % tag)
+        ctx.local('git pull -f')
+        ctx.local('git submodule sync')
+        ctx.local('git submodule update --init --recursive')
+        ctx.local("find . -type f -name '*.pyc' -delete")
+
+    with ctx.lcd(th_ui_src):
         ctx.local('git checkout %s' % tag)
         ctx.local('git pull -f')
         ctx.local('git submodule sync')
@@ -25,33 +35,16 @@ def update_code(ctx, tag):
         ctx.local("find . -type f -name '*.pyc' -delete")
 
 def update_assets(ctx):
-    ctx.remote("{0} grunt build".format(settings.WEB_DIR))
+    ctx.remote("{0}grunt build".format(th_ui_src))
 
 def update_oauth_credentials(ctx):
-
     ctx.local("python2.6 manage.py export_project_credentials")
-
-    # Need to scp the credentials from admin node to workers and web service nodes
-    push_worker_credentials(ctx)
-    push_web_credentials(ctx)
-
-@hostgroups(settings.CELERY_HOSTGROUP, remote_kwargs={'ssh_key': settings.SSH_KEY})
-def push_worker_credentials(ctx):
-    # scp treeherder/treeherder-service/treeherder/etl/data/credentials.json
-    # to hostgroup
-    pass
-
-@hostgroups(settings.WEB_HOSTGROUP, remote_kwargs={'ssh_key': settings.SSH_KEY})
-def push_web_credentials(ctx):
-    # scp treeherder/treeherder-service/treeherder/etl/data/credentials.json
-    # to hostgroup
-    pass
 
 @task
 def update_db(ctx):
     """Update the database schema, if necessary."""
 
-    with ctx.lcd(settings.SRC_DIR):
+    with ctx.lcd(th_service_src):
         ctx.local('python2.6 manage.py syncdb')
         ctx.local('python2.6 manage.py migrate')
 
@@ -59,11 +52,6 @@ def update_db(ctx):
 def checkin_changes(ctx):
     """Use the local, IT-written deploy script to check in changes."""
     ctx.local(settings.DEPLOY_SCRIPT)
-
-def deploy_admin_node(ctx):
-    ctx.local( '{0}/service celerybeat restart'.format(settings.BIN_DIR) )
-    ctx.local( '{0}/service celerymon restart'.format(settings.BIN_DIR) )
-    ctx.local( '{0}/service celery restart'.format(settings.BIN_DIR) )
 
 @hostgroups(settings.WEB_HOSTGROUP, remote_kwargs={'ssh_key': settings.SSH_KEY})
 def deploy_web_app(ctx):
@@ -78,7 +66,6 @@ def deploy_web_app(ctx):
 
     ctx.remote( '{0}/service httpd graceful'.format(settings.BIN_DIR) )
     ctx.remote( '{0}/service gunicorn restart'.format(settings.BIN_DIR) )
-    ctx.remote( '{0}/service socketio-server restart'.format(settings.BIN_DIR) )
 
 @hostgroups(settings.CELERY_HOSTGROUP, remote_kwargs={'ssh_key': settings.SSH_KEY})
 def deploy_workers(ctx):
@@ -89,14 +76,14 @@ def deploy_workers(ctx):
 @task
 def update_info(ctx):
     """Write info about the current state to a publicly visible file."""
-    with ctx.lcd(settings.SRC_DIR):
+    with ctx.lcd(th_service_src):
         ctx.local('date')
         ctx.local('git branch')
         ctx.local('git log -3')
         ctx.local('git status')
         ctx.local('git submodule status')
 
-        ctx.local('git rev-parse HEAD > media/revision')
+        ctx.local('git rev-parse HEAD > webapp/media/revision')
 
 
 @task
@@ -115,14 +102,7 @@ def update(ctx):
 @task
 def deploy(ctx):
     checkin_changes()
-    deploy_admin_node()
     deploy_web_app()
     deploy_workers()
     update_info()
 
-
-@task
-def update_site(ctx, tag):
-    """Update the app to prep for deployment."""
-    pre_update(tag)
-    update()
