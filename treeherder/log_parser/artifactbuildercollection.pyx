0e35319284c16605d87a9f30398cb8895fb5d65c
mdoglio <mauro.doglio@gmail.com>
2014-06-11 10:23:56 +0200

    fixup! Fixes a couple of bugs on data ingestion and log parsing

--- treeherder/log_parser/artifactbuildercollection.pyx
+++ treeherder/log_parser/artifactbuildercollection.pyx
@@ -1,59 +1,60 @@
-import requests
-from contextlib import closing
+import urllib2
+import gzip
+import io
 import logging
-
+from contextlib import closing
 from .artifactbuilders import (BuildbotLogViewArtifactBuilder,
                                BuildbotJobArtifactBuilder)
 
 
 class ArtifactBuilderCollection(object):
     """
-    Run a log through a collection of Artifact Builders to generate artifacts.
+Run a log through a collection of Artifact Builders to generate artifacts.
 
 
-    Architecture
-    ============
+Architecture
+============
 
-    ArtifactBuilderCollection
-    ------------------
-        * Holds one or more instances of ``ArtifactBuilderBase``
-        * If ``builders`` passed in, uses those as the artifact
-          builders, otherwise creates the default artifact builders.
-        * Reads the log from the log handle/url and walks each line
-          calling into each artifact builder with each line for handling
-        * Maintains no state
+ArtifactBuilderCollection
+------------------
+* Holds one or more instances of ``ArtifactBuilderBase``
+* If ``builders`` passed in, uses those as the artifact
+builders, otherwise creates the default artifact builders.
+* Reads the log from the log handle/url and walks each line
+calling into each artifact builder with each line for handling
+* Maintains no state
 
 
-    ArtifactBuilderBase
-    -------------
-        * Base class for all artifact builders`.
-        * Manages:
-            * artifact
-            * line number
-            * parsers
-        * Passes lines into each ``Parser``
+ArtifactBuilderBase
+-------------
+* Base class for all artifact builders`.
+* Manages:
+* artifact
+* line number
+* parsers
+* Passes lines into each ``Parser``
 
-    BuildbotLogViewArtifactBuilder
-    -------------
-        * Parses out content for use in a visual Log Parser
-        * Parsers:
-            * StepParser, which has its own ErrorParser
+BuildbotLogViewArtifactBuilder
+-------------
+* Parses out content for use in a visual Log Parser
+* Parsers:
+* StepParser, which has its own ErrorParser
 
-    BuildbotJobArtifactBuilder
-    -------------
-        * Builds an artifact for the TBPL main UI panel
-        * Parsers:
-            * ErrorParser
-            * TinderboxPrintParser
-    """
+BuildbotJobArtifactBuilder
+-------------
+* Builds an artifact for the TBPL main UI panel
+* Parsers:
+* ErrorParser
+* TinderboxPrintParser
+"""
 
     def __init__(self, url, builders=None, check_errors=True):
         """
-        ``url`` - url of the log to be parsed
-        ``builders`` - ArtifactBuilder instances to generate artifacts.
-                       In omitted, use defaults.
+``url`` - url of the log to be parsed
+``builders`` - ArtifactBuilder instances to generate artifacts.
+In omitted, use defaults.
 
-        """
+"""
 
         self.url = url
         self.artifacts = {}
@@ -74,21 +75,35 @@ class ArtifactBuilderCollection(object):
                 BuildbotJobArtifactBuilder(self.url)
             ]
 
+    def get_log_handle(self, url):
+        """Hook to get a handle to the log with this url"""
+        return urllib2.urlopen(url)
+
     def parse(self):
         """
-        Iterate over each line of the log, running each parser against it.
+Iterate over each line of the log, running each parser against it.
 
-        Stream lines from the gzip file and run each parser against it,
-        building the ``artifact`` as we go.
+Stream lines from the gzip file and run each parser against it,
+building the ``artifact`` as we go.
 
-        """
-        with closing(requests.get(self.url, stream=True)) as response:
-            for line in response.iter_lines():
-                # run each parser on each line of the log
+"""
+
+        handle = None
+        gz_file = None
+        with closing(self.get_log_handle(self.url)) as handle:
+
+            # using BytesIO is a workaround. Apparently this is fixed in
+            # Python 3.2, but not in the 2.x versions. GzipFile wants the
+            # methods seek() and tell(), which don't exist on a normal
+            # fileobj.
+            # interesting write-up here:
+            # http://www.enricozini.org/2011/cazzeggio/python-gzip/
+            with closing(gzip.GzipFile(fileobj=io.BytesIO(handle.read()))) as gz_file:
+                for line in gz_file:
+                    # run each parser on each line of the log
+                    for builder in self.builders:
+                        builder.parse_line(line)
+
+                # gather the artifacts from all builders
                 for builder in self.builders:
-
-                    builder.parse_line(line)
-
-            # gather the artifacts from all builders
-            for builder in self.builders:
-                self.artifacts[builder.name] = builder.get_artifact()
+                    self.artifacts[builder.name] = builder.get_artifact()
