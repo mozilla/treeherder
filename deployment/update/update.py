2248b4fb4b02207456d4e0e0d7e29c31f9d8bc68
Jonathan Eads <superjeads@gmail.com>
2014-06-20 15:40:13 -0700

    stopping memcached to see if that's the problem

--- deployment/update/update.py
+++ deployment/update/update.py
@@ -68,6 +68,9 @@ def deploy_web_app(ctx):
     ctx.remote( '{0}/service gunicorn restart'.format(settings.SBIN_DIR) )
     ctx.remote( '{0}/service socketio-server restart'.format(settings.SBIN_DIR) )
 
+    # REMOVE: testing whether this solves the issue
+    ctx.remote( '{0}/service memcached stop'.format(settings.SBIN_DIR) )
+
 @hostgroups(
     settings.CELERY_HOSTGROUP, remote_kwargs={'ssh_key': settings.SSH_KEY})
 def deploy_workers(ctx):
