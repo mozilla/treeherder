79f48aaa2a9c43ba1bb2edbf1501ac3a97d3e162
Cameron Dawson <cdawson@mozilla.com>
2014-07-01 14:15:00 -0700

    set unclassified failure window to 24 hours

--- treeherder/model/derived/jobs.py
+++ treeherder/model/derived/jobs.py
@@ -149,8 +149,8 @@ class JobsModel(TreeherderModelBase):
     # 6 months in seconds
     DATA_CYCLE_INTERVAL = 15552000
 
-    # last 8 hours in seconds
-    UNCLASSIFIED_FAILURE_RANGE = 28800
+    # last 24 hours in seconds
+    UNCLASSIFIED_FAILURE_RANGE = 86400
 
     @classmethod
     def create(cls, project, host=None):
