e5791f50a24b506fdceace6e9c354b733ac7f452
pnispel <pnispel@gmail.com>
2014-07-15 16:30:16 -0700

    updated to use QUERY_PARAMS.getlist

--- treeherder/webapp/api/performance_data.py
+++ treeherder/webapp/api/performance_data.py
@@ -1,5 +1,4 @@
 import json
-import ast
 
 from rest_framework import viewsets
 from rest_framework.response import Response
@@ -12,7 +11,7 @@ class PerformanceDataViewSet(viewsets.ViewSet):
     This view serves performance charts data
     """
 
-    @action()
+    @link()
     @with_jobs
     def get_signatures_from_properties(self, request, project, jm, pk=None):
         """
@@ -23,7 +22,7 @@ class PerformanceDataViewSet(viewsets.ViewSet):
         """
 
         try:
-            props = request.DATA.get("properties", {})
+            props = json.loads(request.QUERY_PARAMS.get("properties", {}))
         except Exception as e:
             return Response("incorrect parameters", 400)
 
@@ -31,7 +30,7 @@ class PerformanceDataViewSet(viewsets.ViewSet):
 
         return Response(signatures)
 
-    @action()
+    @link()
     @with_jobs
     def get_performance_data(self, request, project, jm, pk=None):
         """
@@ -41,12 +40,9 @@ class PerformanceDataViewSet(viewsets.ViewSet):
         Output: performance charting data
         """
 
-        signature_string = request.DATA.get("signatures", "[]")
-
         try:
-            interval_seconds = abs(int(request.DATA.get("interval_seconds", 0)))
-            signatures = ast.literal_eval(signature_string)
-            signatures = [n.strip() for n in signatures]
+            signatures = request.QUERY_PARAMS.getlist("signatures")
+            interval_seconds = abs(int(request.QUERY_PARAMS.get("interval_seconds", 0)))
         except Exception as e:
             return Response("incorrect parameters", 400)
 
