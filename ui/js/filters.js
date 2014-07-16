4c7587d120a935344891de9db0ec2dbe1a3eca98
mdoglio <mauro.doglio@gmail.com>
2014-07-16 15:28:40 +0100

    Fix bug summary sanitization (1030401 and 1030959)
    The angular sanitize module was failing because some bug summaries
    contain unclosed tags. I added a filter to escape '<' and '>' characters
    before the word highlighter filter. This fixes both bug 1030401 and
    1030959

--- ui/js/filters.js
+++ ui/js/filters.js
@@ -69,3 +69,16 @@ treeherder.filter('highlightCommonTerms', function(){
     };
 });
 
+treeherder.filter('escapeHTML', function() {
+    return function(text){
+        if (text) {
+        return text.
+            replace(/&/g, '&amp;').
+            replace(/</g, '&lt;').
+            replace(/>/g, '&gt;').
+            replace(/'/g, '&#39;').
+            replace(/"/g, '&quot;');
+        }
+        return '';
+    };
+});
