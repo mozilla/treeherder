ca795a03b1a61717b37e77dfdea52a5240187bb2
mdoglio <mauro.doglio@gmail.com>
2014-08-05 12:00:18 +0100

    (Bug 1032643) Tweak bug suggestions

--- ui/plugins/failure_summary/controller.js
+++ ui/plugins/failure_summary/controller.js
@@ -1,63 +1,83 @@
 "use strict";
 
 treeherder.controller('BugsPluginCtrl', [
-    '$scope', '$rootScope', 'ThLog', 'ThJobArtifactModel', 'ThBugJobMapModel',
-    'ThJobClassificationModel', 'thNotify', '$modal', '$q',
+    '$scope', 'ThLog', 'ThJobArtifactModel','$q',
     function BugsPluginCtrl(
-        $scope, $rootScope, ThLog, ThJobArtifactModel, ThBugJobMapModel,
-        ThJobClassificationModel, thNotify, $modal, $q) {
+        $scope, ThLog, ThJobArtifactModel, $q) {
 
         var $log = new ThLog(this.constructor.name);
 
         $log.debug("bugs plugin initialized");
 
         var timeout_promise = null;
+        var failure_results = ["busted", "exception", "testfailed"];
+        var bug_limit = 20;
 
         var update_bugs = function(newValue, oldValue) {
-            $scope.bugs = [];
-            $scope.visible = "open";
-            $scope.show_all = false;
-            $scope.selected_bugs = {};
-            $scope.classification = null;
-
-
-            // fetch artifacts only if the job is finished
-            if (newValue) {
+            $scope.suggestions = [];
+            // retrieve bug suggestions only if the job has failures
+            if(angular.isDefined(newValue) &&
+                    failure_results.indexOf($scope.job.result) !== -1){
                 $scope.tabs.failure_summary.is_loading = true;
                 // if there's a ongoing request, abort it
                 if (timeout_promise !== null) {
                     timeout_promise.resolve();
                 }
                 timeout_promise = $q.defer();
+                
                 ThJobArtifactModel.get_list({
-                    name__in: "Open bugs,Closed bugs",
+                    name: "Bug suggestions",
                     "type": "json",
                     job_id: newValue
                 }, {timeout: timeout_promise})
-                .then(function (response) {
-                    // iterate to retrieve the total num of failures
-                    var searchIdx = {},
-                        idx = 0;
+                .then(function(artifact_list){
+                    // using a temporary array here to not trigger a
+                    // dirty check for every element pushed
+                    var suggestions = [];
+                    if(artifact_list.length > 0){
+                        var artifact = artifact_list[0];
+                        angular.forEach(artifact.blob, function (suggestion) {
 
-                    angular.forEach(response, function (artifact) {
-                        var open_closed = artifact.name === "Open bugs" ? "open" : "closed";
-
-                        // the new format is to use an array
-                        _.each(artifact.blob, function (failure) {
-                            if (!_.has(searchIdx, failure.search)) {
-                                $scope.bugs.push({'search': failure.search,
-                                                  'open': [],
-                                                  'closed': []});
-                                searchIdx[failure.search] = idx;
-                                idx++;
+                            if(suggestion.bugs.open_recent.length > bug_limit){
+                                suggestion.bugs.too_many_open_recent = true;
+                            }else{
+                                suggestion.bugs.too_many_open_recent  = false;
                             }
-                            $scope.bugs[searchIdx[failure.search]][open_closed] = failure.bugs;
+
+                            if(suggestion.bugs.all_others.length > bug_limit){
+                                suggestion.bugs.too_many_all_others = true;
+                            }else{
+                                suggestion.bugs.too_many_all_others = false;
+                            }
+
+                            if(suggestion.bugs.too_many_open_recent
+                                || suggestion.bugs.open_recent.length == 0){
+                                suggestion.open_recent_hidden = true;
+                            }else{
+                                suggestion.open_recent_hidden = false;
+                            }
+
+                            if(suggestion.bugs.too_many_all_others
+                                || suggestion.bugs.all_others.length == 0){
+                                suggestion.all_others_hidden = true;
+                            }else{
+                                suggestion.all_others_hidden = false;
+                            }
+
+
+
+                            suggestions.push(suggestion);
                         });
-                    });
+                        $scope.suggestions = suggestions;
+
+
+                    }
+
                 })
                 .finally(function () {
                     $scope.tabs.failure_summary.is_loading = false;
                 });
+
             }
         };
 
