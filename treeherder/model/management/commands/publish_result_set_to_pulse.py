bfe40dac6086d36585a344b8c29e01c06e328e59
Jonathan Eads <superjeads@gmail.com>
2014-08-26 17:27:56 -0700

    new manage.py command to support testing result_set_ids with the publish_to_pulse task

---
+++ treeherder/model/management/commands/publish_result_set_to_pulse.py
@@ -0,0 +1,34 @@
+from optparse import make_option
+from django.core.management.base import BaseCommand
+from treeherder.model.models import Repository
+from treeherder.model.derived import JobsModel
+from treeherder.model.tasks import publish_to_pulse
+
+class Command(BaseCommand):
+    help = """Cycle data that exceeds the time constraint limit"""
+
+    option_list = BaseCommand.option_list + (
+
+        make_option('--project',
+            action='store_true',
+            dest='project',
+            default='mozilla-inbound',
+            help='Name of the treeherder project: mozilla-inbound, mozilla-aurora etc...'),
+
+        make_option('--result_set_ids',
+            action='store',
+            dest='result_set_ids',
+            default=1,
+            help='Comma delimited list of result set ids to publish to pulse'),
+    )
+
+    def handle(self, *args, **options):
+
+        project = options.get("project")
+
+        result_set_ids = map(
+            lambda x: int(x or 0),
+            options.get("result_set_ids", "1").split(',')
+            )
+
+        publish_to_pulse(project, result_set_ids, 'result_set')
