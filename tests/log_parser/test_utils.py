e35938277af77166d813b4321b9eb036d910a1c4
KWierso <kwierso@gmail.com>
2014-09-09 15:15:08 -0700

    Bug 1059788 - Limit search_term to 100 characters and add a test to confirm it happens

--- tests/log_parser/test_utils.py
+++ tests/log_parser/test_utils.py
@@ -26,6 +26,34 @@ def test_get_error_search_term(line, exp_search_term):
 
     assert actual_search_term == exp_search_term
 
+LONG_LINE_TEST_CASES = (
+    (
+        (
+            'command timed out: 2400 seconds without output running '
+            '[\'/tools/buildbot/bin/python\', '
+            '\'scripts/scripts/android_emulator_unittest.py\', \'--cfg\', '
+            '\'android/androidx86.py\', \'--test-suite\', \'robocop-1\', '
+            '\'--test-suite\', \'robocop-2\', \'--test-suite\', \'robocop-3\', '
+            '\'--test-suite\', \'xpcshell\', \'--blob-upload-branch\', '
+            '\'b2g-inbound\', \'--download-symbols\', \'ondemand\'], '
+            'attempting to kill'
+        ),
+        (
+            'command timed out: 2400 seconds without output running '
+            '[\'/tools/buildbot/bin/python\', \'scripts/scrip'
+        )
+    ),
+)
+
+#command timed out: 2400 seconds without output running ['/tools/buildbot/bin/python', 'scripts/scripts/android_emulator_unittest.py', '--cfg', 'android/androidx86.py', '--test-suite', 'robocop-1', '--test-suite', 'robocop-2', '--test-suite', 'robocop-3', '--test-suite', 'xpcshell', '--blob-upload-branch', 'b2g-inbound', '--download-symbols', 'ondemand'], attempting to kill
+
+@pytest.mark.parametrize(("line", "exp_search_term"), LONG_LINE_TEST_CASES)
+def test_get_long_search_term(line, exp_search_term):
+    """tests that long search terms are capped at 100 characters"""
+
+    actual_search_term = get_error_search_term(line)
+
+    assert actual_search_term == exp_search_term
 
 CRASH_LINE_TEST_CASES = (
     (
