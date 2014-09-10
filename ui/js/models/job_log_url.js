55091eb19cb27b2c9a24295b378a9192941d89a3
AutomatedTester <dburns@mozilla.com>
2014-09-10 01:20:27 +0100

    Bug 1045846: Part 1: Remove the typo in the model name

--- ui/js/models/job_log_url.js
+++ ui/js/models/job_log_url.js
@@ -6,36 +6,36 @@ treeherder.factory('ThJobLogUrlModel', [
 
     // ThJobLogUrlModel is the js counterpart of job_type
 
-    var ThJobLogUrlModelModel = function(data) {
-        // creates a new instance of ThJobLogUrlModelModel
+    var ThJobLogUrlModel = function(data) {
+        // creates a new instance of ThJobLogUrlModel
         // using the provided properties
         angular.extend(this, data);
     };
 
-    ThJobLogUrlModelModel.get_uri = function(){
+    ThJobLogUrlModel.get_uri = function(){
         var url = thUrl.getProjectUrl("/job-log-url/");
         $log.log(url);
         return url;
     };
 
-    ThJobLogUrlModelModel.get_list = function(job_id) {
-        // a static method to retrieve a list of ThJobLogUrlModelModel
-        return $http.get(ThJobLogUrlModelModel.get_uri()+"?job_id="+job_id)
+    ThJobLogUrlModel.get_list = function(job_id) {
+        // a static method to retrieve a list of ThJobLogUrlModel
+        return $http.get(ThJobLogUrlModel.get_uri()+"?job_id="+job_id)
             .then(function(response) {
                 var item_list = [];
                 angular.forEach(response.data, function(elem){
-                    item_list.push(new ThJobLogUrlModelModel(elem));
+                    item_list.push(new ThJobLogUrlModel(elem));
                 });
                 return item_list;
         });
     };
 
-    ThJobLogUrlModelModel.get = function(pk) {
-        // a static method to retrieve a single instance of ThJobLogUrlModelModel
-        return $http.get(ThJobLogUrlModelModel.get_uri()+pk).then(function(response) {
-            return new ThJobLogUrlModelModel(response.data);
+    ThJobLogUrlModel.get = function(pk) {
+        // a static method to retrieve a single instance of ThJobLogUrlModel
+        return $http.get(ThJobLogUrlModel.get_uri()+pk).then(function(response) {
+            return new ThJobLogUrlModel(response.data);
         });
     };
 
-    return ThJobLogUrlModelModel;
+    return ThJobLogUrlModel;
 }]);
