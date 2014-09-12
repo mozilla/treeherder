c62146b79446ecb392822c3c80224c84f17c425e
Cameron Dawson <cdawson@mozilla.com>
2014-09-12 12:06:21 -0700

    Bug 1066790 - make menu more persistent with context menu

--- ui/js/controllers/repository.js
+++ ui/js/controllers/repository.js
@@ -6,5 +6,14 @@ treeherder.controller('RepositoryMenuCtrl', [
         $scope, ThRepositoryModel) {
 
         $scope.groupedRepos = ThRepositoryModel.getOrderedRepoGroups;
+
+        $scope.closeable = true;
+
+        $('.dropdown.keep-open').on({
+            "shown.bs.dropdown": function() { $scope.closable = false; },
+            "click":             function() { $scope.closable = true; },
+            "hide.bs.dropdown":  function() { return $scope.closable; }
+        });
+
     }
 ]);
