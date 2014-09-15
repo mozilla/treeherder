8f9cb684f395af298765d069c6679ff4a1398e49
Cameron Dawson <cdawson@mozilla.com>
2014-09-15 11:11:35 -0700

    debugging repo menu close control

--- ui/js/controllers/repository.js
+++ ui/js/controllers/repository.js
@@ -10,9 +10,43 @@ treeherder.controller('RepositoryMenuCtrl', [
         $scope.closeable = true;
 
         $('.dropdown.keep-open').on({
-            "shown.bs.dropdown": function() { $scope.closable = false; },
-            "click":             function() { $scope.closable = true; },
-            "hide.bs.dropdown":  function() { return $scope.closable; }
+            "shown.bs.dropdown": function(ev) {
+                $scope.closeable = false;
+                console.log("repo menu", "button", "shown.bs.dropdown", $scope.closeable, ev.target);
+            },
+            "click":             function(ev) {
+                $scope.closeable = true;
+                console.log("repo menu", "button", "click", $scope.closeable, ev.target);
+            },
+            "mouseup":             function(ev) {
+//                $scope.closeable = true;
+                console.log("repo menu", "button", "mouseup", $scope.closeable, ev.target);
+            },
+            "hide.bs.dropdown":  function(ev) {
+                console.log("repo menu", "button", "hide.bs.dropdown", $scope.closeable, ev.target);
+                var closeable = $scope.closeable;
+                $scope.closeable = true;
+                return closeable;
+            }
+        });
+
+        $('.repo-dropdown-menu').on({
+            "shown.bs.dropdown": function(ev) {
+                $scope.closeable = false;
+                console.log("repo menu", "dropdown", "shown.bs.dropdown", $scope.closeable, ev.target);
+            },
+            "click":             function(ev) {
+                $scope.closeable = true;
+                console.log("repo menu", "dropdown", "click", $scope.closeable, ev.target);
+            },
+            "mouseup":             function(ev) {
+                $scope.closeable = false;
+                console.log("repo menu", "dropdown", "mouseup", $scope.closeable, ev.target);
+            },
+            "hide.bs.dropdown":  function(ev) {
+                console.log("repo menu", "dropdown", "hide.bs.dropdown", $scope.closeable, ev.target);
+                return $scope.closeable;
+            }
         });
 
     }
