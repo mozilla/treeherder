eb16bf1be8862074d997946b57c246d082182784
Cameron Dawson <cdawson@mozilla.com>
2014-09-15 12:07:48 -0700

    improved event handling for repo menu persistency

--- ui/js/controllers/repository.js
+++ ui/js/controllers/repository.js
@@ -12,18 +12,11 @@ treeherder.controller('RepositoryMenuCtrl', [
         $('.dropdown.keep-open').on({
             "shown.bs.dropdown": function(ev) {
                 $scope.closeable = false;
-                console.log("repo menu", "button", "shown.bs.dropdown", $scope.closeable, ev.target);
             },
             "click":             function(ev) {
                 $scope.closeable = true;
-                console.log("repo menu", "button", "click", $scope.closeable, ev.target);
-            },
-            "mouseup":             function(ev) {
-//                $scope.closeable = true;
-                console.log("repo menu", "button", "mouseup", $scope.closeable, ev.target);
             },
             "hide.bs.dropdown":  function(ev) {
-                console.log("repo menu", "button", "hide.bs.dropdown", $scope.closeable, ev.target);
                 var closeable = $scope.closeable;
                 $scope.closeable = true;
                 return closeable;
@@ -33,18 +26,14 @@ treeherder.controller('RepositoryMenuCtrl', [
         $('.repo-dropdown-menu').on({
             "shown.bs.dropdown": function(ev) {
                 $scope.closeable = false;
-                console.log("repo menu", "dropdown", "shown.bs.dropdown", $scope.closeable, ev.target);
             },
             "click":             function(ev) {
                 $scope.closeable = true;
-                console.log("repo menu", "dropdown", "click", $scope.closeable, ev.target);
             },
             "mouseup":             function(ev) {
                 $scope.closeable = false;
-                console.log("repo menu", "dropdown", "mouseup", $scope.closeable, ev.target);
             },
             "hide.bs.dropdown":  function(ev) {
-                console.log("repo menu", "dropdown", "hide.bs.dropdown", $scope.closeable, ev.target);
                 return $scope.closeable;
             }
         });
