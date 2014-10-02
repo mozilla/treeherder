c6318157086260e9f02d8d282d315711c979951a
Cameron Dawson <cdawson@mozilla.com>
2014-10-02 16:45:07 -0700

    Bug 1077136 - new cleanup_tasks file to avoid circular imports

---
+++ treeherder/etl/cleanup_tasks.py
@@ -0,0 +1,41 @@
+import urllib
+from celery import task, group
+from treeherder.model.derived import RefDataManager
+from .pushlog import MissingHgPushlogProcess
+
+
+@task(name='fetch-missing-push-logs')
+def fetch_missing_push_logs(missing_pushlogs):
+    """
+    Run several fetch_hg_push_log subtasks, one per repository
+    """
+    rdm = RefDataManager()
+    try:
+        repos = filter(lambda x: x['url'], rdm.get_all_repository_info())
+        # create a group of subtasks and apply them
+        g = group(fetch_missing_hg_push_logs.si(
+            repo['name'],
+            repo['url'],
+            missing_pushlogs[repo['name']]
+            )
+            for repo in repos if repo['dvcs_type'] == 'hg' and repo['name'] in missing_pushlogs)
+        g()
+    finally:
+        rdm.disconnect()
+
+
+@task(name='fetch-missing-hg-push-logs', time_limit=3*60)
+def fetch_missing_hg_push_logs(repo_name, repo_url, revisions):
+    """
+    Run a HgPushlog etl process
+
+    ``revisions`` is a list of changeset values truncated to 12 chars.
+    """
+    process = MissingHgPushlogProcess()
+
+    changesetParam = urllib.urlencode({"changeset", revisions})
+    urlStr = repo_url + '/json-pushes/?full=1&' + changesetParam
+
+    process.run(urlStr, repo_name)
+
+
--- treeherder/etl/tasks.py
+++ treeherder/etl/tasks.py
@@ -10,7 +10,7 @@ from .buildapi import (RunningJobsProcess,
                        Builds4hAnalyzer)
 from .bugzilla import BzApiBugProcess
 from .tbpl import OrangeFactorBugRequest, TbplBugRequest, BugzillaBugRequest
-from .pushlog import HgPushlogProcess, MissingHgPushlogProcess
+from .pushlog import HgPushlogProcess
 
 
 @task(name='fetch-buildapi-pending', time_limit=3*60)
@@ -63,41 +63,6 @@ def fetch_hg_push_log(repo_name, repo_url):
     """
     process = HgPushlogProcess()
     process.run(repo_url + '/json-pushes/?full=1', repo_name)
-
-
-@task(name='fetch-missing-push-logs')
-def fetch_missing_push_logs(missing_pushlogs):
-    """
-    Run several fetch_hg_push_log subtasks, one per repository
-    """
-    rdm = RefDataManager()
-    try:
-        repos = filter(lambda x: x['url'], rdm.get_all_repository_info())
-        # create a group of subtasks and apply them
-        g = group(fetch_missing_hg_push_logs.si(
-            repo['name'],
-            repo['url'],
-            missing_pushlogs[repo['name']]
-            )
-            for repo in repos if repo['dvcs_type'] == 'hg' and repo['name'] in missing_pushlogs)
-        g()
-    finally:
-        rdm.disconnect()
-
-
-@task(name='fetch-missing-hg-push-logs', time_limit=3*60)
-def fetch_missing_hg_push_logs(repo_name, repo_url, revisions):
-    """
-    Run a HgPushlog etl process
-
-    ``revisions`` is a list of changeset values truncated to 12 chars.
-    """
-    process = MissingHgPushlogProcess()
-
-    changesetParam = urllib.urlencode({"changeset", revisions})
-    urlStr = repo_url + '/json-pushes/?full=1&' + changesetParam
-
-    process.run(urlStr, repo_name)
 
 
 @task(name='fetch-bugs', time_limit=10 * 60)
