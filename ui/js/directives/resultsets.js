92db3d4392d94596784c9e9178415e2339322ed5
Cameron Dawson <cdawson@mozilla.com>
2014-10-30 17:35:07 -0700

    Revert "bug 1091932 - change {{ }} entries to on-way binding where possible"
    This reverts commit b548ebcc763e112cf079e5de093d529f1555a8ab.

--- ui/js/directives/resultsets.js
+++ ui/js/directives/resultsets.js
@@ -120,6 +120,23 @@ treeherder.directive('thResultStatusCount', [
     };
 }]);
 
+treeherder.directive('thRevision', [
+    '$parse',
+    function($parse) {
+
+    return {
+        restrict: "E",
+        link: function(scope, element, attrs) {
+            scope.$watch('resultset.revisions', function(newVal) {
+                if (newVal) {
+                    scope.revisionUrl = scope.currentRepo.url + "/rev/" + scope.revision.revision;
+                }
+            }, true);
+        },
+        templateUrl: 'partials/main/thRevision.html'
+    };
+}]);
+
 treeherder.directive('thAuthor', function () {
 
     return {
