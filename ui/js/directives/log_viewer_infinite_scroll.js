2d426e011404fb7d24fef71bcdd49ef813f1dccf
Cameron Dawson <cdawson@mozilla.com>
2014-10-31 14:52:59 -0700

    bug 1092339 - fixed logviewer infinite scrolling

--- ui/js/directives/log_viewer_infinite_scroll.js
+++ ui/js/directives/log_viewer_infinite_scroll.js
@@ -9,10 +9,9 @@ treeherder.directive('lvInfiniteScroll', ['$timeout', '$parse', function ($timeo
         element.bind('scroll', function () {
             var raw = element[0];
             var sh = raw.scrollHeight;
-            var onLoadMore = $parse(attr.onLoadMore);
 
             if (raw.scrollTop <= 100) {
-                onLoadMore(scope, {bounds: {top: true}, element: raw}).then(function(haltScrollTop) {
+                scope.loadMore({top: true}, raw).then(function(haltScrollTop) {
                     if (!haltScrollTop) {
                         $timeout(function() {
                             raw.scrollTop = raw.scrollHeight - sh;
@@ -20,7 +19,7 @@ treeherder.directive('lvInfiniteScroll', ['$timeout', '$parse', function ($timeo
                     }
                 });
             } else if (raw.scrollTop >= (raw.scrollHeight - $(element).height() - 100)) {
-                onLoadMore(scope, {bounds: {bottom: true}, element: raw});
+                scope.loadMore({bottom: true}, raw);
             }
         });
     };
