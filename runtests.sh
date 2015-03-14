30d6576b43580e8bc83244866e046481dfb0b266
Ed Morley <emorley@mozilla.com>
2015-03-14 00:06:03 +0000

    Bug 1139894 - Add flake8 to runtests.sh
    flake8 is run on Travis, so let's make it harder to forget to run
    locally for people who use runtests.sh. Eventually we should move all of
    this to setup.py.

--- runtests.sh
+++ runtests.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, you can obtain one at http://mozilla.org/MPL/2.0/.
 
+echo "Running flake8"
+flake8 || { echo "flake8 errors found!"; exit 1; }
+
+echo "Running Python tests"
 py.test tests/$* --cov-report html --cov treeherder
