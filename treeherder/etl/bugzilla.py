175a45a7a40ebae7c5ab06b579e58d67d19a101a
Ed Morley <emorley@mozilla.com>
2015-04-13 18:12:58 +0100

    Bug 1152426 - Only add bugs modified in the last year to the bugscache
    The API query will now be of form:
    bugzilla.mozilla.org/rest/bug?include_fields=id,summary,status
    &keywords=intermittent-failure&chfieldfrom=-1y

--- treeherder/etl/bugzilla.py
+++ treeherder/etl/bugzilla.py
@@ -14,6 +14,7 @@ def get_bz_source_url():
     hostname = settings.BZ_API_URL
     params = {
         'keywords': 'intermittent-failure',
+        'chfieldfrom': '-1y',
         'include_fields': ('id,summary,status,resolution,'
                            'op_sys,cf_crash_signature, '
                            'keywords, last_change_time')
