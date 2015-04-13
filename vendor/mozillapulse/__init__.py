52d5c4fb89b668da882b98653f26200a8a4680a1
Ed Morley <emorley@mozilla.com>
2015-04-14 00:05:25 +0100

    Bug 1153966 - Remove unused MozillaPulse, carrot & amqplib requirements

--- vendor/mozillapulse/__init__.py
+++
@@ -1,6 +0,0 @@
-__all__ = ['config','utils','messages','rfc3339','consumers','publishers']
-
-# Printing throws an error if we are printing using ascii
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
