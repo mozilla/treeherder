b0a0ea7a8c71e5e887c2e0b450aff270bb83c875
Cameron Dawson <cdawson@mozilla.com>
2015-04-29 15:46:11 -0700

    Bug 1144417 - use treeherder-client 1.2 with requests

--- treeherder/etl/th_publisher.py
+++ treeherder/etl/th_publisher.py
@@ -30,11 +30,11 @@ def post_treeherder_collections(th_collections):
                 th_request.get_uri(th_collections[project].endpoint_base)))
         response = th_request.post(th_collections[project])
 
-        if not response or response.status != 200:
+        if not response or response.status_code != 200:
             errors.append({
                 "project": project,
                 "url": th_collections[project].endpoint_base,
-                "message": response.read()
+                "message": response.content
             })
     if errors:
         raise CollectionNotLoadedException(errors)
