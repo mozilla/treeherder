4a8e8370e3c55ab4e082b9b129668ce829656cc1
Ed Morley <emorley@mozilla.com>
2015-05-07 19:18:43 +0100

    Bug 1162526 - Only add mozlog_json URLs to log_references if not null
    The url for items in blobber_properties can sometimes be null, so skip
    adding a mozlog_json entry for them to log_references if so.

--- treeherder/etl/buildapi.py
+++ treeherder/etl/buildapi.py
@@ -192,7 +192,7 @@ class Builds4hTransformerMixin(object):
             if 'blobber_files' in prop:
                 blobber_files = json.loads(prop['blobber_files'])
                 for bf, url in blobber_files.items():
-                    if bf.endswith('_raw.log'):
+                    if bf and url and bf.endswith('_raw.log'):
                         log_reference.append({
                             'url': url,
                             'name': 'mozlog_json'
