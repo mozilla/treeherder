f598198c72470209c698f4099ed748ac65c49857
Cameron Dawson <cdawson@mozilla.com>
2015-05-21 12:31:07 -0700

    Bug 1151806 - Implement chunking for job ingestion - fixed

--- treeherder/etl/th_publisher.py
+++ treeherder/etl/th_publisher.py
@@ -3,6 +3,7 @@
 # file, you can obtain one at http://mozilla.org/MPL/2.0/.
 
 import logging
+import traceback
 
 from django.utils.encoding import python_2_unicode_compatible
 from django.conf import settings
@@ -14,31 +15,36 @@ from treeherder.etl.oauth_utils import OAuthCredentials
 logger = logging.getLogger(__name__)
 
 
-def post_treeherder_collections(th_collections):
+def post_treeherder_collections(th_collections, chunk_size=1):
+
     errors = []
+    cli = TreeherderClient(
+        protocol=settings.TREEHERDER_REQUEST_PROTOCOL,
+        host=settings.TREEHERDER_REQUEST_HOST,
+    )
+
     for project in th_collections:
 
         credentials = OAuthCredentials.get_credentials(project)
-
-        cli = TreeherderClient(
-            protocol=settings.TREEHERDER_REQUEST_PROTOCOL,
-            host=settings.TREEHERDER_REQUEST_HOST,
-        )
 
         logger.info(
             "collection loading request for project {0}: {1}".format(
                 project,
                 th_collections[project].endpoint_base))
-        try:
-            cli.post_collection(project, credentials.get('consumer_key'),
-                                credentials.get('consumer_secret'),
-                                th_collections[project])
-        except Exception, e:
-            errors.append({
-                "project": project,
-                "url": th_collections[project].endpoint_base,
-                "message": str(e)
-            })
+
+        collection_chunks = th_collections[project].get_chunks(chunk_size)
+
+        for collection in collection_chunks:
+            try:
+                cli.post_collection(project, credentials.get('consumer_key'),
+                                    credentials.get('consumer_secret'),
+                                    collection)
+            except Exception:
+                errors.append({
+                    "project": project,
+                    "url": th_collections[project].endpoint_base,
+                    "message": traceback.format_exc()
+                })
 
     if errors:
         raise CollectionNotLoadedException(errors)
