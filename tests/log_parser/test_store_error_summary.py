2db4c0369edd0de179511ec977a4b4210f08e627
Mauro Doglio <mdoglio@mozilla.com>
2015-08-11 11:37:20 +0100

    Bug 1182464 - Add store_error_summary django command

---
+++ tests/log_parser/test_store_error_summary.py
@@ -0,0 +1,29 @@
+import responses
+from django.core.management import call_command
+
+from treeherder.model.models import Failure, Repository, RepositoryGroup
+from ..sampledata import SampleData
+
+
+def test_store_error_summary(activate_responses, jm, eleven_jobs_stored, initial_data):
+    log_path = SampleData().get_log_path("plain-chunked_errorsummary.log")
+    log_url = 'http://my-log.mozilla.org'
+
+    with open(log_path) as log_handler:
+        responses.add(responses.GET, log_url,
+                      body=log_handler.read(), status=200)
+
+    job = jm.get_job(1)[0]
+    repository_group = RepositoryGroup.objects.create(name="repo_group")
+    repository = Repository.objects.create(name=jm.project,
+                                           repository_group=repository_group)
+
+    call_command('store_error_summary', log_url, job['job_guid'], jm.project)
+
+    assert Failure.objects.count() == 1
+
+    failure = Failure.objects.get(pk=1)
+
+    assert failure.job_guid == job['job_guid']
+
+    assert failure.repository == repository
