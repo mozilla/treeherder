89d11c115fad9a5693e7a9d24830bf2235155af9
Ed Morley <emorley@mozilla.com>
2015-09-18 16:54:31 +0100

    Bug 1206108 - Remove unnecessary loadNpmTasks() calls
    Since they're duplicating what the `require('load-grunt-tasks')(grunt)`
    call is already doing. See:
    https://github.com/sindresorhus/load-grunt-tasks/blob/master/readme.md

--- Gruntfile.js
+++ Gruntfile.js
@@ -232,16 +232,6 @@ module.exports = function(grunt) {
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-usemin');
-    grunt.loadNpmTasks('grunt-cache-busting');
-    grunt.loadNpmTasks('grunt-angular-templates');
-    grunt.loadNpmTasks('grunt-html-angular-validate');
-
     // Default tasks
     grunt.registerTask('build', [
         'clean:dist',
