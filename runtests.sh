c554745a2c8d1893c4dbb4d3462f895cd691f7b7
Ed Morley <emorley@mozilla.com>
2015-09-21 22:06:58 +0100

    Bug 1192957 - Add isort to the Travis run
    Any import style deviations will cause the Travis run to fail, with the
    diff output to the Travis log.

--- runtests.sh
+++ runtests.sh
@@ -3,5 +3,9 @@
 echo "Running flake8"
 flake8 || { echo "flake8 errors found!"; exit 1; }
 
+echo "Running isort"
+isort --check-only --diff --quiet \
+ || { echo "isort errors found! Run 'isort' with no options to fix."; exit 1; }
+
 echo "Running Python tests"
 py.test tests/$* --cov-report html --cov treeherder
