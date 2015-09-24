789c6a25435fac2f7fcc2f4b585d53fd59ab8f48
Mauro Doglio <mdoglio@mozilla.com>
2015-09-24 16:04:01 +0200

    Bug 1160111 - Add treeherder.credentials app
    The new app handles treeherder client credentials.
    The content of the credentials table should replace the credentials file
    we currently use for authentication.
    This commit includes orm models, migrations and an admin interface.

---
+++ treeherder/credentials/admin.py
@@ -0,0 +1,30 @@
+import uuid
+
+from django.contrib import admin
+from django_browserid.admin import site as browserid_admin
+
+from .models import Credentials
+
+
+def get_owner_email(credentials):
+    return credentials.owner.email
+get_owner_email.short_description = 'owner'
+
+
+@admin.register(Credentials, site=browserid_admin)
+class CredentialsAdmin(admin.ModelAdmin):
+
+    def reset_secret(self, request, queryset):
+        for credentials in queryset:
+            credentials.secret = uuid.uuid4()
+            credentials.save()
+        self.message_user(request, 'Action successfully completed')
+
+    reset_secret.short_description = 'Reset credentials secret'
+
+    list_select_related = ('owner',)
+    list_display = ['client_id', get_owner_email, 'authorized', 'created', 'modified']
+    search_fields = ['client_id', 'owner__email']
+    list_filter = ['authorized']
+    raw_id_fields = ['owner']
+    actions = [reset_secret]
---
+++ treeherder/credentials/models.py
@@ -0,0 +1,18 @@
+import uuid
+
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Credentials(models.Model):
+    """A list of treeherder api credentials"""
+    client_id = models.SlugField(max_length=32, unique=True)
+    secret = models.UUIDField(default=uuid.uuid4, editable=False)
+    description = models.TextField()
+    owner = models.ForeignKey(User)
+    authorized = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'credentials'
