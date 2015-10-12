650f3f495bb94edab9cf54fbeec741cf434a2f30
Ed Morley <emorley@mozilla.com>
2015-10-12 13:52:04 +0100

    Bug 1213263 - Make capitalisation on credentials pages consistent
    Sets a verbose name for client_id so "ID" always appears capitalised:
    https://docs.djangoproject.com/en/1.8/topics/db/models/#verbose-field-names
    
    Also tweaks the mixed capitalisation for the page title/heading.

--- treeherder/credentials/models.py
+++ treeherder/credentials/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 class Credentials(models.Model):
     """A list of treeherder api credentials"""
-    client_id = models.SlugField(max_length=32, unique=True)
+    client_id = models.SlugField("client ID", max_length=32, unique=True)
     secret = models.UUIDField(default=uuid.uuid4, editable=False)
     description = models.TextField()
     owner = models.ForeignKey(User)
