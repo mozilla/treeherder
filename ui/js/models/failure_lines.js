5e45b6a2f2640e63ee4546beccc5808a33cfda96
Cameron Dawson <cdawson@mozilla.com>
2015-10-14 10:16:09 -0700

    add new files

--- ui/js/models/failure_lines.js
+++ ui/js/models/failure_lines.js
@@ -1,3 +1,36 @@
-/**
- * Created by camd on 10/14/15.
- */
+'use strict';
+
+treeherder.factory('ThFailureLinesModel', [
+    '$http', 'ThLog', 'thUrl',
+    function($http, ThLog, thUrl) {
+
+        var ThFailureLinesModel = function(data) {
+            angular.extend(this, data);
+        };
+
+        ThFailureLinesModel.get_url = function(job_id) {
+            return thUrl.getProjectJobUrl("/failure_lines", job_id);
+        };
+
+        ThFailureLinesModel.get_list = function(job_id, config) {
+            // a static method to retrieve a list of ThFailureLinesModel
+            // the timeout configuration parameter is a promise that can be used to abort
+            // the ajax request
+            config = config || {};
+            var timeout = config.timeout || null;
+            console.log("get_list " + ThFailureLinesModel.get_url(job_id));
+            return $http.get(ThFailureLinesModel.get_url(job_id), {
+                timeout: timeout
+            })
+                .then(function(response) {
+                    console.log("response");
+                    var item_list = [];
+                    angular.forEach(response.data, function(elem){
+                        item_list.push(new ThFailureLinesModel(elem));
+                    });
+                    return item_list;
+                });
+        };
+
+        return ThFailureLinesModel;
+    }]);
--- ui/plugins/error_classification/controller.js
+++ ui/plugins/error_classification/controller.js
@@ -1,3 +1,42 @@
-/**
- * Created by camd on 10/14/15.
- */
+"use strict";
+
+treeherder.controller('ClassificationPluginCtrl', [
+    '$scope', 'ThLog', 'ThFailureLinesModel','$q', 'thTabs', '$timeout',
+    function ClassificationPluginCtrl(
+        $scope, ThLog, ThFailureLinesModel, $q, thTabs, $timeout) {
+        var $log = new ThLog(this.constructor.name);
+
+        $log.debug("error classification plugin initialized");
+
+        var timeoutPromise = null;
+        var requestPromise = null;
+
+        thTabs.tabs.errorClassification.update = function() {
+            var jobId = thTabs.tabs.errorClassification.contentId;
+            // if there's an ongoing timeout, cancel it
+            if (timeoutPromise !== null) {
+                $timeout.cancel(timeoutPromise);
+            }
+            // if there's a ongoing request, abort it
+            if (requestPromise !== null) {
+                requestPromise.resolve();
+            }
+
+            requestPromise = $q.defer();
+
+            thTabs.tabs.errorClassification.is_loading = true;
+            ThFailureLinesModel.get_list(jobId,
+                                         {timeout: requestPromise})
+            .then(function(failureLines) {
+                $scope.failureLines = failureLines
+                $scope.failureLinesLoaded = failureLines.length > 0;
+                if (!$scope.failureLinesLoaded) {
+                    timeoutPromise = $timeout(thTabs.tabs.errorClassification.update, 5000);
+                }
+            })
+            .finally(function() {
+                thTabs.tabs.errorClassification.is_loading = false;
+            });
+        }
+    }
+]);
