1b2f18faac74adbdaa4195bd1b4379ed39d9af0b
Mauro Doglio <mdoglio@mozilla.com>
2015-10-26 13:35:02 +0000

    Bug 1193836 - Update vm provisioning
    This patch replaces the use of init_master_db with django `migrate`
    and `load_initial_data` commands.

--- docker/init.sh
+++ docker/init.sh
@@ -3,7 +3,8 @@
 # Initialize the basics needed to run treeherder-service.
 
 source /etc/profile.d/treeherder.sh
-./manage.py init_master_db --noinput -v 3
+./manage.py migrate
+./manage.py load_initial_data
 ./manage.py init_datasources
 ./docker/generate_test_credentials.py
 
