078f5902d86e5719d9a79c318ce07462631aea26
Mauro Doglio <mdoglio@mozilla.com>
2015-10-27 12:05:03 +0000

    Bug 1193836 - use `--noinput` on migrate
    This is a followup of 829cf22bc482937406a22f1511db7a0db53a0648

--- puppet/manifests/classes/dev.pp
+++ puppet/manifests/classes/dev.pp
@@ -8,7 +8,7 @@ class dev{
 
   exec{"migrate":
     cwd => "${PROJ_DIR}",
-    command => "bash -c 'source /etc/profile.d/treeherder.sh; ${VENV_DIR}/bin/python manage.py migrate'",
+    command => "bash -c 'source /etc/profile.d/treeherder.sh; ${VENV_DIR}/bin/python manage.py migrate --noinput'",
     user => "${APP_USER}",
   }
 
