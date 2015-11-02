50cec5c1fe71a04c0f8da0bef87783bd9ad621c8
Ed Morley <emorley@mozilla.com>
2015-11-02 17:02:33 +0000

    Bug 1221806 - Change the stage/prod Gunicorn timeout from 120s to 30s
    To help prevent excessively demanding queries from having a detrimental
    effect on the API/DBs.

--- docker/gunicorn.sh
+++ docker/gunicorn.sh
@@ -3,9 +3,9 @@
 source /etc/profile.d/treeherder.sh
 ./docker/generate_test_credentials.py
 
-# Use exec so pid 1 is now gunicon instead of bash...
+# Use exec so pid 1 is now gunicorn instead of bash...
 exec gunicorn \
   -w 5 \
   -b 0.0.0.0:8000 \
-  --timeout 120 \
+  --timeout 30 \
   treeherder.config.wsgi:application
