7429074295a9fece3f418e33fa3c35307874018a
William Lachance <wlachance@mozilla.com>
2015-11-03 16:20:30 -0500

    Bug 1221277 - Add a command for removing unwanted performance data

---
+++ treeherder/perf/management/commands/remove_perf_signatures.py
@@ -0,0 +1,24 @@
+from django.core.management.base import (BaseCommand,
+                                         CommandError)
+
+from treeherder.perf.models import (PerformanceDatum,
+                                    PerformanceSignature)
+
+
+class Command(BaseCommand):
+
+    help = "Remove performance data"
+    args = '[signature1] [signature2] ...'
+
+    def handle(self, *args, **options):
+        if len(args) < 1:
+            raise CommandError("Need to specify at least one signature to "
+                               "remove")
+
+        for signature_hash in args:
+            signatures = PerformanceSignature.objects.filter(
+                signature_hash=signature_hash)
+            datums = PerformanceDatum.objects.filter(
+                signature__in=signatures)
+            datums.delete()
+            signatures.delete()
