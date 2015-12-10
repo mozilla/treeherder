796a1f3c4d7345a1c1d10e3cb00ead8daa4161b1
KWierso <kwierso@gmail.com>
2015-12-10 15:00:24 -0500

    Initial server-side framework stuff for filing bugs

---
+++ treeherder/webapp/api/bugzilla.py
@@ -0,0 +1,27 @@
+from time import time
+
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+
+from treeherder.model.derived.jobs import JobDataIntegrityError
+from treeherder.webapp.api.utils import (UrlQueryFilter,
+                                         with_jobs)
+from rest_framework.decorators import (detail_route,
+                                       list_route)
+
+
+class BugzillaViewSet(viewsets.ViewSet):
+    # permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    @list_route(methods=['post'])
+    def create_bug(self, request):
+        """
+        Create a bugzilla bug with passed params
+        """
+
+        params = request.data
+
+
+
+        return Response({"message": "'status' variable is: " + params["status"]})
