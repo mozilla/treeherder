ae0870efefa9fe7f8525c1db210d26f4ed14e0c5
KWierso <kwierso@gmail.com>
2015-12-28 16:56:57 -0800

    linterfix

--- treeherder/webapp/api/bugzilla.py
+++ treeherder/webapp/api/bugzilla.py
@@ -1,16 +1,10 @@
-from time import time
-
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 import requests
 
 from treeherder.etl.common import make_request
-from treeherder.model.derived.jobs import JobDataIntegrityError
-from treeherder.webapp.api.utils import (UrlQueryFilter,
-                                         with_jobs)
-from rest_framework.decorators import (detail_route,
-                                       list_route)
+from rest_framework.decorators import (list_route)
 
 from django.conf import settings
 
@@ -27,7 +21,7 @@ class BugzillaViewSet(viewsets.ViewSet):
         params = request.data
         url = "https://bugzilla-dev.allizom.org/rest/bug"
         headers = {
-          #  'X_BUGZILLA_API_KEY': 'qF8lX6AyGjcZcmSV4tZTmy2F2PbBycQdB9lsp8cB'
+            #'X_BUGZILLA_API_KEY': 'qF8lX6AyGjcZcmSV4tZTmy2F2PbBycQdB9lsp8cB'
             'X_BUGZILLA_API_KEY': settings.TREEHERDER_BUGZILLA_API_KEY
         }
 
