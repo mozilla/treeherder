e8dd161e5bcb3ca4b09dfa83e21e1a54e25fcbf1
KWierso <kwierso@gmail.com>
2015-12-28 17:02:00 -0800

    linterfix

--- treeherder/webapp/api/bugzilla.py
+++ treeherder/webapp/api/bugzilla.py
@@ -1,11 +1,10 @@
 import requests
 from django.conf import settings
 from rest_framework import viewsets
-from rest_framework.decorators import (list_route)
+from rest_framework.decorators import list_route
 from rest_framework.response import Response
 
 from treeherder.etl.common import make_request
-
 
 
 class BugzillaViewSet(viewsets.ViewSet):
