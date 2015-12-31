371ec172de645c93f98b4c74da0755baba25b97d
Wes Kocher <kwierso@gmail.com>
2015-12-30 16:07:14 -0800

    Bug 1117583 - Add an API endpoint that creates a new bug in Bugzilla.
    This endpoint receives a http POST request from the UI containing information about the bug to be filed (product, component, summary, version, description), then formats it properly as a submission to Bugzilla's REST API, using a server-side Bugzilla API key, and adding a "treeherder" comment tag.
    
    The API then passes back either the bug ID (if the submission was successful) or Bugzilla's failure response if something went wrong.

--- treeherder/config/settings_local.example.py
+++ treeherder/config/settings_local.example.py
@@ -1,3 +1,6 @@
+# Switch to using a different bugzilla instance
+BZ_API_URL = "http://bugzilla-dev.allizom.org/"
+
 # Applications useful for development, e.g. debug_toolbar, django_extensions.
 # Always empty in production
 LOCAL_APPS = []
