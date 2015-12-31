7b36946342205b1b1528f9e315fe9f726944eb16
Wes Kocher <kwierso@gmail.com>
2015-12-30 16:07:14 -0800

    Bug 1117583 - Add an API endpoint that creates a new bug in Bugzilla.
    This endpoint receives a http POST request from the UI containing information about the bug to be filed (product, component, summary, version, description), then formats it properly as a submission to Bugzilla's REST API, using a server-side Bugzilla API key, and adding a "treeherder" comment tag and an "intermittent-failure" keyword.
    
    The API then passes back either the bug ID (if the submission was successful) or Bugzilla's failure response if something went wrong.

---
+++ treeherder/webapp/api/bugzilla.py
@@ -0,0 +1,46 @@
+import requests
+from django.conf import settings
+from rest_framework import (status,
+                            viewsets)
+from rest_framework.decorators import list_route
+from rest_framework.response import Response
+
+from treeherder.etl.common import make_request
+
+
+class BugzillaViewSet(viewsets.ViewSet):
+    # permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    @list_route(methods=['post'])
+    def create_bug(self, request):
+        """
+        Create a bugzilla bug with passed params
+        """
+
+        params = request.data
+        url = settings.BZ_API_URL + "/rest/bug"
+        headers = {
+            'X_BUGZILLA_API_KEY': settings.BZ_API_KEY
+        }
+        data = {
+            'product': params["product"],
+            'component': params["component"],
+            'summary': params["summary"],
+            'keywords': 'intermittent-failure',
+            # 'dependson': params[""],
+            # 'blocks': params[""],
+            'version': params["version"],
+            'description': params["description"],
+            'comment_tags': "treeherder",
+            # 'ccstring': params[""],
+        }
+
+        try:
+            response = make_request(url, method='POST', headers=headers, json=data)
+        except requests.exceptions.HTTPError as e:
+            response = e.response
+            # logger.error("HTTPError %s submitting to %s: %s", response.status_code, url, response.text)
+            # return Response({"failure": response}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"failure": response.json(), "headers": headers, "settings": settings.BUGZILLA_API_KEY}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response({"success": response.json()["id"]})
