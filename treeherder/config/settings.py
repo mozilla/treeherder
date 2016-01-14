38662a96208a46a2fb808c73744cd85a3e66d453
Cameron Dawson <cdawson@mozilla.com>
2016-01-14 13:14:50 -0800

    fixup! Bug 1177519 - Back end changes for autoclassification work

--- treeherder/config/settings.py
+++ treeherder/config/settings.py
@@ -335,7 +335,7 @@ MIRROR_CLASSIFICATIONS = True
 ES_HOST = "http://of-elasticsearch-zlb.webapp.scl3.mozilla.com:9200"
 
 # Enable integration between autoclassifier and jobs
-AUTOCLASSIFY_JOBS = env.bool("TREEHERDER_AUTOCLASSIFY_JOBS", default=False)
+AUTOCLASSIFY_JOBS = env.bool("AUTOCLASSIFY_JOBS", default=False)
 
 # timeout for requests to external sources
 # like ftp.mozilla.org or hg.mozilla.org
