903f20419b83c4e102fea68b8cb52fa04652b3ab
Tiramisu 1993 <sabergeass@gmail.com>
2016-01-20 15:37:05 +0800

    Bug 1226693 - Change subtest information store as foreign key

---
+++ treeherder/perf/migrations/0008_performancesignature_parent_signature.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('perf', '0007_alert_status'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='performancesignature',
+            name='parent_signature',
+            field=models.ForeignKey(blank=True, to='perf.PerformanceSignature', null=True),
+        ),
+    ]
