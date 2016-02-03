d5ad93d0384b992a6e9a1e35004f2df5742ef4cd
Cameron Dawson <cdawson@mozilla.com>
2016-02-03 14:17:29 -0800

    Revert "Bug 1241247 - Removing jQuery to speed up generateJobElements()."
    This commit (and/or a follow-on to it) have caused the following UI regressions:
    
    1. On first page load, typing "U" or clicking "Unclassified" button still showed some non-failures
    2. Loading a page with the "group_state=expanded" query string would display no jobs for some
       resultsets.  Clicking the "( + )" job twice would restore them.
    
    These have been reverted until the regressions can be addressed and re-submitted.
    
    This reverts commit 60f475e13e06b19580ba49a0317d59d16547212c.

--- ui/js/directives/treeherder/clonejobs.js
+++ ui/js/directives/treeherder/clonejobs.js
@@ -536,7 +536,8 @@ treeherder.directive('thCloneJobs', [
             el.addClass(col12Cls);
         };
 
-        var getJobTableRowHTML = function(jobGroups) {
+        var renderJobTableRow = function(row, jobTdEl, jobGroups) {
+
             //Empty the job column before populating it
             var btnHTML, countBtnHTML, jobTdHtml = "";
             jobGroups.forEach(function(jobGroup) {
@@ -562,19 +563,14 @@ treeherder.directive('thCloneJobs', [
                         countBtnHTML: countBtnHTML,
                         symbol: jobGroup.symbol,
                         name: jobGroup.name,
-                        grkey: jobGroup.grkey,
-                        display: (btnHTML.indexOf('filter-shown') !== -1 || countBtnHTML.indexOf('filter-shown') !== -1) ? 'inline' : 'none'
+                        grkey: jobGroup.grkey
                     });
                 } else {
                     // Add the job btn spans
                     jobTdHtml += getJobBtnEls(jobGroup);
                 }
             });
-            return jobTdHtml;
-        };
-
-        var renderJobTableRow = function(row, jobTdEl, jobGroups) {
-            jobTdEl.html(getJobTableRowHTML(jobGroups));
+            jobTdEl.html(jobTdHtml);
             row.append(jobTdEl);
             filterPlatform(row);
         };
@@ -902,41 +898,60 @@ treeherder.directive('thCloneJobs', [
         var generateJobElements = function(resultsetAggregateId, resultset) {
 
             var tableEl = $('#' + resultsetAggregateId);
+
             var waitSpanEl = $(tableEl).prev();
             $(waitSpanEl).css('display', 'none');
-            var tableHtml = "";
-            resultset.platforms.forEach(function(platform) {
-                var platformId = thAggregateIds.getPlatformRowId(
+
+            var name, option, platformId, platformKey, row, platformTd, jobTdEl, j;
+            for (j=0; j<resultset.platforms.length; j++) {
+
+                platformId = thAggregateIds.getPlatformRowId(
                     $rootScope.repoName,
                     resultset.id,
-                    platform.name,
-                    platform.option
+                    resultset.platforms[j].name,
+                    resultset.platforms[j].option
                 );
-                // We first determine whether the row has some visible element
-                var display_style = "none";
-                platform.groups.forEach(function(group) {
-                    group.jobs.forEach(function(job) {
-                        if (filterWithRunnable(job)) {
-                            display_style = "table-row";
-                        }
-                    });
-                });
-                var rowHtml = '<tr id="' + platformId + '" style="display: ' + display_style + ';">';
+
+                row = $('#' + platformId);
+
+                if($(row).prop('tagName') !== 'TR'){
+                    // First time the row is being created
+                    row = $('<tr></tr>');
+                    row.prop('id', platformId);
+                } else {
+                    // Clear and re-write the div content if it
+                    // already exists
+                    $(row).empty();
+                }
+
+                name = thPlatformName(resultset.platforms[j].name);
+                option = resultset.platforms[j].option;
+
                 //Add platforms
-                rowHtml += platformInterpolator(
+                platformTd = platformInterpolator(
                     {
-                        'name':thPlatformName(platform.name), 'option':platform.option,
+                        'name':name, 'option':option,
                         'id':thAggregateIds.getPlatformRowId(
                             resultset.id,
-                            platform.name,
-                            platform.option
+                            resultset.platforms[j].name,
+                            resultset.platforms[j].option
                         )
                     }
                 );
-                rowHtml += '<td class="job-row">' + getJobTableRowHTML(platform.groups) + '</td></tr>';
-                tableHtml += rowHtml;
-            });
-            tableEl.html(tableHtml);
+
+                row.append(platformTd);
+
+                // Render the row of job data
+                jobTdEl = $( thCloneHtml.get('jobTdClone').text );
+
+                platformKey = ThResultSetStore.getPlatformKey(
+                    resultset.platforms[j].name, resultset.platforms[j].option
+                );
+
+                renderJobTableRow(row, jobTdEl, resultset.platforms[j].groups);
+
+                tableEl.append(row);
+            }
         };
 
         var $scope = null;
