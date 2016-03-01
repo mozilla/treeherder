748b6302b0ceb12a274a0aa027479e9bae169191
William Lachance <wlachance@mozilla.com>
2016-03-01 12:20:30 -0500

    Bug 1252238 - Add ability to view e10s vs. non-e10s subtest differences

--- ui/js/controllers/perf/e10s.js
+++ ui/js/controllers/perf/e10s.js
@@ -100,6 +100,7 @@ perf.controller('e10sCtrl', [
                                 suite: series.suite,
                                 name: PhSeries.getTestName(series),
                                 lowerIsBetter: series.lowerIsBetter,
+                                hasSubTests: !_.isUndefined(series.subtestSignatures),
                                 values: _.map(data, 'value')
                             };
                         });
@@ -129,6 +130,13 @@ perf.controller('e10sCtrl', [
                                                                            return 'series=[' + [ projectName, sig, 1 ];
                                                                        }).join('&') + ']'
                                 }];
+                                if (resultsMap['base'][baseSig].hasSubTests) {
+                                    cmap.links.push({
+                                        title: 'subtests',
+                                        href: 'perf.html#/e10s_comparesubtest?baseSignature=' + baseSig +
+                                            '&e10sSignature=' + e10sSig
+                                    });
+                                }
                                 if (!$scope.compareResults[testName]) {
                                     $scope.compareResults[testName] = [cmap];
                                 } else {
@@ -136,6 +144,118 @@ perf.controller('e10sCtrl', [
                                 }
                             }
                         });
+                    });
+                });
+            });
+        });
+    }
+]);
+
+perf.controller('e10sSubtestCtrl', [
+    '$state', '$stateParams', '$scope', '$rootScope', '$q', '$http',
+    'ThOptionCollectionModel', 'PhSeries', 'PhCompare', 'thServiceDomain',
+    function e10sCtrl($state, $stateParams, $scope, $rootScope, $q, $http,
+                      ThOptionCollectionModel, PhSeries, PhCompare, thServiceDomain) {
+        var projectName = 'mozilla-inbound';
+        var interval = 86400*2;
+
+        $scope.testList = [];
+        $scope.dataLoading = true;
+        $scope.compareResults = {};
+        $scope.titles = {};
+        var baseSignature = $stateParams.baseSignature;
+        var e10sSignature = $stateParams.e10sSignature;
+
+        $scope.filterOptions = {
+            filter: $stateParams.filter || "",
+            showOnlyImportant: Boolean($stateParams.showOnlyImportant !== undefined &&
+                                       parseInt($stateParams.showOnlyImportant)),
+            showOnlyConfident: Boolean($stateParams.showOnlyConfident !== undefined &&
+                                       parseInt($stateParams.showOnlyConfident)),
+            showOnlyBlockers: Boolean($stateParams.showOnlyBlockers !== undefined &&
+                                      parseInt($stateParams.showOnlyBlockers))
+        };
+        $scope.$watchGroup(['filterOptions.filter',
+                            'filterOptions.showOnlyImportant',
+                            'filterOptions.showOnlyConfident'],
+                           function() {
+                               $state.transitionTo('e10s_comparesubtest', {
+                                   filter: $scope.filterOptions.filter,
+                                   showOnlyImportant: Boolean($scope.filterOptions.showOnlyImportant) ? 1 : undefined,
+                                   showOnlyConfident: Boolean($scope.filterOptions.showOnlyConfident) ? 1 : undefined
+                               }, {
+                                   location: true,
+                                   inherit: true,
+                                   relative: $state.$current,
+                                   notify: false
+                               });
+                           });
+
+        var resultsMap = {
+            e10s: {},
+            base: {}
+        };
+        ThOptionCollectionModel.getMap().then(function(optionCollectionMap) {
+            PhSeries.getAllSeries(projectName, interval, optionCollectionMap).then(function(seriesData) {
+                var baseSeries = _.find(seriesData.seriesList, { signature: baseSignature });
+                var e10sSeries = _.find(seriesData.seriesList, { signature: e10sSignature });
+                var subtestSignatures = baseSeries.subtestSignatures.concat(e10sSeries.subtestSignatures);
+                var summaryTestName = baseSeries.platform + ": " + PhSeries.getTestName(baseSeries);
+                $scope.testList = [summaryTestName];
+                $scope.titles[summaryTestName] = summaryTestName;
+
+                $q.all(_.chunk(subtestSignatures, 20).map(function(seriesChunk) {
+                    var url = thServiceDomain + '/api/project/' + projectName +
+                        '/performance/data/?interval=' + interval +
+                        _.map(seriesChunk, function(signature) {
+                            return "&signatures=" + signature;
+                        }).join("");
+                    return $http.get(url).then(function(response) {
+                        _.forIn(response.data, function(data, signature) {
+                            var series = _.find(seriesData.seriesList, { signature: signature });
+                            var type = (series.options.indexOf('e10s') >= 0) ? 'e10s' : 'base';
+                            resultsMap[type][signature] = {
+                                platform: series.platform,
+                                suite: series.suite,
+                                name: PhSeries.getTestName(series),
+                                lowerIsBetter: series.lowerIsBetter,
+                                values: _.map(data, 'value')
+                            };
+                        });
+                    });
+                })).then(function() {
+                    $scope.dataLoading = false;
+
+                    _.forEach(baseSeries.subtestSignatures, function(subtestSignature) {
+                        // find the base series
+                        var subtestSeries = _.find(seriesData.seriesList, {
+                            signature: subtestSignature
+                        });
+                        var subtestName = PhSeries.getTestName(subtestSeries);
+                        var baseSig = _.find(Object.keys(resultsMap['base']), function (sig) {
+                            return resultsMap['base'][sig].name === subtestName;
+                        });
+                        var e10sSig = _.find(Object.keys(resultsMap['e10s']), function (sig) {
+                            return resultsMap['e10s'][sig].name === subtestName;
+                        });
+                        if (e10sSig && baseSig) {
+                            var cmap = PhCompare.getCounterMap(
+                                subtestName, resultsMap['base'][baseSig],
+                                resultsMap['e10s'][e10sSig]);
+                            cmap.name = subtestName;
+                            cmap.links = [{
+                                title: 'graph',
+                                href: 'perf.html#/graphs?' + _.map([baseSig, e10sSig],
+                                                                   function(sig) {
+                                                                       return 'series=[' + [ projectName, sig, 1 ];
+                                                                   }).join('&') + ']'
+                            }];
+                            if (!$scope.compareResults[summaryTestName]) {
+                                $scope.compareResults[summaryTestName] = [cmap];
+                            } else {
+                                $scope.compareResults[summaryTestName].push(cmap);
+                            }
+                        }
                     });
                 });
             });
