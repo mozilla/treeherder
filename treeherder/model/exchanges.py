363f8ae1d64b5d015496bbb07c6497534934b4a6
Cameron Dawson <cdawson@mozilla.com>
2016-03-02 17:23:48 -0800

    Bug 1199364 - remove pulse publisher for new resultsets.
    This is no longer used by Task Cluster, and probably never will be.

--- treeherder/model/exchanges.py
+++ treeherder/model/exchanges.py
@@ -10,26 +10,6 @@ class TreeherderPublisher(PulsePublisher):
     """
     exchange_prefix = "v1/"
 
-    new_result_set = Exchange(
-        exchange='new-result-set',
-        title="New Result-Set Messages",
-        description="""
-            Whenever a new result-set is created a message featuring the
-            `revision_hash` is published on this exchange.
-        """,
-        routing_keys=[
-            Key(
-                name='project',
-                summary="Project (or branch) that this result-set concerns"
-            ),
-            Key(
-                name='revision_hash',
-                summary="result-set identifier for the message"
-            )
-        ],
-        schema="https://treeherder.mozilla.org/schemas/v1/resultset-message.json#"
-    )
-
     resultset_action = Exchange(
         exchange="resultset-actions",
         title="Actions issued by resultset",
