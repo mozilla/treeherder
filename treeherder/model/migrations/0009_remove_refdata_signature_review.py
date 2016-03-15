0759b41a685c0064a8d9e7d417cd267af791618d
William Lachance <wlachance@mozilla.com>
2016-03-15 16:17:01 -0400

    Bug 1239185 - Remove review fields from reference data signatures

---
+++ treeherder/model/migrations/0009_remove_refdata_signature_review.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('model', '0008__add_best_fields_to_failureline'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='referencedatasignatures',
+            name='review_status',
+        ),
+        migrations.RemoveField(
+            model_name='referencedatasignatures',
+            name='review_timestamp',
+        ),
+    ]
