6716465d9a7cacbc25c3867cb034cb5a49d15cff
James Graham <james@hoppipolla.co.uk>
2016-03-16 17:12:30 +0000

    fixup! Bug 1252854 - Backend work for matching unstructured and structured log summary lines.

---
+++ treeherder/model/migrations/0013_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('model', '0010_machine_name_unique'),
+        ('model', '0012_tasksetmeta'),
+    ]
+
+    operations = [
+    ]
