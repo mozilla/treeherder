44b536205b44200842236b02a1be4cd571e144d0
Ed Morley <emorley@mozilla.com>
2016-03-22 15:08:34 +0000

    Bug 1258700 - Add docstrings to CustomWhiteNoise

--- treeherder/config/whitenoise_custom.py
+++ treeherder/config/whitenoise_custom.py
@@ -4,12 +4,20 @@ from whitenoise.middleware import WhiteNoiseMiddleware
 
 
 class CustomWhiteNoise(WhiteNoiseMiddleware):
+    """
+    Adds two additional features to WhiteNoise:
 
-    # Matches grunt-cache-busting's style of hash filenames.
+    1) Serving index pages for directory paths (such as the site root).
+    2) Setting long max-age headers for files created by grunt-cache-busting.
+    """
+
+    # Matches grunt-cache-busting's style of hash filenames. eg:
+    #   index.min-feae259e2c205af67b0e91306f9363fa.js
     IMMUTABLE_FILE_RE = re.compile(r'\.min-[a-f0-9]{32}\.(js|css)$')
     INDEX_NAME = 'index.html'
 
     def update_files_dictionary(self, *args):
+        """Add support for serving index pages for directory paths."""
         super(CustomWhiteNoise, self).update_files_dictionary(*args)
         index_page_suffix = "/" + self.INDEX_NAME
         index_name_length = len(self.INDEX_NAME)
@@ -21,6 +29,7 @@ class CustomWhiteNoise(WhiteNoiseMiddleware):
         self.files.update(index_files)
 
     def find_file(self, url):
+        """Add support for serving index pages for directory paths when in DEBUG mode."""
         # In debug mode, find_file() is used to serve files directly from the filesystem
         # instead of using the list in `self.files`, so we append the index filename so
         # that will be served if present.
@@ -29,8 +38,7 @@ class CustomWhiteNoise(WhiteNoiseMiddleware):
         return super(CustomWhiteNoise, self).find_file(url)
 
     def is_immutable_file(self, path, url):
-        # Support grunt-cache-busting's style of hash filenames. eg:
-        #   index.min-feae259e2c205af67b0e91306f9363fa.js
+        """Support grunt-cache-busting style filenames when setting long max-age headers."""
         if self.IMMUTABLE_FILE_RE.search(url):
             return True
         # Otherwise fall back to the default method, so we catch filenames in the
