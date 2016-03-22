5aa721075c406e2f4f212f01a83b1df4c22aa17e
James Graham <james@hoppipolla.co.uk>
2016-03-22 13:46:55 +0000

    fixup! Bug 1252854 - Backend work for matching unstructured and structured log summary lines.

--- treeherder/perf/tasks.py
+++ treeherder/perf/tasks.py
@@ -4,7 +4,7 @@ from treeherder.perf.alerts import generate_new_alerts_in_series
 from treeherder.perf.models import PerformanceSignature
 
 
-@task(name='generate-perf-alerts')
+@task(name='generate-alerts')
 def generate_alerts(signature_id):
     signature = PerformanceSignature.objects.get(id=signature_id)
     generate_new_alerts_in_series(signature)
