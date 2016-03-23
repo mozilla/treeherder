ea22b14dde857e75dcf90c7f1aef61905bb9e955
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2016-03-24 01:36:07 +0530

    Bug 1243155 - Removing undefined letters from commit author's initials.

--- ui/js/filters.js
+++ ui/js/filters.js
@@ -70,10 +70,13 @@ treeherder.filter('initials', function() {
     return function(input) {
         var str = input || '';
         var words = str.split(' ');
-        var first = words[0].replace(/[^A-Z]/gi, '')[0];
-        var last = words.slice(-1)[0].replace(/[^A-Z]/gi, '')[0];
-        var initials = first + last;
-
+        var firstLetters = _.filter(_.map(words, function(word) { return word.replace(/[^A-Z]/gi, '')[0]; }));
+        var initials = "";
+        if (firstLetters.length == 1) {
+            initials = firstLetters[0];
+        } else if (firstLetters.length > 1) {
+            initials = firstLetters[0] + firstLetters[firstLetters.length - 1];
+        }
         return '<span class="label label-initials">' + initials + '</span>';
     };
 });
