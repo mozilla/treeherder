7f0058c4e82d3c595c829fd01c1d4b9d06887562
KWierso <kwierso@gmail.com>
2016-03-25 18:28:21 -0700

    lint

--- tests/webapp/api/test_bugzilla.py
+++ tests/webapp/api/test_bugzilla.py
@@ -1,29 +1,30 @@
 import json
-import responses
-import re
+#import responses
+#import re
 
-from django.conf import settings
+#from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from rest_framework.test import APIClient
+
 
 def test_create_bug(webapp, eleven_jobs_stored):
     """
     test creating a bug in bugzilla
     """
 
-    #def request_callback(request):
+    # def request_callback(request):
     #    headers = {}
     #    return(200, headers, {"success":"123456"})
-    #url_re = re.compile(r'https://bugzilla-dev.allizom.org/.*')
-    #responses.add_callback(
+    # url_re = re.compile(r'https://bugzilla-dev.allizom.org/.*')
+    # responses.add_callback(
     #    responses.POST, url_re,
     #    callback=request_callback, content_type="application/json",
-    #)
-    #responses.add(
+    # )
+    # responses.add(
     #    responses.POST, url_re,
     #    body='{"success":"123456"}', status=200, content_type="application/json",
-    #)
+    # )
 
     client = APIClient()
     user = User.objects.create(username="MyName", email="foo@bar.com")
--- treeherder/webapp/api/bugzilla.py
+++ treeherder/webapp/api/bugzilla.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from rest_framework import (status,
                             viewsets)
 from rest_framework.decorators import list_route
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+#from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 
 from treeherder.etl.common import make_request
@@ -19,28 +19,28 @@ class BugzillaViewSet(viewsets.ViewSet):
         """
 
         if settings.BZ_API_KEY is None:
-            return Response({"failure": "Bugzilla API key not defined. This shouldn't happen."},status=status.HTTP_400_BAD_REQUEST)
+            return Response({"failure": "Bugzilla API key not defined. This shouldn't happen."}, status=status.HTTP_400_BAD_REQUEST)
         else:
-          params = request.data
-          url = settings.BZ_API_URL + "/rest/bug"
-          headers = {
-              'X_BUGZILLA_API_KEY': settings.BZ_API_KEY
-          }
-          data = {
-              'product': params["product"],
-              'component': params["component"],
-              'summary': params["summary"],
-              'keywords': params["keywords"],
-              'version': params["version"],
-              'description': params["description"],
-              'comment_tags': "treeherder",
-              # XXX Should implement dependson, blocks, needinfo, and ccstring fields
-          }
+            params = request.data
+            url = settings.BZ_API_URL + "/rest/bug"
+            headers = {
+                'X_BUGZILLA_API_KEY': settings.BZ_API_KEY
+            }
+            data = {
+                'product': params["product"],
+                'component': params["component"],
+                'summary': params["summary"],
+                'keywords': params["keywords"],
+                'version': params["version"],
+                'description': params["description"],
+                'comment_tags': "treeherder",
+                # XXX Should implement dependson, blocks, needinfo, and ccstring fields
+            }
 
-          try:
-              response = make_request(url, method='POST', headers=headers, json=data)
-          except requests.exceptions.HTTPError as e:
-              response = e.response
-              return Response({"failure": response.json()['message'], "headers": headers, "settings": settings.BZ_API_KEY, "url": settings.BZ_API_URL}, status=status.HTTP_400_BAD_REQUEST)
+            try:
+                response = make_request(url, method='POST', headers=headers, json=data)
+            except requests.exceptions.HTTPError as e:
+                response = e.response
+                return Response({"failure": response.json()['message'], "headers": headers, "settings": settings.BZ_API_KEY, "url": settings.BZ_API_URL}, status=status.HTTP_400_BAD_REQUEST)
 
-          return Response({"success": response.json()["id"]})
+            return Response({"success": response.json()["id"]})
