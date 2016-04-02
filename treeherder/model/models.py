aa3b7b6f43be7fc244e45d1c2f2f3ae494697631
James Graham <james@hoppipolla.co.uk>
2016-04-02 16:48:01 +0100

    Make intermittent failure detector more efficient

--- treeherder/model/models.py
+++ treeherder/model/models.py
@@ -624,7 +624,7 @@ class FailureLine(models.Model):
                 score=1)
             new_link.save()
 
-        return classification
+        return classification, new_link
 
     def mark_best_classification_verified(self, classification):
         self.best_classification = classification
@@ -674,7 +674,7 @@ class FailureLine(models.Model):
 
         manual_detector = Matcher.objects.get(name="ManualDetector")
 
-        classification = self.set_classification(manual_detector)
+        classification, _ = self.set_classification(manual_detector)
         self.mark_best_classification_verified(classification)
 
 
