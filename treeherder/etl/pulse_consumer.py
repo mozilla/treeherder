3b788af151362c6d19efb9db416ca1b184770fd3
Cameron Dawson <cdawson@mozilla.com>
2016-04-21 13:32:44 -0700

    Bug 1266229 - Fix Pulse job ingestion prior to enabling it
    This contains several tweaks and fixes that allow us to ingest data from
    a real Task Cluster owned exchange.
    
    One of the main fixes is the way I was binding to the exchange and
    routing keys with the same Queue.  Before, it was re-creating the Queue,
    so would miss some of the bindings.
    
    This will also prune the durable queue if the config has removed some
    exchanges and/or routing keys.

--- treeherder/etl/pulse_consumer.py
+++ treeherder/etl/pulse_consumer.py
@@ -1,9 +1,13 @@
 import json
 import logging
+from urlparse import urlparse
 
+import newrelic.agent
+from django.conf import settings
 from kombu import Queue
 from kombu.mixins import ConsumerMixin
 
+from treeherder.etl.mixins import JsonExtractorMixin
 from treeherder.etl.tasks.pulse_tasks import store_pulse_jobs
 
 logger = logging.getLogger(__name__)
@@ -16,36 +20,67 @@ class JobConsumer(ConsumerMixin):
     def __init__(self, connection):
         self.connection = connection
         self.consumers = []
+        self.queue = None
+        config = settings.PULSE_DATA_INGESTION_CONFIG
+        self.queue_name = "queue/{}/jobs".format(urlparse(config).username)
 
     def get_consumers(self, Consumer, channel):
         return [
             Consumer(**c) for c in self.consumers
         ]
 
-    def listen_to(self, exchange, routing_key, queue_name,
-                  durable=True, auto_delete=False):
-        queue = Queue(
-            name=queue_name,
-            channel=self.connection.channel(),
-            exchange=exchange,
-            routing_key=routing_key,
-            durable=durable,
-            auto_delete=auto_delete
-        )
+    def bind_to(self, exchange, routing_key):
+        if not self.queue:
+            self.queue = Queue(
+                name=self.queue_name,
+                channel=self.connection.channel(),
+                exchange=exchange,
+                routing_key=routing_key,
+                durable=settings.PULSE_DATA_INGESTION_QUEUES_DURABLE,
+                auto_delete=settings.PULSE_DATA_INGESTION_QUEUES_AUTO_DELETE
+            )
+            self.consumers.append(dict(queues=self.queue,
+                                       callbacks=[self.on_message]))
+            # just in case the queue does not already exist on Pulse
+            self.queue.declare()
+        else:
+            self.queue.bind_to(exchange=exchange, routing_key=routing_key)
 
-        self.consumers.append(dict(queues=queue, callbacks=[self.on_message]))
+    def unbind_from(self, exchange, routing_key):
+        self.queue.unbind_from(exchange, routing_key)
 
     def on_message(self, body, message):
         try:
-            jobs = json.loads(body)
+            if isinstance(body, dict):
+                jobs = body
+            else:
+                jobs = json.loads(body)
+
             store_pulse_jobs.apply_async(
                 args=[jobs],
                 routing_key='store_pulse_jobs'
             )
-            message.ack()
 
-        except Exception:
-            logger.error("Unable to load jobs: {}".format(message), exc_info=1)
+        except:
+            logger.error("Unable to load jobs: {}".format(body), exc_info=1)
+            newrelic.agent.record_exception(params=body)
+
+        # if we can't read the message here, then we should ack anyway to get
+        # the bad message off the queue.  The logged error should give us
+        # enough to track it down.
+        message.ack()
 
     def close(self):
         self.connection.release()
+
+
+class PulseApi(JsonExtractorMixin):
+    """
+    Get information from the pulse API
+    """
+
+    def get_bindings(self, queue_name):
+        url = "{}queue/{}/{}".format(settings.PULSE_API_URL,
+                                     queue_name,
+                                     "bindings")
+        return self.extract(url)
