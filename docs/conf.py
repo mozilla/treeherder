7dac747d5b22436c480e4fab8f20e9d7875f8ffa
Ed Morley <emorley@mozilla.com>
2016-05-04 20:08:22 +0100

    Bug 1270239 - Docs: Remove the redundant sphinx_rtd_theme workaround
    The workaround that allowed using sphinx_rtd_theme when building
    locally, is no longer required. See:
    https://github.com/rtfd/readthedocs.org/pull/2115

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,8 @@
 import os
 import sys
 
+import sphinx_rtd_theme
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -106,13 +108,8 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-if not os.environ.get('READTHEDOCS'):
-    # Only import and set the theme if we're building docs locally:
-    # https://docs.readthedocs.org/en/latest/theme.html#how-do-i-use-this-locally-and-on-read-the-docs
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-# otherwise, readthedocs.org uses their theme by default, so no need to specify it
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
