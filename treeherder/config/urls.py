464ae2ffad47e9bac2130de5d4cc6d5c500b08d8
Gregory Szorc <gps@mozilla.com>
2016-05-20 16:56:13 -0700

    Bug 1200501 - Proxy reftest analyzer from hg.mozilla.org
    We don't want hg.mozilla.org advertising Content-Type headers.
    Treeherder currently sends people to hg.mozilla.org to run the
    reftest analyzer.
    
    This commit introduces a very simple proxy endpoint on Treeherder
    that fetches the reftest analyzer from hg.mozilla.org, adds a
    Content-Type header, and sends the raw bits to the client.
    
    URLs referencing hg.mozilla.org have been updated to use the
    new proxy endpoint.

--- treeherder/config/urls.py
+++ treeherder/config/urls.py
@@ -7,7 +7,7 @@ from django_browserid.admin import site as browserid_admin
 from treeherder.credentials.urls import urlpatterns as credentials_patterns
 from treeherder.embed import urls as embed_urls
 from treeherder.webapp.api import urls as api_urls
-from treeherder.webapp.views import LoginView
+from treeherder.webapp.views import LoginView, ReftestAnalyzerView
 
 browserid_admin.copy_registry(admin.site)
 
@@ -18,6 +18,7 @@ urlpatterns = [
    url(r'^admin/', include(browserid_admin.urls)),
    url(r'^docs/', include('rest_framework_swagger.urls')),
    url(r'^credentials/', include(credentials_patterns)),
+   url(r'^reftest-analyzer$', ReftestAnalyzerView.as_view(), name='reftest_analyzer'),
    url(r'', include('django_browserid.urls')),
 ]
 
--- treeherder/webapp/views.py
+++ treeherder/webapp/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import TemplateView
-
+from django.http import HttpResponse
+from django.views.generic import TemplateView, View
+import requests
 
 class LoginView(TemplateView):
     template_name = 'webapp/persona_login.html'
@@ -10,3 +11,19 @@ class LoginView(TemplateView):
         # Django_browserid will validate this so it's safe to pass it through.
         context['next'] = self.request.GET.get('next')
         return context
+
+
+class ReftestAnalyzerView(View):
+    """Proxy for the reftest analyzer.
+
+    hg.mozilla.org doesn't serve appropriate Content-Type headers. So proxy
+    the content and add the appropriate header.
+    """
+    ANALYZER_URL = 'https://hg.mozilla.org/mozilla-central/raw-file/tip/layout/tools/reftest/reftest-analyzer.xhtml'
+
+    def get(self):
+        r = requests.get(self.ANALYZER_URL, timeout=5)
+        response = HttpResponse(content=r.data(), status=r.status_code,
+                                content_type='text/html')
+
+        return response
