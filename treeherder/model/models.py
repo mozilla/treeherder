4e377daf5aa8357ccee9ea6d2d841dfe41ee70e3
camd <cdawson@mozilla.com>
2016-06-08 10:33:30 -0700

    Bug 1278711 - Add unique together index to JobDetail model/table (#1573)
    During job ingestion, we can sometimes get an error of
    MultipleObjectsReturned when creating a JobDetail record.  Adding
    an index for uniqueness will prevent us from getting that while
    using the get_or_create model function.

--- treeherder/model/models.py
+++ treeherder/model/models.py
@@ -571,6 +571,7 @@ class JobDetail(models.Model):
 
     class Meta:
         db_table = "job_detail"
+        unique_together = ("job", "title", "value", "url")
 
     def __str__(self):
         return "{0} {1} {2} {3} {4}".format(self.id,
