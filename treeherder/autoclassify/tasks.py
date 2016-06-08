d75039ae5b9fbf68d82bbec834c62caac032b2cc
James Graham <james@hoppipolla.co.uk>
2016-06-08 10:45:16 +0100

    Bug 1278858 - Add retryable_task decorator that provides the ability to not retry for given exceptions

--- treeherder/autoclassify/tasks.py
+++ treeherder/autoclassify/tasks.py
@@ -1,36 +1,30 @@
 import logging
 
 import newrelic.agent
-from celery import task
 from django.core.management import call_command
 
 from treeherder import celery_app
+from treeherder.workers.task import retryable_task
 
 logger = logging.getLogger(__name__)
 
 
-@task(name='autoclassify', max_retries=10)
+@retryable_task(name='autoclassify', max_retries=10)
 def autoclassify(project, job_guid):
     newrelic.agent.add_custom_parameter("project", project)
     newrelic.agent.add_custom_parameter("job_guid", job_guid)
-    try:
-        logger.info('Running autoclassify')
-        call_command('autoclassify', project, job_guid)
-        celery_app.send_task('detect-intermittents',
-                             [project, job_guid],
-                             routing_key='detect_intermittents')
-    except Exception as e:
-        autoclassify.retry(exc=e, countdown=(1 + autoclassify.request.retries) * 60)
+    logger.info('Running autoclassify')
+    call_command('autoclassify', project, job_guid)
+    celery_app.send_task('detect-intermittents',
+                         [project, job_guid],
+                         routing_key='detect_intermittents')
 
 
-@task(name='detect-intermittents', max_retries=10)
+@retryable_task(name='detect-intermittents', max_retries=10)
 def detect_intermittents(project, job_guid):
     newrelic.agent.add_custom_parameter("project", project)
     newrelic.agent.add_custom_parameter("job_guid", job_guid)
-    try:
-        logger.info('Running detect intermittents')
-        # TODO: Make this list configurable
-        if project == "mozilla-inbound":
-            call_command('detect_intermittents', project, job_guid)
-    except Exception as e:
-        detect_intermittents.retry(exc=e, countdown=(1 + detect_intermittents.request.retries) * 60)
+    logger.info('Running detect intermittents')
+    # TODO: Make this list configurable
+    if project == "mozilla-inbound":
+        call_command('detect_intermittents', project, job_guid)
