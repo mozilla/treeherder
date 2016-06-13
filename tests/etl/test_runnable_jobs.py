32a70427771bb2fb2b12587b3db8c87a094de4a2
Cameron Dawson <cdawson@mozilla.com>
2016-06-13 10:54:22 -0700

    test iterations

--- tests/etl/test_runnable_jobs.py
+++ tests/etl/test_runnable_jobs.py
@@ -1,26 +1,38 @@
 from treeherder.etl.allthethings import RunnableJobsProcess
-from treeherder.model.models import RunnableJob, BuildPlatform, MachinePlatform, JobType
+from treeherder.model.models import (RunnableJob, BuildPlatform,
+                                     MachinePlatform, JobType, Repository)
+from django.forms.models import model_to_dict
 
-
-def test_prune_old_runnable_job(jm, test_repository, eleven_jobs_stored):
+def test_prune_old_runnable_job(jm, eleven_jobs_stored):
 
     """
 
     """
     etl_process = RunnableJobsProcess()
 
-    RunnableJob.objects.create(build_platform=BuildPlatform.objects.all()[0],
-                               machine_platform=MachinePlatform.objects.all()[
-                                   0],
-                               job_type=JobType.objects.all()[0],
-                               option_collection_hash='test1',
-                               ref_data_name='test1',
-                               build_system_type='test1',
-                               repository=test_repository)
+    params = {
+        "build_platform": BuildPlatform.objects.first(),
+        "machine_platform": MachinePlatform.objects.first(),
+        "job_type": JobType.objects.first(),
+        "option_collection_hash": 'test1',
+        "ref_data_name": 'test1',
+        "build_system_type": 'test1',
+        "repository": Repository.objects.first()
+    }
+    RunnableJob.objects.create(**params)
 
-    etl_process.load({test_repository: ["buildername1"]})
+    params["ref_data_name"] = "buildername1"
+    for k, v in params.items():
+        try:
+            params[k] = model_to_dict(v)
+        except:
+            print("oops " + k)
+            print(v)
+            pass
+    etl_process.load({jm.project: [params]})
     rj = RunnableJob.objects.all()
     assert len(rj) == 1
-    assert rj.ref_data_name == "buildername1"
+
+    assert rj[0].ref_data_name == "buildername1"
 
 
