97211859070710e633ea1b71fcee60d9d272d305
Kalpesh Krishna <140070017@iitb.ac.in>
2016-06-29 05:03:50 +0530

    Bug 1254325 - Fixing the small UI error for TaskCluster Add New Jobs (#1625)
    * Bug 1254325 - Fixing the small UI error for TaskCluster Add New Jobs

--- ui/js/models/resultsets_store.js
+++ ui/js/models/resultsets_store.js
@@ -326,7 +326,8 @@ treeherder.factory('ThResultSetStore', [
                 // Since we want to try this only on try first
                 var decisionTaskID = "";
                 if (results && repoName === "try") {
-                    decisionTaskID = results[0].url.substring(results[0].url.indexOf("#") + 1);
+                    var inspectTask = _.where(results, {"title": "Inspect Task"})[0];
+                    decisionTaskID = inspectTask.url.substring(inspectTask.url.indexOf("#") + 1);
                     // Removing last two characters /0
                     decisionTaskID = decisionTaskID.substring(0, decisionTaskID.lastIndexOf('/'));
                 }
