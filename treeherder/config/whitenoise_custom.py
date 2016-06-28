a0ad720017eaa326fa70ed1b21c9b5ea2254ab89
Ed Morley <emorley@mozilla.com>
2016-06-28 18:15:46 +0100

    Bug 1282839 - Make CustomWhiteNoise easier to understand
    The existing behaviour in `update_files_dictionary()` wasn't as obvious
    as it could be. This is a no-op in terms of functionality.

--- treeherder/config/whitenoise_custom.py
+++ treeherder/config/whitenoise_custom.py
@@ -19,21 +19,24 @@ class CustomWhiteNoise(WhiteNoiseMiddleware):
     def update_files_dictionary(self, *args):
         """Add support for serving index pages for directory paths."""
         super(CustomWhiteNoise, self).update_files_dictionary(*args)
-        index_page_suffix = "/" + self.INDEX_NAME
+        index_page_suffix = '/' + self.INDEX_NAME
         index_name_length = len(self.INDEX_NAME)
-        index_files = {}
+        directory_indexes = {}
         for url, static_file in self.files.items():
-            # Add an additional fake filename to serve index pages for '/'.
             if url.endswith(index_page_suffix):
-                index_files[url[:-index_name_length]] = static_file
-        self.files.update(index_files)
+                # For each index file found, add a corresponding URL->content mapping
+                # for the file's parent directory, so that the index page is served for
+                # the bare directory URL ending in '/'.
+                parent_directory_url = url[:-index_name_length]
+                directory_indexes[parent_directory_url] = static_file
+        self.files.update(directory_indexes)
 
     def find_file(self, url):
         """Add support for serving index pages for directory paths when in DEBUG mode."""
         # In debug mode, find_file() is used to serve files directly from the filesystem
         # instead of using the list in `self.files`, so we append the index filename so
         # that will be served if present.
-        if url[-1] == '/':
+        if url.endswith('/'):
             url += self.INDEX_NAME
         return super(CustomWhiteNoise, self).find_file(url)
 
