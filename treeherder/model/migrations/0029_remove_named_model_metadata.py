f86dae6e61f7868fa53007c55cccbab6888140dd
William Lachance <wrlach@gmail.com>
2016-06-28 17:00:42 -0400

    Bug 1282907 - Fix named models to be more sane (#1624)
    * Remove unused members from NamedModel (define in subclass where really
      needed)
    * Enforce uniqueness on name properties

---
+++ treeherder/model/migrations/0029_remove_named_model_metadata.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('model', '0028_test_match_created_index'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='failureclassification',
+            name='active_status',
+        ),
+        migrations.RemoveField(
+            model_name='failureclassification',
+            name='description',
+        ),
+        migrations.RemoveField(
+            model_name='option',
+            name='active_status',
+        ),
+        migrations.RemoveField(
+            model_name='option',
+            name='description',
+        ),
+        migrations.RemoveField(
+            model_name='product',
+            name='active_status',
+        ),
+        migrations.RemoveField(
+            model_name='product',
+            name='description',
+        ),
+        migrations.RemoveField(
+            model_name='repositorygroup',
+            name='active_status',
+        ),
+        migrations.AlterField(
+            model_name='failureclassification',
+            name='name',
+            field=models.CharField(unique=True, max_length=50),
+        ),
+        migrations.AlterField(
+            model_name='option',
+            name='name',
+            field=models.CharField(unique=True, max_length=50),
+        ),
+        migrations.AlterField(
+            model_name='product',
+            name='name',
+            field=models.CharField(unique=True, max_length=50),
+        ),
+        migrations.AlterField(
+            model_name='repositorygroup',
+            name='name',
+            field=models.CharField(unique=True, max_length=50),
+        ),
+    ]
