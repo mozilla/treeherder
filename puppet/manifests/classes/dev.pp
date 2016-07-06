df656b5ca7132fa65f0673df395cc06157cb316a
Ed Morley <emorley@mozilla.com>
2016-07-06 15:45:30 +0100

    Bug 1284853 - Vagrant: Stop creating treeherder-etl credentials & user
    Since the `treeherder-etl` user is no longer used as part of data
    ingestion after bug 1211414, and it doesn't help people wanting to
    submit locally, since it's a pain for them to retrieve the Hawk secret.
    
    Instead it makes more sense for them to just use:
    `./manage.py create_credentials my-test-user`
    ...which then outputs the secret to the console.

--- puppet/manifests/classes/dev.pp
+++ puppet/manifests/classes/dev.pp
@@ -26,20 +26,4 @@ class dev{
     require => Exec["load_initial_data"],
     user => "${APP_USER}",
   }
-
-  exec{"create_superuser":
-    cwd => "${PROJ_DIR}",
-    command => "bash -c 'source /etc/profile.d/treeherder.sh; ${VENV_DIR}/bin/python manage.py createsuperuser --username treeherder --email treeherder@mozilla.com --noinput'",
-    require => Exec["migrate"],
-    user => "${APP_USER}",
-    unless => "bash -c 'source /etc/profile.d/treeherder.sh; ${VENV_DIR}/bin/python manage.py dumpdata auth.User | grep treeherder@mozilla.com'",
-  }
-
-  exec{"create_etl_credentials":
-    cwd => "${PROJ_DIR}",
-    command => "bash -c 'source /etc/profile.d/treeherder.sh; ${VENV_DIR}/bin/python manage.py create_credentials treeherder-etl'",
-    require => Exec["create_superuser"],
-    user => "${APP_USER}",
-    unless => "bash -c 'source /etc/profile.d/treeherder.sh; ${VENV_DIR}/bin/python manage.py dumpdata credentials.Credentials | grep treeherder-etl'",
-  }
 }
