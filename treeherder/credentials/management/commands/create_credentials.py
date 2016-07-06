e98e43285f8c4bf6e95bfc6e65e59fa70e466b0f
Ed Morley <emorley@mozilla.com>
2016-07-06 15:48:04 +0100

    Bug 1284853 - Handle an already existing client_id in create_credentials
    Previously if the requested client_id already existed, the command
    would have raised. Now the existing credentials secret is output, in
    case the user had just forgotten it.

--- treeherder/credentials/management/commands/create_credentials.py
+++ treeherder/credentials/management/commands/create_credentials.py
@@ -4,16 +4,21 @@ from treeherder.credentials.models import Credentials
 
 
 class Command(BaseCommand):
-    help = 'Create a new set of credentials to use with the hawk authentication scheme'
+    help = 'Create or retrieve a set of Hawk credentials for use with the API'
 
     def add_arguments(self, parser):
         parser.add_argument('client_id', type=str)
 
     def handle(self, *args, **options):
-        credentials = Credentials.objects.create(
+        credentials, created = Credentials.objects.get_or_create(
             client_id=options['client_id'],
-            authorized=True
+            defaults={'authorized': True}
         )
 
-        self.stdout.write('Successfully created credentials for "%s"' % credentials.client_id)
+        if created:
+            message = 'Successfully created Hawk client ID "%s"'
+        else:
+            message = 'Hawk client ID "%s" already exists'
+
+        self.stdout.write(message % credentials.client_id)
         self.stdout.write('Secret: %s' % credentials.secret)
