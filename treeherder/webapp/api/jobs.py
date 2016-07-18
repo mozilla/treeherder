282a66b8f0fbb89658c153122fd3ac76b53dde15
camd <cdawson@mozilla.com>
2016-07-18 13:49:48 -0700

    Bug 1287501 - Restrict jobs query last_modified to within last 30 min… (#1702)
    * Bug 1287501 - Restrict jobs query last_modified to within last 30 minutes

--- treeherder/webapp/api/jobs.py
+++ treeherder/webapp/api/jobs.py
@@ -1,4 +1,7 @@
+import datetime
+
 import django_filters
+from dateutil import parser
 from rest_framework import (filters,
                             pagination,
                             viewsets)
@@ -91,10 +94,27 @@ class JobsViewSet(viewsets.ViewSet):
         - return_type (dict)
         """
         MAX_JOBS_COUNT = 2000
+        LAST_MODIFIED_WINDOW = 30  # minutes
+
         filter = UrlQueryFilter(request.query_params)
 
         offset = int(filter.pop("offset", 0))
         count = int(filter.pop("count", 10))
+        if "last_modified" in filter.conditions:
+            datestr = filter.get("last_modified")[1]
+            try:
+                last_modified = parser.parse(datestr)
+                if last_modified < datetime.datetime.utcnow() - \
+                   datetime.timedelta(minutes=LAST_MODIFIED_WINDOW):
+                    return Response(
+                        "`last_modified` of {} is not within last {} minutes".format(
+                            datestr,
+                            LAST_MODIFIED_WINDOW),
+                        status=HTTP_400_BAD_REQUEST)
+            except ValueError:
+                return Response(
+                    "Invalid value for `last_modified`: ".format(datestr),
+                    status=HTTP_400_BAD_REQUEST)
 
         if count > MAX_JOBS_COUNT:
             msg = "Specified count exceeds API MAX_JOBS_COUNT value: {}".format(MAX_JOBS_COUNT)
