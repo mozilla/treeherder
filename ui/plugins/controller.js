1be9538255f1957bef016a44e2ae9b1186e43e49
jgraham <james@hoppipolla.co.uk>
2016-07-21 00:46:10 -0700

    Bug 1288230 - Show autoclassify panel by default for sheriffs (#1713)

--- ui/plugins/controller.js
+++ ui/plugins/controller.js
@@ -57,6 +57,7 @@ treeherder.controller('PluginCtrl', [
         // phase of this feature.
         var showAutoClassifyTab = function() {
             thTabs.tabs.autoClassification.enabled = ($location.search().autoclassify === true ||
+                                                      ($rootScope.user && $rootScope.user.is_staff) ||
                                                       $location.host().indexOf('herokuapp.com') !== -1);
         };
         showAutoClassifyTab();
