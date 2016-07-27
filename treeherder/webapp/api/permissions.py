2818afbb2c9c9d1aac2e00bcda213ad42a5d6be6
Ed Morley <emorley@mozilla.com>
2016-07-27 12:10:15 +0100

    Bug 1275405 - Remove unnecessary check for user.is_authenticated
    Since:
    * It's always true for all users other than `AnonymousUser`.
    * d-r-f's similar `IsAdminUser` implementation doesn't use it:
    https://github.com/tomchristie/django-rest-framework/blob/3.3.3/rest_framework/permissions.py#L49-L55

--- treeherder/webapp/api/permissions.py
+++ treeherder/webapp/api/permissions.py
@@ -10,9 +10,7 @@ class IsStaffOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS or
-                request.user and
-                request.user.is_authenticated() and
-                request.user.is_staff)
+                request.user and request.user.is_staff)
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
