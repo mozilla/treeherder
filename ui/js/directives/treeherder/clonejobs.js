2eafbc819e76fe3b67285613188c412d10a67268
Wes Kocher <kwierso@gmail.com>
2016-07-26 19:30:55 -0700

    Bug 1289651 - Only scroll job into view if it's not already visible

--- ui/js/directives/treeherder/clonejobs.js
+++ ui/js/directives/treeherder/clonejobs.js
@@ -788,9 +788,21 @@ treeherder.directive('thCloneJobs', [
                 } else if (window.innerHeight >= 1000) {
                     scrollOffset = -200;
                 }
-                $('.th-global-content').scrollTo(el, duration, {offset: scrollOffset});
+                if(!el.isOnScreen()) {
+                    $('.th-global-content').scrollTo(el, duration, {offset: scrollOffset});
+                }
             }
 
+        };
+
+        $.fn.isOnScreen = function(){
+            var viewport = {};
+            viewport.top = $(window).scrollTop();
+            viewport.bottom = viewport.top + $(window).height() - $("#info-panel").height();
+            var bounds = {};
+            bounds.top = this.offset().top;
+            bounds.bottom = bounds.top + this.outerHeight();
+            return ((bounds.top <= viewport.bottom) && (bounds.bottom >= viewport.top));
         };
 
         var registerCustomEventCallbacks = function(scope, element, attrs){
