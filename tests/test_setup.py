6425ba8a56201c693bdce4db72d2f6b6e24c6d02
Ed Morley <emorley@mozilla.com>
2016-08-09 21:11:21 +0100

    Bug 1289156 - Add a test to check mysqlclient TLS can't be downgraded
    If mysqlclient has been compiled against a vulnerable version of
    libmysqlclient then this test will fail. There is overlap between this
    and our custom Django system check for ensuring mysqlclient has been
    compiled against libmysqlclient >= 5.7.11, however there advantages in
    having both:
    * the system check is run during deploy, unlike this test
    * however this test is more thorough since it actually checks TLS
    behaviour and not just version numbers (but this method cannot be used
    in the system check run during production deployment, since it relies on
    having a MySQL server instance that doesn't support TLS, to emulate the
    TLS being stripped by an attacker)

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,5 +1,6 @@
 import MySQLdb
 import pytest
+from _mysql_exceptions import OperationalError
 from celery import current_app
 from django.conf import settings
 from django.core.cache import cache
@@ -16,6 +17,34 @@ def db_conn():
         passwd=db_config.get('PASSWORD') or '',
         **db_options
     )
+
+
+def test_mysqlclient_tls_enforced():
+    """
+    Test that if a CA certificate is specified, the connection won't fall back to
+    plaintext, if the server doesn't support TLS (or if a MITM attacker strips it).
+
+    If mysqlclient has been compiled against a vulnerable version of libmysqlclient
+    then this test will fail. There is overlap between this and our custom Django system
+    check for ensuring mysqlclient has been compiled against libmysqlclient >= 5.7.11,
+    however there advantages in having both:
+      * the system check is run during deploy, unlike this test
+      * however this test is more thorough since it actually checks TLS behaviour and not
+        just version numbers (but this method cannot be used in the system check run
+        during production deployment, since it relies on having a MySQL server instance
+        that doesn't support TLS, to emulate the TLS being stripped by an attacker)
+    """
+    db_config = settings.DATABASES['default']
+    with pytest.raises(OperationalError) as e:
+        MySQLdb.connect(
+            host=db_config['HOST'],
+            user=db_config['USER'],
+            passwd=db_config.get('PASSWORD') or '',
+            ssl={
+                'ca': 'foo/bar.pem',
+            }
+        )
+    assert "SSL connection error" in str(e.value)
 
 
 def test_no_missing_migrations():
