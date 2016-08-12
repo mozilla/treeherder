ec7845ff5de94f3c8b157bc830b73e147768c536
Ed Morley <emorley@mozilla.com>
2016-08-12 14:41:59 +0100

    Bug 1296047 - Vagrant: Move environment variables to their own file
    Having them in a separate file is cleaner, makes the discovering where
    the environment variables are set easier, plus means we can symlink the
    file, so future variable changes will take effect immediately, rather
    than needing a re-provision after pulling latest master.

---
+++ puppet/files/treeherder/env.sh
@@ -0,0 +1,13 @@
+# Ensure the vendored libmysqlclient library can be found at run-time.
+export LD_LIBRARY_PATH="/home/vagrant/venv/lib/x86_64-linux-gnu"
+
+export ENABLE_LOCAL_SETTINGS_FILE='True'
+export BROKER_URL='amqp://guest:guest@localhost//'
+export DATABASE_URL='mysql://root@localhost/treeherder'
+export DATABASE_URL_RO='$DATABASE_URL'
+export ELASTICSEARCH_URL='http://localhost:9200'
+
+export TREEHERDER_DEBUG='True'
+export ENABLE_DEBUG_TOOLBAR='True'
+export TREEHERDER_DJANGO_SECRET_KEY='secret-key-of-at-least-50-characters-to-pass-check-deploy'
+export NEW_RELIC_DEVELOPER_MODE='True'
--- puppet/manifests/vagrant.pp
+++ puppet/manifests/vagrant.pp
@@ -9,10 +9,6 @@ $VENV_DIR = "${HOME_DIR}/venv"
 $PS1 = '\[\e[0;31m\]\u\[\e[m\] \[\e[1;34m\]\w\[\e[m\] \$ '
 $THELP_TEXT = 'Type \\"thelp\\" to see a list of Treeherder-specific helper aliases'
 
-# You can make these less generic if you like, but these are box-specific
-# so it's not required.
-$DJANGO_SECRET_KEY = "secret-key-of-at-least-50-characters-to-pass-check-deploy"
-
 Exec {
     path => "${VENV_DIR}/bin:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin",
 }
@@ -24,21 +20,8 @@ line {"etc-hosts":
 }
 
 file {"/etc/profile.d/treeherder.sh":
-    content => "
-# Ensure the vendored libmysqlclient library can be found at run-time.
-export LD_LIBRARY_PATH='${VENV_DIR}/lib/x86_64-linux-gnu'
-
-export ENABLE_LOCAL_SETTINGS_FILE='True'
-export BROKER_URL='amqp://guest:guest@localhost//'
-export DATABASE_URL='mysql://root@localhost/treeherder'
-export DATABASE_URL_RO='$DATABASE_URL'
-export ELASTICSEARCH_URL='http://localhost:9200'
-
-export TREEHERDER_DEBUG='True'
-export ENABLE_DEBUG_TOOLBAR='True'
-export TREEHERDER_DJANGO_SECRET_KEY='${DJANGO_SECRET_KEY}'
-export NEW_RELIC_DEVELOPER_MODE='True'
-"
+    ensure => "link",
+    target => "${PROJ_DIR}/puppet/files/treeherder/env.sh",
 }
 
 file {"/var/log/treeherder/":
