e97b8a349ddfa4a8fcd58c041d4f3876923602fe
William Lachance <wlachance@mozilla.com>
2016-08-17 12:49:28 -0400

    Bug 1295536 - Don't try to store non-compliant perf data via logparser

--- tests/log_parser/test_performance_parser.py
+++ tests/log_parser/test_performance_parser.py
@@ -1,12 +1,30 @@
+import json
+
 from treeherder.log_parser.parsers import PerformanceParser
 
 
 def test_performance_log_parsing_malformed_perfherder_data():
     """
-    If we have one malformed perfherder data line, we should just ignore
-    it and still be able to parse the next one
+    If we have malformed perfherder data lines, we should just ignore
+    them and still be able to parse the valid ones
     """
     parser = PerformanceParser()
+
+    # invalid json
     parser.parse_line("PERFHERDER_DATA: {oh noes i am not valid json}", 1)
+    # doesn't comply with schema
     parser.parse_line("PERFHERDER_DATA: {}", 2)
-    assert parser.get_artifact() == [{}]
+
+    valid_perfherder_data = {
+        "framework": {"name": "talos"}, "suites": [{
+            "name": "basic_compositor_video",
+            "subtests": [{
+                "name": "240p.120fps.mp4_scale_fullscreen_startup",
+                "value": 1234
+            }]
+        }]
+    }
+    parser.parse_line('PERFHERDER_DATA: {}'.format(
+        json.dumps(valid_perfherder_data)), 3)
+
+    assert parser.get_artifact() == [valid_perfherder_data]
