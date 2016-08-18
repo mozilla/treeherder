ccaebfa04be53eb367090a95c3e8bf8b339194f9
Ed Morley <emorley@mozilla.com>
2016-08-18 14:52:21 +0100

    Bug 1296267 - Vagrant: Install Elasticsearch using dpkg
    Using apt-get isn't worth it since:
    * we have to manually add their repository due to it being incompatible
    with add-apt-repository, resulting in a lot of boilerplate
    * we want to pin to a specific version, so don't need apt-get to pull
    new versions from the repository for us
    * the elasticsearch package doesn't declare any dependencies, so don't
    need apt-get for managing them either
    * it's only a development environment, so TLS is fine for security
    
    In addition, the installation process now matches that used in Travis,
    which improves consistency between environments and means in the future
    we could always factor out Elasticsearch install to a shared script
    to avoid duplication.

--- puppet/manifests/classes/elasticsearch.pp
+++ puppet/manifests/classes/elasticsearch.pp
@@ -1,10 +1,16 @@
 class elasticsearch {
+  $es_version = "2.3.5"
+  $es_url = "https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/${es_version}/elasticsearch-${es_version}.deb"
+
   package { 'openjdk-7-jre-headless':
     ensure => installed
   }
 
-  package { 'elasticsearch':
-    ensure => installed
+  # Once we're using Ubuntu 16.04's newer dpkg we can pipe the download and skip creating a file.
+  exec { "install-elasticsearch":
+    user => "${APP_USER}",
+    command => "curl -sSo ${HOME_DIR}/elasticsearch.deb ${es_url} && sudo dpkg -i ${HOME_DIR}/elasticsearch.deb",
+    unless => "test \"$(dpkg-query --show --showformat='\${Version}' elasticsearch 2>&1)\" = '${es_version}'",
   }
 
   service { 'elasticsearch':
@@ -12,7 +18,7 @@ class elasticsearch {
     enable => true,
     require => [
       Package['openjdk-7-jre-headless'],
-      Package['elasticsearch'],
+      Exec['install-elasticsearch'],
     ],
   }
 }
