632579186d00672d13af75c5b0f74f75c7c6fa8a
Rob Wood <rwood@mozilla.com>
2016-09-21 11:13:12 -0400

    Bug 1304362 - Don't include invalid alerts in bug and copy summaries; r=wlach

--- ui/js/models/perf/alerts.js
+++ ui/js/models/perf/alerts.js
@@ -84,7 +84,7 @@ treeherder.factory('PhAlerts', [
                 return !alert.is_regression && alert.visible;}),
             'amount_pct').reverse();
             var regressed = _.sortBy(_.filter(this.alerts, function(alert) {
-                return alert.is_regression && alert.visible;}),
+                return alert.is_regression && alert.visible && !alert.isInvalid();}),
             'amount_pct').reverse();
 
             var formatAlert = function(alert, alertList){
