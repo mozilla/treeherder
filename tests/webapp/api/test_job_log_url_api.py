30f698866dfa94496849c9e410b3588154110a1f
William Lachance <wlachance@mozilla.com>
2016-09-23 16:38:33 -0400

    Bug 1304557 - Allow passing multiple job_id parameters to job log url endpoint

--- tests/webapp/api/test_job_log_url_api.py
+++ tests/webapp/api/test_job_log_url_api.py
@@ -5,20 +5,36 @@ from treeherder.model.models import (Job,
 
 
 def test_get_job_log_urls(test_repository, webapp):
-    job = Job.objects.create(
+    job1 = Job.objects.create(
         repository=test_repository,
         guid='1234',
         project_specific_id=1)
-    JobLog.objects.create(job=job,
+    job2 = Job.objects.create(
+        repository=test_repository,
+        guid='5678',
+        project_specific_id=2)
+
+    JobLog.objects.create(job=job1,
                           name='test_log_1',
                           url='http://google.com',
                           status=JobLog.PENDING)
-    JobLog.objects.create(job=job,
+    JobLog.objects.create(job=job1,
                           name='test_log_2',
                           url='http://yahoo.com',
                           status=JobLog.PARSED)
+    JobLog.objects.create(job=job2,
+                          name='test_log_3',
+                          url='http://yahoo.com',
+                          status=JobLog.PARSED)
+
     resp = webapp.get(reverse('job-log-url-list',
                       kwargs={"project": test_repository.name}) +
                       '?job_id=1')
     assert resp.status_int == 200
     assert len(resp.json) == 2
+
+    resp = webapp.get(reverse('job-log-url-list',
+                      kwargs={"project": test_repository.name}) +
+                      '?job_id=1&job_id=2')
+    assert resp.status_int == 200
+    assert len(resp.json) == 3
