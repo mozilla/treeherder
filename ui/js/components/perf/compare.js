dd28d4a4962124d3b928ebab76a020db3e7d34e5
Rob Wood <rwood@mozilla.com>
2016-09-30 10:24:57 -0400

    Bug 1306601 - Fix e10s dashboard filtering

--- ui/js/components/perf/compare.js
+++ ui/js/components/perf/compare.js
@@ -17,7 +17,7 @@ treeherder.component('phCompareTable', {
         filterByFramework: '@',
         releaseBlockerCriteria: '@'
     },
-    controller: function() {
+    controller: function($element, $attrs) {
         var ctrl = this;
 
         if (!ctrl.baseTitle) {
@@ -49,8 +49,15 @@ treeherder.component('phCompareTable', {
                 return results;
             }
             return _.filter(results, function(result) {
-                var testCondition = key + ' ' + result.name;
+                var testCondition = key + ' ';
+                if ($attrs.type === 'trend') {
+                    testCondition += result.trendResult.name;
+                } else {
+                    testCondition += result.name;
+                }
                 return _.every(ctrl.filterOptions.filter.split(' '), function(matchText) {
+                    if ($attrs.type === 'trend')
+                        return filter(testCondition, matchText) && shouldBeShown(result.trendResult);
                     return filter(testCondition, matchText) && shouldBeShown(result);
                 });
             });
