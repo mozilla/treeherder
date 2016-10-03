da11b508c1c6f7b6c2ff08d448c4287c927957ae
Armen Zambrano G <armenzg@mozilla.com>
2016-10-03 12:56:51 -0400

    Initial SETA django app

---
+++ treeherder/etl/seta.py
@@ -0,0 +1,155 @@
+import logging
+
+from treeherder.seta.common import unique_key
+from treeherder.seta.runnable_jobs import RunnableJobsClient
+
+LOG = logging.getLogger(__name__)
+
+
+class Treecodes:
+    """This class contain all the mapping we need in SETA and makes it works"""
+
+    def __init__(self, repo_name='mozilla-inbound'):
+        # default to query all jobs on mozilla-inbound branch
+        self.jobtypes = []
+        self.jobnames = []
+        ignored_jobs = []
+
+        for job in RunnableJobsClient().query_runnable_jobs(repo_name)['results']:
+            # e.g. web-platform-tests-4
+            # e.g. Ubuntu VM 12.04 x64 mozilla-inbound opt test web-platform-tests-4 OR
+            #      desktop-test-linux64/opt-web-platform-tests-4
+            testtype = job_testtype(job)
+            if self._ignore(testtype):
+                ignored_jobs.append(job['ref_data_name'])
+                continue
+
+            self.jobtypes.append(
+                    unique_key(testtype=testtype,
+                               buildtype=job['platform_option'],
+                               platform=job['platform']))
+            self.jobnames.append({
+                'buildplatform': job['build_system_type'],
+                'buildtype': job['platform_option'],
+                'platform': job['platform'],
+                'ref_data_name': job['ref_data_name'],
+                'testtype': testtype,
+                })
+
+        for ref_data_name in sorted(ignored_jobs):
+            LOG.info('Ignoring {}'.format(ref_data_name))
+
+    def _ignore(self, testtype):
+        if not testtype:
+            return True
+
+        # XXX: This has the danger of falling out of date
+        # https://bugzilla.mozilla.org/show_bug.cgi?id=1325369
+        for i in ('dep', 'nightly', 'non-unified', 'valgrind', 'build'):
+            if testtype.find(i) != -1:
+                return True
+
+    def query_jobtypes(self):
+        """Query all available jobtypes and return it as list"""
+        return self.jobtypes
+
+    def query_jobnames(self):
+        """Query all jobnames including buildtype and groupcode, then return them as list"""
+        return self.jobnames
+
+
+def job_testtype(job):
+    return parse_testtype(
+        build_system_type=job['build_system_type'],
+        job_type_name=job['job_type_name'],
+        platform_option=job['platform_option'],
+        ref_data_name=job['ref_data_name']
+    )
+
+
+def parse_testtype(build_system_type, job_type_name, platform_option, ref_data_name):
+    '''
+    build_system_type - buildbot or taskcluster
+    job_type_name - e.g. Mochitest (JobType.name)
+    platform_option - e.g. opt
+    ref_data_name - buildername or task label
+    '''
+    # XXX: Figure out how to ignore build, lint, etc. jobs
+    # https://bugzilla.mozilla.org/show_bug.cgi?id=1318659
+    if build_system_type == 'buildbot':
+        # The testtype of builbot job can been found in 'ref_data_name'
+        # like web-platform-tests-4 in "Ubuntu VM 12.04 x64 mozilla-inbound
+        # opt test web-platform-tests-4"
+        return ref_data_name.split(' ')[-1]
+    else:
+        # The test name on TaskCluster comes to a sort of combination
+        # (e.g desktop-test-linux64/debug-jittests-3)
+
+        # NOTE: Buildbot bridge tasks always have a Buildbot job associated to it. We will
+        #       ignore any BBB task since we will be analyzing instead the Buildbot job associated
+        #       to it. If BBB tasks were a production system and there was a technical advantage
+        #       we could look into analyzing that instead of the BB job.
+        if ref_data_name.startswith('desktop-test') or \
+           ref_data_name.startswith('android-test'):
+            # we should get "jittests-3" as testtype for a job_type_name like
+            # desktop-test-linux64/debug-jittests-3
+            return transform(job_type_name.split('{buildtype}-'.format(buildtype=platform_option))[-1])
+
+
+def transform(testtype):
+    '''
+    A lot of these transformations are from tasks before task labels and some of them are if we
+    grab data directly from Treeherder jobs endpoint instead of runnable jobs API.
+    '''
+    # XXX: Evaluate which of these transformations are still valid
+    if testtype.startswith('[funsize'):
+        return None
+
+    testtype = testtype.split('/opt-')[-1]
+    testtype = testtype.split('/debug-')[-1]
+
+    # this is plain-reftests for android
+    testtype = testtype.replace('plain-', '')
+
+    # For testtype to 'build' since that will ensure we ignore these jobs
+    # Bug 1318659 - for how mozci could help here
+    testtype = testtype.replace(' Opt', 'build')
+    testtype = testtype.replace(' Debug', 'build')
+    testtype = testtype.replace(' Dbg', 'build')
+    testtype = testtype.replace(' (opt)', 'build')
+    testtype = testtype.replace(' PGO Opt', 'build')
+    testtype = testtype.replace(' Valgrind Opt', 'build')
+    testtype = testtype.replace(' Artifact Opt', 'build')
+    testtype = testtype.replace(' (debug)', 'build')
+
+    testtype = testtype.strip()
+
+    # https://bugzilla.mozilla.org/show_bug.cgi?id=1313844
+    testtype = testtype.replace('browser-chrome-e10s', 'e10s-browser-chrome')
+    testtype = testtype.replace('devtools-chrome-e10s', 'e10s-devtools-chrome')
+    testtype = testtype.replace('[TC] Android 4.3 API15+ ', '')
+
+    testtype = testtype.replace('webgl-', 'gl-')
+
+    return testtype
+
+
+def valid_platform(platform):
+    # We only care about in-tree scheduled tests and ignore out of band system like autophone.
+    # XXX: This can fall out of date
+    # https://bugzilla.mozilla.org/show_bug.cgi?id=1325369
+    return platform not in [
+        'android-4-2-armv7-api15',
+        'android-4-4-armv7-api15',
+        'android-5-0-armv8-api15',
+        'android-5-1-armv7-api15',
+        'android-6-0-armv8-api15',
+        'osx-10-7',  # Build
+        'osx-10-9',
+        'osx-10-11',
+        'other',
+        'taskcluster-images',
+        'windows7-64',   # We don't test 64-bit builds on Windows 7 test infra
+        'windows8-32',  # We don't test 32-bit builds on Windows 8 test infra
+        'Win 6.3.9600 x86_64',
+    ]
---
+++ treeherder/seta/runnable_jobs.py
@@ -0,0 +1,55 @@
+import logging
+
+from treeherder.etl.common import make_request
+
+HEADERS = {
+    'Accept': 'application/json',
+    'User-Agent': 'treeherder-seta',
+}
+
+logger = logging.getLogger(__name__)
+
+
+class RunnableJobsClient():
+    def __init__(self, treeherder_host='https://treeherder.mozilla.org',
+                 tc_index_url='https://index.taskcluster.net/v1/task/gecko.v2.%s.latest.firefox.decision'):
+        self.tc_index_url = tc_index_url
+        self.th_api = treeherder_host + '/api/project/{0}/runnable_jobs/?decision_task_id={1}&format=json'
+        self.cache = {}
+
+    def query_runnable_jobs(self, repo_name, task_id=None):
+        '''Return runnable_jobs data
+
+        Calling this repo without task_id is optimal as we then only cache the latest runnable jobs.
+
+        We do caching because the data does not really change and because bug 1288028 makes calls to
+        the runnable_jobs API very slow.
+        '''
+        if repo_name not in self.cache:
+            self.cache[repo_name] = {}
+
+        if not task_id:
+            task_id = self._query_latest_gecko_decision_task_id(repo_name)
+            self.cache[repo_name]['latest'] = self._query_runnable_jobs(repo_name=repo_name, task_id=task_id)
+            return self.cache[repo_name]['latest']
+        else:
+            if task_id in self.cache:
+                # XXX: In previous code, we were returning None; what should we do for this case?
+                logger.info("We have already processed the data from this task (%s)." % task_id)
+                return self.cache[repo_name][task_id]
+            else:
+                logger.info("We're going to fetch new runnable jobs data.")
+                self.cache[repo_name][task_id] = self._query_runnable_jobs(repo_name=repo_name, task_id=task_id)
+                return self.cache[repo_name][task_id]
+
+    def _query_latest_gecko_decision_task_id(self, repo_name):
+        url = self.tc_index_url % repo_name
+        logger.info('Fetching {}'.format(url))
+        latest_task = make_request(url, headers={'accept-encoding': 'json'}).json()
+        task_id = latest_task['taskId']
+        logger.info('For {} we found the task id: {}'.format(repo_name, task_id))
+        return task_id
+
+    def _query_runnable_jobs(self, repo_name, task_id):
+        url = self.th_api.format(repo_name, task_id)
+        return make_request(url, headers=HEADERS).json()
