5dfe4596f7f268d0a3181304f0203ea871a06070
Armen Zambrano G <armenzg@mozilla.com>
2016-10-04 13:05:00 -0400

    Create hello world page for setadetails endpoint

--- treeherder/seta/urls.py
+++ treeherder/seta/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'seta'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    # XXX Can we have a more meaningful name for this endpoint?
+    url(r'^setadetails/$', views.setadetails, name='setadetails'),
 ]
--- treeherder/seta/views.py
+++ treeherder/seta/views.py
@@ -1,4 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
     return render(request, 'seta/index.html', {})
+
+def setadetails(request):
+    return HttpResponse('Hello world!')
