b1a5ca19757b5d838ce07fef51ebe65630378907
Armen Zambrano G <armenzg@mozilla.com>
2016-10-04 15:42:49 -0400

    Revert "Import seta.html as index.html; do not use seta.js for now"
    This reverts commit 2320a00c7e0bb756b97c8d82a343bd1905f63236.

--- treeherder/seta/views.py
+++ treeherder/seta/views.py
@@ -1,8 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
 
 def index(request):
-    return render(request, 'seta/index.html', {})
+    return HttpResponse("Hello world! Welcome to SETA!")
 
 def setadetails(request):
     return HttpResponse('Hello world!')
