98b543a62ee555a4dfb305a5d5cd0cccc5b2d2f2
Armen Zambrano G <armenzg@mozilla.com>
2016-10-04 12:57:06 -0400

    Import seta.html as index.html; do not use seta.js for now

--- treeherder/seta/views.py
+++ treeherder/seta/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello world! Welcome to SETA!")
+    return render(request, 'seta/index.html', {})
