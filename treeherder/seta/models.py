7288954c888b2933cdafb522b90b30c1305ce0b7
Armen Zambrano G <armenzg@mozilla.com>
2016-10-06 14:21:03 -0400

    Add basic tests for JobPriorities and TaskRequests

--- treeherder/seta/models.py
+++ treeherder/seta/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class TaskRequests(models.Model):
+    ''' Track TaskCluster requests for SETA information.
+
+    The Gecko decision task executes on every push and it queries SETA for information.
+    This information is used the graph generation code to determine which jobs
+    to exclude on that run or not.
+    '''
+    repo_name = models.CharField(max_length=128, primary_key=True)  # e.g. mozilla-inbound
+    counter = models.IntegerField()  # Number of times TC has reached the API
+    last_request = models.DateTimeField()  # Last time TC reached the API
+    reset_delta = models.IntegerField()  # Maximum number of pushes before SETA is skipped
+
+    def __str__(self):
+        return '%s/%s/%s'.format(
+            self.repo_name,
+            self.counter,
+            self.reset_delta
+        )
+
+
+class JobPriorities(models.Model):
+    # e.g. web-platform-tests-1; {opt,pgo,debug}; windows8-64; 5; 5400; {buildbot,taskcluster,*}
+    testtype = models.CharField(max_length=128)
+    buildtype = models.CharField(max_length=64)
+    platform = models.CharField(max_length=64)
+    priority = models.IntegerField()
+    timeout = models.IntegerField()
+    expires = models.DateTimeField()
+    buildsystem = models.CharField(max_length=64)
+
+    def has_expired(self):
+        now = timezone.now()
+        return self.expires < now
+
+    def __str__(self):
+        return '%s/%s/%s'.format(
+            self.testtype,
+            self.buildype,
+            self.priority
+        )
--- treeherder/seta/tests.py
+++ treeherder/seta/tests.py
@@ -1,3 +1,30 @@
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
 
-# Create your tests here.
+from .models import TaskRequests, JobPriorities
+
+
+class TaskRequestsTests(TestCase):
+    def test_create_instance(self):
+        TaskRequests(repo_name='mozilla-central', counter=0, last_request=timezone.now(),
+                     reset_delta=0)
+
+
+class JobPrioritiesTests(TestCase):
+    def test_create_instance(self):
+        JobPriorities(testtype='web-platform-tests-1', buildtype='opt', platform='windows8-64',
+                    priority=1, timeout=5400, expires=timezone.now(), buildsystem='taskcluster')
+
+    def test_expired_job_priority(self):
+        yesterday = timezone.now() - datetime.timedelta(days=1)
+        jb = JobPriorities(testtype='web-platform-tests-1', buildtype='opt', platform='windows8-64',
+                         priority=1, timeout=5400, expires=yesterday, buildsystem='taskcluster')
+        self.assertIs(jb.has_expired(), True)
+
+    def test_not_expired_job_priority(self):
+        tomorrow = timezone.now() + datetime.timedelta(days=1)
+        jb = JobPriorities(testtype='web-platform-tests-1', buildtype='opt', platform='windows8-64',
+                         priority=1, timeout=5400, expires=tomorrow, buildsystem='taskcluster')
+        self.assertIs(jb.has_expired(), False)
