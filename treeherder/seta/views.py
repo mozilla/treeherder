f74a6cca66f7daa87c2f023e02e01eb45eb7df82
Armen Zambrano G <armenzg@mozilla.com>
2016-10-13 14:17:33 -0400

    Fix flake8 issue

--- treeherder/seta/views.py
+++ treeherder/seta/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 
+
 def index(request):
     return HttpResponse("Hello world! Welcome to SETA!")
+
 
 def setadetails(request):
     return HttpResponse('Hello world!')
