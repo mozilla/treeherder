980343742ce710179564feddfd1f949885536e5e
Ed Morley <emorley@mozilla.com>
2016-10-14 00:15:45 +0100

    Bug 1310053 - Don't retry tasks on ValueError
    Since retries are not going to succeed either.

--- treeherder/workers/task.py
+++ treeherder/workers/task.py
@@ -14,6 +14,7 @@ class retryable_task(object):
 
     NON_RETRYABLE_EXCEPTIONS = (
         TypeError,
+        ValueError,
         IntegrityError,
         ProgrammingError,
         UnicodeDecodeError,
