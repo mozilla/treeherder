d643df25946fdbc2e47f434f85c3483f93b79df8
Armen Zambrano G <armenzg@mozilla.com>
2016-10-13 15:00:49 -0400

    Switch SETA tests to use py.test style + isort errors

--- tests/seta/tests.py
+++ tests/seta/tests.py
@@ -1,18 +1,18 @@
 import datetime
 
 from django.utils import timezone
-from django.test import TestCase
 
-from .models import TaskRequest, JobPriority
+from .models import (JobPriority,
+                     TaskRequest)
 
 
-class TaskRequestTests(TestCase):
+class TaskRequestTests():
     def test_create_instance(self):
-        TaskRequest(repo_name='mozilla-central', counter=0, last_request=timezone.now(),
-                     reset_delta=0)
+        TaskRequest(repo_name='mozilla-central', counter=0,
+                    last_request=timezone.now(), reset_delta=0)
 
 
-class JobPriorityTests(TestCase):
+class JobPriorityTests():
     def test_create_instance(self):
         JobPriority(testtype='web-platform-tests-1', buildtype='opt', platform='windows8-64',
                     priority=1, timeout=5400, expires=timezone.now(), buildsystem='taskcluster')
@@ -21,10 +21,10 @@ class JobPriorityTests(TestCase):
         yesterday = timezone.now() - datetime.timedelta(days=1)
         jb = JobPriority(testtype='web-platform-tests-1', buildtype='opt', platform='windows8-64',
                          priority=1, timeout=5400, expires=yesterday, buildsystem='taskcluster')
-        self.assertIs(jb.has_expired(), True)
+        assert jb.has_expired() is True
 
     def test_not_expired_job_priority(self):
         tomorrow = timezone.now() + datetime.timedelta(days=1)
         jb = JobPriority(testtype='web-platform-tests-1', buildtype='opt', platform='windows8-64',
                          priority=1, timeout=5400, expires=tomorrow, buildsystem='taskcluster')
-        self.assertIs(jb.has_expired(), False)
+        assert jb.has_expired() is False
