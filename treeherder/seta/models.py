a9dc05e00e9b1b70af3ef3331de162e48af0edc6
Armen Zambrano G <armenzg@mozilla.com>
2016-10-13 08:00:55 -0400

    Feedback from wlach

--- treeherder/seta/models.py
+++ treeherder/seta/models.py
@@ -1,28 +1,30 @@
+from treeherder.model.models import Repository
+
 from django.db import models
 from django.utils import timezone
 
 
-class TaskRequests(models.Model):
+class TaskRequest(models.Model):
     ''' Track TaskCluster requests for SETA information.
 
     The Gecko decision task executes on every push and it queries SETA for information.
     This information is used the graph generation code to determine which jobs
     to exclude on that run or not.
     '''
-    repo_name = models.CharField(max_length=128, primary_key=True)  # e.g. mozilla-inbound
+    repository = models.ForeignKey(Repository)
     counter = models.IntegerField()  # Number of times TC has reached the API
     last_request = models.DateTimeField()  # Last time TC reached the API
     reset_delta = models.IntegerField()  # Maximum number of pushes before SETA is skipped
 
     def __str__(self):
         return '%s/%s/%s'.format(
-            self.repo_name,
+            self.repository,
             self.counter,
             self.reset_delta
         )
 
 
-class JobPriorities(models.Model):
+class JobPriority(models.Model):
     # e.g. web-platform-tests-1; {opt,pgo,debug}; windows8-64; 5; 5400; {buildbot,taskcluster,*}
     testtype = models.CharField(max_length=128)
     buildtype = models.CharField(max_length=64)
