76769964bd9d8ea7a7f4ff5b69a6296c083c1d82
Armen Zambrano G <armenzg@mozilla.com>
2016-10-26 14:34:16 -0400

    Register high value jobs end point

--- treeherder/webapp/api/urls.py
+++ treeherder/webapp/api/urls.py
@@ -17,6 +17,7 @@ from treeherder.webapp.api import (artifact,
                                    refdata,
                                    resultset,
                                    runnable_jobs,
+                                   seta,
                                    text_log_summary,
                                    text_log_summary_line)
 
@@ -133,6 +134,9 @@ default_router.register(r'performance/framework',
 default_router.register(r'performance/bug-template',
                         performance_data.PerformanceBugTemplateViewSet,
                         base_name='performance-bug-template')
+default_router.register(r'seta/high-value-jobs',
+                        seta.SetaHighValueJobsViewSet,
+                        base_name='seta-high-value-jobs')
 default_router.register(r'bugzilla', bugzilla.BugzillaViewSet,
                         base_name='bugzilla')
 default_router.register(r'jobdetail', jobs.JobDetailViewSet,
