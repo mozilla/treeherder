df94e618c4f858063bfacc70987fcd02e83794b0
Armen Zambrano G <armenzg@mozilla.com>
2016-11-01 16:25:40 -0400

    Accept build_system_type as a parameter for low value jobs API

--- treeherder/webapp/api/seta.py
+++ treeherder/webapp/api/seta.py
@@ -1,6 +1,7 @@
-from rest_framework import (filters,
-                            serializers,
+from rest_framework import (serializers,
+                            status,
                             viewsets)
+from rest_framework.response import Response
 
 from treeherder.seta.models import JobPriority
 
@@ -8,9 +9,16 @@ from treeherder.seta.models import JobPriority
 class SetaJobPrioritySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = JobPriority
-        fields = ('testtype')
+        fields = ['testtype']
 
+class SetaLowValueJobsViewSet(viewsets.ViewSet):
+    def list(self, request, format=None):
+        build_system_type = request.query_params.get('build_system_type', '*')
 
-class SetaLowValueJobsViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = JobPriority.objects.exclude(buildsystem='buildbot')
-    serializer_class = SetaJobPrioritySerializer
+        if build_system_type not in ('buildbot', 'taskcluster', '*'):
+            error_message = 'Valid build_system_type values are buildbot, taskcluster or *'
+            return Response(error_message, status=status.HTTP_400_BAD_REQUEST)
+
+        ret = [build_system_type]
+
+        return Response(ret)
