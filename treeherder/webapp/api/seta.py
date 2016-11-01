cf9dc9725f537c494811186a087ccaf4f24ca73c
Armen Zambrano G <armenzg@mozilla.com>
2016-11-01 16:33:22 -0400

    For low value jobs endpoint, process taskcluster and buildbot requests separately

--- treeherder/webapp/api/seta.py
+++ treeherder/webapp/api/seta.py
@@ -1,3 +1,5 @@
+import datetime
+
 from rest_framework import (serializers,
                             status,
                             viewsets)
@@ -11,14 +13,33 @@ class SetaJobPrioritySerializer(serializers.HyperlinkedModelSerializer):
         model = JobPriority
         fields = ['testtype']
 
+
 class SetaLowValueJobsViewSet(viewsets.ViewSet):
+    def _taskcluster(self):
+        return Response(['TODO - taskcluster'])
+
+    def _buildbot(self):
+        job_priorities = JobPriority.objects.exclude(buildsystem='buildbot')
+        today = str(datetime.date.today())
+        ret = {
+            'jobtypes': {
+                today: []
+            }
+        }
+        for job in job_priorities:
+            # XXX: This is not really the data Buildbot wants. This is just for now
+            ret['jobtypes'][today].append(job.testtype)
+
+        return Response(ret)
+
     def list(self, request, format=None):
         build_system_type = request.query_params.get('build_system_type', '*')
 
         if build_system_type not in ('buildbot', 'taskcluster', '*'):
-            error_message = 'Valid build_system_type values are buildbot, taskcluster or *'
+            error_message = 'Valid build_system_type values are buildbot or taskcluster.'
             return Response(error_message, status=status.HTTP_400_BAD_REQUEST)
 
-        ret = [build_system_type]
-
-        return Response(ret)
+        if build_system_type == 'buildbot':
+            return self._buildbot()
+        else:
+            return self._taskcluster()
