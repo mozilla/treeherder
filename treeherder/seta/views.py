6b1ea213ac2d7573fd352b8aea82a48a0a8ab792
Armen Zambrano G <armenzg@mozilla.com>
2016-11-02 15:57:14 -0400

    Remove original seta views and urls since we now use webapp api

--- treeherder/seta/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("Hello world! Welcome to SETA!")
-
-
-def setadetails(request):
-    return HttpResponse('Hello world!')
