730c65df6da0599a7d507b7b2c88039a8c964b62
Armen Zambrano G <armenzg@mozilla.com>
2016-11-02 15:54:26 -0400

    Do not use classes + use objects.create() instead of save()

--- tests/seta/tests.py
+++ tests/seta/tests.py
@@ -5,60 +5,63 @@ from django.utils import timezone
 from .models import (JobPriority,
                      TaskRequest)
 
+TOMORROW = timezone.now() + datetime.timedelta(days=1)
+YESTERDAY = timezone.now() + datetime.timedelta(days=1)
 
-class TaskRequestTests():
-    def test_create_instance(self):
-        tr = TaskRequest(repo_name='mozilla-central', counter=0,
-                         last_request=timezone.now(), reset_delta=0)
-        tr.save()
+# TaskRequests test
+def test_create_instance():
+    TaskRequest.objects.create(repo_name='mozilla-central',
+                               counter=0,
+                               last_request=timezone.now(),
+                               reset_delta=0)
 
 
-class JobPriorityTests():
-    def test_create_instance(self):
-        jb = JobPriority(
-            testtype='web-platform-tests-1', buildtype='opt', platform='windows8-64',
-            priority=1, timeout=5400, expiration_date=timezone.now(), buildsystem='taskcluster')
-        jb.save()
+# JobPriority tests
+def test_expired_job_priority():
+    JobPriority.objects.create(testtype='web-platform-tests-1',
+                               buildtype='opt',
+                               platform='windows8-64',
+                               priority=1,
+                               timeout=5400,
+                               expiration_date=YESTERDAY,
+                               buildsystem='taskcluster')
+    assert jb.has_expired() is True
 
-    def test_expired_job_priority(self):
-        yesterday = timezone.now() - datetime.timedelta(days=1)
-        jb = JobPriority(
-            testtype='web-platform-tests-1', buildtype='opt', platform='windows8-64',
-            priority=1, timeout=5400, expiration_date=yesterday, buildsystem='taskcluster')
-        jb.save()
-        assert jb.has_expired() is True
+def test_not_expired_job_priority():
+    jb = JobPriority.objects.create(testtype='web-platform-tests-1',
+                                    buildtype='opt',
+                                    platform='windows8-64',
+                                    priority=1,
+                                    timeout=5400,
+                                    expiration_date=TOMORROW,
+                                    buildsystem='taskcluster')
+    assert jb.has_expired() is False
 
-    def test_not_expired_job_priority(self):
-        tomorrow = timezone.now() + datetime.timedelta(days=1)
-        jb = JobPriority(
-            testtype='web-platform-tests-1', buildtype='opt', platform='windows8-64',
-            priority=1, timeout=5400, expiration_date=tomorrow, buildsystem='taskcluster')
-        jb.save()
-        assert jb.has_expired() is False
+def test_null_testtype():
+    '''The expiration date accepts null values'''
+    # XXX: This should probably raise an exception
+    JobPriority.objects.create(testtype=None,
+                               buildtype='opt',
+                               platform='windows8-64',
+                               priority=1,
+                               timeout=5400,
+                               expiration_date=TOMORROW,
+                               buildsystem='taskcluster')
 
-    def test_null_testtype(self):
-        '''The expiration date accepts null values'''
-        tomorrow = timezone.now() + datetime.timedelta(days=1)
-        jb = JobPriority(
-            testtype=None, buildtype='opt', platform='windows8-64',
-            priority=1, timeout=5400, expiration_date=tomorrow, buildsystem='taskcluster')
-        jb.save()
+def test_null_expiration_date():
+    '''The expiration date accepts null values'''
+    JobPriority.objects.create(testtype='web-platform-tests-2',
+                               buildtype='opt',
+                               platform='windows8-64',
+                               priority=1,
+                               timeout=5400,
+                               expiration_date=None,
+                               buildsystem='taskcluster')
 
-    def test_null_expiration_date(self):
-        '''The expiration date accepts null values'''
-        jb = JobPriority(
-            testtype='web-platform-tests-1', buildtype='opt', platform='windows8-64',
-            priority=1, timeout=5400, expiration_date=None, buildsystem='taskcluster')
-        jb.save()
-
-    def test_prevent_duplicates(self):
-        '''The expiration date accepts null values'''
-        tomorrow = timezone.now() + datetime.timedelta(days=1)
-        jb = JobPriority(
-            testtype='web-platform-tests-1', buildtype='opt', platform='windows8-64',
-            priority=1, timeout=5400, expiration_date=tomorrow, buildsystem='taskcluster')
-        jb.save()
-        jb = JobPriority(
-            testtype='web-platform-tests-1', buildtype='opt', platform='windows8-64',
-            priority=1, timeout=5400, expiration_date=tomorrow, buildsystem='taskcluster')
-        jb.save()
+def test_prevent_duplicates():
+    '''The expiration date accepts null values'''
+    JobPriority.objects.create(testtype='web-platform-tests-1', buildtype='opt', platform='windows8-64',
+                               priority=1, timeout=5400, expiration_date=TOMORROW, buildsystem='taskcluster')
+    # XXX: Should this not raise an exception?
+    JobPriority.objects.create(testtype='web-platform-tests-1', buildtype='opt', platform='windows8-64',
+                               priority=1, timeout=5400, expiration_date=TOMORROW, buildsystem='taskcluster')
