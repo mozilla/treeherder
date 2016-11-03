98148a8d8b9a8a0d4a89e9c8a2b9b2aa753d872e
James Graham <james@hoppipolla.co.uk>
2016-11-03 15:38:29 +0000

    Bug 1321798 - Rewrite the autoclassification UI
    This rewrite is intended to address the numerous problems that prevented
    the old autoclassify UI from recieving wide adoption, and makes the
    following changes:
    
    * Moves the code to use angular 1.5 components, for maintainability
    
    * Reduces the number of HTTP requests to one GET / one POST (associated
      with the TextLogError objects), for performance.
    
    * Adds the concept of a selection so that operations can be performed on
      multiple lines at once
    
    * Reorganises the panel a bit to fit better with the rest of the
      treeherder UI.
    
    * Removes the "Ignore Others" action, which, though occasionally helpful,
      was based on an unreliable heuristic.
    
    * Reduces the number of unhelpful bug suggestions displayed by default
      where there are many, and ensures that the most plausible suggestions
      are shown first.
    
    * Adds keybindings for all operations, so that it is possible to use the
      autoclassify panel without touching the mouse.
    
    * Adds basic heuristics for which option to select by default.

---
+++ ui/js/models/text_log_errors.js
@@ -0,0 +1,48 @@
+'use strict';
+
+treeherder.factory('ThTextLogErrorsModel', [
+    '$http', '$q', 'ThLog', 'thUrl',
+    function($http, $q, ThLog, thUrl) {
+
+        var ThTextLogErrorsModel = function(data) {
+            angular.extend(this, data);
+        };
+
+        ThTextLogErrorsModel.getUrl = function(job_id) {
+            return thUrl.getProjectJobUrl("/text_log_errors/", job_id);
+        };
+
+        ThTextLogErrorsModel.getList = function(job_id, config) {
+            // a static method to retrieve a list of ThTextLogErrorsModel
+            // the timeout configuration parameter is a promise that can be used to abort
+            // the ajax request
+            config = config || {};
+            var timeout = config.timeout || null;
+            return $http.get(ThTextLogErrorsModel.getUrl(job_id), {
+                timeout: timeout,
+                cache: false
+            })
+            .then(function(response) {
+                var item_list = [];
+                angular.forEach(response.data, function(elem){
+                    item_list.push(new ThTextLogErrorsModel(elem));
+                });
+                return item_list;
+            });
+        };
+
+        ThTextLogErrorsModel.verify = function(lineId, bestClassification, bugNumber) {
+            return $http.put(thUrl.getRootUrl("/text-log-error/" + lineId + "/"),
+                             {best_classification: bestClassification,
+                              bug_number: bugNumber});
+        };
+
+        ThTextLogErrorsModel.verifyMany = function(data) {
+            if (!data.length) {
+                return $q.resolve();
+            }
+            return $http.put(thUrl.getRootUrl("/text-log-error/"), data);
+        };
+
+        return ThTextLogErrorsModel;
+    }]);
