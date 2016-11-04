aa991be35bdf7cbe6d36add2622328abfd778e2c
William Lachance <wlachance@mozilla.com>
2016-11-04 15:36:00 -0400

    Bug 1311185 - Handle duplicate revisions in legacy databases

--- treeherder/model/management/commands/migrate_resultset_orm.py
+++ treeherder/model/management/commands/migrate_resultset_orm.py
@@ -55,7 +55,7 @@ class Command(BaseCommand):
         try:
             print rs_id,
             push = Push.objects.get(repository=repository,
-                                    revision__startswith=revision)
+                                    revision__startswith=revision[:12])
             if skip_existing:
                 # already exists and we're skipping existing entries
                 return
