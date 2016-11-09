91e2609ac26e0514dc0ec2f1c8e0afd40984549b
Armen Zambrano G <armenzg@mozilla.com>
2016-11-09 07:28:32 -0500

    Use unique_key from the common module

--- treeherder/seta/common.py
+++ treeherder/seta/common.py
@@ -11,3 +11,8 @@ def get_root_dir():
 
 def get_runnable_jobs_path():
     return os.path.join(get_root_dir(), 'runnable_jobs.json')
+
+
+def unique_key(testtype, buildtype, platform):
+    '''This makes sure that we order consistently this unique identifier'''
+    return (testtype, buildtype, platform)
