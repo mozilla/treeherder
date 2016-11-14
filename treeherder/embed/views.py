dba0eb1ef1b8901d423621fd20a1d947a05d804d
William Lachance <wrlach@gmail.com>
2016-11-14 16:55:18 -0500

    Bug 1311185 - Fix embed widget (#1978)

--- treeherder/embed/views.py
+++ treeherder/embed/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.clickjacking import xframe_options_exempt
 from django.views.generic.base import TemplateView
 
 from treeherder.model.derived import JobsModel
+from treeherder.model.models import Push
 
 
 class ResultsetStatusView(TemplateView):
@@ -12,21 +13,24 @@ class ResultsetStatusView(TemplateView):
 
     def get_context_data(self, **kwargs):
         assert "repository" in kwargs and "revision" in kwargs
+        (repository_name, revision) = (kwargs['repository'], kwargs['revision'])
         context = super(ResultsetStatusView, self).get_context_data(**kwargs)
-        with JobsModel(kwargs['repository']) as jm:
-            resultset_list = jm.get_resultset_all_revision_lookup(
-                [kwargs['revision']])
-            if not resultset_list:
-                raise Http404("No resultset found for revision {0}".format(
-                    kwargs['revision']))
-            result_set_id = resultset_list[kwargs['revision']]['id']
-            resultset_status_dict = jm.get_push_status(result_set_id)
+        pushes = Push.objects.filter(
+            repository__name=repository_name,
+            commits__revision__startswith=revision).values_list(
+                'id', flat=True)
+        if not len(pushes):
+            raise Http404("No resultset found for revision {0}".format(
+                revision))
+        push_id = pushes[0]
+        with JobsModel(repository_name) as jm:
+            resultset_status_dict = jm.get_push_status(push_id)
             update_needed = (('pending' in resultset_status_dict) or
                              ('running' in resultset_status_dict) or
                              not resultset_status_dict)
             context['update_needed'] = update_needed
             context['resultset_status_dict'] = resultset_status_dict
-            return context
+        return context
 
     # TODO: Replace with the class-based method_decorator once using Django 1.9.
     @method_decorator(xframe_options_exempt)
