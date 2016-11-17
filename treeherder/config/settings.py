a07cbff56e1f2d811529d61b8ba5451f0ac88e57
James Graham <james@hoppipolla.co.uk>
2016-11-17 16:55:02 +0000

    Bug 1321802 - Basic autoclassification of precise matches in unstructured data

--- treeherder/config/settings.py
+++ treeherder/config/settings.py
@@ -457,7 +457,9 @@ SILENCED_SYSTEM_CHECKS = [
 # Enable integration between autoclassifier and jobs
 AUTOCLASSIFY_JOBS = env.bool("AUTOCLASSIFY_JOBS", default=True)
 # Ordered list of matcher classes to use during autoclassification
-AUTOCLASSIFY_MATCHERS = ["PreciseTestMatcher", "CrashSignatureMatcher",
+AUTOCLASSIFY_MATCHERS = ["PreciseTestMatcher",
+                         "PreciseUnstructuredMatcher",
+                         "CrashSignatureMatcher",
                          "ElasticSearchTestMatcher"]
 DETECT_INTERMITTENTS = env.bool("DETECT_INTERMITTENTS", default=False)
 
