26363389894186225db12577427b3a590fab8376
Armen Zambrano G <armenzg@mozilla.com>
2016-11-17 10:20:27 -0500

    Make the seta analyze failures check for failures

--- treeherder/seta/tasks.py
+++ treeherder/seta/tasks.py
@@ -1,7 +1,9 @@
 from cerely import task
 
+from treeherder.seta.analyze_failures import AnalyzeFailures
+
 
 @task(name='seta-analyze-failures', time_limit=20*60)
 def seta_analyze_failures():
     '''We analyze the starred test failures of the last 90 days which were fixed by a commit.'''
-    raise Exception('To be implemented')
+    AnalyzeFailures().run()
