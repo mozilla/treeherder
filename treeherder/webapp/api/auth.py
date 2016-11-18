ba8165c23934f65df3bbd809f75af94e77a2a258
camd <cdawson@mozilla.com>
2016-11-18 10:54:48 -0800

    Bug 1273034 - Transition to Taskcluster Auth from Persona (#1922)
    In this commit, Sheriff access is still maintained in the
    Treeherder DB, rather than using the scopes derived from
    LDAP.
    
    For local usage with Vagrant, this requires accessing
    Treeherder with localhost instead of
    local.treeherder.mozilla.org
    
    Loggin in to the Django Admin is not enabled in this
    branch.  Do use the admin, you must first login through
    the normal Treeherder front-end.  Then the admin will
    be accessible if the user has the privileges to do so.
    
    Persona login will still be technically possible through the
    login.taskcluster.net site.  But that choice will go away
    shortly.

--- treeherder/webapp/api/auth.py
+++ treeherder/webapp/api/auth.py
@@ -1,7 +1,22 @@
-import newrelic.agent
-from rest_framework.exceptions import AuthenticationFailed
+import logging
 
+import newrelic.agent
+from django.conf import settings
+from django.contrib.auth import (authenticate,
+                                 login,
+                                 logout)
+from rest_framework import viewsets
+from rest_framework.decorators import list_route
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.response import Response
+from taskcluster.exceptions import (TaskclusterConnectionError,
+                                    TaskclusterRestFailure)
+
+from treeherder.auth.backends import TaskclusterAuthenticationFailed
 from treeherder.credentials.models import Credentials
+from treeherder.webapp.api.serializers import UserSerializer
+
+logger = logging.getLogger(__name__)
 
 
 def hawk_lookup(id):
@@ -16,3 +31,62 @@ def hawk_lookup(id):
         'key': str(credentials.secret),
         'algorithm': 'sha256'
     }
+
+
+def _get_port(request):
+    """
+    Return the port number for the request as a string.
+
+    Code taken from Django 1.10.  When we switch to that, remove
+    this function
+    """
+    if settings.USE_X_FORWARDED_PORT and 'HTTP_X_FORWARDED_PORT' in request.META:
+        port = request.META['HTTP_X_FORWARDED_PORT']
+    else:
+        port = request.META['SERVER_PORT']
+    return str(port)
+
+
+class TaskclusterAuthViewSet(viewsets.ViewSet):
+
+    @list_route()
+    def login(self, request):
+        """
+        Verify credentials with Taskcluster
+        """
+        auth_header = request.META.get("HTTP_TCAUTH", None)
+        host = request.get_host().split(":")[0]
+        # when we switch to Django 1.10, use ``request.get_port``
+        port = _get_port(request)
+
+        try:
+            user = authenticate(auth_header=auth_header,
+                                host=host,
+                                port=int(port))
+            if not user:
+                raise AuthenticationFailed("User not authenticated.")
+
+            if not user.is_active:
+                raise AuthenticationFailed("This user has been disabled.")
+
+            login(request, user)
+
+            return Response(UserSerializer(user).data)
+        except TaskclusterAuthenticationFailed as ex:
+            # This is an error where the user wasn't able to log in
+            # for some reason.
+            logger.warning("Error authenticating with Taskcluster", exc_info=ex)
+            raise AuthenticationFailed(ex.message)
+        except (TaskclusterConnectionError,
+                TaskclusterRestFailure) as ex:
+            # This indicates an error that may require attention by the
+            # Treeherder or Taskcluster teams.  Logging this to New Relic to
+            # increase visibility.
+            newrelic.agent.record_exception()
+            logger.exception("Error communicating with Taskcluster", exc_info=ex)
+            raise AuthenticationFailed(ex.message)
+
+    @list_route()
+    def logout(self, request):
+        logout(request)
+        return Response("User logged out")
