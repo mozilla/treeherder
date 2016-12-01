c19d08ec0816f784a470ec2c236e45cf1f58e796
Cameron Dawson <cdawson@mozilla.com>
2016-12-01 13:39:18 -0800

    review fixes

--- treeherder/auth/backends.py
+++ treeherder/auth/backends.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.reverse import reverse
 from taskcluster.sync import Auth
+from taskcluster.utils import scope_match
 
 try:
     from django.utils.encoding import smart_bytes
@@ -37,58 +38,43 @@ class TaskclusterAuthBackend(object):
          'expires': '2016-10-31T17:40:45.692Z'}
     """
 
-    def _get_scope_value(self, scopes, scope_prefix):
-        for scope in scopes:
-            if scope.startswith(scope_prefix):
-                return scope[len(scope_prefix):]
-        return None
-
-    def _get_email(self, result):
+    def _get_email(self, client_id):
         """
         Get the user's email from the mozilla-user scope
 
         For more info on scopes:
         https://docs.taskcluster.net/manual/3rdparty#authenticating-with-scopes
         """
-        # Try finding the email in the mozilla-user scope
-        email = self._get_scope_value(result["scopes"], "assume:mozilla-user:")
 
-        if email and re.search(r'.+@.+', email):
-            return email
-        else:
-            # Try finding the email in the clientId.
-            match = re.search(
-                r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)",
-                result["clientId"])
-            if match:
-                return match.group(0)
-
-        # If we get here, we couldn't find a valid email.  So deny login.
-        raise TaskclusterAuthenticationFailed(
-            "Unable to determine email for clientId: '{}'. Scope 'assume:mozilla-user': '{}'".format(
-                result["clientId"], email))
+        # Try finding the email in the clientId.
+        # Credit for regex to http://emailregex.com/ Python section
+        match = re.search(
+            r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)",
+            client_id)
+        if match:
+            return match.group(0)
+        return ""
 
     def _get_user(self, email):
         """
-        Try to find an exising user that matches the email.
+        Try to find an existing user that matches the email.
 
         TODO: Switch to using ``username`` instead of email once we are on
         Django 1.10 in Bug 1311967.  will need to migrate existing hashed
-        usenames at that point.
+        usernames at that point.
 
         """
 
         # Since there is a unique index on username, but not on email,
         # it is POSSIBLE there could be two users with the same email and
         # different usernames.  Not very likely, but this is safer.
-        user = User.objects.filter(email=email)
-
+        users = User.objects.filter(email=email)
         # if we didn't find any, then raise an exception so we create a new
         # user
-        if not user:
+        if not users:
             raise ObjectDoesNotExist
 
-        return user.first()
+        return users.first()
 
     def authenticate(self, auth_header=None, host=None, port=None):
         if not auth_header:
@@ -109,32 +95,40 @@ class TaskclusterAuthBackend(object):
 
         if result["status"] != "auth-success":
             logger.warning("Error logging in: {}".format(result["message"]))
-            raise TaskclusterAuthenticationFailed(result["message"])
+            raise TaskclusterAuthenticationFailedException(result["message"])
 
+        client_id = result["clientId"]
+        email = self._get_email(client_id)
         # TODO: remove this size limit when we upgrade to django 1.10
         # in Bug 1311967
-        email = self._get_email(result)
-        if len(email) <= 30:
-            username = email
+        if len(client_id) <= 30:
+            username = client_id
         else:
             username = base64.urlsafe_b64encode(
-                hashlib.sha1(smart_bytes(email)).digest()
+                hashlib.sha1(smart_bytes(client_id)).digest()
                 ).rstrip(b'=')[-30:]
 
         try:
-            # Find the user by their email.
-            user = self._get_user(email)
-            user.username = username
+            if scope_match(result["scopes"],
+                           [["assume:mozilla-user:{}".format(email)]]):
+                # Find the user by their email.
+                user = self._get_user(email)
+                # update the username
+                user.username = username
+                user.save()
+                return user
+
+            return User.objects.get(username=username)
 
         except ObjectDoesNotExist:
-            # the user doesn't already exist, create it.
-            logger.warning("Creating new user: {}".format(username))
-            user = User(email=email,
-                        username=username,
-                        )
-
-        user.save()
-        return user
+            if email:
+                # the user doesn't already exist, create it.
+                logger.warning("Creating new user: {}".format(username))
+                user = User.objects.create_user(email=email,
+                                                username=username)
+                user.save()
+                return user
+            raise NoEmailException("No email in clientId.  Email required.")
 
     def get_user(self, user_id):
         try:
@@ -143,5 +137,9 @@ class TaskclusterAuthBackend(object):
             return None
 
 
-class TaskclusterAuthenticationFailed(Exception):
+class TaskclusterAuthenticationFailedException(Exception):
+    pass
+
+
+class NoEmailException(Exception):
     pass
