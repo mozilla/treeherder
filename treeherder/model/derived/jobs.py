9139940c8ab4f0de30b7877ee3051409f0e39d10
William Lachance <wlachance@mozilla.com>
2016-12-09 17:44:21 -1000

    final fixup

--- treeherder/model/derived/jobs.py
+++ treeherder/model/derived/jobs.py
@@ -276,7 +276,7 @@ class JobsModel(TreeherderModelBase):
         Job.objects.filter(push_id=push_id, state='pending').update(
             state='completed',
             result='usercancel',
-            last_updated=datetime.datetime.now())
+            last_modified=datetime.now())
 
     def trigger_missing_jobs(self, requester, push_id):
         publish_resultset_action.apply_async(
