bfa3def90f616bfc2c4f56005b19359736996a03
William Lachance <wrlach@gmail.com>
2016-12-29 15:14:48 -0500

    Bug 1315808 -  New log viewer (#2050)
    * Written in react.js for speed
    * Does not require custom treeherder backend API's
    * Improved scrolling
    * Fixes issues in URL when linking directly to line numbers

--- ui/js/directives/treeherder/log_viewer_steps.js
+++ ui/js/directives/treeherder/log_viewer_steps.js
@@ -1,9 +1,9 @@
 'use strict';
 
-treeherder.directive('lvLogSteps', ['$timeout', '$q', function ($timeout, $q) {
+treeherder.directive('lvLogSteps', ['$timeout', $timeout => {
     function getOffsetOfStep (order) {
-        var el = $('.lv-step[order="' + order + '"]');
-        var parentOffset = el.parent().offset();
+        const el = $('.lv-step[order="' + order + '"]');
+        const parentOffset = el.parent().offset();
 
         return el.offset().top -
                parentOffset.top + el.parent().scrollTop() -
@@ -15,32 +15,12 @@ treeherder.directive('lvLogSteps', ['$timeout', '$q', function ($timeout, $q) {
     return {
         restrict: 'A',
         templateUrl: 'partials/logviewer/lvLogSteps.html',
-        link: function (scope) {
-            scope.scrollTo = function($event, step, linenumber) {
-                scope.currentLineNumber = linenumber;
-
-                scope.loadMore({}).then(function () {
-                    $timeout(function () {
-                        var raw = $('.lv-log-container')[0];
-                        var line = $('.lv-log-line[line="' + linenumber + '"]');
-                        raw.scrollTop += line.offset().top - $('.run-data').outerHeight() -
-                                         $('.navbar').outerHeight() - 9;
-                    });
-                }, function () {
-                    // there is an error so bomb out
-                    return $q.reject();
-                });
-
-                if (scope.displayedStep && scope.displayedStep.order === step.order) {
-                    $event.stopPropagation();
-                }
-            };
-
-            scope.toggleSuccessfulSteps = function () {
+        link: (scope) => {
+            scope.toggleSuccessfulSteps = () => {
                 scope.showSuccessful = !scope.showSuccessful;
 
-                var firstError = scope.steps.filter(function (step) {
-                    return step.result && step.result !== "success";
+                const firstError = scope.steps.filter(step => {
+                    return step.result && step.result !== 'success';
                 })[0];
 
                 if (!firstError) {
@@ -48,26 +28,10 @@ treeherder.directive('lvLogSteps', ['$timeout', '$q', function ($timeout, $q) {
                 }
 
                 // scroll to the first error
-                $timeout(function () {
-                    var scrollTop = getOffsetOfStep(firstError.order);
-                    $('.steps-data').scrollTop( scrollTop );
-                });
-            };
+                $timeout(() => {
+                    const scrollTop = getOffsetOfStep(firstError.order);
 
-            scope.displayLog = function (step, state) {
-                scope.displayedStep = step;
-                scope.currentLineNumber = step.started_line_number;
-                scope.selectedBegin = step.started_line_number;
-                scope.selectedEnd = step.finished_line_number;
-                scope.loadMore({}).then(function () {
-                    $timeout(function () {
-                        var raw = $('.lv-log-container')[0];
-                        var line = $('.lv-log-line[line="' + step.started_line_number + '"]');
-                        if (state !== 'initialLoad') {
-                            raw.scrollTop += line.offset().top - $('.run-data').outerHeight() -
-                                             $('.navbar').outerHeight() - 9;
-                        }
-                    });
+                    $('.steps-data').scrollTop(scrollTop);
                 });
             };
         }
