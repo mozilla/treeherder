f397b9819ba0499ece4dc84c177dca5907155e5c
Ed Morley <emorley@mozilla.com>
2017-01-04 23:54:25 +0000

    Bug 1326207 - Replace Django system check hack with --fail-level
    Django 1.10 added a new `--fail-level` option that allows overriding the
    default of only exiting 1 for ERROR and above, avoiding the need to rely
    on awk hacks:
    https://docs.djangoproject.com/en/1.10/ref/django-admin/#check

--- runtests.sh
+++ runtests.sh
@@ -12,11 +12,7 @@ isort --check-only --diff --quiet \
 
 echo "Running Django system checks"
 # See .travis.yml for explanation of the environment variable overriding.
-# Replace awk with `--fail-level WARNING` once we're using Django 1.10, since in
-# previous versions an exit code of 1 is hard-coded to only ERROR and above:
-# https://github.com/django/django/commit/287532588941d2941e19c4cd069bcbd8af889203
-SITE_URL="https://treeherder.dev" TREEHERDER_DEBUG="False" ./manage.py check --deploy 2>&1 \
- | awk '/^WARNINGS/{err=1} {print} END{exit err}' || { exit 1; }
+SITE_URL="https://treeherder.dev" TREEHERDER_DEBUG="False" ./manage.py check --deploy --fail-level WARNING || { exit 1; }
 
 echo "Running Python tests"
 py.test tests/
