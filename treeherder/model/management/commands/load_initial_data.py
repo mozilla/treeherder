1f31ad188d238861f4c78ef827b47a0b98c46e13
Armen Zambrano <armenzg@mozilla.com>
2017-01-05 16:50:00 -0500

    We should populate runnable jobs as soon as Treeherder starts
    This ensures that tools that rely on runnable jobs having data can work
    as expected from the beginning.

--- treeherder/model/management/commands/load_initial_data.py
+++ treeherder/model/management/commands/load_initial_data.py
@@ -12,4 +12,5 @@ class Command(BaseCommand):
                      'failure_classification',
                      'performance_framework',
                      'performance_bug_templates')
+        call_command('update_runnable_jobs')
         call_command('load_preseed')
