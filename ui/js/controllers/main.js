8639505692b017cc98f2cb83ec4b4ee1f2caf1c3
James Graham <james@hoppipolla.co.uk>
2017-01-11 10:15:07 +0000

    Restore u and i shortcuts; use - for 'ignore line'

--- ui/js/controllers/main.js
+++ ui/js/controllers/main.js
@@ -313,7 +313,7 @@ treeherderApp.controller('MainCtrl', [
 
         var keyShortcuts = [
             // Shortcut: toggle display in-progress jobs (pending/running)
-            ['h i', function() {
+            ['i', function() {
                 $scope.$evalAsync($scope.toggleInProgress());
             }],
 
@@ -379,7 +379,7 @@ treeherderApp.controller('MainCtrl', [
             }],
 
             // Shortcut: display only unclassified failures
-            ['h u', function() {
+            ['u', function() {
                 $scope.$evalAsync($scope.toggleUnclassifiedFailures);
             }],
 
@@ -485,7 +485,7 @@ treeherderApp.controller('MainCtrl', [
             }],
 
             // Shortcut: ignore selected in the autoclasify panel
-            ['i', function() {
+            ['-', function() {
                 if (thTabs.selectedTab === "autoClassification") {
                     $scope.$evalAsync($rootScope.$emit(thEvents.autoclassifyIgnore));
                 }
