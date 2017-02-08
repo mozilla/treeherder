e438c9fc45a177cbfca887ef3bdf4c0c8bbfd505
Brian Stack <bstack@mozilla.com>
2017-02-08 16:59:52 -0500

    Bug 1289824 - Add add-all-talos support directly to tc (#2092)

--- ui/js/services/taskcluster.js
+++ ui/js/services/taskcluster.js
@@ -8,6 +8,26 @@ treeherder.factory('thTaskcluster', ['$window', '$rootScope', 'localStorageServi
                 credentials: localStorageService.get('taskcluster.credentials') || {},
             });
         });
-        return client;
+
+        return {
+            client: function() { return client; },
+            refreshTimestamps: function(task) {
+                // Take a taskcluster task and make all of the timestamps
+                // new again. This is pretty much lifted verbatim from
+                // mozilla_ci_tools which is used by pulse_actions.
+                // We need to do this because action tasks are created with
+                // timestamps for expires/created/deadline that are based
+                // on the time of the original decision task creation. We must
+                // update to the current time, or Taskcluster will reject the
+                // task upon creation.
+                task.expires = client.fromNow('366 days');
+                task.created = client.fromNow(0);
+                task.deadline = client.fromNow('1 day');
+                _.map(task.payload.artifacts, function(artifact) {
+                    artifact.expires = client.fromNow('365 days');
+                });
+                return task;
+            },
+        };
     }
 ]);
