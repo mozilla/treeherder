f7c1cf4d5069de528282c1a08caf9d5242d1e74e
jgraham <james@hoppipolla.co.uk>
2017-02-13 16:33:31 +0000

    Cleanup bugfiler.js (#2151)
    Changes split out from the autoclassification UI rewrite branch, and
    are just minor cleanup of the existing code.

--- ui/js/controllers/bugfiler.js
+++ ui/js/controllers/bugfiler.js
@@ -9,6 +9,8 @@ treeherder.controller('BugFilerCtrl', [
         thPinboard, thEvents, fullLog, parsedLog, reftest, selectedJob, allFailures,
         thNotify) {
 
+        var bzBaseUrl = "https://bugzilla.mozilla.org/";
+
         $scope.omittedLeads = ["TEST-UNEXPECTED-FAIL", "PROCESS-CRASH", "TEST-UNEXPECTED-ERROR"];
 
         /**
@@ -113,7 +115,7 @@ treeherder.controller('BugFilerCtrl', [
             var productSearch = $scope.productSearch;
 
             if (productSearch) {
-                $http.get("https://bugzilla.mozilla.org/rest/prod_comp_search/" + productSearch + "?limit=5").then(function(request) {
+                $http.get(bzBaseUrl + "rest/prod_comp_search/" + productSearch + "?limit=5").then(function(request) {
                     var data = request.data;
                     // We can't file unless product and component are provided, this api can return just product. Cut those out.
                     for (var i = data.products.length - 1; i >= 0; i--) {
@@ -194,48 +196,50 @@ treeherder.controller('BugFilerCtrl', [
 
             // Fetch product information from bugzilla to get version numbers, then submit the new bug
             // Only request the versions because some products take quite a long time to fetch the full object
-            $.ajax("https://bugzilla.mozilla.org/rest/product/" + productString + "?include_fields=versions").done(function(productJSON) {
-                var productObject = productJSON.products[0];
+            $http.get(bzBaseUrl + "rest/product/" + productString + "?include_fields=versions")
+                .then(function(response) {
+                    var productJSON = response.data;
+                    var productObject = productJSON.products[0];
 
-                // Find the newest version for the product that is_active
-                var version = _.findLast(productObject.versions, function(version) {
-                    return version.is_active === true;
-                });
+                    // Find the newest version for the product that is_active
+                    var version = _.findLast(productObject.versions, function(version) {
+                        return version.is_active === true;
+                    });
 
-                $http({
-                    url: "api/bugzilla/create_bug/",
-                    method: "POST",
-                    data: {
-                        "product": productString,
-                        "component": componentString,
-                        "summary": summarystring,
-                        "keywords": keywords,
-                        "version": version.name,
-                        "blocks": blocks,
-                        "depends_on": dependsOn,
-                        "see_also": seeAlso,
-                        "comment": descriptionStrings,
-                        "comment_tags": "treeherder"
-                    }
-                }).then(function successCallback(json) {
-                    if (json.data.failure) {
-                        var errorString = "";
-                        for (var i = 0; i < json.data.failure.length; i++) {
-                            errorString += json.data.failure[i];
+                    return $http({
+                        url: "api/bugzilla/create_bug/",
+                        method: "POST",
+                        data: {
+                            "product": productString,
+                            "component": componentString,
+                            "summary": summarystring,
+                            "keywords": keywords,
+                            "version": version.name,
+                            "blocks": blocks,
+                            "depends_on": dependsOn,
+                            "see_also": seeAlso,
+                            "comment": descriptionStrings,
+                            "comment_tags": "treeherder"
                         }
-                        errorString = JSON.parse(errorString);
-                        thNotify.send("Bugzilla error: " + errorString.message, "danger", true);
+                    });
+                })
+                .then((response) => {
+                    var data = response.data;
+                    if (data.failure) {
+                        var error = JSON.parse(data.failure.join(""));
+                        thNotify.send("Bugzilla error: " + error.message, "danger", true);
                         $scope.toggleForm(false);
                     } else {
                         // Auto-classify this failure now that the bug has been filed and we have a bug number
-                        thPinboard.addBug({id:json.data.success});
+                        thPinboard.addBug({id: data.success});
                         $rootScope.$evalAsync($rootScope.$emit(thEvents.saveClassification));
 
                         // Open the newly filed bug in a new tab or window for further editing
-                        window.open("https://bugzilla.mozilla.org/show_bug.cgi?id=" + json.data.success);
+                        window.open(bzBaseUrl + "show_bug.cgi?id=" + data.success);
                         $scope.cancelFiler();
                     }
-                }, function errorCallback(response) {
+                })
+                .catch((response) => {
                     var failureString = "Bug Filer API returned status " + response.status + " (" + response.statusText + ")";
                     if (response.data && response.data.failure) {
                         failureString += "\n\n" + response.data.failure;
@@ -246,7 +250,6 @@ treeherder.controller('BugFilerCtrl', [
                     thNotify.send(failureString, "danger");
                     $scope.toggleForm(false);
                 });
-            });
         };
 
         /*
