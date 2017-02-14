5d1f732cbadf0d4b7563eb13a3f34fdab86186ba
William Lachance <wlachance@mozilla.com>
2017-02-14 14:18:37 -0500

    Bug 1339547 - Basic set of selenium tests for treeherder

---
+++ tests/selenium/test_basics.py
@@ -0,0 +1,41 @@
+import pytest
+from django.core.management import call_command
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+@pytest.fixture
+def initial_data(transactional_db):
+    call_command('load_initial_data')
+
+
+@pytest.mark.selenium
+def test_treeherder_main(initial_data, live_server, selenium):
+    '''
+    Tests that the default treeherder page loads ok and we can
+    click the repository menu
+    '''
+    # selenium = webdriver.Firefox()
+    # driver_.server_url = live_server.url
+    # selenium = webdriver()
+    selenium.maximize_window()
+    selenium.get(live_server.url)
+    repo_button = WebDriverWait(selenium, 10).until(
+        EC.presence_of_element_located((By.ID, 'repoLabel'))
+    )
+    repo_button.click()
+    assert selenium.find_element_by_id('repo-dropdown').is_displayed()
+
+
+@pytest.mark.selenium
+def test_perfherder_main(initial_data, live_server, selenium):
+    '''
+    This tests that the basic graphs view load and we can click the add tests button
+    '''
+    selenium.get(live_server.url + '/perf.html')
+    add_test_button = WebDriverWait(selenium, 10).until(
+        EC.presence_of_element_located((By.ID, 'add-test-data-button'))
+    )
+    add_test_button.click()
+    assert selenium.find_elements_by_class_name('modal-header')
