42ee3c1ce8d185b1830d033152bacb9c5337a2e3
Rob Wood <rwood@mozilla.com>
2017-02-14 09:44:20 -0500

    Bug 1330354 - Use select_related in get_failures_fixed_by_commit() (#2162)
    To prevent thousands of MySQL requests that cause both the API endpoint
    and seta-analyse-failures task to time out.

--- tests/seta/test_analyze_failures.py
+++ tests/seta/test_analyze_failures.py
@@ -5,4 +5,10 @@ from treeherder.seta.analyze_failures import get_failures_fixed_by_commit
 
 @pytest.mark.django_db()
 def test_analyze_failures(fifteen_jobs_with_notes, failures_fixed_by_commit):
-    assert get_failures_fixed_by_commit() == failures_fixed_by_commit
+    ret = get_failures_fixed_by_commit()
+    exp = failures_fixed_by_commit
+
+    assert sorted(ret.keys()) == sorted(exp.keys())
+
+    for key in exp:
+        assert sorted(ret[key]) == sorted(exp[key])
