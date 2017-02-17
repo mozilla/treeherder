d683e0389bec13570719ac43aad23562e8d3efe7
Ed Morley <emorley@mozilla.com>
2017-02-17 16:07:56 +0000

    [WIP] Update from Elasticsearch 2.x to 5.x
    * ES 5.x now requires JDK 8, and Ubuntu 14.04 only ships with openjdk 7,
      so a third party PPA must be used in Vagrant:
      https://launchpad.net/~openjdk-r/+archive/ubuntu/ppa
    * ES 5.x changed the way it manages the heap size, such that:
      - The variables for controlling it have changed (now set via eg
        `ES_JAVA_OPTS="-Xms1g -Xmx1g"` or in the jvm.options file). See:
        https://www.elastic.co/guide/en/elasticsearch/reference/5.2/heap-size.html
      - The default heap size has increased from ~(min:256MB, max:1GB) to
        (min: 2GB, max: 2GB) which causes OOM in the VM, unless either
        lowered back down or the VM RAM increased.
    * The Python ES clients must be updated to the latest releases:
      https://elasticsearch-py.readthedocs.io/en/master/#compatibility
    * Even after that, there are test failures.

--- puppet/manifests/classes/elasticsearch.pp
+++ puppet/manifests/classes/elasticsearch.pp
@@ -1,8 +1,9 @@
 class elasticsearch {
-  $es_version = "2.3.5"
-  $es_url = "https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/${es_version}/elasticsearch-${es_version}.deb"
+  $es_version = "5.2.1"
+  $es_url = "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${es_version}.deb"
+  $es_config_file = "/etc/default/elasticsearch"
 
-  package { 'openjdk-7-jre-headless':
+  package { 'openjdk-8-jre-headless':
     ensure => installed
   }
 
@@ -10,15 +11,23 @@ class elasticsearch {
   exec { "install-elasticsearch":
     user => "${APP_USER}",
     command => "curl -sSo ${HOME_DIR}/elasticsearch.deb ${es_url} && sudo dpkg -i ${HOME_DIR}/elasticsearch.deb",
+    notify => Service['elasticsearch'],
     unless => "test \"$(dpkg-query --show --showformat='\${Version}' elasticsearch 2>&1)\" = '${es_version}'",
+  }
+
+  exec { "set-elasticsearch-heap-size":
+    require => Exec['install-elasticsearch'],
+    command => "echo 'ES_JAVA_OPTS=\"-Xms256m -Xmx1g\"' >> ${es_config_file}",
+    unless => "grep 'ES_JAVA_OPTS=\"-Xms256m -Xmx1g\"' ${es_config_file}",
+    notify => Service['elasticsearch'],
   }
 
   service { 'elasticsearch':
     ensure => running,
     enable => true,
     require => [
-      Package['openjdk-7-jre-headless'],
-      Exec['install-elasticsearch'],
+      Package['openjdk-8-jre-headless'],
+      Exec['set-elasticsearch-heap-size'],
     ],
   }
 }
--- puppet/manifests/classes/init.pp
+++ puppet/manifests/classes/init.pp
@@ -7,10 +7,18 @@ class init {
         creates => "/etc/apt/sources.list.d/fkrull-deadsnakes-python2_7-trusty.list",
     }
 
+    # Ubuntu 14.04 newest openjdk is openjdk-7, so we have to use a third party PPA:
+    # https://launchpad.net/~openjdk-r/+archive/ubuntu/ppa
+    exec { "add_openjdk_ppa":
+        command => "sudo add-apt-repository ppa:openjdk-r/ppa",
+        creates => "/etc/apt/sources.list.d/openjdk-r-ppa-trusty.list",
+    }
+
     exec { "update_apt":
         command => "sudo apt-get update",
         require => [
             Exec["add_python27_ppa"],
+            Exec["add_openjdk_ppa"],
         ]
     }
 }
