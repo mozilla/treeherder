075accf70dd48e41bd89b00983727e3f9518df7b
Robert Wood <rwood@Roberts-MacBook-Pro-324.local>
2017-02-22 16:02:00 -0500

    Bug 1339836 - SETA: Optimize analyze_failures

--- tests/seta/test_analyze_failures.py
+++ tests/seta/test_analyze_failures.py
@@ -1,10 +1,13 @@
 import pytest
+from mock import patch
 
 from treeherder.seta.analyze_failures import get_failures_fixed_by_commit
+from treeherder.seta.settings import SETA_FIXED_BY_COMMIT_REPOS
 
 
 @pytest.mark.django_db()
-def test_analyze_failures(fifteen_jobs_with_notes, failures_fixed_by_commit):
+def test_analyze_failures(fifteen_jobs_with_notes, failures_fixed_by_commit, test_repository):
+    SETA_FIXED_BY_COMMIT_REPOS.append(test_repository.name)
     ret = get_failures_fixed_by_commit()
     exp = failures_fixed_by_commit
 
