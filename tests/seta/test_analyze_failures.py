544fc47175bb2f5c633e9697cf9ba4a940221a2a
Rob Wood <rwood@mozilla.com>
2017-02-28 12:26:19 -0500

    Bug 1342451 - SETA: Fix test_analyze_failures (#2209)

--- tests/seta/test_analyze_failures.py
+++ tests/seta/test_analyze_failures.py
@@ -3,9 +3,8 @@ import pytest
 from treeherder.seta.analyze_failures import get_failures_fixed_by_commit
 
 
-@pytest.mark.skip("Failing now that taskcluster jobs being ingested: bug 1342451")
 @pytest.mark.django_db()
-def test_analyze_failures(fifteen_jobs_with_notes, failures_fixed_by_commit):
+def test_analyze_failures(fifteen_jobs_with_notes, failures_fixed_by_commit, patched_seta_fixed_by_commit_repos):
     ret = get_failures_fixed_by_commit()
     exp = failures_fixed_by_commit
 
