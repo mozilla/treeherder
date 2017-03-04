c19bfb2da9462353ee6d93f2cfa2696df9146797
Casey Williams <cwillia5@gmail.com>
2017-03-03 16:03:43 -0800

    Bug 1336556 - Cache angular partials with webpack
    Load the contents of the various angular partials into the template
    cache at build time so that they don't need to be built separately.

---
+++ ui/js/cache-templates.js
@@ -0,0 +1,23 @@
+'use strict';
+// This is a run block which, when used on an angular module, loads all
+// of the partials in /ui/partials and /ui/plugins into the Angular
+// template cache. This means that ng-includes and templateUrls will not
+// actually need to request a partial file at any point.
+module.exports = ['$templateCache', ($templateCache) => {
+    const partialsReq = require.context('../partials', true, /\.(tmpl|html)$/);
+    partialsReq.keys().forEach(template => {
+        const keyPath = `partials${template.substring(1)}`;
+        $templateCache.put(
+            keyPath,
+            partialsReq(template)
+        );
+    });
+    const pluginsReq = require.context('../plugins', true, /\.(tmpl|html)$/);
+    pluginsReq.keys().forEach(template => {
+        const keyPath = `plugins${template.substring(1)}`;
+        $templateCache.put(
+            keyPath,
+            pluginsReq(template)
+        );
+    });
+}];
