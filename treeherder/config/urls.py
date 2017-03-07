9930d9e344195c226796ec637131425daff95d1e
Sebastin Santy <sebastinssanty@gmail.com>
2017-03-08 00:40:32 +0530

    Add env option for GRAPHQL

--- treeherder/config/urls.py
+++ treeherder/config/urls.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.conf.urls import (include,
                               url)
 from django.contrib import admin
-from graphene_django.views import GraphQLView
 from rest_framework_swagger.views import get_swagger_view
 
 from treeherder.credentials.urls import urlpatterns as credentials_patterns
@@ -17,12 +16,17 @@ urlpatterns = [
    url(r'^admin/', include(admin.site.urls)),
    url(r'^docs/', get_swagger_view(title='Treeherder API')),
    url(r'^credentials/', include(credentials_patterns)),
-   url(r'^graphql', GraphQLView.as_view(graphiql=True)),
-   url(r'^graphiql', include('django_graphiql.urls')),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [
         url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
+
+if settings.GRAPHQL:
+    from graphene_django.views import GraphQLView
+    urlpatterns += [
+        url(r'^graphql', GraphQLView.as_view(graphiql=True)),
+        url(r'^graphiql', include('django_graphiql.urls')),
     ]
