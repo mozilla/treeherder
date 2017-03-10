c0a81efdba35ee9b9abab71ec6435160962edaa3
Alisha <anejaalisha@yahoo.com>
2017-03-10 22:57:29 +1100

    Bug 1330391 - Added reset seta management command

--- treeherder/model/management/commands/load_initial_data.py
+++ treeherder/model/management/commands/load_initial_data.py
@@ -12,4 +12,4 @@ class Command(BaseCommand):
                      'failure_classification',
                      'performance_framework',
                      'performance_bug_templates')
-        call_command('load_preseed')
+        call_command('reset_seta')
--- treeherder/seta/management/commands/reset_seta.py
+++ treeherder/seta/management/commands/reset_seta.py
@@ -1,13 +1,17 @@
 from django.core.management.base import BaseCommand
 
+from treeherder.seta.analyze_failures import AnalyzeFailures
+from treeherder.seta.models import JobPriority
 from treeherder.seta.preseed import load_preseed
 
 
 class Command(BaseCommand):
-    help = 'Update job priority table with data based on preseed.json'
+    help = 'Erase all JobPriority entries and reset SETA'
 
     def __init__(self, *args, **kwargs):
         super(Command, self).__init__(*args, **kwargs)
 
     def handle(self, *args, **options):
+        JobPriority.objects.all().delete()
+        AnalyzeFailures(**options).run()
         load_preseed()
