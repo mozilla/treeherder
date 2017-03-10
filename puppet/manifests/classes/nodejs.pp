c77f8ff85c07da28ea01f725f6aaf8567db32f1e
Ed Morley <emorley@mozilla.com>
2017-03-10 23:12:43 +0000

    Bug 1343624 - Vagrant: Run yarn install during provision
    This will give a working environment out of the box, which will be
    necessary once the Neutrino work lands in bug 1336556.

--- puppet/manifests/classes/nodejs.pp
+++ puppet/manifests/classes/nodejs.pp
@@ -15,4 +15,13 @@ class nodejs {
     require => Package['nodejs'],
   }
 
+  exec{"yarn-install":
+    cwd => "${PROJ_DIR}",
+    user => "${APP_USER}",
+    # We have to use `--no-bin-links` to work around symlink issues with Windows hosts.
+    # TODO: Switch the flag to a global yarn pref once yarn adds support.
+    command => "yarn install --no-bin-links",
+    require => Package['yarn'],
+  }
+
 }
