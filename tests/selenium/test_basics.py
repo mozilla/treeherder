ae037dadb56ce95a87b498f4c3738644abda8ac0
Wes Kocher <kwierso@gmail.com>
2017-03-17 13:37:15 -0700

    1

--- tests/selenium/test_basics.py
+++ tests/selenium/test_basics.py
@@ -6,7 +6,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-from treeherder.model.models import (FailureClassification,
+from treeherder.model.models import (Commit,
+                                     FailureClassification,
                                      Job,
                                      JobNote,
                                      Push,
