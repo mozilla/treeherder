ae6cc36b6b5b0e7af68d2e9ad054da4b65469c54
Ed Morley <emorley@mozilla.com>
2017-03-21 19:09:29 +0000

    Bug 1348375 - Re-add wildcard imports in treeherder.etl.tasks
    Since it turns out Celery uses them during task auto-discovery.
    
    This partially reverts commit 63372232164bf09ae4378e07e196c407a0b29229.

---
+++ treeherder/etl/tasks/__init__.py
@@ -0,0 +1,5 @@
+# Used by Celery for task auto-discovery.
+from .buildapi_tasks import *  # noqa
+from .pulse_tasks import *  # noqa
+from .classification_mirroring_tasks import *  # noqa
+from .tasks import *  # noqa
