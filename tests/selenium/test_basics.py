6dc39d6bf98f72ea5c19e79c7417e0f7a84a47a7
KWierso <KWierso@users.noreply.github.com>
2017-03-21 18:05:36 -0700

    Bug 1348276 - Fix getting revision comments for title strings (#2264) r=emorley

--- tests/selenium/test_basics.py
+++ tests/selenium/test_basics.py
@@ -1,8 +1,14 @@
+import datetime
+
 import pytest
 from django.core.management import call_command
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+
+from treeherder.model.models import (Commit,
+                                     Push,
+                                     Repository)
 
 pytestmark = pytest.mark.selenium
 
@@ -37,3 +43,25 @@ def test_perfherder_main(initial_data, live_server, selenium):
     WebDriverWait(selenium, 10).until(
         EC.presence_of_element_located((By.ID, 'performance-test-chooser'))
     )
+
+
+def test_treeherder_single_commit_titles(initial_data, live_server, selenium):
+    '''
+    This tests that page titles are correct
+    '''
+    push = Push.objects.create(repository=Repository.objects.get(name='mozilla-central'),
+                               revision="4c45a777949168d16c03a4cba167678b7ab65f76",
+                               author="foo@bar.com",
+                               time=datetime.datetime.now())
+
+    Commit.objects.create(push=push,
+                          revision="4c45a777949168d16c03a4cba167678b7ab65f76",
+                          author="foo@bar.com",
+                          comments="Bug 12345 - This is a message")
+
+    selenium.get(live_server.url + '/#/jobs?repo=mozilla-central&revision=4c45a777949168d16c03a4cba167678b7ab65f76')
+
+    WebDriverWait(selenium, 30).until(
+        EC.presence_of_element_located((By.CLASS_NAME, 'revision-comment'))
+    )
+    assert selenium.title == "[0] mozilla-central: Bug 12345 - This is a message"
