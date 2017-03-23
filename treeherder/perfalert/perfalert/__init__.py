7ef4f32631fc92ce73cfd0c2bef35edb2eca4e34
William Lachance <wlachance@mozilla.com>
2017-03-23 10:12:51 -0400

    Bug 1344358 - Make perf regression detection work better w/ retriggers
    Retriggering performance jobs to generate better performance numbers
    is becoming increasingly common, but our old regression detection
    code didn't really account for that case -- in particular, it
    had a bug that would allow for a regression to be generated *inside*
    the data for a particular revision (if there were enough).
    
    This commit fixes that bug and also changes the behaviour of the
    linear weighting function so that retriggers on a particular revision
    will be weighted the same (rather than declining in importance)

--- treeherder/perfalert/perfalert/__init__.py
+++ treeherder/perfalert/perfalert/__init__.py
@@ -1,4 +1,7 @@
-def analyze(data, weight_fn=None):
+import copy
+
+
+def analyze(revision_data, weight_fn=None):
     """Returns the average and sample variance (s**2) of a list of floats.
 
     `weight_fn` is a function that takes a list index and a window width, and
@@ -9,13 +12,27 @@ def analyze(data, weight_fn=None):
     if weight_fn is None:
         weight_fn = default_weights
 
-    n = len(data)
-    weights = [weight_fn(i, n) for i in range(n)]
-    weighted_sum = sum(data[i] * weights[i] for i in range(n))
-    weighted_avg = weighted_sum / sum(weights) if n > 0 else 0.0
+    # get a weighted average for the full set of data -- this is complicated
+    # by the fact that we might have multiple data points from each revision
+    # which we would want to weight equally -- do this by creating a set of
+    # weights only for each bucket containing (potentially) multiple results
+    # for each value
+    num_revisions = len(revision_data)
+    weights = [weight_fn(i, num_revisions) for i in range(num_revisions)]
+    weighted_sum = 0
+    sum_of_weights = 0
+    for i in range(num_revisions):
+        weighted_sum += sum(value * weights[i] for value in
+                            revision_data[i].values)
+        sum_of_weights += weights[i] * len(revision_data[i].values)
+    weighted_avg = weighted_sum / sum_of_weights if num_revisions > 0 else 0.0
 
-    variance = (sum(pow(d-weighted_avg, 2) for d in data) / (n-1)) if n > 1 else 0.0
-    return {"avg": weighted_avg, "n": n, "variance": variance}
+    # now that we have a weighted average, we can calculate the variance of the
+    # whole series
+    all_data = [v for datum in revision_data for v in datum.values]
+    variance = (sum(pow(d-weighted_avg, 2) for d in all_data) / (len(all_data)-1)) if len(all_data) > 1 else 0.0
+
+    return {"avg": weighted_avg, "n": len(all_data), "variance": variance}
 
 
 def default_weights(i, n):
@@ -36,7 +53,7 @@ def linear_weights(i, n):
 
 
 def calc_t(w1, w2, weight_fn=None):
-    """Perform a Students t-test on the two lists of data.
+    """Perform a Students t-test on the two sets of revision data.
 
     See the analyze() function for a description of the `weight_fn` argument.
     """
@@ -55,38 +72,37 @@ def calc_t(w1, w2, weight_fn=None):
     return delta_s / (((s1['variance'] / s1['n']) + (s2['variance'] / s2['n'])) ** 0.5)
 
 
-class Datum(object):
-    def __init__(self, push_timestamp, value, testrun_timestamp=None,
-                 testrun_id=None, revision_id=None, state='good'):
+class RevisionDatum(object):
+    '''
+    This class represents a specific revision and the set of values for it
+    '''
+    def __init__(self, push_timestamp, push_id, values):
+
         # Date code was pushed
         self.push_timestamp = push_timestamp
-        # Value of this point
-        self.value = value
 
-        # Which test run was this
-        self.testrun_id = testrun_id
-        # What revision this data is for
-        self.revision_id = revision_id
+        # What revision this data is for (usually, but not guaranteed
+        # to be increasing with push_timestamp)
+        self.push_id = push_id
+
+        # data values associated with this revision
+        self.values = copy.copy(values)
 
         # t-test score
         self.t = 0
-        # Whether a perf regression is found
-        self.state = state
+
+        # Whether a perf regression or improvement was found
+        self.change_detected = False
 
     def __cmp__(self, o):
-        # only compare value to make sorting deterministic
-        # in cases where we have multiple datapoints with
-        # the same push timestamp
-        return cmp(
-            (self.push_timestamp, self.testrun_id, self.value),
-            (o.push_timestamp, o.testrun_id, o.value),
-        )
+        return self.push_timestamp < o.push_timestamp
 
     def __repr__(self):
-        return "<%s: %s, %s, %.3f, %.3f, %s>" % (self.push_timestamp,
-                                                 self.revision_id,
-                                                 self.testrun_id, self.value,
-                                                 self.t, self.state)
+        values_str = '[ %s ]' % ', '.join(['%.3f' % value for value in
+                                          self.values])
+        return "<%s: %s, %s, %.3f, %s>" % (self.push_timestamp,
+                                           self.push_id, values_str,
+                                           self.t, self.change_detected)
 
 
 def detect_changes(data, min_back_window=12, max_back_window=24,
@@ -94,65 +110,69 @@ def detect_changes(data, min_back_window=12, max_back_window=24,
     # Use T-Tests
     # Analyze test data using T-Tests, comparing data[i-j:i] to data[i:i+k]
     data = sorted(data)
-    (j, k) = (min_back_window, fore_window)
-    good_data = []
 
-    num_points = len(data) - k + 1
     last_seen_regression = 0
-    for i in range(num_points):
+    for i in range(1, len(data)):
         di = data[i]
-        # if we haven't seen something that looks like a change in a
-        # while, incorporate some extra historical data into our t-test
-        # calculation
-        if last_seen_regression > min_back_window:
-            additional_back_window = min(last_seen_regression,
-                                         max_back_window - min_back_window)
-        else:
-            additional_back_window = 0
-        jw = [d.value for d in good_data[-1 * (j + additional_back_window):]]
-        kw = [d.value for d in data[i:i+k]]
 
-        # Reverse the backward data so that the current point is at the
-        # start of the window.
-        jw.reverse()
+        # keep on getting previous data until we've either got at least 12
+        # data points *or* we've hit the maximum back window
+        jw = []
+        di.amount_prev_data = 0
+        prev_indice = i - 1
+        while di.amount_prev_data < max_back_window and prev_indice >= 0 and (
+                (i - prev_indice) <= min(max(last_seen_regression,
+                                             min_back_window),
+                                         max_back_window)):
+            jw.append(data[prev_indice])
+            di.amount_prev_data += len(jw[-1].values)
+            prev_indice -= 1
+
+        # accumulate present + future data until we've got at least 12 values
+        kw = []
+        di.amount_next_data = 0
+        next_indice = i
+        while di.amount_next_data < fore_window and next_indice < len(data):
+            kw.append(data[next_indice])
+            di.amount_next_data += len(kw[-1].values)
+            next_indice += 1
 
         di.historical_stats = analyze(jw)
         di.forward_stats = analyze(kw)
 
-        if len(jw) >= min_back_window:
-            di.t = abs(calc_t(jw, kw, linear_weights))
-            # add additional historical data points next time if we
-            # haven't detected a likely regression
-            if di.t > t_threshold:
-                last_seen_regression = 0
-            else:
-                last_seen_regression += 1
+        di.t = abs(calc_t(jw, kw, linear_weights))
+        # add additional historical data points next time if we
+        # haven't detected a likely regression
+        if di.t > t_threshold:
+            last_seen_regression = 0
         else:
-            # Assume it's ok, we don't have enough data
-            di.t = 0
-
-        good_data.append(di)
+            last_seen_regression += 1
 
     # Now that the t-test scores are calculated, go back through the data to
     # find where changes most likely happened.
-    for i in range(1, len(good_data) - 1):
-        di = good_data[i]
+    for i in range(1, len(data)):
+        di = data[i]
+
+        # if we don't have enough data yet, skip for now (until more comes
+        # in)
+        if di.amount_prev_data < min_back_window or di.amount_next_data < fore_window:
+            continue
+
         if di.t <= t_threshold:
             continue
 
         # Check the adjacent points
-        prev = good_data[i-1]
+        prev = data[i-1]
         if prev.t > di.t:
             continue
-        next = good_data[i+1]
-        if next.t > di.t:
-            continue
+        # next may or may not exist if it's the last in the series
+        if (i+1) < len(data):
+            next = data[i+1]
+            if next.t > di.t:
+                continue
 
         # This datapoint has a t value higher than the threshold and higher
         # than either neighbor.  Mark it as the cause of a regression.
-        di.state = 'regression'
+        di.change_detected = True
 
-    # Return all but the first and last points whose scores we calculated,
-    # since we can only produce a final decision for a point whose scores
-    # were compared to both of its neighbors.
-    return data[1:num_points-1]
+    return data
