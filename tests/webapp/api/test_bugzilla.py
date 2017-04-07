c0b7396f8587fcd6f6704b505bdbaaf70123396e
Wes Kocher <kwierso@gmail.com>
2017-04-07 16:38:06 -0700

    test

--- tests/webapp/api/test_bugzilla.py
+++ tests/webapp/api/test_bugzilla.py
@@ -159,3 +159,60 @@ def test_create_unauthenticated_bug(webapp, eleven_jobs_stored, activate_respons
 
     print content
     assert content['detail'] == "Authentication credentials were not provided."
+
+def test_create_bug_with_long_crash_signature(webapp, eleven_jobs_stored, activate_responses):
+    """
+    test successfully creating a bug in bugzilla
+    """
+
+    def request_callback(request):
+        headers = {}
+        requestdata = json.loads(request.body)
+        requestheaders = request.headers
+        print requestdata
+        print requestheaders
+        assert requestheaders['x-bugzilla-api-key'] == "12345helloworld"
+        assert requestdata['product'] == "Bugzilla"
+        assert requestdata['description'] == "Filed by: MyName\n\nIntermittent Description"
+        assert requestdata['component'] == "Administration"
+        assert requestdata['summary'] == "Intermittent summary"
+        assert requestdata['comment_tags'] == "treeherder"
+        assert requestdata['version'] == "4.0.17"
+        assert requestdata['keywords'] == ["intermittent-failure"]
+        assert requestdata['cf_crash_signature'] == "[@crashsig]"
+        assert requestdata['depends_on'] == "123"
+        assert requestdata['blocks'] == "1234"
+        assert requestdata['see_also'] == "12345"
+        resp_body = {"id": 323}
+        return(200, headers, json.dumps(resp_body))
+
+    responses.add_callback(
+        responses.POST, "https://thisisnotbugzilla.org/rest/bug",
+        callback=request_callback, match_querystring=False,
+        content_type="application/json",
+    )
+
+    client = APIClient()
+
+    crashsig = 'x' * 2050
+    resp = client.post(
+        reverse("bugzilla-create-bug"),
+        {
+            "product": "Bugzilla",
+            "component": "Administration",
+            "summary": "Intermittent summary",
+            "version": "4.0.17",
+            "comment": "Intermittent Description",
+            "comment_tags": "treeherder",
+            "keywords": ["intermittent-failure"],
+            "crash_signature": crashsig,
+            "depends_on": "123",
+            "blocks": "1234",
+            "see_also": "12345",
+        }
+    )
+
+    content = json.loads(resp.content)
+
+    print content
+    assert content['detail'] == "Authentication credentials were not provided."
