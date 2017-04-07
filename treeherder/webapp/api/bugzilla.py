955ff518590fb705cdded854941af9a64094741d
Wes Kocher <kwierso@gmail.com>
2017-04-07 16:46:28 -0700

    oops

--- treeherder/webapp/api/bugzilla.py
+++ treeherder/webapp/api/bugzilla.py
@@ -22,7 +22,7 @@ class BugzillaViewSet(viewsets.ViewSet):
         params = request.data
 
         if params.get("crash_signature") != None:
-            if len(params.get("crash_signature") > 2048):
+            if len(params.get("crash_signature")) > 2048:
                 return Response({"failure": "Crash signature can't be more than 2048 characters"},
                                 status=HTTP_400_BAD_REQUEST)
 
