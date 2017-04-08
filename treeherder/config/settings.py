7fc784a1521b7fab63f51a91c4d1b59a14d61a4f
Wes Kocher <kwierso@gmail.com>
2017-04-07 21:01:04 -0700

    Bug 1354438 - Give the Bug Filer control of its own API URL
    BZ_API_URL currently is used for both the bug filer and fetching bug suggestions through the rest of Treeherder. At the moment, both of them point to production bmo's API. Bug suggestions coming from the bugzilla instance that actually has most of the intermittent bugs filed makes sense. Filing bugs from staging Treeherder's bug filer into production bmo makes less sense, so this patch lets you specify a different API URL.
    
    If the new environmental variable isn't present, the bug filer will still default to production bmo, so this should be deployable without any configuration changes, and everything should still just work.

--- treeherder/config/settings.py
+++ treeherder/config/settings.py
@@ -438,8 +438,13 @@ PARSER_MAX_STEP_ERROR_LINES = 100
 PARSER_MAX_SUMMARY_LINES = 200
 FAILURE_LINES_CUTOFF = 35
 
+# BZ_API_URL is used to fetch bug suggestions from bugzilla
+# BUGFILER_API_URL is used when filing bugs
+# these are no longer necessarily the same so stage treeherder can submit
+# to stage bmo, while suggestions can still be fetched from prod bmo
 BZ_API_URL = "https://bugzilla.mozilla.org"
-BZ_API_KEY = env("BUGZILLA_API_KEY", default=None)
+BUGFILER_API_URL = env("BUGZILLA_API_URL", default="https://bugzilla.mozilla.org")
+BUGFILER_API_KEY = env("BUGZILLA_API_KEY", default=None)
 
 ORANGEFACTOR_SUBMISSION_URL = "https://brasstacks.mozilla.com/orangefactor/api/saveclassification"
 ORANGEFACTOR_HAWK_ID = "treeherder"
