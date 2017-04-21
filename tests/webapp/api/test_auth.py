505722af119e8f8390ed8abc066a2f0ca573f475
Ed Morley <emorley@mozilla.com>
2017-04-21 13:39:46 +0100

    Import Auth from taskcluster rather than taskcluster.sync
    Since the latter no longer exists in taskcluster 1.x, whereas the former
    works in all versions.

--- tests/webapp/api/test_auth.py
+++ tests/webapp/api/test_auth.py
@@ -6,7 +6,7 @@ from rest_framework import status
 from rest_framework.decorators import APIView
 from rest_framework.response import Response
 from rest_framework.test import APIRequestFactory
-from taskcluster.sync import Auth
+from taskcluster import Auth
 
 from treeherder.model.models import User
 from treeherder.webapp.api import permissions
