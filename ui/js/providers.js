d5c067c3bfdcf7e4d77c0a3e7f4e85179a4be674
James Graham <james@hoppipolla.co.uk>
2017-05-07 00:25:50 +0100

    Bug 1362812 - Consider wpt reftests to be reftests.
    These aren't in the "reftest" job group but do have "reftest" in the
    job title. This change is required to ensure that they get the reftest
    analyzer links.

--- ui/js/providers.js
+++ ui/js/providers.js
@@ -299,7 +299,8 @@ treeherder.provider('thReftestStatus', function() {
     this.$get = function() {
         return function(job) {
             if (job.job_group_name) {
-                return (job.job_group_name.toLowerCase().indexOf('reftest') !== -1);
+                return (job.job_group_name.toLowerCase().indexOf('reftest') !== -1 ||
+                        job.job_type_name.toLowerCase().indexOf('reftest') !== -1);
             }
         };
     };
