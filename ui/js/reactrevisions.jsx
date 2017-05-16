c1dd40614d49d095ea89f1a1d0ea44376d400f54
Wes Kocher <kwierso@gmail.com>
2017-05-16 16:05:54 -0700

    Bug 1340265 - Add a copy revision button for each revision

--- ui/js/reactrevisions.jsx
+++ ui/js/reactrevisions.jsx
@@ -1,5 +1,8 @@
 'use strict';
 
+const { Tooltip, OverlayTrigger } = require('react-bootstrap');
+const FontAwesome = require('react-fontawesome');
+
 const MoreRevisionsLink = (props) => (
     <li>
         <a href={props.href}
@@ -15,9 +18,10 @@ MoreRevisionsLink.propTypes = {
 };
 
 const RevisionItem = (props) => {
-    let email, name, userTokens, escapedComment, escapedCommentHTML, initialsHTML, tags;
+    let email, name, userTokens, escapedComment, escapedCommentHTML, initialsHTML, tags, revision;
 
     userTokens = props.revision.author.split(/[<>]+/);
+    revision = props.revision.revision;
     name = userTokens[0].trim();
     if (userTokens.length > 1) email = userTokens[1];
     initialsHTML = { __html: props.initialsFilter(name) };
@@ -31,9 +35,35 @@ const RevisionItem = (props) => {
         tags += "backout";
     }
 
+    var tooltip = (<Tooltip id="tooltip">Copy revision to clipboard</Tooltip>);
+
+    const revisionClick = function(rv, ev) {
+        var textField = document.createElement('textarea');
+        textField.innerText = rv;
+        var el = ev.target.parentNode;
+        /* DOESN'T WORK, tooltip.props.children is readonly
+        console.log(el,tooltip);
+        tooltip.props.children = "COPIED!";
+        $(el).attr('data-original-title', "COPIED!").tooltip('show');
+        window.setTimeout(function() {
+            $(el).attr('data-original-title',
+                "Copy revision hash to clipboard").tooltip('show');
+        }, 2000);
+        */
+        document.body.appendChild(textField);
+        textField.select();
+        document.execCommand('copy');
+        textField.remove();
+    };
+
     return <li className="clearfix">
         <span className="revision" data-tags={tags}>
             <span className="revision-holder">
+                <OverlayTrigger placement="right" overlay={tooltip}>
+                    <span className="copy-revision-button-holder" onClick={revisionClick.bind(this,revision)}>
+                        <FontAwesome className="copy-revision-button" name="clipboard"></FontAwesome>
+                    </span>
+                </OverlayTrigger>
                 <a title={`Open revision ${props.revision.revision} on ${props.repo.url}`}
                    href={props.repo.getRevisionHref(props.revision.revision)}
                    data-ignore-job-clear-on-click>
