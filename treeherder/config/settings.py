67e9c763698dbb338c487eb5e4817537ff414910
Shruti Jasoria <shrutijasoria1996@gmail.com>
2017-06-19 12:10:50 +0530

    Bug 1372640 - Add django-filters to INSTALLED_APPS
    The jobdetail browsable API pages were returning HTTP 500 since as of
    carltongibson/django-filter#580 it's necessary to add django-filter to
    INSTALLED_APPS: https://django-filter.readthedocs.io/en/latest/guide/install.html

--- treeherder/config/settings.py
+++ treeherder/config/settings.py
@@ -142,6 +142,7 @@ INSTALLED_APPS = [
     'rest_framework_swagger',
     'hawkrest',
     'corsheaders',
+    'django_filters',
     'graphene_django',
     # treeherder apps
     'treeherder.model',
