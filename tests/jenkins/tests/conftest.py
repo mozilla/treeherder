d15c2cb819e144d455ba7c2f221cbfd0f5266239
Stephen Donner <stephen.donner@gmail.com>
2017-07-25 13:42:10 -0700

    Add marionette capabilities

--- tests/jenkins/tests/conftest.py
+++ tests/jenkins/tests/conftest.py
@@ -1,6 +1,14 @@
 import pytest
 
 
+@pytest.fixture
+def capabilities(request, capabilities):
+    driver = request.config.getoption('driver')
+    if capabilities.get('browserName', driver).lower() == 'firefox':
+        capabilities['marionette'] = True
+    return capabilities
+
+    
 @pytest.fixture(scope='session')
 def session_capabilities(session_capabilities):
     session_capabilities.setdefault('tags', []).append('treeherder')
