02135f71f37c6848d41a1724bb49d745ec4a4a68
Ed Morley <edmorley@users.noreply.github.com>
2017-10-02 00:40:30 +0100

    Bug 1364894 - eslint --fix for newline-per-chained-call
    https://eslint.org/docs/rules/newline-per-chained-call
    https://github.com/airbnb/javascript#whitespace--chains

--- ui/js/perfapp.js
+++ ui/js/perfapp.js
@@ -10,57 +10,67 @@ perf.config(['$compileProvider', '$httpProvider', '$stateProvider', '$urlRouterP
         $httpProvider.defaults.xsrfCookieName = 'csrftoken';
         $httpProvider.useApplyAsync(true);
 
-        $stateProvider.state('alerts', {
-            title: 'Alerts',
-            templateUrl: 'partials/perf/alertsctrl.html',
-            url: '/alerts?id&status&framework&filter&hideImprovements&hideDwnToInv&page',
-            controller: 'AlertsCtrl'
-        }).state('graphs', {
-            title: 'Graphs',
-            templateUrl: 'partials/perf/graphsctrl.html',
-            url: '/graphs?timerange&series&highlightedRevisions&highlightAlerts&zoom&selected',
-            controller: 'GraphsCtrl'
-        }).state('compare', {
-            title: 'Compare',
-            templateUrl: 'partials/perf/comparectrl.html',
-            url: '/compare?originalProject&originalRevision?&newProject&newRevision&hideMinorChanges&framework&filter&showOnlyImportant&showOnlyConfident&selectedTimeRange?',
-            controller: 'CompareResultsCtrl'
-        }).state('comparesubtest', {
-            title: 'Compare - Subtests',
-            templateUrl: 'partials/perf/comparesubtestctrl.html',
-            url: '/comparesubtest?originalProject&originalRevision?&newProject&newRevision&originalSignature&newSignature&filter&showOnlyImportant&showOnlyConfident&framework&selectedTimeRange?',
-            controller: 'CompareSubtestResultsCtrl'
-        }).state('comparechooser', {
-            title: 'Compare Chooser',
-            templateUrl: 'partials/perf/comparechooserctrl.html',
-            url: '/comparechooser?originalProject&originalRevision&newProject&newRevision',
-            controller: 'CompareChooserCtrl'
-        }).state('e10s_trend', {
-            title: 'e10s Trend Dashboard',
-            templateUrl: 'partials/perf/e10s-trend.html',
-            url: '/e10s-trend?filter&showOnlyImportant&showOnlyConfident&showOnlyBlockers&repo&basedate&newdate&timerange&revision',
-            controller: 'e10sTrendCtrl'
-        }).state('e10s_trendsubtest', {
-            title: 'e10s Trend Dashboard - subtests',
-            templateUrl: 'partials/perf/e10s-trend-subtest.html',
-            url: '/e10s_trendsubtest?filter&showOnlyImportant&showOnlyConfident&showOnlyBlockers&repo&basedate&newdate&timerange&revision&baseSignature&e10sSignature',
-            controller: 'e10sTrendSubtestCtrl'
-        }).state('dashboard', {
-            title: 'Perfherder Dashboard',
-            templateUrl: 'partials/perf/dashboard.html',
-            url: '/dashboard?topic&filter&showOnlyImportant&showOnlyConfident&showOnlyBlockers&repo&timerange&revision',
-            controller: 'dashCtrl'
-        }).state('dashboardsubtest', {
-            title: 'Perfherder Dashboard - Subtests',
-            templateUrl: 'partials/perf/dashboardsubtest.html',
-            url: '/dashboardsubtest?topic&filter&showOnlyImportant&showOnlyConfident&baseSignature&variantSignature&repo&timerange&revision',
-            controller: 'dashSubtestCtrl'
-        }).state('comparesubtestdistribution', {
-            title: 'Compare Subtest Distribution',
-            templateUrl: 'partials/perf/comparesubtestdistribution.html',
-            url: '/comparesubtestdistribution?originalProject&newProject&originalRevision&newRevision&originalSubtestSignature?newSubtestSignature',
-            controller: 'CompareSubtestDistributionCtrl'
-        });
+        $stateProvider
+            .state('alerts', {
+                title: 'Alerts',
+                templateUrl: 'partials/perf/alertsctrl.html',
+                url: '/alerts?id&status&framework&filter&hideImprovements&hideDwnToInv&page',
+                controller: 'AlertsCtrl'
+            })
+            .state('graphs', {
+                title: 'Graphs',
+                templateUrl: 'partials/perf/graphsctrl.html',
+                url: '/graphs?timerange&series&highlightedRevisions&highlightAlerts&zoom&selected',
+                controller: 'GraphsCtrl'
+            })
+            .state('compare', {
+                title: 'Compare',
+                templateUrl: 'partials/perf/comparectrl.html',
+                url: '/compare?originalProject&originalRevision?&newProject&newRevision&hideMinorChanges&framework&filter&showOnlyImportant&showOnlyConfident&selectedTimeRange?',
+                controller: 'CompareResultsCtrl'
+            })
+            .state('comparesubtest', {
+                title: 'Compare - Subtests',
+                templateUrl: 'partials/perf/comparesubtestctrl.html',
+                url: '/comparesubtest?originalProject&originalRevision?&newProject&newRevision&originalSignature&newSignature&filter&showOnlyImportant&showOnlyConfident&framework&selectedTimeRange?',
+                controller: 'CompareSubtestResultsCtrl'
+            })
+            .state('comparechooser', {
+                title: 'Compare Chooser',
+                templateUrl: 'partials/perf/comparechooserctrl.html',
+                url: '/comparechooser?originalProject&originalRevision&newProject&newRevision',
+                controller: 'CompareChooserCtrl'
+            })
+            .state('e10s_trend', {
+                title: 'e10s Trend Dashboard',
+                templateUrl: 'partials/perf/e10s-trend.html',
+                url: '/e10s-trend?filter&showOnlyImportant&showOnlyConfident&showOnlyBlockers&repo&basedate&newdate&timerange&revision',
+                controller: 'e10sTrendCtrl'
+            })
+            .state('e10s_trendsubtest', {
+                title: 'e10s Trend Dashboard - subtests',
+                templateUrl: 'partials/perf/e10s-trend-subtest.html',
+                url: '/e10s_trendsubtest?filter&showOnlyImportant&showOnlyConfident&showOnlyBlockers&repo&basedate&newdate&timerange&revision&baseSignature&e10sSignature',
+                controller: 'e10sTrendSubtestCtrl'
+            })
+            .state('dashboard', {
+                title: 'Perfherder Dashboard',
+                templateUrl: 'partials/perf/dashboard.html',
+                url: '/dashboard?topic&filter&showOnlyImportant&showOnlyConfident&showOnlyBlockers&repo&timerange&revision',
+                controller: 'dashCtrl'
+            })
+            .state('dashboardsubtest', {
+                title: 'Perfherder Dashboard - Subtests',
+                templateUrl: 'partials/perf/dashboardsubtest.html',
+                url: '/dashboardsubtest?topic&filter&showOnlyImportant&showOnlyConfident&baseSignature&variantSignature&repo&timerange&revision',
+                controller: 'dashSubtestCtrl'
+            })
+            .state('comparesubtestdistribution', {
+                title: 'Compare Subtest Distribution',
+                templateUrl: 'partials/perf/comparesubtestdistribution.html',
+                url: '/comparesubtestdistribution?originalProject&newProject&originalRevision&newRevision&originalSubtestSignature?newSubtestSignature',
+                controller: 'CompareSubtestDistributionCtrl'
+            });
         $urlRouterProvider.otherwise('/graphs');
     }]).run(['$rootScope', '$state', '$stateParams', function ($rootScope, $state, $stateParams) {
         $rootScope.$state = $state;
