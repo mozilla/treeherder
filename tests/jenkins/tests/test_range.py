dee4c349eabe9ae9695fd91ce67f9fca4ef8be61
Dave Hunt <dave.hunt@gmail.com>
2017-10-25 00:04:57 +0100

    Jenkins: Improve range tests by working around bug 1411264 and picking result sets relative to the end of the list (#2871)

--- tests/jenkins/tests/test_range.py
+++ tests/jenkins/tests/test_range.py
@@ -6,17 +6,18 @@ from pages.treeherder import TreeherderPage
 @pytest.mark.nondestructive
 def test_set_as_top_of_range(base_url, selenium):
     page = TreeherderPage(selenium, base_url).open()
-    current_top_of_range = page.result_sets[0].datestamp
-    page.result_sets[1].set_as_top_of_range()
-    page.wait_for_page_to_load()
-    new_top_of_range = page.result_sets[0].datestamp
-    assert not new_top_of_range == current_top_of_range
+    result_sets = page.result_sets
+    datestamp = result_sets[1].datestamp
+    assert result_sets[0].datestamp != datestamp
+    result_sets[1].set_as_top_of_range()
+    assert page.result_sets[0].datestamp == datestamp
 
 
 @pytest.mark.nondestructive
 def test_set_as_bottom_of_range(base_url, selenium):
     page = TreeherderPage(selenium, base_url).open()
-    current_bottom_of_range = page.result_sets[9].datestamp
-    page.result_sets[0].set_as_bottom_of_range()
-    new_bottom_of_range = page.result_sets[0].datestamp
-    assert not new_bottom_of_range == current_bottom_of_range
+    result_sets = page.result_sets
+    datestamp = result_sets[-2].datestamp
+    assert result_sets[-1].datestamp != datestamp
+    page.result_sets[-2].set_as_bottom_of_range()
+    assert page.result_sets[-1].datestamp == datestamp
