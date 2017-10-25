c6575c9524959362ffc311b33fba3b602038a81d
Dave Hunt <dave.hunt@gmail.com>
2017-10-25 16:25:52 +0100

    Optimise test_filter_panel_reset_button by introducing locators for in-progress jobs

--- tests/jenkins/tests/test_filter_panel.py
+++ tests/jenkins/tests/test_filter_panel.py
@@ -45,11 +45,11 @@ def test_filter_panel_reset_button(base_url, selenium):
     """Open Treeherder page, hide jobs in progress, reset filters button and
     verify in progress jobs are displayed"""
     page = TreeherderPage(selenium, base_url).open()
-    assert any(j for j in page.all_jobs if j.in_progress)
+    assert page.all_in_progress_jobs
     page.filter_job_in_progress()
     assert not page.nav_filter_in_progress_is_selected
-    assert not any(j for j in page.all_jobs if j.in_progress)
+    assert not page.all_in_progress_jobs
     page.click_on_filters_panel()
     page.reset_filters()
     assert page.nav_filter_in_progress_is_selected
-    assert any(j for j in page.all_jobs if j.in_progress)
+    assert page.all_in_progress_jobs
