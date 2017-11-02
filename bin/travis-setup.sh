624d2f43a9def6bf78f41f2f646bf67c3c5756cb
Ed Morley <501702+edmorley@users.noreply.github.com>
2017-11-02 23:44:02 +0000

    Bug 1414094 - Travis: Move setup steps to a reusable script
    This:
    * reduces duplication
    * opens the door to sharing functionality with `vagrant/setup.sh`
    * will make it easier to visualise the Travis bootstrap process
      when moving both Travis and Vagrant to a unified Docker environment.

---
+++ bin/travis-setup.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# This script must be sourced, so that the environment variables are set in the calling shell.
+export BROKER_URL='amqp://guest:guest@localhost:5672//'
+export DATABASE_URL='mysql://root@localhost/test_treeherder'
+export ELASTICSEARCH_URL='http://127.0.0.1:9200'
+export TREEHERDER_DJANGO_SECRET_KEY='secretkey-of-at-50-characters-to-pass-check-deploy'
+
+setup_services() {
+    echo '-----> Installing Elasticsearch'
+    curl -sSfo /tmp/elasticsearch.deb 'https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.5.0.deb'
+    sudo dpkg -i --force-confold /tmp/elasticsearch.deb
+    sudo service elasticsearch restart
+
+    # Using tmpfs for the MySQL data directory reduces pytest runtime by 30%.
+    echo '-----> Creating RAM disk for MySQL'
+    sudo stop mysql
+    sudo mkdir /mnt/ramdisk
+    sudo mount -t tmpfs -o size=1024m tmpfs /mnt/ramdisk
+    sudo mv /var/lib/mysql /mnt/ramdisk
+    sudo ln -s /mnt/ramdisk/mysql /var/lib/mysql
+
+    echo '-----> Installing MySQL'
+    sudo cp vagrant/mysql.cnf /etc/mysql/conf.d/treeherder.cnf
+    # Use the upstream APT repo since the latest Ubuntu trusty package is MySQL 5.6.
+    echo 'deb http://repo.mysql.com/apt/ubuntu/ trusty mysql-5.7' | sudo tee /etc/apt/sources.list.d/mysql.list > /dev/null
+    sudo -E apt-get -yqq update
+    sudo -E apt-get -yqq install --no-install-recommends --allow-unauthenticated mysql-server libmysqlclient-dev
+
+    echo '-----> Starting rabbitmq-server'
+    sudo service rabbitmq-server start
+
+    echo '-----> Waiting for Elasticsearch to be ready'
+    while ! curl "${ELASTICSEARCH_URL}" &> /dev/null; do sleep 1; done
+}
+
+setup_python_env() {
+    # Use a clean virtualenv rather than the one given to us, to work around:
+    # https://github.com/travis-ci/travis-ci/issues/4873
+    if [[ ! -f "${HOME}/venv/bin/python" ]]; then
+        echo '-----> Creating virtualenv'
+        virtualenv -p python "${HOME}/venv"
+    fi
+    export PATH="${HOME}/venv/bin:${PATH}"
+
+    echo '-----> Running pip install'
+    pip install --require-hashes -r requirements/common.txt -r requirements/dev.txt
+}
+
+setup_geckodriver() {
+    echo '-----> Installing geckodriver'
+    curl -sSfL 'https://github.com/mozilla/geckodriver/releases/download/v0.19.1/geckodriver-v0.19.1-linux64.tar.gz' | tar -zxC "${HOME}/bin"
+}
+
+setup_js_env() {
+    echo '-----> Installing Firefox'
+    curl -sSfL 'https://download.mozilla.org/?product=firefox-latest&lang=en-US&os=linux64' | tar -jxC "${HOME}"
+    export PATH="${HOME}/firefox:${PATH}"
+
+    echo '-----> Starting xvfb'
+    # Required for Karma tests (http://docs.travis-ci.com/user/gui-and-headless-browsers/)
+    # Remove when we switch to using Firefox in headless mode.
+    export DISPLAY=':99.0'
+    sh -e /etc/init.d/xvfb start
+
+    echo '-----> Installing yarn'
+    curl -sSfL 'https://yarnpkg.com/latest.tar.gz' | tar -xz --strip-components=1 -C "${HOME}"
+
+    echo '-----> Running yarn install'
+    # `--frozen-lockfile` will catch cases where people have forgotten to update `yarn.lock`.
+    # `--no-bin-links` is only necessary on Windows hosts, but we include here to ensure
+    # that the package.json scripts aren't relying on symlinks that won't exist elsewhere.
+    yarn install --frozen-lockfile --no-bin-links
+}
+
+for task in "$@"; do
+    "setup_${task}"
+done
+
+# Restore shell options since this script is sourced, so affects the caller:
+# https://github.com/travis-ci/travis-ci/issues/5434
+set +euo pipefail
