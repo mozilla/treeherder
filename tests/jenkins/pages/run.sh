117b5fcb8ad7c5859deae856f32271996e147380
Ed Morley <501702+edmorley@users.noreply.github.com>
2017-11-03 14:51:44 +0000

    Bug 1414255 - Fix shellcheck warnings about sourced files
    ```
    SC1090: Can't follow non-constant source. Use a directive to specify location.
    SC1091: Not following: /etc/profile.d/treeherder.sh was not specified as input (see shellcheck -x).
    ```
    
    See:
    https://github.com/koalaman/shellcheck/wiki/SC1090
    https://github.com/koalaman/shellcheck/wiki/SC1091

--- tests/jenkins/pages/run.sh
+++ tests/jenkins/pages/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -ex
 
+# shellcheck source=/dev/null
 source ./venv_modules/bin/activate
 firefox-ui-functional tests/test_tp.py --binary firefox/FirefoxNightly.app/Contents/MacOS/firefox-bin -v
