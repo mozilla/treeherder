c96bb5ac24b114486027997d878b03b522b5a531
Dave Hunt <dave.hunt@gmail.com>
2017-11-10 09:40:09 +0000

    Fix locators for dropdowns

--- tests/jenkins/pages/base.py
+++ tests/jenkins/pages/base.py
@@ -14,7 +14,7 @@ class Base(Page):
     class Header(Region):
 
         _root_locator = (By.ID, 'th-global-navbar')
-        _dropdown_menu_switch_page_locator = (By.CSS_SELECTOR, '.open ul > li a')
+        _dropdown_menu_switch_page_locator = (By.CSS_SELECTOR, 'ul.show > li a')
         _dropdown_menu_locator = (By.ID, 'th-logo')
 
         def switch_page_using_dropdown(self):
