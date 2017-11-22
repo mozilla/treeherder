427d217d718277557f62ecef3ea328a6c9fb8ec6
Jonathan French <tojonmz@gmail.com>
2017-11-22 17:54:51 -0500

    Bug 1419966 - Angularize start/end query examples in the User Guide

--- ui/js/controllers/userguide.js
+++ ui/js/controllers/userguide.js
@@ -1,6 +1,16 @@
 "use strict";
 
-userguideApp.controller('UserguideCtrl',
-    function () {
+userguideApp.controller('UserguideCtrl', ['$scope',
+    function UserguideCtrl($scope) {
+
+        // Used for dynamic startdate, enddate param examples
+        let d = new Date();
+        let y = d.getFullYear();
+        let m = d.getMonth() + 1;
+        let sd = d.getDate() - 2;
+        let ed = d.getDate();
+
+        $scope.startDate = y + '-' + m + '-' + sd;
+        $scope.endDate = y + '-' + m + '-' + ed;
     }
-);
+]);
