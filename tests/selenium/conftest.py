5dd4e3b01f4e9d6f4ef8058386bbc09e3f416625
Dave Hunt <dave.hunt@gmail.com>
2017-11-27 15:27:17 +0000

    Bug 1420180 - Only register base_url and related fixtures within Selenium test module

--- tests/selenium/conftest.py
+++ tests/selenium/conftest.py
@@ -2,6 +2,16 @@ import pytest
 from django.core.management import call_command
 
 
+@pytest.fixture(scope="session")
+def base_url(live_server):
+    return live_server.url
+
+
+@pytest.fixture(scope='session')
+def sensitive_url(request, base_url):
+    pass
+
+
 @pytest.fixture(autouse=True)
 def initial_data(transactional_db):
     call_command('load_initial_data')
