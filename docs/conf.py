644b0d900f4bbc8eaa944389848beca7a9578700
Ed Morley <501702+edmorley@users.noreply.github.com>
2017-11-28 15:39:18 +0000

    Bug 1413975 - Docs: Disable unused Sphinx extensions
    This means less to both install and run at build time, hopefully
    speeding up the build and reducing the chance of races/locking issues.

--- docs/conf.py
+++ docs/conf.py
@@ -12,32 +12,12 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import os
-import sys
-
 import sphinx_rtd_theme
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('..'))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "treeherder.config.settings")
 
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
 # needs_sphinx = '1.0'
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.coverage',
-    'sphinx.ext.viewcode',
-    'sphinxcontrib.httpdomain',
-]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
