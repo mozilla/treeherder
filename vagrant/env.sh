076083373c4559feffa38f3419eff3440b073659
Ed Morley <501702+edmorley@users.noreply.github.com>
2017-12-20 14:04:26 +0000

    Vagrant: Remove redundant environment variables (#3051)
    * `ENABLE_LOCAL_SETTINGS_FILE` is no longer references anywhere.
    * `GRAPHQL` now defaults to `True`, so doesn't need to be overridden.

--- vagrant/env.sh
+++ vagrant/env.sh
@@ -2,7 +2,6 @@
 
 export PATH="${HOME}/firefox:${HOME}/python/bin:${PATH}"
 
-export ENABLE_LOCAL_SETTINGS_FILE='True'
 export BROKER_URL='amqp://guest:guest@localhost//'
 export DATABASE_URL='mysql://root@localhost/treeherder'
 export ELASTICSEARCH_URL='http://localhost:9200'
@@ -12,7 +11,6 @@ export ENABLE_DEBUG_TOOLBAR='True'
 export TREEHERDER_DJANGO_SECRET_KEY='secret-key-of-at-least-50-characters-to-pass-check-deploy'
 export NEW_RELIC_CONFIG_FILE='newrelic.ini'
 export NEW_RELIC_DEVELOPER_MODE='True'
-export GRAPHQL='True'
 
 # Enable Firefox headless mode, avoiding the need for xvfb.
 export MOZ_HEADLESS=1
