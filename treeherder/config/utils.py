525eaeb1c1a8bf903201b2c40d869516406a1c11
Ed Morley <501702+edmorley@users.noreply.github.com>
2018-01-12 23:07:25 +0000

    Bug 1384518 - Add a utils.py helper for hostname parsing

--- treeherder/config/utils.py
+++ treeherder/config/utils.py
@@ -1,9 +1,12 @@
 from urlparse import urlparse
 
 
+def hostname(url):
+    return urlparse(url).hostname
+
+
 def connection_should_use_tls(url):
-    hostname = urlparse(url).hostname
     # Services such as RabbitMQ/Elasticsearch running on Travis do not yet have TLS
     # certificates set up. We could try using TLS locally using self-signed certs,
     # but until Travis has support it's not overly useful.
-    return hostname != 'localhost'
+    return hostname(url) != 'localhost'
