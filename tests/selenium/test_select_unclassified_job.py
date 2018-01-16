984a21649f6a20170a2c9f0eaf4c3bee32fdf80f
Dave Hunt <dave.hunt@gmail.com>
2018-01-16 16:06:08 +0000

    Migrate test_select_next_unclassified_job to Travis suite

---
+++ tests/selenium/test_select_unclassified_job.py
@@ -0,0 +1,28 @@
+import pytest
+
+from pages.treeherder import Treeherder
+from treeherder.etl.jobs import store_job_data
+from treeherder.model.models import Job
+
+
+@pytest.fixture
+def test_jobs(eleven_job_blobs, failure_classifications, test_repository):
+    jobs = eleven_job_blobs[0:2]
+    for job in jobs:
+        job['job']['result'] = 'testfailed'
+    store_job_data(test_repository, jobs)
+    return [Job.objects.get(id=i) for i in range(1, len(jobs) + 1)]
+
+
+def test_select_next_unclassified_job(base_url, selenium, test_jobs):
+    page = Treeherder(selenium, base_url).open()
+    for i in range(len(test_jobs)):
+        page.select_next_unclassified_job()
+        assert page.all_jobs[i].selected
+
+
+def test_select_previous_unclassified_job(base_url, selenium, test_jobs):
+    page = Treeherder(selenium, base_url).open()
+    for i in reversed(range(len(test_jobs))):
+        page.select_previous_unclassified_job()
+        assert page.all_jobs[i].selected
