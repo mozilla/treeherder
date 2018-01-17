3390fbcfaebcd68f7f1e300961fbee6918fb1050
Hassan Ali <helfi92@gmail.com>
2018-01-17 10:39:10 -0500

    Create 'authApp' which takes care of auth
    Purpose of files:
    `ui/js/auth.js` - creates the auth application
    `ui/js/services/auth.js` - creates `thAuth` service responsible for
    performing login, silent token renewal and saving relevant credentials
    `ui/js/components/auth/auth.js` - callback component that runs when auth0
    redirects the user to /login.html
    `ui/entry-login.js` - imports the relevant files required by `authApp`
    `ui/login.html` - html file rendered when the Auth0 redirects to
    /login.html
    
    Note: Since the token renewal happens in an invisible iframe, I think the
    the backend setting needs to be changed to allow frames from the same origin.

---
+++ ui/entry-login.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Webpack entry point for login.html
+// Scripts and styles included here are automatically included on the page at build time
+import './js/config';
+
+// Styles
+import 'font-awesome/css/font-awesome.css';
+
+// Vendor JS
+import 'angular';
+import 'angular-local-storage';
+import 'auth0-js';
+import 'angular-auth0';
+
+// Auth JS
+import './js/services/auth.js';
+import './js/components/auth/auth.js';
