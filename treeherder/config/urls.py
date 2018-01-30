dcb2a3efb3367e1a4db53f8d3da5a4b2a098d730
Ed Morley <501702+edmorley@users.noreply.github.com>
2018-01-30 23:49:58 +0000

    Bug 1065784 - Remove Django admin (#3170)
    Since the only things we used it for were:
    * editing users - but that doesn't work any more (bug 1346740) so
      requires a manual DB edit as is it, and we won't need to edit users
      at all once LDAP groups are used for permissions (bug 1273092)
    * marking pending Hawk credentials as approved - however we're not
      accepting any new requests for Hawk credentials (bug 1433011)
    * resetting the secret key for Hawk credentials - however we've not
      used this feature once in the entire time we've used Hawk - and its
      trivial via MySQLWorkbench.
    
    As as added bonus removing Django admin:
    * reduces the work required to add a CSP header
    * speeds up `collectstatic` (which is run during deploy) by 30%
    * reduces the risk of giving `is_staff` permissions (which have to be
      given to sheriffs, but also allowed admin access)
    * reduces attack surface in general
    
    In addition to the Django admin app, `django.contrib.messages` app
    and the auth/messages context processors have been removed, since
    after bug 1433011, admin was the only remaining consumer of them:
    https://docs.djangoproject.com/en/1.11/ref/contrib/admin/

--- treeherder/config/urls.py
+++ treeherder/config/urls.py
@@ -1,17 +1,13 @@
 from django.conf import settings
 from django.conf.urls import (include,
                               url)
-from django.contrib import admin
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework_swagger.views import get_swagger_view
 
 from treeherder.webapp.api import urls as api_urls
 
-admin.site.login_template = 'webapp/admin_login.html'
-
 urlpatterns = [
    url(r'^api/', include(api_urls)),
-   url(r'^admin/', admin.site.urls),
    url(r'^docs/', get_swagger_view(title='Treeherder API')),
 ]
 
