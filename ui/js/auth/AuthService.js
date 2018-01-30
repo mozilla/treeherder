82f5579baddd72412fea7eaae6888487d37846ac
Hassan Ali <helfi92@gmail.com>
2018-01-29 21:51:59 -0500

    Switch indentation of JS & JSON to 2 spaces (new files)

--- ui/js/auth/AuthService.js
+++ ui/js/auth/AuthService.js
@@ -4,104 +4,104 @@ import { OIDCCredentialAgent } from 'taskcluster-client-web';
 import { userSessionFromAuthResult, renew, loggedOutUser } from './auth-utils';
 
 export default class AuthService {
-    constructor() {
-        this.renewalTimer = null;
-    }
+  constructor() {
+    this.renewalTimer = null;
+  }
 
-    _fetchUser(userSession) {
-        const loginUrl = `${location.protocol}//${location.host}/api/auth/login/`;
+  _fetchUser(userSession) {
+    const loginUrl = `${location.protocol}//${location.host}/api/auth/login/`;
 
-        return new Promise(async (resolve, reject) => {
-            const userResponse = await fetch(loginUrl, {
-                headers: {
-                    authorization: `Bearer ${userSession.accessToken}`,
-                    idToken: userSession.idToken,
-                    expiresAt: userSession.expiresAt
-                },
-                method: 'GET',
-                credentials: 'same-origin'
-            });
+    return new Promise(async (resolve, reject) => {
+      const userResponse = await fetch(loginUrl, {
+        headers: {
+          authorization: `Bearer ${userSession.accessToken}`,
+          idToken: userSession.idToken,
+          expiresAt: userSession.expiresAt
+        },
+        method: 'GET',
+        credentials: 'same-origin'
+      });
 
-            const user = await userResponse.json();
+      const user = await userResponse.json();
 
-            if (!userResponse.ok) {
-                reject(
-                    new Error(user.detail || userResponse.statusText)
-                );
-            }
-
-            resolve(user);
-        });
-    }
-
-    _clearRenewalTimer() {
-        if (this.renewalTimer) {
-            clearTimeout(this.renewalTimer);
-            this.renewalTimer = null;
-        }
-    }
-
-    async _renewAuth() {
-        try {
-            if (!localStorage.getItem('treeherder.userSession')) {
-                return;
-            }
-
-            const authResult = await renew();
-
-            if (authResult) {
-                await this.saveCredentialsFromAuthResult(authResult);
-
-                return this.resetRenewalTimer();
-            }
-        } catch (err) {
-            this.logout();
-            /* eslint-disable no-console */
-            console.error('Could not renew login:', err);
-        }
-    }
-
-    resetRenewalTimer() {
-        const userSession = JSON.parse(localStorage.getItem('treeherder.userSession'));
-
-        this._clearRenewalTimer();
-
-        if (userSession) {
-            let timeout = Math.max(0, new Date(userSession.renewAfter) - Date.now());
-
-            // apply up to a few minutes to it randomly. This avoids
-            // multiple tabs all trying to renew at the same time.
-            if (timeout > 0) {
-                timeout += Math.random() * 5 * 1000 * 60;
-            }
-
-            // create renewal timer
-            this._clearRenewalTimer();
-            this.renewalTimer = setTimeout(() => this._renewAuth(), timeout);
-        }
-    }
-
-    logout() {
-        localStorage.removeItem('treeherder.userSession');
-        localStorage.setItem('treeherder.user', JSON.stringify(loggedOutUser));
-    }
-
-    async saveCredentialsFromAuthResult(authResult) {
-        const userSession = userSessionFromAuthResult(authResult);
-        const credentialAgent = new OIDCCredentialAgent({
-            accessToken: userSession.accessToken,
-            oidcProvider: 'mozilla-auth0'
-        });
-        const taskclusterCredentials = await credentialAgent.getCredentials();
-
-        const user = await this._fetchUser(userSession);
-
-        // Update taskcluster client credentials
-        window.dispatchEvent(
-            new CustomEvent('taskcluster-credentials', { detail: taskclusterCredentials })
+      if (!userResponse.ok) {
+        reject(
+          new Error(user.detail || userResponse.statusText)
         );
+      }
 
-        localStorage.setItem('treeherder.userSession', JSON.stringify(userSession));
-        localStorage.setItem('treeherder.user', JSON.stringify(user));
+      resolve(user);
+    });
+  }
+
+  _clearRenewalTimer() {
+    if (this.renewalTimer) {
+      clearTimeout(this.renewalTimer);
+      this.renewalTimer = null;
     }
+  }
+
+  async _renewAuth() {
+    try {
+      if (!localStorage.getItem('treeherder.userSession')) {
+        return;
+      }
+
+      const authResult = await renew();
+
+      if (authResult) {
+        await this.saveCredentialsFromAuthResult(authResult);
+
+        return this.resetRenewalTimer();
+      }
+    } catch (err) {
+      this.logout();
+      /* eslint-disable no-console */
+      console.error('Could not renew login:', err);
+    }
+  }
+
+  resetRenewalTimer() {
+    const userSession = JSON.parse(localStorage.getItem('treeherder.userSession'));
+
+    this._clearRenewalTimer();
+
+    if (userSession) {
+      let timeout = Math.max(0, new Date(userSession.renewAfter) - Date.now());
+
+      // apply up to a few minutes to it randomly. This avoids
+      // multiple tabs all trying to renew at the same time.
+      if (timeout > 0) {
+        timeout += Math.random() * 5 * 1000 * 60;
+      }
+
+      // create renewal timer
+      this._clearRenewalTimer();
+      this.renewalTimer = setTimeout(() => this._renewAuth(), timeout);
+    }
+  }
+
+  logout() {
+    localStorage.removeItem('treeherder.userSession');
+    localStorage.setItem('treeherder.user', JSON.stringify(loggedOutUser));
+  }
+
+  async saveCredentialsFromAuthResult(authResult) {
+    const userSession = userSessionFromAuthResult(authResult);
+    const credentialAgent = new OIDCCredentialAgent({
+      accessToken: userSession.accessToken,
+      oidcProvider: 'mozilla-auth0'
+    });
+    const taskclusterCredentials = await credentialAgent.getCredentials();
+
+    const user = await this._fetchUser(userSession);
+
+    // Update taskcluster client credentials
+    window.dispatchEvent(
+      new CustomEvent('taskcluster-credentials', { detail: taskclusterCredentials })
+    );
+
+    localStorage.setItem('treeherder.userSession', JSON.stringify(userSession));
+    localStorage.setItem('treeherder.user', JSON.stringify(user));
+  }
 }
--- ui/js/auth/auth-utils.js
+++ ui/js/auth/auth-utils.js
@@ -4,55 +4,55 @@ import { WebAuth } from 'auth0-js';
 import { fromNow } from 'taskcluster-client-web';
 
 export const webAuth = new WebAuth({
-    clientID: 'q8fZZFfGEmSB2c5uSI8hOkKdDGXnlo5z',
-    domain: 'auth.mozilla.auth0.com',
-    responseType: 'id_token token',
-    audience: 'login.taskcluster.net',
-    redirectUri: `${location.protocol}//${location.host}/login.html`,
-    scope: 'full-user-credentials openid profile email'
+  clientID: 'q8fZZFfGEmSB2c5uSI8hOkKdDGXnlo5z',
+  domain: 'auth.mozilla.auth0.com',
+  responseType: 'id_token token',
+  audience: 'login.taskcluster.net',
+  redirectUri: `${location.protocol}//${location.host}/login.html`,
+  scope: 'full-user-credentials openid profile email'
 });
 
 export const userSessionFromAuthResult = (authResult) => {
-    const expiresAt = JSON.stringify((authResult.expiresIn * 1000) + Date.now());
-    const userSession = {
-        idToken: authResult.idToken,
-        accessToken: authResult.accessToken,
-        fullName: authResult.idTokenPayload.nickname,
-        picture: authResult.idTokenPayload.picture,
-        oidcSubject: authResult.idTokenPayload.sub,
-        // expiresAt is used by the django backend to expire the user session
-        expiresAt,
-        // per https://wiki.mozilla.org/Security/Guidelines/OpenID_connect#Session_handling
-        renewAfter: fromNow('15 minutes')
-    };
+  const expiresAt = JSON.stringify((authResult.expiresIn * 1000) + Date.now());
+  const userSession = {
+    idToken: authResult.idToken,
+    accessToken: authResult.accessToken,
+    fullName: authResult.idTokenPayload.nickname,
+    picture: authResult.idTokenPayload.picture,
+    oidcSubject: authResult.idTokenPayload.sub,
+    // expiresAt is used by the django backend to expire the user session
+    expiresAt,
+    // per https://wiki.mozilla.org/Security/Guidelines/OpenID_connect#Session_handling
+    renewAfter: fromNow('15 minutes')
+  };
 
-    return userSession;
+  return userSession;
 };
 
 // Wrapper around webAuth's renewAuth
 export const renew = () => (
-    new Promise((resolve, reject) => {
-        webAuth.renewAuth({}, (error, authResult) => {
-            if (error) {
-                return reject(error);
-            }
+  new Promise((resolve, reject) => {
+    webAuth.renewAuth({}, (error, authResult) => {
+      if (error) {
+        return reject(error);
+      }
 
-            return resolve(authResult);
-        });
-    })
+      return resolve(authResult);
+    });
+  })
 );
 
 // Wrapper around webAuth's parseHash
 export const parseHash = qs => (
-    new Promise((resolve, reject) => {
-        webAuth.parseHash(qs, (error, authResult) => {
-            if (error) {
-                return reject(error);
-            }
+  new Promise((resolve, reject) => {
+    webAuth.parseHash(qs, (error, authResult) => {
+      if (error) {
+        return reject(error);
+      }
 
-            return resolve(authResult);
-        });
-    })
+      return resolve(authResult);
+    });
+  })
 );
 
 export const loggedOutUser = { is_staff: false, username: "", email: "", loggedin: false };
