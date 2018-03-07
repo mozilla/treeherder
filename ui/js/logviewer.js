8ebaa1f499abc37ebf339b8c89682d103c023e11
Ed Morley <501702+edmorley@users.noreply.github.com>
2018-03-07 19:25:58 +0000

    Bug 1443832 - Disable compilation of class and comment directives (#3313)
    Since we don't use them, and disabling improves compile performance:
    https://docs.angularjs.org/guide/production#disable-comment-and-css-class-directives

--- ui/js/logviewer.js
+++ ui/js/logviewer.js
@@ -6,8 +6,11 @@ const logViewerApp = angular.module('logviewer', [treeherderModule.name]);
 
 logViewerApp.config(['$compileProvider', '$locationProvider', '$resourceProvider',
     function ($compileProvider, $locationProvider, $resourceProvider) {
-        // Disable debug data, as recommended by https://docs.angularjs.org/guide/production
+        // Disable debug data & legacy comment/class directive syntax, as recommended by:
+        // https://docs.angularjs.org/guide/production
         $compileProvider.debugInfoEnabled(false);
+        $compileProvider.commentDirectivesEnabled(false);
+        $compileProvider.cssClassDirectivesEnabled(false);
 
         // Revert to the legacy Angular <=1.5 URL hash prefix to save breaking existing links:
         // https://docs.angularjs.org/guide/migration#commit-aa077e8
