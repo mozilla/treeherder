0ff338a1602995f62a7751970980e169285a7b7e
George Hickman <george@ghickman.co.uk>
2018-03-16 16:04:50 +0000

    Bug 1320451 - Enable django_extensions when in DEBUG mode

--- treeherder/config/settings.py
+++ treeherder/config/settings.py
@@ -134,6 +134,9 @@ INSTALLED_APPS = [
 if ENABLE_DEBUG_TOOLBAR:
     INSTALLED_APPS.append('debug_toolbar')
 
+if DEBUG:
+    INSTALLED_APPS.append('django_extensions')
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error.
