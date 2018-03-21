2e8f09655f2f9c72965416f9cc1c6f1de67a5263
Ed Morley <501702+edmorley@users.noreply.github.com>
2018-03-21 16:43:23 +0000

    Bug 1451518 - Replace HTMLParser with six.moves.html_parser
    https://pythonhosted.org/six/#module-six.moves

--- treeherder/log_parser/parsers.py
+++ treeherder/log_parser/parsers.py
@@ -1,10 +1,10 @@
 import json
 import logging
 import re
-from HTMLParser import HTMLParser
 
 import jsonschema
 from django.conf import settings
+from six.moves.html_parser import HTMLParser
 
 from treeherder.etl.buildbot import RESULT_DICT
 
