ae310fa63ad70ea8dc282ef7c17c0ac335e71a64
George Hickman <george@ghickman.co.uk>
2018-04-27 09:12:32 +0100

    Build the Elasticsearch connection with optional auth vars

--- treeherder/services/elasticsearch/connection.py
+++ treeherder/services/elasticsearch/connection.py
@@ -1,5 +1,27 @@
+import os
+
 from django.conf import settings
 from elasticsearch import Elasticsearch
 
 url = settings.ELASTICSEARCH_URL
-es_conn = Elasticsearch(url) if url else None
+
+
+def build_connection(url):
+    """
+    Build an Elasticsearch connection with the given url
+
+    Elastic.co's Heroku addon doesn't create credientials with access to the
+    cluster by default so they aren't exposed in the URL they provide either.
+    This function works around the situation by grabbing our credentials from
+    the environment via Django settings and building a connection with them.
+    """
+    username = os.environ.get('ELASTICSEARCH_USERNAME')
+    password = os.environ.get('ELASTICSEARCH_PASSWORD')
+
+    if username and password:
+        return Elasticsearch(url, http_auth=(username, password))
+
+    return Elasticsearch(url)
+
+
+es_conn = build_connection(url) if url else None
