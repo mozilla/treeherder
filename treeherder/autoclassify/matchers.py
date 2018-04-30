2f8b49226fe7746fbc4157056ed827bb6e086805
George Hickman <george@ghickman.co.uk>
2018-04-30 15:26:22 +0100

    Only log/send event when we have results from Elasticsearch
    This should make the results graph in NewRelic more pertiant by showing
    the number of searches that returned results instead of the number of
    searches made.

--- treeherder/autoclassify/matchers.py
+++ treeherder/autoclassify/matchers.py
@@ -214,17 +214,18 @@ class ElasticSearchTestMatcher(Matcher):
                          failure_line.expected, failure_line.message)
             raise
 
-        args = (
-            text_log_error.id,
-            failure_line.id,
-            len(results),
-        )
-        logger.info('text_log_error=%i failure_line=%i Elasticsearch produced %i results' % args)
-        newrelic.agent.record_custom_event('es_matches', {
-            'num_results': len(results),
-            'text_log_error_id': text_log_error.id,
-            'failure_line_id': failure_line.id,
-        })
+        if len(results) > 1:
+            args = (
+                text_log_error.id,
+                failure_line.id,
+                len(results),
+            )
+            logger.info('text_log_error=%i failure_line=%i Elasticsearch produced %i results' % args)
+            newrelic.agent.record_custom_event('es_matches', {
+                'num_results': len(results),
+                'text_log_error_id': text_log_error.id,
+                'failure_line_id': failure_line.id,
+            })
 
         scorer = MatchScorer(failure_line.message)
         matches = [(item, item['message']) for item in results]
