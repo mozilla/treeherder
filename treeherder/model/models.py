9bac4f367cf371b807041056a2ca13b571ca4ce3
George Hickman <george@ghickman.co.uk>
2018-06-05 13:09:21 +0100

    Don't return classification from set_classification
    This is no longer necessary now we always pass in a classification.

--- treeherder/model/models.py
+++ treeherder/model/models.py
@@ -816,7 +816,7 @@ class JobNote(models.Model):
 
         for bug_number in add_bugs:
             classification, _ = ClassifiedFailure.objects.get_or_create(bug_number=bug_number)
-            classification, _ = text_log_error.set_classification("ManualDetector", classification)
+            text_log_error.set_classification("ManualDetector", classification)
 
         if len(add_bugs) == 1 and not existing_bugs:
             text_log_error.mark_best_classification_verified(classification)
@@ -1198,14 +1198,12 @@ class TextLogError(models.Model):
         if classification is None:
             classification = ClassifiedFailure.objects.create()
 
-        match = TextLogErrorMatch.objects.create(
+        TextLogErrorMatch.objects.create(
             text_log_error=self,
             classified_failure=classification,
             matcher_name=matcher_name,
             score=1,
         )
-
-        return classification, match
 
     def mark_best_classification(self, classification):
         """
