e72653cb69c94ab8c90b42d80973f6e32be41a3b
Eliott Vincent <oss@eliottvincent.com>
2018-06-07 19:38:31 +0200

    Bug 1467293 - add predicate method to filter()

--- ui/js/models/perf/series.js
+++ ui/js/models/perf/series.js
@@ -9,7 +9,7 @@ treeherder.factory('PhSeries', ['$http', '$q', function ($http, $q) {
     const _getTestName = function (signatureProps) {
         // only return suite name if testname is identical, and handle
         // undefined test name
-        return _.uniq([signatureProps.suite, signatureProps.test].filter()).join(" ");
+        return _.uniq([signatureProps.suite, signatureProps.test].filter(item => item)).join(" ");
     };
 
     const _getSeriesOptions = function (signatureProps, optionCollectionMap) {
