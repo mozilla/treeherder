mozilla/treeherder

commit e40793192110f53bb5ecb94cb81c81c0a0a986b2

Bug 1467394 - convert _.flatten() usages to ES6 (#3627)

ui/js/models/resultsets_store.js
@@ -148,7 +148,7 @@ treeherder.factory('ThResultSetStore', [
             lastPolltime = Date.now();
             jobUpdatesPromise
                 .then(function (jobList) {
-                    jobList = _.flatten(jobList);
+                    jobList = jobList.reduce((a, b) => [...a, ...b], []);
                     if (jobList.length > 0) {
                         lastJobUpdate = getLastModifiedJobTime(jobList);
                         var jobListByPush = Object.values(

ui/js/controllers/logviewer.js
@@ -1,5 +1,3 @@
-import _ from 'lodash';
-
 import logViewerApp from '../logviewer';
 import { getInspectTaskUrl, getReftestUrl } from "../../helpers/url";
 import { isReftest } from "../../helpers/job";
@@ -214,7 +212,7 @@ logViewerApp.controller('LogviewerCtrl', [
 
             TextLogStepModel.get($scope.job_id).then((textLogSteps) => {
                 let shouldPost = true;
-                const allErrors = _.flatten(textLogSteps.map(s => s.errors));
+                const allErrors = (textLogSteps.map(s => s.errors)).reduce((a, b) => [...a, ...b], []);
                 const q = $location.search();
                 $scope.steps = textLogSteps;
 

ui/js/controllers/perf/alerts.js
@@ -389,11 +389,11 @@ perf.controller('AlertsCtrl', [
         $scope.resetAlerts = function (alertSummary) {
             // We need to update not only the summary when resetting the alert,
             // but other summaries affected by the change
-            const summariesToUpdate = [alertSummary].concat(_.flatten(
+            const summariesToUpdate = [alertSummary].concat((
                 alertSummary.alerts.filter(alert => alert.selected).map(
                 alert => (_.find($scope.alertSummaries, alertSummary =>
                         alertSummary.id === alert.related_summary_id) || [])
-                )));
+                )).reduce((a, b) => [...a, ...b], []));
 
             alertSummary.modifySelectedAlerts({
                 status: phAlertStatusMap.UNTRIAGED.id,
@@ -478,14 +478,14 @@ perf.controller('AlertsCtrl', [
                         });
                     }
 
-                    summary.downstreamSummaryIds = _.uniq(_.flatten(
+                    summary.downstreamSummaryIds = _.uniq((
                         summary.alerts.map((alert) => {
                             if (alert.status === phAlertStatusMap.DOWNSTREAM.id &&
                                 alert.summary_id !== summary.id) {
                                 return alert.summary_id;
                             }
                             return [];
-                        })));
+                        })).reduce((a, b) => [...a, ...b], []));
 
                 });
 

ui/js/controllers/perf/graphs.js
@@ -1005,7 +1005,7 @@ perf.controller('TestChooserCtrl', ['$scope', '$uibModalInstance',
             })).then(function (seriesList) {
                 // we get a list of lists because we are getting the results
                 // of multiple promises, filter that down to one flat list
-                seriesList = _.flatten(seriesList);
+                seriesList = seriesList.reduce((a, b) => [...a, ...b], []);
 
                 // filter out tests which are already displayed
                 $scope.testsToAdd = seriesList.filter(series =>

ui/plugins/controller.js
@@ -267,13 +267,13 @@ treeherder.controller('PluginCtrl', [
                         $scope.reftestUrl = `${getReftestUrl($scope.job_log_urls[0].url)}&only_show_unexpected=1`;
                     }
 
-                    const performanceData = _.flatten(Object.values(results[3]));
+                    const performanceData = (Object.values(results[3])).reduce((a, b) => [...a, ...b], []);
                     if (performanceData) {
                         const signatureIds = _.uniq(_.map(performanceData, 'signature_id'));
                         $q.all(_.chunk(signatureIds, 20).map(
                             signatureIdChunk => PhSeries.getSeriesList($scope.repoName, { id: signatureIdChunk })
                         )).then((seriesListList) => {
-                            const seriesList = _.flatten(seriesListList);
+                            const seriesList = seriesListList.reduce((a, b) => [...a, ...b], []);
                             $scope.perfJobDetail = performanceData.map(d => ({
                                 series: seriesList.find(s => d.signature_id === s.id),
                                 ...d

commit 4845fbd2ef08d21aa94c88c72224682b66d11b2c

Fix job updating
Unfortunately I didn't notice that commit e40793192110f53bb5ecb94cb81c81c0a0a986b2 broke updating.

This was due to there being an unnecessary _.flatten which got converted to a reduce.  Just removing the
line fixes updating.

ui/js/models/resultsets_store.js
@@ -148,7 +148,6 @@ treeherder.factory('ThResultSetStore', [
             lastPolltime = Date.now();
             jobUpdatesPromise
                 .then(function (jobList) {
-                    jobList = jobList.reduce((a, b) => [...a, ...b], []);
                     if (jobList.length > 0) {
                         lastJobUpdate = getLastModifiedJobTime(jobList);
                         // group joblist by 'result_set_id'