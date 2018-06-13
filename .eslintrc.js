e4f933a0bb70a607f5775604cd92f6d8c3c58a58
Ed Morley <501702+edmorley@users.noreply.github.com>
2018-06-13 16:28:06 -0700

    Bug 1468650 - Use caching with ESLint & lint more files
    Enabling the ESLint caching reduces `yarn lint` time by 60%. See:
    https://eslint.org/docs/user-guide/command-line-interface#caching
    
    This also adds JS files in the repo root to the list of files that
    are linted, and fixes a subsequent lint error in `.eslintrc.js`.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const Neutrino = require('neutrino');
+
 const api = new Neutrino([path.resolve('./neutrino-custom/development.js')]);
 
 module.exports = api.custom.eslintrc();
