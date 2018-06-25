00db66c0b98daacc46ca994d29582e97f579e30d
Cameron Dawson <cdawson@mozilla.com>
2018-06-25 13:23:56 -0700

    Bug 1468623 - Fix resizing job details / pinboard panel

--- ui/job-view/JobView.jsx
+++ ui/job-view/JobView.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { react2angular } from 'react2angular/index.es2015';
+import SplitPane from 'react-split-pane';
 import { createBrowserHistory } from 'history';
 
 import treeherder from '../js/treeherder';
@@ -9,7 +10,36 @@ import DetailsPanel from './details/DetailsPanel';
 import ActiveFilters from './navbars/ActiveFilters';
 import PushList from './PushList';
 
+const DEFAULT_DETAILS_PCT = 40;
+
+const getWindowHeight = function () {
+  const windowHeight = window.innerHeight;
+  const navBar = document.getElementById('th-global-navbar');
+  const navBarHeight = navBar ? navBar.clientHeight : 0;
+
+  return windowHeight - navBarHeight;
+};
+
 class JobView extends React.Component {
+  static getDerivedStateFromProps(props) {
+    return JobView.getSplitterDimensions(props);
+  }
+
+
+  static getSplitterDimensions(props) {
+    const { selectedJob } = props;
+    const defaultPushListPct = selectedJob ? 100 - DEFAULT_DETAILS_PCT : 100;
+    // calculate the height of the details panel to use if it has not been
+    // resized by the user.
+    const defaultDetailsHeight = defaultPushListPct < 100 ?
+      DEFAULT_DETAILS_PCT / 100 * getWindowHeight() : 0;
+
+    return {
+      defaultPushListPct,
+      defaultDetailsHeight,
+    };
+  }
+
   constructor(props) {
     super(props);
 
@@ -25,11 +55,15 @@ class JobView extends React.Component {
        ...this.thJobFilters.getNonFieldFiltersArray(),
        ...this.thJobFilters.getFieldFiltersArray(),
       ],
+      pushListPct: props.selectedJob ? 100 - DEFAULT_DETAILS_PCT : 100,
     };
   }
 
   componentDidMount() {
     this.toggleFieldFilterVisible = this.toggleFieldFilterVisible.bind(this);
+    this.updateDimensions = this.updateDimensions.bind(this);
+
+    window.addEventListener('resize', this.updateDimensions);
 
     this.$rootScope.$on(thEvents.toggleFieldFilterVisible, () => {
       this.toggleFieldFilterVisible();
@@ -49,41 +83,71 @@ class JobView extends React.Component {
     this.setState({ isFieldFilterVisible: !this.state.isFieldFilterVisible });
   }
 
+  updateDimensions() {
+    this.setState(JobView.getSplitterDimensions(this.props));
+  }
+
+  handleSplitChange(latestSplitSize) {
+    this.setState({
+      latestSplitPct: latestSplitSize / getWindowHeight() * 100,
+    });
+  }
+
   render() {
     const {
       user, repoName, revision, currentRepo, selectedJob, $injector,
     } = this.props;
-    const { isFieldFilterVisible, filterBarFilters } = this.state;
+    const {
+      isFieldFilterVisible, filterBarFilters,
+      defaultPushListPct, defaultDetailsHeight, latestSplitPct,
+    } = this.state;
+    // SplitPane will adjust the CSS height of the top component, but not the
+    // bottom component.  So the scrollbars won't work in the DetailsPanel when
+    // we resize.  Therefore, we must calculate the new
+    // height of the DetailsPanel based on the current height of the PushList.
+    // Reported this upstream: https://github.com/tomkp/react-split-pane/issues/282
+    const pushListPct = latestSplitPct === undefined || !selectedJob ?
+      defaultPushListPct :
+      latestSplitPct;
+    const detailsHeight = latestSplitPct === undefined || !selectedJob ?
+      defaultDetailsHeight :
+      getWindowHeight() * (1 - latestSplitPct / 100);
 
     return (
-      <React.Fragment>
-        {(isFieldFilterVisible || !!filterBarFilters.length) && <ActiveFilters
-          $injector={$injector}
-          filterBarFilters={filterBarFilters}
-          history={this.history}
-          isFieldFilterVisible={isFieldFilterVisible}
-          toggleFieldFilterVisible={this.toggleFieldFilterVisible}
-        />}
-        <div id="th-global-content" className="th-global-content">
-          <span className="th-view-content">
-            <PushList
-              user={user}
-              repoName={repoName}
-              revision={revision}
-              currentRepo={currentRepo}
-              $injector={$injector}
-            />
-          </span>
+      <SplitPane
+        split="horizontal"
+        size={`${pushListPct}%`}
+        onDragFinished={size => this.handleSplitChange(size)}
+      >
+        <div className="d-flex flex-column w-100">
+          {(isFieldFilterVisible || !!filterBarFilters.length) && <ActiveFilters
+            $injector={$injector}
+            filterBarFilters={filterBarFilters}
+            history={this.history}
+            isFieldFilterVisible={isFieldFilterVisible}
+            toggleFieldFilterVisible={this.toggleFieldFilterVisible}
+          />}
+          <div id="th-global-content" className="th-global-content">
+            <span className="th-view-content">
+              <PushList
+                user={user}
+                repoName={repoName}
+                revision={revision}
+                currentRepo={currentRepo}
+                $injector={$injector}
+              />
+            </span>
+          </div>
         </div>
         <DetailsPanel
-          className={selectedJob ? '' : 'hidden'}
+          resizedHeight={detailsHeight}
           currentRepo={currentRepo}
           repoName={repoName}
           selectedJob={selectedJob}
           user={user}
           $injector={$injector}
         />
-      </React.Fragment>
+      </SplitPane>
     );
   }
 }
