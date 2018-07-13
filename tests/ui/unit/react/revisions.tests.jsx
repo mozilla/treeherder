3a53cb7e7b5079a49614e1ae12fbdb87ddbb71fd
Cameron Dawson <cdawson@mozilla.com>
2018-07-13 09:12:11 -0700

    Bug 1450038 - Linting cleanup in one of the tests

--- tests/ui/unit/react/revisions.tests.jsx
+++ tests/ui/unit/react/revisions.tests.jsx
@@ -4,7 +4,7 @@ import { mount } from 'enzyme';
 import { Revision, Initials } from '../../../../ui/job-view/Revision';
 import {
   RevisionList,
-  MoreRevisionsLink
+  MoreRevisionsLink,
 } from '../../../../ui/job-view/RevisionList';
 
 describe('Revision list component', () => {
@@ -15,52 +15,52 @@ describe('Revision list component', () => {
     const repo = {
       id: 2,
       repository_group: {
-        name: "development",
-        description: ""
+        name: 'development',
+        description: '',
       },
-      name: "mozilla-inbound",
-      dvcs_type: "hg",
-      url: "https://hg.mozilla.org/integration/mozilla-inbound",
+      name: 'mozilla-inbound',
+      dvcs_type: 'hg',
+      url: 'https://hg.mozilla.org/integration/mozilla-inbound',
       branch: null,
-      codebase: "gecko",
-      description: "",
-      active_status: "active",
+      codebase: 'gecko',
+      description: '',
+      active_status: 'active',
       performance_alerts_enabled: true,
-      pushlogURL: "https://hg.mozilla.org/integration/mozilla-inbound/pushloghtml"
+      pushlogURL: 'https://hg.mozilla.org/integration/mozilla-inbound/pushloghtml',
     };
     // Mock these simple functions so we don't have to call ThRepositoryModel.load() first to use them
     repo.getPushLogHref = revision => `${repo.pushlogURL}?changeset=${revision}`;
 
         const push = {
             id: 151371,
-            revision: "5a110ad242ead60e71d2186bae78b1fb766ad5ff",
+            revision: '5a110ad242ead60e71d2186bae78b1fb766ad5ff',
             revision_count: 3,
-            author: "ryanvm@gmail.com",
+            author: 'ryanvm@gmail.com',
             push_timestamp: 1481326280,
             repository_id: 2,
             revisions: [{
                 result_set_id: 151371,
                 repository_id: 2,
-                revision: "5a110ad242ead60e71d2186bae78b1fb766ad5ff",
-                author: "André Bargull <andre.bargull@gmail.com>",
-                comments: "Bug 1319926 - Part 2: Collect telemetry about deprecated String generics methods. r=jandem"
+                revision: '5a110ad242ead60e71d2186bae78b1fb766ad5ff',
+                author: 'André Bargull <andre.bargull@gmail.com>',
+                comments: 'Bug 1319926 - Part 2: Collect telemetry about deprecated String generics methods. r=jandem',
             }, {
                 result_set_id: 151371,
                 repository_id: 2,
-                revision: "07d6bf74b7a2552da91b5e2fce0fa0bc3b457394",
-                author: "André Bargull <andre.bargull@gmail.com>",
-                comments: "Bug 1319926 - Part 1: Warn when deprecated String generics methods are used. r=jandem"
+                revision: '07d6bf74b7a2552da91b5e2fce0fa0bc3b457394',
+                author: 'André Bargull <andre.bargull@gmail.com>',
+                comments: 'Bug 1319926 - Part 1: Warn when deprecated String generics methods are used. r=jandem',
             }, {
                 result_set_id: 151371,
                 repository_id: 2,
-                revision: "e83eaf2380c65400dc03c6f3615d4b2cef669af3",
-                author: "Frédéric Wang <fred.wang@free.fr>",
-                comments: "Bug 1322743 - Add STIX Two Math to the list of math fonts. r=karlt"
-            }]
+                revision: 'e83eaf2380c65400dc03c6f3615d4b2cef669af3',
+                author: 'Frédéric Wang <fred.wang@free.fr>',
+                comments: 'Bug 1322743 - Add STIX Two Math to the list of math fonts. r=karlt',
+            }],
         };
         mockData = {
             push,
-            repo
+            repo,
         };
     repo.getRevisionHref = () => `${repo.url}/rev/${push.revision}`;
   });
@@ -96,25 +96,25 @@ describe('Revision item component', () => {
     const repo = {
       id: 2,
       repository_group: {
-        name: "development",
-        description: ""
+        name: 'development',
+        description: '',
       },
-      name: "mozilla-inbound",
-      dvcs_type: "hg",
-      url: "https://hg.mozilla.org/integration/mozilla-inbound",
+      name: 'mozilla-inbound',
+      dvcs_type: 'hg',
+      url: 'https://hg.mozilla.org/integration/mozilla-inbound',
       branch: null,
-      codebase: "gecko",
-      description: "",
-      active_status: "active",
+      codebase: 'gecko',
+      description: '',
+      active_status: 'active',
       performance_alerts_enabled: true,
-      pushlogURL: "https://hg.mozilla.org/integration/mozilla-inbound/pushloghtml"
+      pushlogURL: 'https://hg.mozilla.org/integration/mozilla-inbound/pushloghtml',
     };
     const revision = {
       result_set_id: 151371,
       repository_id: 2,
-      revision: "5a110ad242ead60e71d2186bae78b1fb766ad5ff",
-      author: "André Bargull <andre.bargull@gmail.com>",
-      comments: "Bug 1319926 - Part 2: Collect telemetry about deprecated String generics methods. r=jandem"
+      revision: '5a110ad242ead60e71d2186bae78b1fb766ad5ff',
+      author: 'André Bargull <andre.bargull@gmail.com>',
+      comments: 'Bug 1319926 - Part 2: Collect telemetry about deprecated String generics methods. r=jandem',
     };
     // Mock these simple functions so we don't have to call ThRepositoryModel.load() first to use them
     repo.getRevisionHref = () => `${repo.url}/rev/${revision.revision}`;
@@ -122,7 +122,7 @@ describe('Revision item component', () => {
 
     mockData = {
       revision,
-      repo
+      repo,
     };
   });
 
@@ -137,7 +137,7 @@ describe('Revision item component', () => {
     expect(link.props().title).toEqual(`Open revision ${mockData.revision.revision} on ${mockData.repo.url}`);
   });
 
-  it(`renders the contributors' initials`, () => {
+  it('renders the contributors\' initials', () => {
     const wrapper = mount(
       <Revision
         repo={mockData.repo}
@@ -160,7 +160,7 @@ describe('Revision item component', () => {
   });
 
   it('marks the revision as backed out if the words "Back/Backed out" appear in the comments', () => {
-    mockData.revision.comments = "Backed out changeset a6e2d96c1274 (bug 1322565) for eslint failure";
+    mockData.revision.comments = 'Backed out changeset a6e2d96c1274 (bug 1322565) for eslint failure';
     let wrapper = mount(
       <Revision
         repo={mockData.repo}
@@ -168,7 +168,7 @@ describe('Revision item component', () => {
       />);
     expect(wrapper.find({ 'data-tags': 'backout' }).length).toEqual(1);
 
-    mockData.revision.comments = "Back out changeset a6e2d96c1274 (bug 1322565) for eslint failure";
+    mockData.revision.comments = 'Back out changeset a6e2d96c1274 (bug 1322565) for eslint failure';
     wrapper = mount(
       <Revision
         repo={mockData.repo}
@@ -193,29 +193,35 @@ describe('More revisions link component', () => {
 });
 
 describe('initials filter', function () {
-  const email = "foo@bar.baz";
+  const email = 'foo@bar.baz';
   it('initializes a one-word name', function () {
     const name = 'Starscream';
-    const initials = mount(<Initials title={`${name}: ${email}`}
-                                     author={name}
-    />);
+    const initials = mount(
+      <Initials
+        title={`${name}: ${email}`}
+        author={name}
+      />);
     expect(initials.html()).toEqual('<span title="Starscream: foo@bar.baz"><span class="user-push-icon"><i class="fa fa-user-o" aria-hidden="true" data-job-clear-on-click="true"></i></span><div class="icon-superscript user-push-initials" data-job-clear-on-click="true">S</div></span>');
   });
 
   it('initializes a two-word name', function () {
     const name = 'Optimus Prime';
-    const initials = mount(<Initials title={`${name}: ${email}`}
-                                     author={name}
-    />);
+    const initials = mount(
+      <Initials
+        title={`${name}: ${email}`}
+        author={name}
+      />);
     const userPushInitials = initials.find('.user-push-initials');
     expect(userPushInitials.html()).toEqual('<div class="icon-superscript user-push-initials" data-job-clear-on-click="true">OP</div>');
   });
 
   it('initializes a three-word name', function () {
     const name = 'Some Other Transformer';
-    const initials = mount(<Initials title={`${name}: ${email}`}
-                                     author={name}
-    />);
+    const initials = mount(
+      <Initials
+        title={`${name}: ${email}`}
+        author={name}
+      />);
     const userPushInitials = initials.find('.user-push-initials');
     expect(userPushInitials.html()).toEqual('<div class="icon-superscript user-push-initials" data-job-clear-on-click="true">ST</div>');
   });
