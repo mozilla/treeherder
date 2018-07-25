d2eee92b22e1e2ccd8cb0f70b7078ad3a9279d6b
Sarah Clements <sclements313@gmail.com>
2018-07-25 11:35:12 -0700

    Add summary to Intermittents Commenter log a summary with number of weekly or daily comments when on stage/dev

--- treeherder/intermittents_commenter/commenter.py
+++ treeherder/intermittents_commenter/commenter.py
@@ -169,6 +169,10 @@ class Commenter(object):
                 # sleep between comment submissions to avoid overwhelming servers
                 time.sleep(1)
 
+        if settings.COMMENTER_API_KEY is None:
+            # confirm number of comments when on stage/dev
+            logger.info('There were {} comments for this {} task.'.format(len(all_bug_changes), 'weekly' if self.weekly_mode else 'daily'))
+
     def open_file(self, filename, load):
         with open('treeherder/intermittents_commenter/{}'.format(filename), 'r') as myfile:
             if load:
