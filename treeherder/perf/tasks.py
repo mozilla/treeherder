ae0cc471d15009e10f120d63c3ecf9c62b32e1f9
Ed Morley <501702+edmorley@users.noreply.github.com>
2018-07-31 16:27:43 +0100

    Bug 1113235 - Make all non-periodic tasks automatically retry
    Most non-periodic (ie not schedule via celery beat) were already
    automatically retrying, apart from these two.

--- treeherder/perf/tasks.py
+++ treeherder/perf/tasks.py
@@ -1,11 +1,11 @@
 import newrelic.agent
-from celery import task
 
 from treeherder.perf.alerts import generate_new_alerts_in_series
 from treeherder.perf.models import PerformanceSignature
+from treeherder.workers.task import retryable_task
 
 
-@task(name='generate-alerts')
+@retryable_task(name='generate-alerts', max_retries=10)
 def generate_alerts(signature_id):
     newrelic.agent.add_custom_parameter("signature_id", str(signature_id))
     signature = PerformanceSignature.objects.get(id=signature_id)
