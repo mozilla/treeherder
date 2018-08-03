bd5d167100915e4bf79e094ba50bea5111e1b85e
George Hickman <george@ghickman.co.uk>
2018-08-03 09:29:47 +0100

    Bug 1281821 - Split PULSE_DATA_INGESTION_SOURCES into three separate settings

--- treeherder/services/pulse/sources.py
+++ treeherder/services/pulse/sources.py
@@ -3,6 +3,25 @@ import environ
 env = environ.Env()
 
 
+exchanges = env.list("PULSE_JOB_EXCHANGES", default=[
+    "exchange/taskcluster-treeherder/v1/jobs",
+    # "exchange/fxtesteng/jobs",
+    # ... other CI systems
+])
+projects = env.list("PULSE_JOB_PROJECTS", default=[
+    "#",
+    # some specific repos TC can ingest from
+    # "mozilla-central.#",
+    # "mozilla-inbound.#",
+])
+destinations = env.list("PULSE_JOB_DESTINATIONS", default=[
+    "#",
+    # "production",
+    # "staging",
+    # "tc-treeherder",
+])
+
+
 def get_job_sources():
     """
     Get Job ingestion source locations.
@@ -14,28 +33,12 @@ def get_job_sources():
     <destination>.<project> Wildcards such as ``#`` and ``*`` are supported for
     either field.
     """
-    sources = env.json(
-        "PULSE_DATA_INGESTION_SOURCES",
-        default=[
-            {
-                "exchange": "exchange/taskcluster-treeherder/v1/jobs",
-                "projects": [
-                    '#'
-                    # some specific repos TC can ingest from
-                    # 'mozilla-central.#',
-                    # 'mozilla-inbound.#'
-                ],
-                "destinations": [
-                    '#'
-                    # 'production',
-                    # 'staging'
-                ],
-            },
-            # ... other CI systems
-        ],
-    )
-
-    return sources
+    for exchange in exchanges:
+        yield {
+            "exchange": exchange,
+            "projects": projects,
+            "destinations": destinations,
+        }
 
 
 def get_push_sources():
@@ -62,5 +65,5 @@ def get_push_sources():
     return sources
 
 
-job_sources = get_job_sources()
+job_sources = list(get_job_sources())
 push_sources = get_push_sources()
