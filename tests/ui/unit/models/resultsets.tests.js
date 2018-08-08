fd8e455241b33539e69d522c257a83fb0369a374
Cameron Dawson <cdawson@mozilla.com>
2018-08-08 12:59:53 -0700

    Fix resultset tests fetching
    Some models changed to use ``fetch`` so we needed to use ``fetchMock`` instead of
    ``$httpBackend`` for setting expected results.

--- tests/ui/unit/models/resultsets.tests.js
+++ tests/ui/unit/models/resultsets.tests.js
@@ -1,3 +1,4 @@
+import * as fetchMock from 'fetch-mock';
 
 import { getProjectUrl } from '../../../../ui/helpers/url';
 
@@ -15,50 +16,36 @@ describe('ThResultSetStore', function () {
                                 ThResultSetStore, ThRepositoryModel) {
 
         $httpBackend = $injector.get('$httpBackend');
-        jasmine.getJSONFixtures().fixturesPath='base/tests/ui/mock';
+        jasmine.getJSONFixtures().fixturesPath = 'base/tests/ui/mock';
 
-        $httpBackend.whenGET('https://treestatus.mozilla-releng.net/trees/mozilla-inbound').respond(
-            {
-                "result": {
-                    "status": "approval required",
-                    "message_of_the_day": "I before E",
-                    "tree": "mozilla-inbound",
-                    "reason": ""
-                }
-            }
-
-        );
-
-        $httpBackend.whenGET(getProjectUrl('/jobs/0/unclassified_failure_count/', foregroundRepo)).respond(
-            {
-                "unclassified_failure_count": 1152,
-                "repository": "mozilla-inbound"
-            }
+        fetchMock.get(
+          'https://treestatus.mozilla-releng.net/trees/mozilla-inbound',
+          {
+              result: {
+                  status: 'approval required',
+                  message_of_the_day: 'I before E',
+                  tree: 'mozilla-inbound',
+                  reason: '',
+              },
+          },
         );
 
         $httpBackend.whenGET(getProjectUrl('/resultset/?count=10&full=true', foregroundRepo)).respond(
             getJSONFixture('push_list.json'),
         );
 
-
-        $httpBackend.whenGET(getProjectUrl('/jobs/?count=2000&result_set_id=1&return_type=list', foregroundRepo)).respond(
-            getJSONFixture('job_list/job_1.json')
+        fetchMock.get(
+          getProjectUrl('/jobs/?return_type=list&result_set_id=1&count=2000', foregroundRepo),
+          getJSONFixture('job_list/job_1.json'),
         );
 
-        $httpBackend.whenGET(getProjectUrl('/jobs/?count=2000&result_set_id=2&return_type=list', foregroundRepo)).respond(
-            getJSONFixture('job_list/job_2.json')
+        fetchMock.get(
+          getProjectUrl('/jobs/?return_type=list&result_set_id=2&count=2000', foregroundRepo),
+          getJSONFixture('job_list/job_2.json'),
         );
 
         $httpBackend.whenGET('/api/repository/').respond(
-            getJSONFixture('repositories.json')
-        );
-
-        $httpBackend.whenGET('/api/jobtype/').respond(
-            getJSONFixture('job_type_list.json')
-        );
-
-        $httpBackend.whenGET('/api/jobgroup/').respond(
-            getJSONFixture('job_group_list.json')
+          getJSONFixture('repositories.json'),
         );
 
         rootScope = $rootScope.$new();
@@ -73,6 +60,10 @@ describe('ThResultSetStore', function () {
 
         $httpBackend.flush();
     }));
+
+    afterEach(() => {
+      fetchMock.restore();
+    });
 
     /*
         Tests ThResultSetStore
