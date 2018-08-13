7c3ab3a5394194fe82581f3d488d715a5dd3d058
Sarah Clements <sclements313@gmail.com>
2018-08-13 12:31:44 -0700

    Bug 1482833 - fix commenter celery task Change intermittents-commenter task soft and hard time limints

--- treeherder/intermittents_commenter/tasks.py
+++ treeherder/intermittents_commenter/tasks.py
@@ -3,7 +3,7 @@ from celery import task
 from treeherder.intermittents_commenter.commenter import Commenter
 
 
-@task(name='intermittents-commenter', soft_time_limit=30 * 60, time_limit=31 * 60)
+@task(name='intermittents-commenter', soft_time_limit=75 * 60, time_limit=76 * 60)
 def run_commenter(weekly_mode=False):
     """Run the intermittents commenter in either daily or weekly mode."""
     process = Commenter(weekly_mode=weekly_mode)
