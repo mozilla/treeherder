bcb0d5e9a498ffa0b180a77c3ea4d841a75d7c12
George Hickman <george@ghickman.co.uk>
2018-08-14 16:04:01 +0100

    Set a default for the Pulse connection if PULSE_URL isn't set

--- treeherder/services/pulse/connection.py
+++ treeherder/services/pulse/connection.py
@@ -7,7 +7,7 @@ env = environ.Env()
 # ingestion queues for the exchanges specified in ``PULSE_SOURCE_EXCHANGES``.
 # See https://pulse.mozilla.org/whats_pulse for more info.
 # Example: "amqp://myuserid:mypassword@pulse.mozilla.org:5672/?ssl=1"
-config = env.url("PULSE_URL")
+config = env.url("PULSE_URL", default="amqp://guest:guest@localhost//")
 
 
 def build_connection(url):
