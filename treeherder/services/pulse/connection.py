96db2547cc8a8f78921980f277ac60cb87dc49fe
George Hickman <ghickman@users.noreply.github.com>
2018-08-15 19:06:38 +0100

    Default for Pulse Connection (#3921)
    Make Pulse Connection Lazy

--- treeherder/services/pulse/connection.py
+++ treeherder/services/pulse/connection.py
@@ -1,18 +1,19 @@
 import environ
+from django.utils.functional import SimpleLazyObject
 from kombu import Connection
 
 env = environ.Env()
 
-# Used to specify the PulseGuardian account that will be used to create
-# ingestion queues for the exchanges specified in ``PULSE_SOURCE_EXCHANGES``.
-# See https://pulse.mozilla.org/whats_pulse for more info.
-# Example: "amqp://myuserid:mypassword@pulse.mozilla.org:5672/?ssl=1"
-config = env.url("PULSE_URL")
+
+def build_connection():
+    """Build a Kombu Broker connection to Mozilla Pulse."""
+    # Used to specify the PulseGuardian account that will be used to create
+    # ingestion queues for the exchanges specified in ``PULSE_SOURCE_EXCHANGES``.
+    # See https://pulse.mozilla.org/whats_pulse for more info.
+    # Example: "amqp://myuserid:mypassword@pulse.mozilla.org:5672/?ssl=1"
+    pulse_url = env.url("PULSE_URL")
+
+    return Connection(pulse_url.get_url())
 
 
-def build_connection(url):
-    """Build a Kombu Broker connection to Mozilla Pulse with the given url."""
-    return Connection(url)
-
-
-pulse_conn = build_connection(config.geturl())
+pulse_conn = SimpleLazyObject(build_connection)
