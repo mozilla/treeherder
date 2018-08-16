41712aea6da455b1616300ce6f9cded6e703c32f
Ed Morley <501702+edmorley@users.noreply.github.com>
2018-08-16 17:19:41 +0100

    Revert "Default for Pulse Connection (#3921)"
    This reverts commit 96db2547cc8a8f78921980f277ac60cb87dc49fe, to
    try and fix Pulse ingestion on `master`.

--- treeherder/services/pulse/connection.py
+++ treeherder/services/pulse/connection.py
@@ -1,19 +1,18 @@
 import environ
-from django.utils.functional import SimpleLazyObject
 from kombu import Connection
 
 env = environ.Env()
 
-
-def build_connection():
-    """Build a Kombu Broker connection to Mozilla Pulse."""
-    # Used to specify the PulseGuardian account that will be used to create
-    # ingestion queues for the exchanges specified in ``PULSE_SOURCE_EXCHANGES``.
-    # See https://pulse.mozilla.org/whats_pulse for more info.
-    # Example: "amqp://myuserid:mypassword@pulse.mozilla.org:5672/?ssl=1"
-    pulse_url = env.url("PULSE_URL")
-
-    return Connection(pulse_url.get_url())
+# Used to specify the PulseGuardian account that will be used to create
+# ingestion queues for the exchanges specified in ``PULSE_SOURCE_EXCHANGES``.
+# See https://pulse.mozilla.org/whats_pulse for more info.
+# Example: "amqp://myuserid:mypassword@pulse.mozilla.org:5672/?ssl=1"
+config = env.url("PULSE_URL")
 
 
-pulse_conn = SimpleLazyObject(build_connection)
+def build_connection(url):
+    """Build a Kombu Broker connection to Mozilla Pulse with the given url."""
+    return Connection(url)
+
+
+pulse_conn = build_connection(config.geturl())
