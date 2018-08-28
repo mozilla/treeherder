e26db77ae91f884a19cfc533eaa5641ed4e8c98e
Cameron Dawson <cdawson@mozilla.com>
2018-08-28 09:43:11 -0700

    Bug 1486176 - Fix app not showing on Treestatus error

--- ui/job-view/headerbars/SecondaryNavBar.jsx
+++ ui/job-view/headerbars/SecondaryNavBar.jsx
@@ -6,6 +6,7 @@ import { getBtnClass } from '../../helpers/job';
 import { getRepo, getUrlParam } from '../../helpers/location';
 import WatchedRepo from './WatchedRepo';
 import RepositoryModel from '../../models/repository';
+import WatchedRepoErrorBoundary from './WatchedRepoErrorBoundary';
 
 const MAX_WATCHED_REPOS = 3;
 const WATCHED_REPOS_STORAGE_KEY = 'thWatchedRepos';
@@ -220,14 +221,16 @@ export default class SecondaryNavBar extends React.Component {
         <span className="justify-content-between w-100 d-flex flex-wrap">
           <span className="d-flex push-left watched-repos">
             {watchedRepos.map(watchedRepo => (
-              <WatchedRepo
-                key={watchedRepo.name}
-                repo={watchedRepo}
-                repoName={repoName}
-                $injector={$injector}
-                unwatchRepo={this.unwatchRepo}
-                setCurrentRepoTreeStatus={setCurrentRepoTreeStatus}
-              />
+              <WatchedRepoErrorBoundary key={watchedRepo.name} repoName={watchedRepo.name}>
+                <WatchedRepo
+                  key={watchedRepo.name}
+                  repo={watchedRepo}
+                  repoName={repoName}
+                  $injector={$injector}
+                  unwatchRepo={this.unwatchRepo}
+                  setCurrentRepoTreeStatus={setCurrentRepoTreeStatus}
+                />
+              </WatchedRepoErrorBoundary>
             ))}
           </span>
           <form role="search" className="form-inline flex-row">
