1373569aa3e3b7d1b3c10e73691ba351ca50a59c
Cameron Dawson <cdawson@mozilla.com>
2018-08-28 15:35:13 -0700

    Add generic ErrorBoundary

---
+++ ui/shared/ErrorBoundary.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false,
+      error: null,
+    };
+  }
+
+  componentDidCatch(error) {
+    this.setState({
+      hasError: true,
+      error,
+    });
+  }
+
+  render() {
+    const { children, errorClasses, message } = this.props;
+    const { hasError, error } = this.state;
+
+    if (hasError) {
+      return <span className={errorClasses}>{message} {error.toString()}</span>;
+    }
+    return children;
+  }
+}
+
+ErrorBoundary.propTypes = {
+  children: PropTypes.object.isRequired,
+  errorClasses: PropTypes.string,
+  message: PropTypes.string,
+};
+
+ErrorBoundary.defaultProps = {
+  errorClasses: '',
+  message: '',
+};
