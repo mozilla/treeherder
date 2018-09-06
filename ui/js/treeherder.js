e326128a9e004f6fefb303ae03cfa16541ee7e48
Ed Morley <501702+edmorley@users.noreply.github.com>
2018-09-06 21:23:46 +0100

    Bug 1489284 - Remove unused angular-resource
    The last usage of it was removed in bug 1450039. See:
    https://docs.angularjs.org/api/ngResource
    
    Since the DI arguments were already being updated, I've also removed
    the configuration of `$logProvider`, since we no longer use `$log`.

--- ui/js/treeherder.js
+++ ui/js/treeherder.js
@@ -1,8 +1,6 @@
 import angular from 'angular';
-import ngResource from 'angular-resource';
 import ngSanitize from 'angular-sanitize';
 
 export default angular.module('treeherder', [
-  ngResource,
   ngSanitize,
 ]);
