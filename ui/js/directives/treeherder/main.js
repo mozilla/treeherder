4c9dba47283f7bb7fe3e145c065a0d217c8e818b
Cameron Dawson <cdawson@mozilla.com>
2018-09-14 16:01:21 -0700

    Dead Angular directive code removal

--- ui/js/directives/treeherder/main.js
+++ ui/js/directives/treeherder/main.js
@@ -1,49 +1,6 @@
 import treeherder from '../../treeherder';
 import thNotificationsBoxTemplate from '../../../partials/main/thNotificationsBox.html';
 
-// Allow copy to system clipboard during hover
-treeherder.directive('copyValue', [
-    function () {
-        return {
-            restrict: 'A',
-            link: function (scope, element, attrs) {
-                const cont = document.getElementById('clipboard-container');
-                const clip = document.getElementById('clipboard');
-                element.on('mouseenter', function () {
-                    cont.style.display = 'block';
-                    clip.value = attrs.copyValue;
-                    clip.focus();
-                    clip.select();
-                });
-                element.on('mouseleave', function () {
-                    cont.style.display = 'none';
-                    clip.value = '';
-                });
-            },
-        };
-    },
-]);
-
-// Prevent default behavior on left mouse click. Useful
-// for html anchor's that need to do in application actions
-// on left click but default href type functionality on
-// middle or right mouse click.
-treeherder.directive('preventDefaultOnLeftClick', [
-    function () {
-        return {
-            restrict: 'A',
-            link: function (scope, element) {
-                element.on('click', function (event) {
-                    if (event.which === 1) {
-                        event.preventDefault();
-                    }
-                    element.blur();
-                });
-            },
-        };
-    },
-]);
-
 treeherder.directive('stopPropagationOnLeftClick', [
     function () {
         return {
@@ -71,20 +28,3 @@ treeherder.directive('thNotificationBox', [
             },
         };
     }]);
-
-treeherder.directive('bugInput', function () {
-    return {
-        restrict: 'A',
-        link: function (scope, elem) {
-            elem.on('invalid', function (event) {
-                event.target.setCustomValidity('Please enter a bug number');
-            });
-            elem.on('input', function (event) {
-                event.target.setCustomValidity('');
-                event.target.value = event.target.value.trim();
-            });
-            elem.attr('type', 'text');
-            elem.attr('pattern', '\\s*\\d+\\s*');
-        },
-    };
-});
