c1ceb871fd7e9e73348fc66eb1aa91f61c5c8137
Cameron Dawson <cdawson@mozilla.com>
2018-09-17 10:56:43 -0700

    Bug 1491532 - Fix clicking next-N with tochange param

--- ui/job-view/pushes/PushList.jsx
+++ ui/job-view/pushes/PushList.jsx
@@ -121,18 +121,27 @@ export default class PushList extends React.Component {
   }
 
   getNextPushes(count) {
-    const revision = getUrlParam('revision');
+    const params = getAllUrlParams();
+
     this.setState({ loadingPushes: true });
-    if (revision) {
+    if (params.has('revision')) {
+      // We are viewing a single revision, but the user has asked for more.
+      // So we must replace the ``revision`` param with ``tochange``, which
+      // will make it just the top of the range.  We will also then get a new
+      // ``fromchange`` param after the fetch.
       this.skipNextPageReload = true;
-      const params = getAllUrlParams();
+      const revision = params.get('revision');
       params.delete('revision');
       params.set('tochange', revision);
       setLocation(params);
+    } else if (params.has('startdate')) {
+      // We are fetching more pushes, so we don't want to limit ourselves by
+      // ``startdate``.  And after the fetch, ``startdate`` will be invalid,
+      // and will be replaced on the location bar by ``fromchange``.
+      this.skipNextPageReload = true;
+      setUrlParam('startdate', null);
     }
-    this.ThResultSetStore.fetchPushes(count)
-      .then(this.updateUrlFromchange);
-
+    this.ThResultSetStore.fetchPushes(count).then(this.updateUrlFromchange);
   }
 
   /**
