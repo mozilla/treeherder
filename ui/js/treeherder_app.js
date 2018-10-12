f02eb044ca990cb80e8f23a5766d27dc741c8f6f
Cameron Dawson <cdawson@mozilla.com>
2018-10-12 14:03:14 -0700

    Remove dependency of the unused Angular httpProvider

--- ui/js/treeherder_app.js
+++ ui/js/treeherder_app.js
@@ -8,17 +8,13 @@ const treeherderApp = angular.module('treeherder.app', [
   uiBootstrap,
 ]);
 
-treeherderApp.config(['$compileProvider', '$httpProvider',
-    function ($compileProvider, $httpProvider) {
+treeherderApp.config(['$compileProvider',
+    function ($compileProvider) {
         // Disable debug data & legacy comment/class directive syntax, as recommended by:
         // https://docs.angularjs.org/guide/production
         $compileProvider.debugInfoEnabled(false);
         $compileProvider.commentDirectivesEnabled(false);
         $compileProvider.cssClassDirectivesEnabled(false);
-
-        $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
-        $httpProvider.defaults.xsrfCookieName = 'csrftoken';
-        $httpProvider.useApplyAsync(true);
     }]);
 
 export default treeherderApp;
