ab89d70a61f8edadf733297e2708fecc9d6e6ccb
florin.strugariu <bebe@mozilla.ro>
2018-10-26 13:59:31 +0300

    Bug 1470473 Add more basic perfherder selenium tests

--- tests/selenium/test_perfherder.py
+++ tests/selenium/test_perfherder.py
@@ -6,3 +6,46 @@ def test_add_test_data(base_url, selenium):
     page = Perfherder(selenium, base_url).open()
     page.add_test_data()
     # FIXME: Add more coverage.
+
+
+def test_load_test_data(base_url, selenium, test_perf_data):
+    """
+        Test that user is able to select test data from the "test chooser" and
+        the correct test data si displayed
+    """
+
+    test_data = test_perf_data.first()
+
+    perf_page = Perfherder(selenium, base_url).open()
+    select_test_modal = perf_page.add_test_data()
+
+    select_test_modal.select_test(test_data)
+
+    test_signatures = perf_page.series_list()
+
+    assert len(test_signatures) == 1
+    assert test_signatures[0].test_name_text == "%s %s %s %s" % (test_data.signature.suite,
+                                                                 test_data.signature.test,
+                                                                 test_data.signature.extra_options.split(' ')[1],
+                                                                 test_data.signature.extra_options.split(' ')[0])
+
+    assert test_signatures[0].project_name_text == test_data.repository.name
+    assert test_signatures[0].platform_text == test_data.signature.platform.platform
+    assert test_signatures[0].signature_text == test_data.signature.signature_hash
+
+
+def test_verify_graph_tool_tip(base_url, selenium, test_perf_data):
+    """Test graph tooltip information is according to test data"""
+
+    test_data = test_perf_data.first()
+    perf_page = Perfherder(selenium, base_url)
+    perf_page.driver.get("%s/perf.html#/graphs?timerange=31536000"
+                         "&series=test_treeherder_jobs,1,1,1"
+                         "&selected=test_treeherder_jobs,1,1,1,1" % base_url)
+
+    tool_tip = perf_page.tool_tip
+    tool_tip.wait_for_region_to_load()
+
+    assert tool_tip.is_tooltip_visible
+    assert tool_tip.series_text == "%s (%s)" % (test_data.signature.test, test_data.repository.name)
+    assert tool_tip.platform_text == test_data.signature.platform.platform
