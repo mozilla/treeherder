085195b3da7d21541a3631b1be51204f012d6da7
Sarah Clements <sclements313@gmail.com>
2018-11-27 12:25:41 -0800

    more changes

--- treeherder/intermittents_commenter/management/commands/run_intermittents_commenter.py
+++ treeherder/intermittents_commenter/management/commands/run_intermittents_commenter.py
@@ -11,17 +11,13 @@ class Command(BaseCommand):
        default is daily and non-test mode, use flags for weekly, auto and test mode."""
 
     def add_arguments(self, parser):
-        parser.add_argument('-m', '--mode', dest='weekly_mode', nargs='?', choices=['weekly', 'auto'], default=False, help='generate comment summaries based on auto or weekly mode')
+        parser.add_argument('-m', '--mode', dest='mode', nargs='?', choices=['weekly', 'auto'], default=False, help='generate comment summaries based on auto or weekly mode; defaults to daily')
         parser.add_argument('--dry-run', action='store_true', dest='dry_run', help='output comments to stdout rather than submitting to Bugzilla')
 
     def handle(self, *args, **options):
-        mode = options['weekly_mode']
-        weekly_mode = mode
-
-        if mode == 'weekly':
-            weekly_mode = True
-        elif mode == 'auto':
-            weekly_mode = calendar.day_name[date.today().weekday()] == 'Monday'
+        mode = options['mode']
+        is_monday = calendar.day_name[date.today().weekday()] == 'Monday'
+        weekly_mode = (mode == 'weekly') or (mode == 'auto' and is_monday)
 
         process = Commenter(weekly_mode=weekly_mode, dry_run=options['dry_run'])
         process.run()
