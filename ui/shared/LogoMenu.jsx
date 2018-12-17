b970cd094cfb518c602745f1e25b0ca187338b59
Wes Kocher <kwierso@gmail.com>
2018-12-16 23:11:22 -0800

    Bug 1514668 - Convert LogoMenu.jsx to Reactstrap dropdown and use it in IFV navbar

--- ui/shared/LogoMenu.jsx
+++ ui/shared/LogoMenu.jsx
@@ -1,39 +1,64 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {
+  ButtonDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from 'reactstrap';
 
 const choices = [
   { url: '/', text: 'Treeherder' },
   { url: '/perf.html', text: 'Perfherder' },
-  { url: '/intermittent-failures.html', text: 'Intermittent Failures' },
+  { url: '/intermittent-failures.html', text: 'Intermittent Failures View' },
 ];
 
-export default function LogoMenu(props) {
-  const { menuText, menuImage } = props;
+export default class LogoMenu extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const menuChoices = choices.filter(choice => choice.text !== menuText);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      dropdownOpen: false,
+    };
+  }
 
-  return (
-    <span className="dropdown">
-      <button
-        id="th-logo"
-        title="Treeherder services"
-        data-toggle="dropdown"
-        className="btn btn-view-nav dropdown-toggle"
-        type="button"
-      >
-        <img src={menuImage} alt={menuText} />
-      </button>
-      <ul className="dropdown-menu" role="menu" aria-labelledby="th-logo">
-        {menuChoices.map(choice => (
-          <li key={choice.text}>
-            <a href={choice.url} className="dropdown-item">
-              {choice.text}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </span>
-  );
+  toggle() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen,
+    });
+  }
+
+  render() {
+    const { menuText, menuImage } = this.props;
+
+    const menuChoices = choices.filter(choice => choice.text !== menuText);
+    return (
+      <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+        <DropdownToggle
+          className="btn-view-nav"
+          id="th-logo"
+          caret
+          title="Treeherder services"
+        >
+          {menuImage ? (
+            <img src={menuImage} alt={menuText} />
+          ) : (
+            <span className="lightorange">{menuText}</span>
+          )}
+        </DropdownToggle>
+        <DropdownMenu>
+          {menuChoices.map(choice => (
+            <DropdownItem key={choice.text}>
+              <a href={choice.url} className="dropdown-item">
+                {choice.text}
+              </a>
+            </DropdownItem>
+          ))}
+        </DropdownMenu>
+      </ButtonDropdown>
+    );
+  }
 }
 
 LogoMenu.propTypes = {
