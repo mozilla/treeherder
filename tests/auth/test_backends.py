ddceb0e395f4af979f9d8fa3b19645cfbd386547
Hassan Ali <helfi92@gmail.com>
2019-01-09 10:25:27 -0500

    Add test

--- tests/auth/test_backends.py
+++ tests/auth/test_backends.py
@@ -16,6 +16,7 @@ SessionStore = import_module(settings.SESSION_ENGINE).SessionStore
     ('user_info', 'exp_username', 'exp_exception'),
     [({'sub': 'email', 'email': 'biped@mozilla.com'}, 'email/biped@mozilla.com', False),  # email clientId
      ({'sub': 'ad|Mozilla-LDAP|biped', 'email': 'biped@mozilla.com'}, 'mozilla-ldap/biped@mozilla.com', False),  # ldap clientId
+     ({'sub': 'oauth2|biped', 'email': 'biped@mozilla.com'}, 'oauth2/biped@mozilla.com', False),  # FirefoxAccount clientId
      ({'sub': 'github|0000', 'email': 'biped@gmail.com'}, 'github/biped@gmail.com', False),  # github clientId
      ({'sub': 'google-oauth2|0000', 'email': 'biped@mozilla.com'}, 'google/biped@mozilla.com', False),  # google clientId
      ({'sub': 'meh', 'email': 'biped@mozilla.com'}, 'None', True),  # invalid clientId, exception
