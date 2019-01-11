2cb0cb54cabbf9e565bacce9c98c2d20a2ef3e56
ionutgoldan <gold.ionut@gmail.com>
2019-01-11 15:23:17 +0200

    Bug 1519323 - Hook up prop params

--- ui/perfherder/RevisionInformation.jsx
+++ ui/perfherder/RevisionInformation.jsx
@@ -21,70 +21,63 @@ export default class RevisionInformation extends React.Component {
       selectedTimeRange,
     } = this.props;
 
-    console.log(`selectedTimeRange: ${JSON.stringify(selectedTimeRange)}`);
-
     return (
-      // <ul className="list-inline push-information">
-      //   {originalRevision && (
-      //     <li className="list-inline-item">
-      //       <strong>Base</strong> -
-      //       <a href="{originalRevision}">{originalRevision}</a>
-      //       `(${originalProject.name}) - ${originalResultSet.author}` -
-      //       <span>{originalResultSet.comments}</span>
-      //     </li>
-      //   )}
-      //   {selectedTimeRange && (
-      //     <li className="list-inline-item">
-      //       <strong>Base</strong> -
-      //       <a href="{originalRevision}">{originalRevision}</a>
-      //       `(${originalProject.name}) - ${selectedTimeRange.text}``
-      //     </li>
-      //   )}
-      //   <li className="list-inline-item">
-      //     <strong>New</strong> -
-      //     <a href="{{newRevision | getRevisionUrl:newProject.name}}">
-      //       {newRevision}
-      //     </a>
-      //     `(${newProject.name}) - ${newResultSet.author}`
-      //     <span>{newResultSet.comments}</span>
-      //   </li>
-      // </ul>
-      <ul>
-        <li>originalProject: {JSON.stringify(originalProject)}</li>
-        <li>originalRevision: {JSON.stringify(originalRevision)}</li>
-        <li>originalResultSet: {JSON.stringify(originalResultSet)}</li>
-        <li>newProject: {JSON.stringify(newProject)}</li>
-        <li>newRevision: {JSON.stringify(newRevision)}</li>
-        <li>newResultSet: {JSON.stringify(newResultSet)}</li>
-        <li>selectedTimeRange: {JSON.stringify(selectedTimeRange)}</li>
-      </ul>
+      <div>
+        <ul className="list-inline push-information">
+          {originalRevision && (
+            <li className="list-inline-item">
+              <strong>Base</strong> -&nbsp;
+              <a href="{originalRevision}">{originalRevision}</a>
+              &nbsp;({originalProject.name}) - {originalResultSet.author} -
+              <span>{originalResultSet.comments}</span>
+            </li>
+          )}
+          {selectedTimeRange && (
+            <li className="list-inline-item">
+              <strong>Base</strong> -
+              <a href="{originalRevision}">{originalRevision}</a>
+              &nbsp;({originalProject.name}) - {selectedTimeRange.text}
+            </li>
+          )}
+          <li className="list-inline-item">
+            <strong>New</strong> -&nbsp;
+            <a href="{{newRevision | getRevisionUrl:newProject.name}}">
+              {newRevision}
+            </a>
+            &nbsp;({newProject.name}) - {newResultSet.author}
+            <span>{newResultSet.comments}</span>
+          </li>
+        </ul>
+        <ul>
+          <li>originalProject: {JSON.stringify(originalProject)}</li>
+          <li>originalRevision: {JSON.stringify(originalRevision)}</li>
+          <li>originalResultSet: {JSON.stringify(originalResultSet)}</li>
+          <li>newProject: {JSON.stringify(newProject)}</li>
+          <li>newRevision: {JSON.stringify(newRevision)}</li>
+          <li>newResultSet: {JSON.stringify(newResultSet)}</li>
+          <li>selectedTimeRange: {JSON.stringify(selectedTimeRange)}</li>
+        </ul>
+      </div>
     );
   }
 }
 
 RevisionInformation.propTypes = {
-  originalProject: PropTypes.object.isRequired,
+  originalProject: PropTypes.object,
   originalRevision: PropTypes.string.isRequired,
-  newProject: PropTypes.object.isRequired,
+  newProject: PropTypes.object,
   newRevision: PropTypes.string.isRequired,
-  originalResultSet: PropTypes.object.isRequired,
-  newResultSet: PropTypes.object.isRequired,
+  originalResultSet: PropTypes.object,
+  newResultSet: PropTypes.object,
   selectedTimeRange: PropTypes.number,
 };
 
 RevisionInformation.defaultProps = {
+  originalProject: {},
+  newProject: {},
+  originalResultSet: {},
+  newResultSet: {},
   selectedTimeRange: undefined,
 };
 
-perf.component(
-  'revisionInformation',
-  react2angular(RevisionInformation, [
-    'originalProject',
-    'originalRevision',
-    'newProject',
-    'newRevision',
-    'originalResultSet',
-    'newResultSet',
-    'selectedTimeRange',
-  ]),
-);
+perf.component('revisionInformation', react2angular(RevisionInformation));
