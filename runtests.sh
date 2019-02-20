1f3fac9db5b36ff9f154ae939634f20365301e85
Ed Morley <501702+edmorley@users.noreply.github.com>
2019-02-20 10:30:56 +0000

    Bug 1330474 - Switch from Python 2.7 to Python 3.6
    This makes Heroku/Vagrant use Python 3.6.8 instead of Python 2.7, and
    inverts the versions used in the Travis testing matrix - leaving all
    unit tests running against Python 2.7 to make it easier to roll back
    if needs be. The Vagrant provision script and Heroku Python buildpack will
    automatically detect the changed Python version and purge the existing
    installation/site-packages prior to installing the new version.
    
    We're using Python 3.6 rather than 3.7, since latest Celery/Kombu do not
    yet support Python 3.7 (and we're on an older version anyway; bug 1337717).
    Bug 1529243 is filed for updating to Python 3.7 later.

--- runtests.sh
+++ runtests.sh
@@ -7,7 +7,7 @@ echo "Running pip check"
 pip check
 
 echo "Checking CELERY_QUEUES matches Procfile"
-python -3 -bb ./lints/queuelint.py
+python -bb ./lints/queuelint.py
 
 echo "Running flake8"
 flake8 || { echo "flake8 errors found!"; exit 1; }
@@ -21,7 +21,7 @@ git grep -El '^#!/.+\b(bash|sh)\b' | xargs shellcheck
 
 echo "Running Django system checks"
 # See .travis.yml for explanation of the environment variable overriding.
-SITE_URL="https://treeherder.dev" TREEHERDER_DEBUG="False" python -3 -bb ./manage.py check --deploy --fail-level WARNING
+SITE_URL="https://treeherder.dev" TREEHERDER_DEBUG="False" python -bb ./manage.py check --deploy --fail-level WARNING
 
 echo "Running Python tests"
-python -3 -bb -m pytest tests/
+python -bb -m pytest tests/
