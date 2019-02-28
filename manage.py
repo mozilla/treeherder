ac5df34e2b19bd701755bf6c9b3f93985e03362e
Ed Morley <501702+edmorley@users.noreply.github.com>
2019-02-28 14:22:40 +0000

    Bug 1529243 - Update Python from 3.6 to 3.7
    Now possible since Celery/Kombu have been updated in #4722 to version
    that are compatible with Python 3.7.
    
    Notable changes:
    https://docs.python.org/3/whatsnew/3.7.html

--- manage.py
+++ manage.py
@@ -5,16 +5,26 @@ import sys
 import warnings
 
 # Display deprecation warnings, which are hidden by default:
-# https://docs.python.org/3.6/library/warnings.html#default-warning-filters
+# https://docs.python.org/3.7/library/warnings.html#default-warning-filters
 warnings.simplefilter('default', DeprecationWarning)
 
 # Suppress noisy warnings from dependencies
+
 # Reported in https://support.newrelic.com/tickets/338064
-warnings.filterwarnings('ignore', category=DeprecationWarning, module='newrelic.api.import_hook')
-# Remove when we update to Kombu 4+ (bug 1337717)
-warnings.filterwarnings('ignore', category=DeprecationWarning, module='kombu.five')
+warnings.filterwarnings('ignore', category=DeprecationWarning, module='newrelic')
+
 # https://github.com/encode/django-rest-framework/issues/6317
 warnings.filterwarnings('ignore', category=DeprecationWarning, module='markdown.util')
+
+# "Using or importing the ABCs from 'collections' instead of from 'collections.abc' is deprecated, and in 3.8 it will stop working"
+# corsheaders/checks.py -> https://github.com/ottoyiu/django-cors-headers/issues/374
+# jinja2/runtime.py -> https://github.com/pallets/jinja/pull/867
+# orderedmultidict/orderedmultidict.py -> https://github.com/gruns/orderedmultidict/pull/20
+# promise/promise_list.py -> https://github.com/syrusakbary/promise/pull/67
+warnings.filterwarnings('ignore', category=DeprecationWarning, message=r'Using or importing the ABCs .*')
+
+# "the imp module is deprecated in favour of importlib; see the module's documentation for alternative uses"
+warnings.filterwarnings('ignore', category=DeprecationWarning, module='celery.utils.imports')
 
 if __name__ == "__main__":
     os.environ["DJANGO_SETTINGS_MODULE"] = "treeherder.config.settings"
