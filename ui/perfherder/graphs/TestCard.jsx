ff0c580cb0f0716a421fedd4d66dcbfb5d2961bf
Sarah Clements <sclements313@gmail.com>
2019-06-11 15:39:21 -0700

    Bug 1519995 - Convert Graphs View to react - test cards section Move the cards for selected test data to react; update styling

---
+++ ui/perfherder/graphs/TestCard.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormGroup, Input } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+export class TestContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checked: this.props.series.visible,
+    };
+  }
+
+  updateSelectedTest = () => {
+    const { checked } = this.state;
+    const { series, showHideSeries } = this.props;
+    this.setState({ checked: !checked });
+    showHideSeries(series.signature);
+  };
+
+  render() {
+    const { series, addTestData, removeSeries } = this.props;
+    const { checked } = this.state;
+    const subtitleStyle = 'p-0 mb-1 border-0 text-muted text-left';
+    return (
+      <FormGroup check className="pl-0">
+        <span
+          className="close mr-3 my-2 ml-2"
+          onClick={() => removeSeries(series.projectName, series.signature)}
+        >
+          <FontAwesomeIcon icon={faTimes} size="xs" title="" />
+        </span>
+        <Input
+          className="show-hide-check"
+          type="checkbox"
+          checked={checked}
+          aria-label="Show/Hide series"
+          title="Show/Hide series"
+          onChange={this.updateSelectedTest}
+        />
+        <div className="graph-legend-card border pl-5 py-3 pr-3">
+          <p
+            className={`p-0 mb-1 pointer border-0 ${
+              checked ? series.color : 'text-muted'
+            } text-left`}
+            onClick={() => addTestData('addRelatedConfigs', series.signature)}
+            title="Add related configurations"
+            type="button"
+          >
+            {series.name}
+          </p>
+          <p
+            className={subtitleStyle}
+            onClick={() => addTestData('addRelatedBranches', series.signature)}
+            title="Add related branches"
+            type="button"
+          >
+            {series.projectName}
+          </p>
+          <p
+            className={subtitleStyle}
+            onClick={() => addTestData('addRelatedPlatforms', series.signature)}
+            title="Add related branches"
+            type="button"
+          >
+            {series.platform}
+          </p>
+          <span className="small">{`${series.signature.slice(0, 16)}...`}</span>
+        </div>
+      </FormGroup>
+    );
+  }
+}
+
+TestContainer.propTypes = {
+  series: PropTypes.PropTypes.shape({
+    visible: PropTypes.bool,
+  }).isRequired,
+  addTestData: PropTypes.func,
+  removeSeries: PropTypes.func,
+  showHideSeries: PropTypes.func,
+};
+
+TestContainer.defaultProps = {
+  showHideSeries: undefined,
+  addTestData: undefined,
+  removeSeries: undefined,
+};
+
+export default TestContainer;
