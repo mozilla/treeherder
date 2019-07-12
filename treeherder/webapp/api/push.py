3038d08bbd690d54adc4c2d2e8623170e625d3f5
Cameron Dawson <cdawson@mozilla.com>
2019-07-12 12:26:49 -0700

    Bug 1563371 - Find non-gecko decision tasks when needed
    This paves the way to having retriggers work for NSS-Try and
    a few other repos.  That repo still needs to generate the right
    artifacts.  But this is the Treeherder side of that work.

--- treeherder/webapp/api/push.py
+++ treeherder/webapp/api/push.py
@@ -269,10 +269,10 @@ class PushViewSet(viewsets.ViewSet):
         Return the decision task ids for the pushes.
         """
         push_ids = request.query_params.getlist('push_ids')
-        job_type = JobType.objects.get(name='Gecko Decision Task')
+        job_types = JobType.objects.filter(name__contains='Decision Task')
         decision_jobs = Job.objects.filter(
             push_id__in=push_ids,
-            job_type=job_type,
+            job_type__in=job_types,
             result='success',
         ).select_related('taskcluster_metadata')
 
