d60d243467710a0dba413924d2badcf3692c80a2
Cameron Dawson <cdawson@mozilla.com>
2019-08-26 14:10:49 -0700

    added unit tests

---
+++ tests/model/test_option_collection.py
@@ -0,0 +1,6 @@
+from treeherder.model.models import OptionCollection
+
+
+def test_option_collection_map(sample_option_collections):
+    option_map = OptionCollection.objects.get_option_collection_map()
+    assert option_map == {'option_hash1': 'opt1', 'option_hash2': 'opt2'}
