0b8f5825ddd99cae7da1087ec9793b7346400446
Cameron Dawson <cdawson@mozilla.com>
2019-10-01 10:46:34 -0700

    Disable per file: eslint jsx-a11y/control-has-associated-label

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,7 +30,6 @@ module.exports = {
     'consistent-return': 'off',
     'default-case': 'off',
     'jsx-a11y/click-events-have-key-events': 'off',
-    'jsx-a11y/control-has-associated-label': 'off',
     'jsx-a11y/no-noninteractive-element-interactions': 'off',
     'jsx-a11y/no-static-element-interactions': 'off',
     'no-alert': 'off',
--- ui/perfherder/compare/CompareTable.jsx
+++ ui/perfherder/compare/CompareTable.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+
 import React from 'react';
 import { Button, Table } from 'reactstrap';
 import PropTypes from 'prop-types';
