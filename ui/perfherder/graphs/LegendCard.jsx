b774521fb802c38aa2999e7922407740e307f672
Cameron Dawson <cdawson@mozilla.com>
2019-10-01 10:51:36 -0700

    Disable per file: eslint jsx-a11y/no-noninteractive-element-interactions

--- ui/perfherder/graphs/LegendCard.jsx
+++ ui/perfherder/graphs/LegendCard.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormGroup, Input } from 'reactstrap';
