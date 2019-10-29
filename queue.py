b896dc1bc8be25d86414e792e064a36c2d8abbba
Mohamad Nurul Ihsan <44545824+ikhsanlosiver@users.noreply.github.com>
2019-10-29 20:53:12 +0800

    Create queue.py

---
+++ queue.py
@@ -0,0 +1,13 @@
+class Queue: 
+    def __init__(self):
+        self.items=[] 
+    def isEmpty(self):
+        return self.items==[] 
+    def enqueue(self,item):
+        self.items.insert(0,item) 
+    def dequeue(self):
+        return self.items.pop() 
+    def size(self):
+        return len(self.items)
+    def getList(self):
+        return self.items
