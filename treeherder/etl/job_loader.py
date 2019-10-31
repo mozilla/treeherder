mozilla/treeherder

commit 9de99b4711ba301fd43a68902c5beea9ddbef2ff

Bug 1395254 - Consume Taskcluster Pulse messages from standard queue exchanges
Currently, Treeherder consumes Pulse messages from an intermediary service called `taskcluster-treeherder`.
Such service needs to be shut down and its functionality imported into Treeherder.

In order to do this we need to switch to the standard Taskcluster exchanges as defined in here:
https://docs.taskcluster.net/docs/reference/platform/queue/exchanges

On a first pass we are only including the code from `taskcluster-treeherder` without changing
much of Treeherder's code. The code is translated from Javascript to Python and only some minor
code changes were done to reduce the difficulty on porting the code without introducing bugs.
Internally, on this first pass, we will still have an intermediary data structure representing
what `taskcluster-treeherder` is emitting, however, we will stop consuming the messages
from it and be able to shut it down.

Instead of consuming from one single exchange we will be consuming multiple ones. Each one representing
a different kind of task (e.g. pending vs running).

In order to test this change you need to open 5 terminal windows and follow these steps:

* On the first window run `docker-compose up`
* On the next three windows `export PULSE_URL="amqp://foo:bar@pulse.mozilla.org:5671/?ssl=1"` and run the following commands:
      * `docker-compose run -e PULSE_URL backend ./manage.py pulse_listener_jobs`
      * `docker-compose run -e PULSE_URL backend ./manage.py pulse_listener_tasks`
      * `docker-compose run -e PULSE_URL backend ./manage.py pulse_listener_pushes`
* On the last window run `docker-compose run backend celery -A treeherder worker -B --concurrency 5`
* Open on your browser `http://localhost:5000`

This is just a summary from [the docs](https://treeherder.readthedocs.io/pulseload.html).

= ETL management commands =
This change also introduces two ETL management command that can be executed like this:

== Ingest push and tasks ==

treeherder/etl/job_loader.py
@@ -8,7 +8,7 @@ import slugid
 from treeherder.etl.common import to_timestamp
 from treeherder.etl.exceptions import MissingPushException
 from treeherder.etl.jobs import store_job_data
-from treeherder.etl.schema import job_json_schema
+from treeherder.etl.schema import get_json_schema
 from treeherder.model.models import (Push,
                                      Repository)
 
@@ -55,7 +55,10 @@ class JobLoader:
                 if pulse_job["state"] != "unscheduled":
                     try:
                         self.validate_revision(repository, pulse_job)
-                        store_job_data(repository, [self.transform(pulse_job)])
+                        transformed_job = self.transform(pulse_job)
+                        store_job_data(repository, [transformed_job])
+                        # Returning the transformed_job is only for testing purposes
+                        return transformed_job
                     except AttributeError:
                         logger.warning("Skipping job due to bad attribute", exc_info=1)
             except Repository.DoesNotExist:
@@ -131,6 +134,7 @@ class JobLoader:
             platform_src = pulse_job[v] if v in pulse_job else default_platform
             x["job"][k] = self._get_platform(platform_src)
 
+        # TODO: Improve the code https://bugzilla.mozilla.org/show_bug.cgi?id=1560596
         # add some taskcluster metadata if it's available
         # currently taskcluster doesn't pass the taskId directly, so we'll
         # derive it from the guid, where it is stored in uncompressed
@@ -307,7 +311,7 @@ class JobLoader:
 
     def _is_valid_job(self, pulse_job):
         try:
-            jsonschema.validate(pulse_job, job_json_schema)
+            jsonschema.validate(pulse_job, get_json_schema("pulse-job.yml"))
         except (jsonschema.ValidationError, jsonschema.SchemaError) as e:
             logger.error("JSON Schema validation error during job ingestion: %s", e)
             return False

treeherder/etl/tasks/pulse_tasks.py
@@ -1,10 +1,13 @@
 """
 This module contains tasks related to pulse job ingestion
 """
+import asyncio
+
 import newrelic.agent
 
 from treeherder.etl.job_loader import JobLoader
 from treeherder.etl.push_loader import PushLoader
+from treeherder.etl.taskcluster_pulse.handler import handleMessage
 from treeherder.workers.task import retryable_task
 
 
@@ -15,8 +18,24 @@ def store_pulse_jobs(pulse_job, exchange, routing_key):
     """
     newrelic.agent.add_custom_parameter("exchange", exchange)
     newrelic.agent.add_custom_parameter("routing_key", routing_key)
-
     JobLoader().process_job(pulse_job)
+
+
+@retryable_task(name='store-pulse-tasks', max_retries=10)
+def store_pulse_tasks(pulse_job, exchange, routing_key):
+    """
+    Fetches tasks from Taskcluster
+    """
+    loop = asyncio.get_event_loop()
+    newrelic.agent.add_custom_parameter("exchange", exchange)
+    newrelic.agent.add_custom_parameter("routing_key", routing_key)
+    # handleMessage expects messages in this format
+    runs = loop.run_until_complete(handleMessage({
+        "exchange": exchange,
+        "payload": pulse_job,
+    }))
+    for run in runs:
+        JobLoader().process_job(run)
 
 
 @retryable_task(name='store-pulse-pushes', max_retries=10)

Procfile
@@ -37,10 +37,14 @@ celery_scheduler: REMAP_SIGTERM=SIGQUIT newrelic-admin run-program celery beat -
 # NB: These should not be scaled up to more than 1 of each.
 # TODO: Merge these two listeners into one since they use so little CPU each (bug 1530965).
 pulse_listener_pushes: newrelic-admin run-program ./manage.py pulse_listener_pushes
+# We will remove this soon after this commit is deployed
 pulse_listener_jobs: newrelic-admin run-program ./manage.py pulse_listener_jobs
+pulse_listener_tasks: newrelic-admin run-program ./manage.py pulse_listener_tasks
 
 # Processes pushes/jobs from Pulse that were collected by `pulse_listener_{pushes,jobs)`.
-worker_store_pulse_data: REMAP_SIGTERM=SIGQUIT newrelic-admin run-program celery worker -A treeherder --without-gossip --without-mingle --without-heartbeat -Q store_pulse_pushes,store_pulse_jobs --concurrency=3
+worker_store_pulse_data: REMAP_SIGTERM=SIGQUIT newrelic-admin run-program celery worker -A treeherder --without-gossip --without-mingle --without-heartbeat -Q store_pulse_pushes,store_pulse_tasks --concurrency=3
+# We will remove this soon after this commit is deployed
+worker_store_pulse_old: REMAP_SIGTERM=SIGQUIT newrelic-admin run-program celery worker -A treeherder --without-gossip --without-mingle --without-heartbeat -Q store_pulse_jobs --concurrency=3
 
 # Handles the log parsing tasks scheduled by `worker_store_pulse_data` as part of job ingestion.
 worker_log_parser: REMAP_SIGTERM=SIGQUIT newrelic-admin run-program celery worker -A treeherder --without-gossip --without-mingle --without-heartbeat -Q log_parser,log_parser_fail,log_autoclassify,log_autoclassify_fail --concurrency=7

misc/compare_pushes.py
@@ -1,0 +1,116 @@
+#!/usr/bin/env python
+""" Script to compare two pushes from different Treeherder instances"""
+import argparse
+import logging
+import pprint
+import uuid
+
+import slugid
+
+from deepdiff import DeepDiff
+from thclient import TreeherderClient
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+logger = logging.getLogger(__name__)
+HOSTS = {
+    "localhost": "http://localhost:8000",
+    "stage": "https://treeherder.allizom.org",
+    "production": "https://treeherder.mozilla.org"
+}
+
+
+def remove_some_attributes(job, production_job):
+    # I belive these differences are expected since they are dependant to when the data
+    # was inserted inside of the database
+    del job["build_platform_id"]
+    del job["id"]
+    del job["job_group_id"]
+    del job["job_type_id"]
+    del job["last_modified"]
+    del job["push_id"]
+    del job["result_set_id"]
+    del production_job["build_platform_id"]
+    del production_job["id"]
+    del production_job["job_group_id"]
+    del production_job["job_type_id"]
+    del production_job["last_modified"]
+    del production_job["push_id"]
+    del production_job["result_set_id"]
+
+    if job.get("end_timestamp"):
+        del job["end_timestamp"]
+        del job["start_timestamp"]
+        del production_job["end_timestamp"]
+        del production_job["start_timestamp"]
+
+    if job.get("failure_classification_id"):
+        del job["failure_classification_id"]
+        del production_job["failure_classification_id"]
+
+
+def print_url_to_taskcluster(job_guid):
+    job_guid = job["job_guid"]
+    (decoded_task_id, _) = job_guid.split("/")
+    # As of slugid v2, slugid.encode() returns a string not bytestring under Python 3.
+    taskId = slugid.encode(uuid.UUID(decoded_task_id))
+    logger.info("https://taskcluster-ui.herokuapp.com/tasks/%s", taskId)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Compare a push from a Treeherder instance to the production instance.")
+    parser.add_argument("--host", default="localhost",
+                        help="Host to compare. It defaults to localhost")
+    parser.add_argument("--revision", required=True,
+                        help="Revision to compare")
+    parser.add_argument("--project", default="mozilla-central",
+                        help="Project to compare. It defaults to mozilla-central")
+
+    args = parser.parse_args()
+
+    th_instance = TreeherderClient(server_url=HOSTS[args.host])
+    th_instance_pushid = th_instance.get_pushes("mozilla-central", revision=args.revision)[0]["id"]
+    th_instance_jobs = th_instance.get_jobs(args.project, push_id=th_instance_pushid, count=None) or []
+
+    production = TreeherderClient(server_url=HOSTS["production"])
+    production_pushid = production.get_pushes("mozilla-central", revision=args.revision)[0]["id"]
+    production_jobs = production.get_jobs(args.project, push_id=production_pushid, count=None)
+
+    production_dict = {}
+    for job in production_jobs:
+        production_dict[job["job_guid"]] = job
+
+    th_instance_dict = {}
+    th_instance_not_found = []
+    for job in th_instance_jobs:
+        production_job = production_dict.get(job["job_guid"])
+        if production_job is None:
+            th_instance_not_found.append(job)
+        else:
+            # You can use this value in a url with &selectedJob=
+            jobId = job["id"]
+            remove_some_attributes(job, production_job)
+
+            differences = DeepDiff(job, production_dict[job["job_guid"]])
+            if differences:
+                pprint.pprint(differences)
+                logger.info(jobId)
+            else:
+                # Delete jobs that don"t have any differences
+                del production_dict[job["job_guid"]]
+
+    logger.info("We have found: %s jobs on %s instance.", len(th_instance_jobs), args.host)
+    logger.info("We have found: %s jobs on the production instance.", len(production_jobs))
+
+    if production_dict:
+        logger.info("There are the first 10 production jobs we do not have th_instancely. Follow the link to investigate.")
+        for job in list(production_dict.values())[0:10]:
+            print_url_to_taskcluster(job["job_guid"])
+
+    if th_instance_not_found:
+        logger.info("Number of jobs not found th_instancely: %s jobs", len(th_instance_not_found))
+        for job in th_instance_not_found:
+            print_url_to_taskcluster(job["job_guid"])
+
+    if production_dict is None and th_instance_not_found is None:
+        logger.info("We have not found any differences between the two pushes!! :D")

schemas/task-treeherder-config.yml
@@ -1,0 +1,134 @@
+$schema: 'http://json-schema.org/draft-07/schema#'
+title: 'Treeherder Configuration'
+description: |
+  Definition of the Treeherder configuration data that can be contained within
+  a task definition under task.extra.treeherder.  This information is useful for
+  determining job properties to report to Treeherder.
+type: object
+properties:
+  reason:
+    description: |
+      Examples include:
+      - scheduled
+      - scheduler
+      - Self-serve: Rebuilt by foo@example.com
+      - Self-serve: Requested by foo@example.com
+      - The Nightly scheduler named 'mozilla-inbound periodic' triggered this build
+      - unknown
+    type: 'string'
+    minLength: 1
+    maxLength: 125
+  tier:
+    type: 'integer'
+    description: |
+      Tiers are used for classifying jobs according to the Sheriffing policy.
+      These jobs can be hidden based on exclusion profiles within Treeherder and
+      display of these jobs toggled by UI settings.
+
+      By default jobs which do not specify a tier will be classified as Tier 1.
+    minimum: 1
+    maximum: 3
+  jobKind:
+    type: 'string'
+    description: |
+      jobKind specifies the type of task that should be reported to Treeherder.
+      The jobKind could cause Treeherder to display/treat the task differently.
+      For instance, tasks with a jobKind of 'build' will be reported as red when
+      the task fails, 'test' as orange, and any jobs not specifying jobKind or
+      'other' will be red.
+    default: 'other'
+    enum:
+      - build
+      - test
+      - other
+  machine:
+    type: 'object'
+    properties:
+      platform:
+        type: 'string'
+        description: |
+          The platform specified here maps to platforms that Treeherder recognizes.
+          Jobs with the same platform will be displayed within the same row on
+          Treeherder and obey any ordering that is defined'.
+
+          If no build platform is specified, the workerType specified for the job
+          will be used.
+        pattern: '^[A-Za-z0-9_-]+$'
+        minLength: 1
+        maxLength: 50
+      os:
+        type: 'string'
+        pattern: '^[A-Za-z0-9_-]+$'
+        minLength: 1
+        maxLength: 25
+      architecture:
+        type: 'string'
+        pattern: '^[A-Za-z0-9_-]+$'
+        minLength: 1
+        maxLength: 25
+    additionalProperties: false
+    required: [platform]
+  labels:
+    title: 'labels'
+    description: |
+      Labels are a dimension of a platform.  The values here can vary wildly,
+      so most strings are valid for this.  The list of labels that are used
+      is malleable going forward.
+
+      These were formerly known as "Collection" calling labels now so they
+      can be understood to be just strings that denotes a characteristic of the job.
+
+      These labels will be used for grouping jobs with a particular job platform.
+      For instance, a job with the label "debug" will be put into the debug platform
+      on Treeherder.  By default, if no label is specified, the job will be classified
+      as "opt"
+
+      Some examples of labels that have been used:
+        opt    Optimize Compiler GCC optimize flags
+        debug  Debug flags passed in
+        pgo    Profile Guided Optimization - Like opt, but runs with profiling, then builds again using that profiling
+        asan   Address Sanitizer
+        tsan   Thread Sanitizer Build
+    type: 'array'
+    uniqueItems: false
+    items:
+      type: 'string'
+      minLength: 1
+      maxLength: 50
+      pattern: '^[A-Za-z0-9_-]+$'
+  symbol:
+    title: 'symbol'
+    description: |
+      This is the symbol that will appear in a Treeherder resultset for a
+      given push.  This symbol could be something such as "B" or a number representing
+      the current chunk.
+    type: 'string'
+    minLength: 0
+    maxLength: 25
+  groupName:
+    title: 'group name'
+    type: 'string'
+    minLength: 1
+    maxLength: 100
+  groupSymbol:
+    title: 'group symbol'
+    description: |
+      Group Symbol is the symbol that job symbols will be grouped under.  This
+      is useful if there is a particular group of jobs that should be displayed
+      together.  For example, a test suite named "Media Tests" with the group symbol
+      of "ME" would have all jobs with that group symbol appear as
+      ME(symbol 1, symbol 2, ...).
+    type: 'string'
+    minLength: 1
+    maxLength: 25
+  productName:
+    description: |
+      Examples include:
+      -  'firefox'
+      -  'taskcluster'
+      -  'xulrunner'
+    type: 'string'
+    minLength: 1
+    maxLength: 125
+required: [symbol]
+additionalProperties: true

tests/etl/test_job_loader.py
@@ -1,9 +1,13 @@
 import copy
+import uuid
 
 import pytest
+import responses
+import slugid
 
 from treeherder.etl.exceptions import MissingPushException
 from treeherder.etl.job_loader import JobLoader
+from treeherder.etl.taskcluster_pulse.handler import handleMessage
 from treeherder.model.models import (Job,
                                      JobDetail,
                                      JobLog,
@@ -35,12 +39,69 @@ def transformed_pulse_jobs(sample_data, test_repository):
     return jobs
 
 
+def mock_artifact(taskId, runId, artifactName):
+    # Mock artifact with empty body
+    baseUrl = "https://queue.taskcluster.net/v1/task/{taskId}/runs/{runId}/artifacts/{artifactName}"
+    responses.add(
+        responses.GET,
+        baseUrl.format(taskId=taskId, runId=runId, artifactName=artifactName),
+        body="",
+        content_type='text/plain',
+        status=200)
+
+
+@pytest.fixture
+async def new_pulse_jobs(sample_data, test_repository, push_stored):
+    revision = push_stored[0]["revisions"][0]["revision"]
+    pulseMessages = copy.deepcopy(sample_data.taskcluster_pulse_messages)
+    tasks = copy.deepcopy(sample_data.taskcluster_tasks)
+    jobs = []
+    # Over here we transform the Pulse messages into the intermediary taskcluster-treeherder
+    # generated messages
+    for message in list(pulseMessages.values()):
+        taskId = message["payload"]["status"]["taskId"]
+        task = tasks[taskId]
+        # If we pass task to handleMessage we won't hit the network
+        taskRuns = await handleMessage(message, task)
+        # handleMessage returns [] when it is a task that is not meant for Treeherder
+        for run in reversed(taskRuns):
+            mock_artifact(taskId, run["retryId"], "public/logs/live_backing.log")
+            run["origin"]["project"] = test_repository.name
+            run["origin"]["revision"] = revision
+            jobs.append(run)
+    return jobs
+
+
+@pytest.fixture
+def new_transformed_jobs(sample_data, test_repository, push_stored):
+    revision = push_stored[0]["revisions"][0]["revision"]
+    jobs = copy.deepcopy(sample_data.taskcluster_transformed_jobs)
+    for job in jobs.values():
+        job["revision"] = revision
+    return jobs
+
+
 def test_job_transformation(pulse_jobs, transformed_pulse_jobs):
     import json
     jl = JobLoader()
     for idx, pulse_job in enumerate(pulse_jobs):
         assert jl._is_valid_job(pulse_job)
         assert transformed_pulse_jobs[idx] == json.loads(json.dumps(jl.transform(pulse_job)))
+
+
+@responses.activate
+def test_new_job_transformation(new_pulse_jobs, new_transformed_jobs, failure_classifications):
+    jl = JobLoader()
+    for message in new_pulse_jobs:
+        # "task_id" which is not really the task_id
+        job_guid = message["taskId"]
+        (decoded_task_id, _) = job_guid.split("/")
+        # As of slugid v2, slugid.encode() returns a string not bytestring under Python 3.
+        taskId = slugid.encode(uuid.UUID(decoded_task_id))
+        transformed_job = jl.process_job(message)
+        # Not all messages from Taskcluster will be processed
+        if transformed_job:
+            assert new_transformed_jobs[taskId] == transformed_job
 
 
 def test_ingest_pulse_jobs(pulse_jobs, test_repository, push_stored,

tests/etl/test_job_schema.py
@@ -1,7 +1,7 @@
 import jsonschema
 import pytest
 
-from treeherder.etl.schema import job_json_schema
+from treeherder.etl.schema import get_json_schema
 
 # The test data in this file are a representative sample-set from
 # production Treeherder
@@ -16,7 +16,7 @@ def test_group_symbols(sample_data, group_symbol):
     job["origin"]["project"] = "proj"
     job["origin"]["revision"] = "1234567890123456789012345678901234567890"
     job["display"]["groupSymbol"] = group_symbol
-    jsonschema.validate(job, job_json_schema)
+    jsonschema.validate(job, get_json_schema("pulse-job.yml"))
 
 
 @pytest.mark.parametrize("job_symbol", ['1.1g', '1g', '20', 'A', 'GBI10', 'en-US-1'])
@@ -28,4 +28,4 @@ def test_job_symbols(sample_data, job_symbol):
     job["origin"]["project"] = "proj"
     job["origin"]["revision"] = "1234567890123456789012345678901234567890"
     job["display"]["jobSymbol"] = job_symbol
-    jsonschema.validate(job, job_json_schema)
+    jsonschema.validate(job, get_json_schema("pulse-job.yml"))

tests/sampledata.py
@@ -31,6 +31,18 @@ class SampleData:
                   os.path.dirname(__file__))) as f:
             self.text_log_summary = json.load(f)
 
+        with open("{0}/sample_data/pulse_consumer/taskcluster_pulse_messages.json".format(
+                  os.path.dirname(__file__))) as f:
+            self.taskcluster_pulse_messages = json.load(f)
+
+        with open("{0}/sample_data/pulse_consumer/taskcluster_tasks.json".format(
+                  os.path.dirname(__file__))) as f:
+            self.taskcluster_tasks = json.load(f)
+
+        with open("{0}/sample_data/pulse_consumer/taskcluster_transformed_jobs.json".format(
+                  os.path.dirname(__file__))) as f:
+            self.taskcluster_transformed_jobs = json.load(f)
+
         with open("{0}/sample_data/pulse_consumer/job_data.json".format(
                   os.path.dirname(__file__))) as f:
             self.pulse_jobs = json.load(f)

commit 20c060fe5baed5562c92ea9b092ccf9b87af1eab

If a pulse_job is None consider it as an invalid job
This is a regression introduced in https://github.com/mozilla/treeherder/pull/5196

With the [PR that just landed](https://github.com/mozilla/treeherder/pull/5546) it becomes more noticeable.

treeherder/etl/job_loader.py
@@ -326,6 +326,8 @@ class JobLoader:
         return resmap[result]
 
     def _is_valid_job(self, pulse_job):
+        if pulse_job is None:
+            return False
         try:
             # e.g. mozilla-l10n-automation-bot@users.noreply.github.com
             # Changing the pulse schema will also require a schema change

treeherder/etl/tasks/pulse_tasks.py
@@ -29,7 +29,8 @@ def store_pulse_tasks(pulse_job, exchange, routing_key, root_url='https://taskcl
         "root_url": root_url,
     }))
     for run in runs:
-        JobLoader().process_job(run, root_url)
+        if run:
+            JobLoader().process_job(run, root_url)
 
 
 @retryable_task(name='store-pulse-pushes', max_retries=10)