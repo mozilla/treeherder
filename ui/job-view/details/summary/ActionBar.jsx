d93ee3c1dc3c2816b5f2d7419ca16ac90d2ade1d
Alexandra Lobovikova <aalexandralobovikova@gmail.com>
2019-11-09 00:15:47 +0300

    bug-1554287 - remove zooming effect and add scroll to selected job (#5508)

--- ui/job-view/details/summary/ActionBar.jsx
+++ ui/job-view/details/summary/ActionBar.jsx
@@ -11,11 +11,17 @@ import {
   faRedo,
   faThumbtack,
   faTimesCircle,
+  faCrosshairs,
 } from '@fortawesome/free-solid-svg-icons';
 
 import { thEvents } from '../../../helpers/constants';
 import { formatTaskclusterError } from '../../../helpers/errorMessage';
-import { isReftest, isPerfTest, isTestIsolatable } from '../../../helpers/job';
+import {
+  isReftest,
+  isPerfTest,
+  isTestIsolatable,
+  findJobInstance,
+} from '../../../helpers/job';
 import { getInspectTaskUrl, getReftestUrl } from '../../../helpers/url';
 import JobModel from '../../../models/job';
 import TaskclusterModel from '../../../models/taskcluster';
@@ -478,6 +484,21 @@ class ActionBar extends React.PureComponent {
                   </a>
                 </li>
               ))}
+            <li>
+              <Button
+                id="find-job-btn"
+                title="Scroll to selection"
+                className="actionbar-nav-btn btn icon-blue bg-transparent border-0"
+                onClick={() =>
+                  findJobInstance(jobLogUrls[0] && jobLogUrls[0].job_id, true)
+                }
+              >
+                <FontAwesomeIcon
+                  icon={faCrosshairs}
+                  title="Find job instance"
+                />
+              </Button>
+            </li>
             {this.canCancel() && (
               <li>
                 <Button
