289671db343b9a8443620eac72a569a9f7e6c85d
Mell Y <pong7219@gmail.com>
2019-12-13 15:39:32 -0300

    Bug 1603251 - Improve expand/collapse component for screen reader users

--- ui/push-health/ClassificationGroup.jsx
+++ ui/push-health/ClassificationGroup.jsx
@@ -72,6 +72,7 @@ class ClassificationGroup extends React.PureComponent {
       currentRepo,
     } = this.props;
     const expandIcon = detailsShowing ? faMinusSquare : faPlusSquare;
+    const expandTitle = detailsShowing ? 'Minus sign' : 'Plus sign';
 
     return (
       <Row className={`justify-content-between ${className}`}>
@@ -80,12 +81,14 @@ class ClassificationGroup extends React.PureComponent {
             className="pointable w-100"
             onClick={this.toggleDetails}
             color={headerColor}
+            role="button"
+            aria-expanded={detailsShowing}
           >
             {name} : {Object.keys(group).length}
             <FontAwesomeIcon
               icon={expandIcon}
               className="ml-1"
-              title="expand"
+              title={expandTitle}
             />
           </Badge>
         </h4>
