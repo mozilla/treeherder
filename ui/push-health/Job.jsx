9c453b460f93243e71dc6fc34b08f3e870cf04a3
Cameron Dawson <cdawson@mozilla.com>
2020-01-03 13:07:29 -0800

    Bug 1602971 - Improve display of LogViewer icon

--- ui/push-health/Job.jsx
+++ ui/push-health/Job.jsx
@@ -2,10 +2,11 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
+import Tooltip from 'react-tooltip-lite';
 
 import { getBtnClass } from '../helpers/job';
 import { getJobsUrl, getLogViewerUrl } from '../helpers/url';
-import logviewerIcon from '../img/logviewerIcon.png';
+import logviewerIcon from '../img/logviewerIcon.svg';
 
 class Job extends PureComponent {
   render() {
@@ -19,36 +20,53 @@ class Job extends PureComponent {
     const resultStatus = state === 'completed' ? result : state;
 
     return (
-      <span className="mr-2" key={id}>
-        <a
-          className={`btn job-btn filter-shown btn-sm mt-1 ${getBtnClass(
-            result,
-            failureClassificationId,
-          )} border`}
-          href={getJobsUrl({ selectedJob: job.id, repo, revision })}
-          title={`${jobName} - ${resultStatus}`}
+      <span className="ml-1">
+        <Tooltip
+          tagName="span"
+          useDefaultStyles
+          content={
+            <span className="mr-2" key={id}>
+              <div>{jobName}</div>
+              <div>Result: {resultStatus}</div>
+              {job.result === 'testfailed' && (
+                <div>
+                  Open Log Viewer:
+                  <a
+                    className="logviewer-btn ml-1"
+                    href={getLogViewerUrl(job.id, repo)}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    title="Open the Log Viewer for this job"
+                  >
+                    <img
+                      style={{ height: '18px' }}
+                      alt="Logviewer"
+                      src={logviewerIcon}
+                      className="logviewer-icon text-light mb-1"
+                    />
+                  </a>
+                </div>
+              )}
+            </span>
+          }
         >
-          {jobSymbol}
-        </a>
-        {failureClassificationId !== 1 && (
-          <FontAwesomeIcon icon={faStar} title="Classified" />
-        )}
-        {job.result === 'testfailed' && (
           <a
-            className="logviewer-btn"
-            href={getLogViewerUrl(job.id, repo)}
-            target="_blank"
-            rel="noopener noreferrer"
-            title="Open the Log Viewer for this job"
+            className={`btn job-btn filter-shown btn-sm mt-1 ${getBtnClass(
+              result,
+              failureClassificationId,
+            )} border`}
+            href={getJobsUrl({ selectedJob: job.id, repo, revision })}
           >
-            <img
-              style={{ height: '18px' }}
-              alt="Logviewer"
-              src={logviewerIcon}
-              className="logviewer-icon text-dark mb-1"
-            />
+            {jobSymbol}
           </a>
-        )}
+          {failureClassificationId !== 1 && (
+            <FontAwesomeIcon
+              icon={faStar}
+              title="Classified"
+              color="lightgray"
+            />
+          )}
+        </Tooltip>
       </span>
     );
   }
