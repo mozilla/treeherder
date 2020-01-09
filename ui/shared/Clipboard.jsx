77d6d7bae7053e06a54fd024761a7e27fd790de0
Tom Prince <mozilla@hocat.ca>
2020-01-09 13:54:39 -0700

    Use functional component.

--- ui/shared/Clipboard.jsx
+++ ui/shared/Clipboard.jsx
@@ -2,22 +2,14 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClipboard } from '@fortawesome/free-regular-svg-icons';
 
-export default class Clipboard extends React.PureComponent {
-  static copyToClipboard(text) {
-    navigator.clipboard.writeText(text);
-  }
+const Clipboard = ({ title, text }) => {
+  const copyToClipboard = () => navigator.clipboard.writeText(text);
 
-  render() {
-    const { title, text } = this.props;
+  return (
+    <button type="button" title={title} onClick={copyToClipboard}>
+      <FontAwesomeIcon icon={faClipboard} />
+    </button>
+  );
+};
 
-    return (
-      <button
-        type="button"
-        title={title}
-        onClick={() => Clipboard.copyToClipboard(text)}
-      >
-        <FontAwesomeIcon icon={faClipboard} />
-      </button>
-    );
-  }
-}
+export { Clipboard as default };
