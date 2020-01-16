7d94199b2e06610d2abe82a2c4ab608953eca5fa
Sarah Clements <sclements@mozilla.com>
2020-01-16 10:59:09 -0800

    Bug 1609140 - Perfherder tooltip fix (#5822)
    Add CSS change to allow cursor selection of text on the graph tooltip and
    add the Clipboard component to make copying easier

--- ui/shared/Clipboard.jsx
+++ ui/shared/Clipboard.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClipboard } from '@fortawesome/free-regular-svg-icons';
+import { Button } from 'reactstrap';
 
-const Clipboard = ({ description, text }) => {
+const Clipboard = ({ description, text, outline }) => {
   if (!text) {
     return null;
   }
@@ -11,24 +12,28 @@ const Clipboard = ({ description, text }) => {
   const copyToClipboard = () => navigator.clipboard.writeText(text);
 
   return (
-    <button
+    <Button
       type="button"
       title={`Copy ${description}`}
       onClick={copyToClipboard}
-      className="px-1 mx-1"
+      className="py-0 px-1"
+      color="light"
+      outline={outline}
     >
       <FontAwesomeIcon icon={faClipboard} />
-    </button>
+    </Button>
   );
 };
 
 Clipboard.propTypes = {
   description: PropTypes.string.isRequired,
   text: PropTypes.string,
+  outline: PropTypes.bool,
 };
 
 Clipboard.defaultProps = {
   text: null,
+  outline: false,
 };
 
 export { Clipboard as default };
