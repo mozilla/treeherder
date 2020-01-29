3614c93df81602c89524130cda599c179e31a9c4
shubhank-saxena <saxena.shubhank.19@gmail.com>
2020-01-29 23:22:11 +0000

    fixed coverage.sh and added after_success in travis.yml

--- tests/coverage.sh
+++ tests/coverage.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
-pytest tests/ --runslow --ignore=tests=selenium/
-pytest --cov=./ --cov-report=xml
-codecov -t 995256be-3a1f-4d10-b181-e24f88603b6b && exit
+#!/usr/bin/env bash
+
+exec pytest --cov=./ --cov-report=xml
