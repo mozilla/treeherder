4a1034146423ae714b1e8dc89897d8e4a2b6795d
Armen Zambrano G <armenzg@mozilla.com>
2020-02-05 14:22:53 -0500

    Define default env variables

--- runchecks.sh
+++ runchecks.sh
@@ -23,7 +23,16 @@ echo "Running test docs generation"
 mkdocs build
 
 echo "Running Django system checks"
+# We can remove these env variables once the default values are properly set:
+# https://github.com/mozilla/treeherder/issues/5926
+BROKER_URL=localhost//guest:guest@rabbitmq//
+DATABASE_URL=mysql://root@localhost:3306/treeherder
+REDIS_URL=redis://localhost:6379
+SITE_URL=http://backend:8000/
+TREEHERDER_DEBUG=True
+TREEHERDER_DJANGO_SECRET_KEY=secret-key-of-at-least-50-characters-to-pass-check-deploy
+NEW_RELIC_DEVELOPER_MODE=True
 # Several security features in settings.py (eg setting HSTS headers) are conditional on
 # 'https://' being in the site URL. In addition, we override the test environment's debug
 # value so the tests pass. The real environment variable will be checked during deployment.
-SITE_URL="https://treeherder.dev" TREEHERDER_DEBUG="False" python ./manage.py check --deploy --fail-level WARNING
+python ./manage.py check --deploy --fail-level WARNING
