a2e7e81adc48e3545f90341be39936df8d4abef0
Armen Zambrano <armenzg@users.noreply.github.com>
2020-02-06 19:21:28 -0500

    Bug 1611502 - Do not run initialize_data in Docker container if DATABASE_URL is being used (#5940)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -14,7 +14,10 @@ while ! nc -z "${DATABASE_HOST}" "${DATABASE_PORT}" &> /dev/null; do
     sleep 1;
 done
 
-# Initialize migrations and SETA
-./initialize_data.sh
+# Only execute if we're using the mysql container
+if [ "${DATABASE_URL}" == "mysql://root@mysql/treeherder" ]; then
+    # Initialize migrations and SETA
+    ./initialize_data.sh
+fi
 
 exec "$@"
