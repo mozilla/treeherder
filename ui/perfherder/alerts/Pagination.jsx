104e3e477a33faec605e2dc3cd2c4eaf4641eb73
Suyash Salampuria <44131389+SuyashSalampuria@users.noreply.github.com>
2020-02-12 20:21:07 +0530

    Bug 1565480 - Add pagination to the top

---
+++ ui/perfherder/alerts/Pagination.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
+
+class PaginationGroup extends React.Component {
+  navigatePage = page => {
+    const { fetchData, updateParams } = this.props;
+    fetchData(undefined, false, parseInt(page, 10));
+    updateParams({ page });
+  };
+
+  render() {
+    const { pageNums, page, count } = this.props;
+    return (
+      /* The first and last pagination navigation links
+         aren't working correctly (icons aren't visible)
+         so they haven't been added */
+      <Pagination aria-label={`Page ${page}`}>
+        {page > 1 && (
+          <PaginationItem>
+            <PaginationLink
+              className="text-info"
+              previous
+              onClick={() => this.navigatePage(page - 1)}
+            />
+          </PaginationItem>
+        )}
+        {pageNums.map(num => (
+          <PaginationItem
+            key={num}
+            active={num === page}
+            className="text-info pagination-active"
+          >
+            <PaginationLink
+              className="text-info"
+              onClick={() => this.navigatePage(num)}
+            >
+              {num}
+            </PaginationLink>
+          </PaginationItem>
+        ))}
+        {page < count && (
+          <PaginationItem>
+            <PaginationLink
+              className="text-info"
+              next
+              onClick={() => this.navigatePage(page + 1)}
+            />
+          </PaginationItem>
+        )}
+      </Pagination>
+    );
+  }
+}
+
+PaginationGroup.propTypes = {
+  pageNums: PropTypes.array.isRequired,
+  page: PropTypes.number,
+  count: PropTypes.number,
+  fetchData: PropTypes.func.isRequired,
+  updateParams: PropTypes.func.isRequired,
+};
+
+PaginationGroup.defaultProps = {
+  page: 1,
+  count: 1,
+};
+
+export default PaginationGroup;
