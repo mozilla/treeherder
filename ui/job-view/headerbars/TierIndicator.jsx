413cb559ce59dc870eeb08549bb2f27dad4b8726
Suyash Salampuria <44131389+SuyashSalampuria@users.noreply.github.com>
2020-02-19 22:37:20 +0530

    Bug 1594895 - Add a Navbar toggle for job Tiers (#5944)

---
+++ ui/job-view/headerbars/TierIndicator.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ButtonGroup, Button } from 'reactstrap';
+
+const TIERS = ['1', '2', '3'];
+
+export default function TierIndicator(props) {
+  const { filterModel } = props;
+  const shownTiers = filterModel.urlParams.tier || [];
+
+  return (
+    <ButtonGroup>
+      {TIERS.map(tier => {
+        const isOnlyTier = shownTiers.length === 1 && tier === shownTiers[0];
+        return (
+          <Button
+            onClick={() => filterModel.toggleFilter('tier', tier)}
+            title={
+              isOnlyTier
+                ? "Can't Toggle because least one tier must be selected at all times"
+                : `Toogle tier ${tier} jobs`
+            }
+            disabled={isOnlyTier}
+            outline={!shownTiers.includes(tier)}
+          >
+            {tier}
+          </Button>
+        );
+      })}
+    </ButtonGroup>
+  );
+}
+
+TierIndicator.propTypes = {
+  filterModel: PropTypes.object.isRequired,
+};
