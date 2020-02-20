6710bb43e66d79715638e06a74592f7c504668e0
Kimberly Sereduck <kimberlythegeek@gmail.com>
2020-02-20 00:51:27 -0700

    Bug 1614404 - Group common repositories

--- ui/shared/DropdownMenuItems.jsx
+++ ui/shared/DropdownMenuItems.jsx
@@ -4,27 +4,46 @@ import { DropdownMenu, DropdownItem } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
+const createDropdownItem = (item, namespace, updateData, selectedItem) => {
+  return (
+    <DropdownItem
+      tag="a"
+      key={`${namespace}${item}`}
+      onClick={() => updateData(item)}
+    >
+      <FontAwesomeIcon
+        icon={faCheck}
+        className={`mr-1 ${selectedItem === item ? '' : 'hide'}`}
+        title={selectedItem === item ? 'Checked' : ''}
+      />
+      {item}
+    </DropdownItem>
+  );
+};
+
 const DropdownMenuItems = ({
   selectedItem,
   updateData,
   options,
+  pinned,
   namespace = '',
 }) => (
   <DropdownMenu className="overflow-auto dropdown-menu-height">
-    {options.map(item => (
-      <DropdownItem
-        tag="a"
-        key={`${namespace}${item}`}
-        onClick={() => updateData(item)}
-      >
-        <FontAwesomeIcon
-          icon={faCheck}
-          className={`mr-1 ${selectedItem === item ? '' : 'hide'}`}
-          title={selectedItem === item ? 'Checked' : ''}
-        />
-        {item}
-      </DropdownItem>
-    ))}
+    {/* Items pinned to top of dropdown */}
+    {pinned.length > 0 &&
+      options
+        .filter(item => pinned.includes(item))
+        .sort((a, b) => a > b)
+        .map(item =>
+          createDropdownItem(item, namespace, updateData, selectedItem),
+        )}
+    {pinned.length > 0 && <DropdownItem divider />}
+    {options
+      .filter(item => !pinned.includes(item))
+      .sort((a, b) => a > b)
+      .map(item =>
+        createDropdownItem(item, namespace, updateData, selectedItem),
+      )}
   </DropdownMenu>
 );
 
@@ -32,11 +51,13 @@ DropdownMenuItems.propTypes = {
   updateData: PropTypes.func,
   selectedItem: PropTypes.string,
   options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  pinned: PropTypes.arrayOf(PropTypes.string),
 };
 
 DropdownMenuItems.defaultProps = {
   updateData: null,
   selectedItem: null,
+  pinned: [],
 };
 
 export default DropdownMenuItems;
