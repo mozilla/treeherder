b533ca58936813a2fd7b46516409e2d7f91013c2
Armen Zambrano G <armenzg@mozilla.com>
2020-02-25 10:28:36 -0500

    Running autoclassify as debug message

--- treeherder/autoclassify/tasks.py
+++ treeherder/autoclassify/tasks.py
@@ -12,6 +12,6 @@ logger = logging.getLogger(__name__)
 @retryable_task(name='autoclassify', max_retries=10)
 def autoclassify(job_id):
     newrelic.agent.add_custom_parameter("job_id", str(job_id))
-    logger.info('Running autoclassify')
+    logger.debug('Running autoclassify')
     job = Job.objects.select_related("repository").get(id=job_id)
     match_errors(job)
