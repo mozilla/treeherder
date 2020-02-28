ab28c4ccb43b1b2ca9bf1041a6cdc54e4701533d
Cameron Dawson <cdawson@mozilla.com>
2020-02-28 14:23:37 -0800

    Bug 1618751 - Make the RevisionList from job-view a shared component
    Also make the clipboard copy feature only visible on hover.

---
+++ ui/shared/Revision.jsx
@@ -0,0 +1,118 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
+import { Row } from 'reactstrap';
+
+import { parseAuthor } from '../helpers/revision';
+
+import BugLinkify from './BugLinkify';
+import Clipboard from './Clipboard';
+
+export function AuthorInitials(props) {
+  const str = props.author || '';
+  const words = str.split(' ');
+  const firstLetters = words
+    .map(word => word.replace(/[^A-Z]/gi, '')[0])
+    .filter(firstLetter => typeof firstLetter !== 'undefined');
+  let initials = '';
+
+  if (firstLetters.length === 1) {
+    // eslint-disable-next-line prefer-destructuring
+    initials = firstLetters[0];
+  } else if (firstLetters.length > 1) {
+    initials = firstLetters[0] + firstLetters[firstLetters.length - 1];
+  }
+
+  return (
+    <span title={props.title} className="text-nowrap">
+      <span className="text-secondary">
+        <FontAwesomeIcon icon={faUser} />
+      </span>
+      <span className="ml-1 icon-superscript font-italic font-weight-bold text-secondary user-push-initials">
+        {initials}
+      </span>
+    </span>
+  );
+}
+
+AuthorInitials.propTypes = {
+  author: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+export class Revision extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      clipboardVisible: false,
+    };
+  }
+
+  showClipboard = show => {
+    this.setState({ clipboardVisible: show });
+  };
+
+  isBackout = () => {
+    const {
+      revision: { comments },
+    } = this.props;
+    const comment = comments.split('\n')[0];
+
+    return comment.search('Backed out') >= 0 || comment.search('Back out') >= 0;
+  };
+
+  render() {
+    const {
+      revision: { comments, author, revision },
+      repo,
+    } = this.props;
+    const { clipboardVisible } = this.state;
+    const { name, email } = parseAuthor(author);
+    const commitRevision = revision;
+    const commentColor = this.isBackout() ? 'text-danger' : 'text-secondary';
+
+    return (
+      <Row
+        className="revision ml-1 flex-nowrap"
+        onMouseEnter={() => this.showClipboard(true)}
+        onMouseLeave={() => this.showClipboard(false)}
+        data-testid="revision"
+      >
+        <span className="pl-4 pr-1 text-nowrap">
+          <Clipboard
+            description="full hash"
+            text={commitRevision}
+            visible={clipboardVisible}
+          />
+          <a
+            title={`Open revision ${commitRevision} on ${repo.url}`}
+            href={repo.getRevisionHref(commitRevision)}
+            className="text-monospace commit-sha"
+          >
+            {commitRevision.substring(0, 12)}
+          </a>
+        </span>
+        <AuthorInitials title={`${name}: ${email}`} author={name} />
+        <span
+          title={comments}
+          className={`ml-2 revision-comment overflow-hidden text-nowrap ${commentColor}`}
+        >
+          <em>
+            <BugLinkify>{comments}</BugLinkify>
+          </em>
+        </span>
+      </Row>
+    );
+  }
+}
+
+Revision.propTypes = {
+  revision: PropTypes.shape({
+    comments: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    revision: PropTypes.string.isRequired,
+  }).isRequired,
+  repo: PropTypes.object.isRequired,
+};
