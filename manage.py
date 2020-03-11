3376918a93d71e76f86ad1c770a1946d75bd20b3
Tarek Ziade <tarek@ziade.org>
2020-03-11 17:27:57 +0100

    remove github3 from deps

--- manage.py
+++ manage.py
@@ -10,9 +10,6 @@ warnings.simplefilter('default', DeprecationWarning)
 
 # Suppress noisy warnings from dependencies
 
-# github3.py still uses collections for ABC classes
-warnings.filterwarnings('ignore', category=DeprecationWarning, module='github3')
-
 # Reported in https://support.newrelic.com/tickets/338064
 warnings.filterwarnings('ignore', category=DeprecationWarning, module='newrelic')
 
