dbc75636e925fab1685957238ba9a1065099da4c
Armen Zambrano G <armenzg@mozilla.com>
2020-03-12 10:45:39 -0400

    Add this back

--- ui/models/filter.js
+++ ui/models/filter.js
@@ -279,6 +279,10 @@ export default class FilterModel {
       return null;
     }
 
+    if (field === 'test_paths' && job[field]) {
+      // Make all paths unix style
+      return job[field].map(testPath => testPath.replace(/\\/g, /\//));
+    }
     return job[field];
   };
 
