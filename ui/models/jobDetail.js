2c592246969c4faa692b2875b934da7989632dbb
Sarah Clements <sclements313@gmail.com>
2020-03-31 11:58:23 -0700

    * modify pushHealth json file to add task and run ids * update repositories fixture with firefox-ci root url * update use of tc root url and fix push health tests

--- ui/models/jobDetail.js
+++ ui/models/jobDetail.js
@@ -1,7 +1,7 @@
 import { createQueryParams, getApiUrl } from '../helpers/url';
 
 // This API is being depreciated for uploaded artifact retrieval
-// (use the getArtifactsUrl helper to fetch from taskcluster); 
+// (use the getArtifactsUrl helper to fetch from taskcluster);
 // see bug 1603249 for details
 export default class JobDetailModel {
   static getJobDetails(params, signal) {
