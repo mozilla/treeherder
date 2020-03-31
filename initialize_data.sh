1e29aa7826fc66a8d863efdde3ab920a6d29ab0d
Armen Zambrano G <armenzg@mozilla.com>
2020-03-31 08:51:49 -0400

    Permit running initialize_data.sh from virtualenv

--- initialize_data.sh
+++ initialize_data.sh
@@ -2,8 +2,10 @@
 # Make non-zero exit codes & other errors fatal.
 set -euo pipefail
 
+export DATABASE_URL=${DATABASE_URL:-mysql://root@127.0.0.1:3306/treeherder}
 # Only execute if we're using the Mysql container
-if [ "${DATABASE_URL}" == "mysql://root@mysql/treeherder" ]; then
+if [ "${DATABASE_URL}" == "mysql://root@mysql/treeherder" ] ||
+   [ "${DATABASE_URL}" == "mysql://root@127.0.0.1:3306/treeherder" ]; then
     # Initialize migrations and SETA
     echo '-----> Running Django migrations and loading reference data'
     ./manage.py migrate --noinput
