e58bd2f64d5c9c5a01f2c021588e239c3e1c29cf
Kyle Lahnakoski <kyle@lahnakoski.com>
2020-04-01 23:01:14 -0400

    revert

--- tests/extract/conftest.py
+++ tests/extract/conftest.py
@@ -3,11 +3,13 @@ import os
 import pytest
 from mo_logs import (Log,
                      constants,
-                     startup)
+                     startup,
+                     strings)
 from mo_logs.convert import unix2datetime
 from mo_math.randoms import Random
 from mo_times import Date
 
+from treeherder.config.settings import DATABASES
 from treeherder.extract import extract_jobs
 from treeherder.model.models import (ClassifiedFailure,
                                      Commit,
@@ -204,7 +206,13 @@ def extract_job_settings():
     os.environ["NEW_RELIC_APP_NAME"] = "testing"
     os.environ["BIGQUERY_PRIVATE_KEY_ID"] = "1"
     os.environ["BIGQUERY_PRIVATE_KEY"] = "1"
-    os.environ["DATABASE_URL"] = "mysql://root@127.0.0.1:3306/treeherder"
+
+    # USE THE TEST SCHEMA
+    db_url = os.environ["DATABASE_URL"]
+    db_url = db_url.replace(
+        strings.between(db_url, "/", None), DATABASES["default"]["TEST"]["NAME"]
+    )
+    os.environ["DATABASE_URL"] = db_url
 
     settings = startup.read_settings(filename=extract_jobs.CONFIG_FILE, complain=False)
     settings.source.database.ssl = None  # NOT REQUIRED FOR TEST DATABASE
