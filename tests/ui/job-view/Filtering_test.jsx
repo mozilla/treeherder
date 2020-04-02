9f5a9e7498d58d8dd5ab5e2c644048a7819b9171
Sarah Clements <sclements313@gmail.com>
2020-04-02 12:20:00 -0700

    fix test

--- tests/ui/job-view/Filtering_test.jsx
+++ tests/ui/job-view/Filtering_test.jsx
@@ -189,6 +189,10 @@ describe('Filtering', () => {
         [],
       );
       fetchMock.get(getApiUrl('/jobdetail/?job_id=259537372'), { results: [] });
+      fetchMock.get(
+        'https://firefox-ci-tc.services.mozilla.com/api/queue/v1/task/JFVlnwufR7G9tZu_pKM0dQ/runs/0/artifacts',
+        { artifacts: [] },
+      );
       fetchMock.get(getProjectUrl('/note/?job_id=259537372', 'autoland'), []);
       fetchMock.get(
         getProjectUrl('/bug-job-map/?job_id=259537372', 'autoland'),
