690010736f37d5e33f7c590b54796d07c5b2cac4
Kyle Lahnakoski <klahnakoski@mozilla.com>
2020-04-17 16:47:24 -0400

    Bug 1631082 - Fix extraction of alerts into BigQuery (#6316)
    * add alert tests, fix alert extract
    
    * enable extract testing in travis
    
    Co-authored-by: Kyle Lahnakoski <kyle@lahnakoski.com>

--- tests/extract/conftest.py
+++ tests/extract/conftest.py
@@ -1,31 +1,30 @@
 import os
 
 import pytest
-from mo_logs import (Log,
-                     constants,
-                     startup,
-                     strings)
+from jx_mysql.mysql import MySQL
+
+from mo_logs import Log, constants, startup
 from mo_logs.convert import unix2datetime
 from mo_math.randoms import Random
 from mo_times import Date
-
-from treeherder.config.settings import DATABASES
-from treeherder.extract import extract_jobs
-from treeherder.model.models import (ClassifiedFailure,
-                                     Commit,
-                                     FailureClassification,
-                                     FailureLine,
-                                     Job,
-                                     JobDetail,
-                                     JobLog,
-                                     Option,
-                                     OptionCollection,
-                                     Push,
-                                     Repository,
-                                     RepositoryGroup,
-                                     TaskclusterMetadata,
-                                     TextLogError,
-                                     TextLogStep)
+from treeherder.extract import extract_jobs, extract_alerts
+from treeherder.model.models import (
+    ClassifiedFailure,
+    Commit,
+    FailureClassification,
+    FailureLine,
+    Job,
+    JobDetail,
+    JobLog,
+    Option,
+    OptionCollection,
+    Push,
+    Repository,
+    RepositoryGroup,
+    TaskclusterMetadata,
+    TextLogError,
+    TextLogStep,
+)
 
 
 @pytest.fixture
@@ -60,19 +59,17 @@ def complex_job(
         push=push,
         revision="ae6bb3a1066959a8c43d003a3caab0af769455bf",
         author="testing@mozilla.com",
-        comments="no comment"
+        comments="no comment",
     )
     Commit.objects.create(
         push=push,
         revision="0123456789012345678901234567890123456789",
         author="testing2@mozilla.com",
-        comments="no comment2"
+        comments="no comment2",
     )
 
     debug = Option.objects.create(name="debug")
-    oc = OptionCollection.objects.create(
-        option_collection_hash=Random.base64(5), option=debug
-    )
+    oc = OptionCollection.objects.create(option_collection_hash=Random.base64(5), option=debug)
 
     job = Job.objects.create(
         autoclassify_status=1,
@@ -106,19 +103,15 @@ def complex_job(
             "name": "Unnamed step",
             "result": 7,
             "started_line_number": 0,
-        }
+        },
     )
 
     TextLogError.objects.create(
         step=text_log_step, line="line contents here", line_number=619845839
     )
-    TextLogError.objects.create(
-        step=text_log_step, line="ERROR! more line contents", line_number=6
-    )
+    TextLogError.objects.create(step=text_log_step, line="ERROR! more line contents", line_number=6)
 
-    TaskclusterMetadata.objects.create(
-        job=job, retry_id=0, task_id="WWb9ExAvQUa78ku0DIxdSQ"
-    )
+    TaskclusterMetadata.objects.create(job=job, retry_id=0, task_id="WWb9ExAvQUa78ku0DIxdSQ")
 
     JobDetail.objects.create(
         job_id=job.id,
@@ -126,7 +119,7 @@ def complex_job(
             "title": "artifact uploaded",
             "url": "https://example.com/api/queue/v1/task/WWb9ExAvQUa78ku0DIxdSQ/runs/0/artifacts/public/test_info/wpt_raw.log",
             "value": "wpt_raw.log",
-        }
+        },
     )
     JobDetail.objects.create(
         job_id=job.id,
@@ -134,7 +127,7 @@ def complex_job(
             "title": "artifact uploaded",
             "url": "https://example.com/api/queue/v1/task/WWb9ExAvQUa78ku0DIxdSQ/runs/0/artifacts/public/test_info/wptreport.json",
             "value": "wptreport.json",
-        }
+        },
     )
     JobDetail.objects.create(job_id=job.id, **{"title": "CPU usage", "value": "26.8%"})
     JobDetail.objects.create(
@@ -178,7 +171,7 @@ def complex_job(
             "modified": 0,
             "stackwalk_stderr": 1578432686,
             "stackwalk_stdout": 1578432686,
-        }
+        },
     )
     FailureLine.objects.create(
         job_log=job_logs1,
@@ -194,27 +187,44 @@ def complex_job(
             "stackwalk_stderr": 1578432686,
             "stackwalk_stdout": 1578432686,
             "test": "/custom-elements/upgrading.html",
-        }
+        },
     )
 
     return job
 
 
 @pytest.fixture
-def extract_job_settings():
+def env_setup():
     # These values not directly accessed during testing, but the code requires that they be present.
     os.environ["NEW_RELIC_APP_NAME"] = "testing"
     os.environ["BIGQUERY_PRIVATE_KEY_ID"] = "1"
     os.environ["BIGQUERY_PRIVATE_KEY"] = "1"
 
-    # USE THE TEST SCHEMA
-    db_url = os.environ["DATABASE_URL"]
-    db_url = db_url.replace(
-        strings.between(db_url, "/", None), DATABASES["default"]["TEST"]["NAME"]
-    )
-    os.environ["DATABASE_URL"] = db_url
+    # THE DOCKER ENV IS DIFFERENT FROM THE DEV ENVIRONMENT
+    attempt = [
+        "mysql://root@127.0.0.1:3306/test_treeherder",
+        "mysql://root@mysql:3306/test_treeherder",
+    ]
+    for a in attempt:
+        try:
+            MySQL(host=a)
+            os.environ["DATABASE_URL"] = a
+        except Exception:
+            pass
 
+
+@pytest.fixture
+def extract_job_settings(env_setup):
     settings = startup.read_settings(filename=extract_jobs.CONFIG_FILE, complain=False)
+    settings.source.database.ssl = None  # NOT REQUIRED FOR TEST DATABASE
+    constants.set(settings.constants)
+    Log.start(settings.debug)
+    return settings
+
+
+@pytest.fixture
+def extract_alert_settings(env_setup):
+    settings = startup.read_settings(filename=extract_alerts.CONFIG_FILE, complain=False)
     settings.source.database.ssl = None  # NOT REQUIRED FOR TEST DATABASE
     constants.set(settings.constants)
     Log.start(settings.debug)
