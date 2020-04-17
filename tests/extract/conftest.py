bc457188301319e54c2af2222d479ec57d16edae
Kyle Lahnakoski <kyle@lahnakoski.com>
2020-04-17 14:11:15 -0400

    fix database url

--- tests/extract/conftest.py
+++ tests/extract/conftest.py
@@ -1,12 +1,11 @@
 import os
 
 import pytest
-from mo_logs import Log, constants, startup, strings
+
+from mo_logs import Log, constants, startup
 from mo_logs.convert import unix2datetime
 from mo_math.randoms import Random
 from mo_times import Date
-
-from treeherder.config.settings import DATABASES
 from treeherder.extract import extract_jobs, extract_alerts
 from treeherder.model.models import (
     ClassifiedFailure,
@@ -199,15 +198,7 @@ def env_setup():
     os.environ["NEW_RELIC_APP_NAME"] = "testing"
     os.environ["BIGQUERY_PRIVATE_KEY_ID"] = "1"
     os.environ["BIGQUERY_PRIVATE_KEY"] = "1"
-
-    # USE THE TEST SCHEMA
-    db_url = os.environ["DATABASE_URL"]
-    db_url = db_url.replace(
-        strings.between(db_url, "/", None), DATABASES["default"]["TEST"]["NAME"]
-    )
-    os.environ["DATABASE_URL"] = db_url
-
-    return
+    os.environ["DATABASE_URL"] = "mysql://root@localhost:3306/treeherder"
 
 
 @pytest.fixture
