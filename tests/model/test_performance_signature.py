4e54068bcb8756dc73d8a72f45767d0770c6fae7
Alexandru Irimovici <airimovici@mozilla.com>
2020-04-23 14:07:45 +0300

    clone signature in test

--- tests/model/test_performance_signature.py
+++ tests/model/test_performance_signature.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from treeherder.perf.models import PerformanceSignature
 
 
@@ -7,20 +5,8 @@ def test_performance_signatures_with_different_applications(test_perf_signature)
     assert PerformanceSignature.objects.count() == 1
 
     # create a performance signature that only differs from another existing one by the application name
-    PerformanceSignature.objects.create(
-        repository=test_perf_signature.repository,
-        signature_hash=test_perf_signature.signature_hash,
-        framework=test_perf_signature.framework,
-        platform=test_perf_signature.platform,
-        option_collection=test_perf_signature.option_collection,
-        suite=test_perf_signature.suite,
-        test=test_perf_signature.test,
-        application='chrome',
-        has_subtests=test_perf_signature.has_subtests,
-        tags=test_perf_signature.tags,
-        extra_options=test_perf_signature.extra_options,
-        measurement_unit=test_perf_signature.measurement_unit,
-        last_updated=datetime.now(),
-    )
+    test_perf_signature.id = None
+    test_perf_signature.application = 'chrome'
+    test_perf_signature.save()
 
     assert PerformanceSignature.objects.count() == 2
