7bbfcadb81b7157b88237a78c24b33f3d1d32404
SuyashSalampuria <suyash546@gmail.com>
2020-05-12 19:35:42 +0530

    add tests for FuzzyJobFinder component

--- tests/ui/job-view/fuzzy_test.jsx
+++ tests/ui/job-view/fuzzy_test.jsx
@@ -1,235 +1,69 @@
-import Fuse from 'fuse.js';
+import React from 'react';
+import { mount } from 'enzyme/build';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import configureMockStore from 'redux-mock-store';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 
-const testFuseArray = [
-  {
-    title: "Old Man's War",
-    author: {
-      firstName: 'John',
-      lastName: 'Scalzi',
+import fuzzyJobList from '../mock/job_list/fuzzy_jobs/fuzzyJobList.json';
+import filteredFuzzyList from '../mock/job_list/fuzzy_jobs/filteredFuzzyList.json';
+import searchMochitest from '../mock/job_list/fuzzy_jobs/searchMochitest.json';
+import FuzzyJobFinder from '../../../ui/job-view/pushes/FuzzyJobFinder';
+
+const mockStore = configureMockStore([thunk]);
+
+describe('FuzzyJobFinder', () => {
+  const isOpen = true;
+  const decisionTaskId = 'YHKMjYZeSSmEZTrAPdRIag';
+  const id = 705037;
+  const currentRepo = {
+    id: 77,
+    repository_group: {
+      name: 'development',
+      description:
+        'Collection of repositories where code initially lands in the development process',
     },
-  },
-  {
-    title: 'The Lock Artist',
-    author: {
-      firstName: 'Steve',
-      lastName: 'Hamilton',
-    },
-  },
-  {
-    title: 'HTML5',
-    author: {
-      firstName: 'Remy',
-      lastName: 'Sharp',
-    },
-  },
-  {
-    title: 'Right Ho Jeeves',
-    author: {
-      firstName: 'P.D',
-      lastName: 'Woodhouse',
-    },
-  },
-  {
-    title: 'The Code of the Wooster',
-    author: {
-      firstName: 'P.D',
-      lastName: 'Woodhouse',
-    },
-  },
-  {
-    title: 'Thank You Jeeves',
-    author: {
-      firstName: 'P.D',
-      lastName: 'Woodhouse',
-    },
-  },
-  {
-    title: 'The DaVinci Code',
-    author: {
-      firstName: 'Dan',
-      lastName: 'Brown',
-    },
-  },
-  {
-    title: 'Angels & Demons',
-    author: {
-      firstName: 'Dan',
-      lastName: 'Brown',
-    },
-  },
-  {
-    title: 'The Silmarillion',
-    author: {
-      firstName: 'J.R.R',
-      lastName: 'Tolkien',
-    },
-  },
-  {
-    title: 'Syrup',
-    author: {
-      firstName: 'Max',
-      lastName: 'Barry',
-    },
-  },
-  {
-    title: 'The Lost Symbol',
-    author: {
-      firstName: 'Dan',
-      lastName: 'Brown',
-    },
-  },
-  {
-    title: 'The Book of Lies',
-    author: {
-      firstName: 'Brad',
-      lastName: 'Meltzer',
-    },
-  },
-  {
-    title: 'Lamb',
-    author: {
-      firstName: 'Christopher',
-      lastName: 'Moore',
-    },
-  },
-  {
-    title: 'Fool',
-    author: {
-      firstName: 'Christopher',
-      lastName: 'Moore',
-    },
-  },
-  {
-    title: 'Incompetence',
-    author: {
-      firstName: 'Rob',
-      lastName: 'Grant',
-    },
-  },
-  {
-    title: 'Fat',
-    author: {
-      firstName: 'Rob',
-      lastName: 'Grant',
-    },
-  },
-  {
-    title: 'Colony',
-    author: {
-      firstName: 'Rob',
-      lastName: 'Grant',
-    },
-  },
-  {
-    title: 'Backwards, Red Dwarf',
-    author: {
-      firstName: 'Rob',
-      lastName: 'Grant',
-    },
-  },
-  {
-    title: 'The Grand Design',
-    author: {
-      firstName: 'Stephen',
-      lastName: 'Hawking',
-    },
-  },
-  {
-    title: 'The Book of Samson',
-    author: {
-      firstName: 'David',
-      lastName: 'Maine',
-    },
-  },
-  {
-    title: 'The Preservationist',
-    author: {
-      firstName: 'David',
-      lastName: 'Maine',
-    },
-  },
-  {
-    title: 'Fallen',
-    author: {
-      firstName: 'David',
-      lastName: 'Maine',
-    },
-  },
-  {
-    title: 'Monster 1959',
-    author: {
-      firstName: 'David',
-      lastName: 'Maine',
-    },
-  },
-];
-const searchDavid = [
-  {
-    title: 'The Book of Samson',
-    author: {
-      firstName: 'David',
-      lastName: 'Maine',
-    },
-  },
-  {
-    title: 'The Preservationist',
-    author: {
-      firstName: 'David',
-      lastName: 'Maine',
-    },
-  },
-  {
-    title: 'Fallen',
-    author: {
-      firstName: 'David',
-      lastName: 'Maine',
-    },
-  },
-  {
-    title: 'Monster 1959',
-    author: {
-      firstName: 'David',
-      lastName: 'Maine',
-    },
-  },
-  {
-    title: 'The DaVinci Code',
-    author: {
-      firstName: 'Dan',
-      lastName: 'Brown',
-    },
-  },
-  {
-    title: 'Angels & Demons',
-    author: {
-      firstName: 'Dan',
-      lastName: 'Brown',
-    },
-  },
-  {
-    title: 'The Lost Symbol',
-    author: {
-      firstName: 'Dan',
-      lastName: 'Brown',
-    },
-  },
-  {
-    title: 'The Grand Design',
-    author: {
-      firstName: 'Stephen',
-      lastName: 'Hawking',
-    },
-  },
-];
-test('Fuzzy search gives expected results', async () => {
-  const options = {
-    // http://fusejs.io/ describes the options available
-    keys: ['title', 'author.firstName'],
-    threshold: 0.6, // This seems like a good threshold to remove most false matches, lower is stricter
-    matchAllTokens: true,
-    tokenize: true,
+    name: 'autoland',
+    dvcs_type: 'hg',
+    url: 'https://hg.mozilla.org/integration/autoland',
+    branch: null,
+    codebase: 'gecko',
+    description: 'The destination for automatically landed Firefox commits.',
+    active_status: 'active',
+    performance_alerts_enabled: true,
+    expire_performance_data: false,
+    is_try_repo: false,
+    tc_root_url: 'https://firefox-ci-tc.services.mozilla.com',
+    pushLogUrl: 'https://hg.mozilla.org/integration/autoland/pushloghtml',
+    revisionHrefPrefix: 'https://hg.mozilla.org/integration/autoland/rev/',
   };
-  // expected results on searching David
-  const fuse = new Fuse(testFuseArray, options);
-  expect(fuse.search('David')).toStrictEqual(searchDavid);
+  const store = mockStore({ fuzzyList: [] });
+  const testFuzzyJobFinder = mount(
+    <Provider store={store}>
+      <FuzzyJobFinder
+        isOpen={isOpen}
+        toggle={() => {}}
+        jobList={fuzzyJobList}
+        filteredJobList={filteredFuzzyList}
+        className="fuzzy-modal"
+        pushId={id}
+        decisionTaskId={decisionTaskId}
+        currentRepo={currentRepo}
+      />
+    </Provider>,
+  );
+  test('Fuzzy search gives expected results', async () => {
+    const { getByTitle } = render(testFuzzyJobFinder);
+    const inputElement = await waitFor(() =>
+      getByTitle('Filter the list of runnable jobs'),
+    );
+    await waitFor(() =>
+      fireEvent.change(inputElement, { target: { value: 'Mochitest' } }),
+    );
+    await waitFor(() =>
+      fireEvent.keyDown(inputElement, { key: 'Enter', code: 'Enter' }),
+    );
+    const { fuzzyList } = testFuzzyJobFinder.state();
+    expect(fuzzyList).toStrictEqual(searchMochitest);
+  });
 });
