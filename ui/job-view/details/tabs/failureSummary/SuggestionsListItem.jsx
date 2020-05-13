c9ecaf6f648aa98cacae2db710fd9fd5ebc615a6
Greg Tatum <tatum.creative@gmail.com>
2020-05-13 13:40:39 -0500

    Refactor the bug suggestions JSX into a dynamic array

--- ui/job-view/details/tabs/failureSummary/SuggestionsListItem.jsx
+++ ui/job-view/details/tabs/failureSummary/SuggestionsListItem.jsx
@@ -22,9 +22,83 @@ export default class SuggestionsListItem extends React.Component {
     }));
   };
 
-  render() {
-    const { suggestion, toggleBugFiler, selectedJobFull } = this.props;
+  renderBugSuggestions() {
+    const { suggestion, selectedJobFull } = this.props;
     const { suggestionShowMore } = this.state;
+
+    const suggestions = [];
+
+    // Open recent bugs
+    if (suggestion.valid_open_recent) {
+      suggestions.push(
+        <ul
+          className="list-unstyled failure-summary-bugs"
+          key="open-recent-bugs"
+        >
+          {suggestion.bugs.open_recent.map((bug) => (
+            <BugListItem
+              key={bug.id}
+              bug={bug}
+              suggestion={suggestion}
+              selectedJobFull={selectedJobFull}
+            />
+          ))}
+        </ul>,
+      );
+    }
+
+    // All other bugs
+    if (suggestion.valid_all_others && suggestion.valid_open_recent) {
+      suggestions.push(
+        <Button
+          key="show-hide-more"
+          color="link"
+          rel="noopener"
+          onClick={this.clickShowMore}
+          className="show-hide-more"
+        >
+          Show / Hide more
+        </Button>,
+      );
+    }
+
+    if (
+      suggestion.valid_all_others &&
+      (suggestionShowMore || !suggestion.valid_open_recent)
+    ) {
+      suggestions.push(
+        <ul className="list-unstyled failure-summary-bugs" key="all-others">
+          {suggestion.bugs.all_others.map((bug) => (
+            <BugListItem
+              key={bug.id}
+              bug={bug}
+              suggestion={suggestion}
+              bugClassName={bug.resolution !== '' ? 'strike-through' : ''}
+              title={bug.resolution !== '' ? bug.resolution : ''}
+              selectedJobFull={selectedJobFull}
+            />
+          ))}
+        </ul>,
+      );
+    }
+
+    if (
+      suggestion.bugs.too_many_open_recent ||
+      (suggestion.bugs.too_many_all_others && !suggestion.valid_open_recent)
+    ) {
+      suggestions.push(
+        <mark key="too-many">
+          Exceeded max {thBugSuggestionLimit} bug suggestions, most of which are
+          likely false positives.
+        </mark>,
+      );
+    }
+
+    return suggestions;
+  }
+
+  render() {
+    const { suggestion, toggleBugFiler } = this.props;
 
     return (
       <li>
@@ -40,57 +114,7 @@ export default class SuggestionsListItem extends React.Component {
           </Button>
           <span>{suggestion.search}</span>
         </div>
-
-        {/* <!--Open recent bugs--> */}
-        {suggestion.valid_open_recent && (
-          <ul className="list-unstyled failure-summary-bugs">
-            {suggestion.bugs.open_recent.map((bug) => (
-              <BugListItem
-                key={bug.id}
-                bug={bug}
-                suggestion={suggestion}
-                selectedJobFull={selectedJobFull}
-              />
-            ))}
-          </ul>
-        )}
-
-        {/* <!--All other bugs--> */}
-        {suggestion.valid_all_others && suggestion.valid_open_recent && (
-          <Button
-            color="link"
-            rel="noopener"
-            onClick={this.clickShowMore}
-            className="show-hide-more"
-          >
-            Show / Hide more
-          </Button>
-        )}
-
-        {suggestion.valid_all_others &&
-          (suggestionShowMore || !suggestion.valid_open_recent) && (
-            <ul className="list-unstyled failure-summary-bugs">
-              {suggestion.bugs.all_others.map((bug) => (
-                <BugListItem
-                  key={bug.id}
-                  bug={bug}
-                  suggestion={suggestion}
-                  bugClassName={bug.resolution !== '' ? 'strike-through' : ''}
-                  title={bug.resolution !== '' ? bug.resolution : ''}
-                  selectedJobFull={selectedJobFull}
-                />
-              ))}
-            </ul>
-          )}
-
-        {(suggestion.bugs.too_many_open_recent ||
-          (suggestion.bugs.too_many_all_others &&
-            !suggestion.valid_open_recent)) && (
-          <mark>
-            Exceeded max {thBugSuggestionLimit} bug suggestions, most of which
-            are likely false positives.
-          </mark>
-        )}
+        {this.renderBugSuggestions()}
       </li>
     );
   }
