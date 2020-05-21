3de4e38f6f8f9ccb58ea42126ddffe76119d3022
Sarah Clements <sclements313@gmail.com>
2020-05-21 15:09:04 -0700

    Catch DataError in failureline.py

--- treeherder/log_parser/failureline.py
+++ treeherder/log_parser/failureline.py
@@ -5,7 +5,7 @@ from itertools import islice
 import newrelic.agent
 from django.conf import settings
 from django.db import transaction
-from django.db.utils import IntegrityError, OperationalError
+from django.db.utils import IntegrityError, OperationalError, DataError
 from requests.exceptions import HTTPError
 
 from treeherder.etl.text import astral_filter
@@ -50,6 +50,8 @@ def write_failure_lines(job_log, log_iter):
     with transaction.atomic():
         try:
             failure_lines = create(job_log, log_list)
+        except DataError as e:
+            logger.warning("Got DataError inserting failure_line: {}".format(e.args))
         except OperationalError as e:
             logger.warning("Got OperationalError inserting failure_line")
             # Retry iff this error is the "incorrect String Value" error
