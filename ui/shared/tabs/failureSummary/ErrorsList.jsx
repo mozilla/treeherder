ea8002412cea0ce125425729872ea88ff4481fdb
Sarah Clements <sclements313@gmail.com>
2020-06-01 15:57:16 -0700

    replace TextLogStep model in DetailsPanel and logviewer App

--- ui/shared/tabs/failureSummary/ErrorsList.jsx
+++ ui/shared/tabs/failureSummary/ErrorsList.jsx
@@ -2,11 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function ErrorsList(props) {
-  const errorListItem = props.errors.map((error, key) => (
-    <li
-      key={key} // eslint-disable-line react/no-array-index-key
-    >
-      {error.name} : {error.result}.
+  const errorListItem = props.errors.map((error) => (
+    <li key={error.line_number}>
+      {error.line}
       <a
         title="Open in Log Viewer"
         target="_blank"
@@ -22,7 +20,7 @@ export default function ErrorsList(props) {
     <li>
       No Bug Suggestions Available.
       <br />
-      <span className="font-weight-bold">Unsuccessful Execution Steps</span>
+      <span className="font-weight-bold">Failure Lines</span>
       <ul>{errorListItem}</ul>
     </li>
   );
