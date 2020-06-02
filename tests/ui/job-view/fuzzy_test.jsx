09b3100f75f6d591abecfe90170dad51397695a8
SuyashSalampuria <suyash546@gmail.com>
2020-06-03 00:07:47 +0530

    modifies tests

--- tests/ui/job-view/fuzzy_test.jsx
+++ tests/ui/job-view/fuzzy_test.jsx
@@ -50,7 +50,7 @@ describe('FuzzyJobFinder', () => {
       />
     </Provider>
   );
-  const initialList =
+  const initialJobs =
     'addon-tps-xpiartifact-build-linux64-artifact/optbuild-android-aarch64-gcp/debugbuild-android-aarch64-gcp/optbuild-android-aarch64/debugbuild-android-aarch64/optbuild-android-aarch64/pgobuild-android-api-16-gcp/debugbuild-android-api-16-gcp/optbuild-android-api-16/debugbuild-android-api-16/optbuild-android-api-16/pgobuild-android-geckoview-docs/optbuild-android-x86-fuzzing/debugbuild-android-x86-gcp/optbuild-android-x86/optbuild-android-x86_64-asan-fuzzing/optbuild-android-x86_64-gcp/debugbuild-android-x86_64-gcp/optbuild-android-x86_64/debugbuild-android-x86_64/optbuild-fat-aar-android-geckoview-fat-aar/optbuild-linux-devedition/optbuild-linux-gcp/debugbuild-linux-gcp/optbuild-linux-reproduced/optbuild-linux-rusttests/debugbuild-linux-rusttests/optbuild-linux-shippable/optbuild-linux/debugbuild-linux/optbuild-linux64-aarch64/optbuild-linux64-add-on-devel/optbuild-linux64-asan-fuzzing-ccov/optbuild-linux64-asan-fuzzing/optbuild-linux64-asan/debugbuild-linux64-asan/optbuild-linux64-ccov/optbuild-linux64-devedition/optbuild-linux64-fuzzing-ccov/optbuild-linux64-fuzzing/debugbuild-linux64-gcp/debugbuild-linux64-gcp/optbuild-linux64-noopt/debugbuild-linux64-plain/debugbuild-linux64-plain/optbuild-linux64-rusttests/debugtest-windows10-64/opt-reftest-fis-e10s-2test-windows10-64/opt-reftest-no-accel-e10s-1test-windows10-64/opt-reftest-no-accel-e10s-2test-windows10-64/opt-reftest-no-accel-e10s-3test-windows10-64/opt-reftest-no-accel-e10s-4test-windows10-64/opt-talos-bcv-e10stest-windows10-64/opt-talos-chrome-e10stest-windows10-64/opt-talos-damp-e10stest-windows10-64/opt-talos-dromaeojs-e10stest-windows10-64/opt-talos-g1-e10stest-windows10-64/opt-talos-g4-e10stest-windows10-64/opt-talos-g5-e10stest-windows10-64/opt-talos-other-e10s';
   const searchLinuxResults =
     'build-linux-devedition/optbuild-linux-gcp/debugbuild-linux-gcp/optbuild-linux-reproduced/optbuild-linux-rusttests/debugbuild-linux-rusttests/optbuild-linux-shippable/optbuild-linux/debugbuild-linux/optbuild-linux64-aarch64/optbuild-linux64-add-on-devel/optbuild-linux64-asan-fuzzing-ccov/optbuild-linux64-asan-fuzzing/optbuild-linux64-asan/debugbuild-linux64-asan/optbuild-linux64-ccov/optbuild-linux64-devedition/optbuild-linux64-fuzzing-ccov/optbuild-linux64-fuzzing/debugbuild-linux64-gcp/debugbuild-linux64-gcp/optbuild-linux64-noopt/debugbuild-linux64-plain/debugbuild-linux64-plain/optbuild-linux64-rusttests/debugartifact-build-linux64-artifact/opt';
@@ -58,10 +58,18 @@ describe('FuzzyJobFinder', () => {
   test('Fuzzy search gives expected results', async () => {
     const { getByTitle, getByTestId } = await render(testFuzzyJobFinder);
     const inputElement = getByTitle('Filter the list of runnable jobs');
+    // fuzzyList is the select Input block where the searched jobs are displayed
     const fuzzySearchList = getByTestId('fuzzyList');
 
-    expect(fuzzyJobList).toHaveLength(60);
-    await waitFor(() => expect(fuzzySearchList).toHaveTextContent(initialList));
+    await waitFor(() => {
+      expect(fuzzySearchList).toHaveTextContent(initialJobs);
+      expect(fuzzySearchList).toHaveLength(60);
+      // Next line Gives error  TypeError: fuzzySearchList.map is not a function
+      const fuzzySearchArray = fuzzySearchList.options.map(
+        (job) => job.lastChild.innerHTML,
+      );
+      expect(fuzzySearchArray).toBe(expect.arrayContaining(fuzzyJobList));
+    });
 
     await fireEvent.change(inputElement, { target: { value: 'linux' } });
 
