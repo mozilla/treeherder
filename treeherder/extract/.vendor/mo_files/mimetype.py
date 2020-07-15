0ef152909513a66024cef086babfda5ed7c61067
Kyle Lahnakoski <kyle@lahnakoski.com>
2020-07-15 18:40:04 -0400

    remove

--- treeherder/extract/.vendor/mo_files/mimetype.py
+++
@@ -1,6 +0,0 @@
-from mo_future import text
-
-BINARY = text("application/octet-stream")
-GZIP = text("application/gzip")
-JSON = text("application/json")
-ZIP = text("application/zip")
--- treeherder/extract/.vendor/mo_math/vendor/strangman/README.md
+++
@@ -1,1 +0,0 @@
-snagged from http://www.nmr.mgh.harvard.edu/Neural_Systems_Group/gary/python.html
