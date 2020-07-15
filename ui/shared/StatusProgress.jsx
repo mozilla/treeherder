84c674541e28ac7660ca1579831343ef1f4c3ba1
Suyash Salampuria <suyash546@gmail.com>
2020-07-16 00:51:44 +0530

    Bug 1647447 - Replace Push Health progess bar with donut graph (#6601)

--- ui/shared/StatusProgress.jsx
+++ ui/shared/StatusProgress.jsx
@@ -1,23 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Progress } from 'reactstrap';
+import { VictoryPie } from 'victory';
+
+import { getPercentComplete } from '../helpers/display';
 
 const StatusProgress = (props) => {
   const {
     counts: { success, testfailed, busted, running, pending },
   } = props;
   const failed = testfailed || 0 + busted || 0;
+  const percentComplete = props.counts ? getPercentComplete(props.counts) : 0;
 
   return (
-    <Progress
-      multi
-      title={`${failed} failed, ${success} success, ${running} running, ${pending} pending`}
-    >
-      <Progress bar color="danger" value={failed} />
-      <Progress bar color="success" value={success} />
-      <Progress bar color="info" value={running} striped />
-      <Progress bar color="secondary" value={pending} />
-    </Progress>
+    <div className="relative">
+      <VictoryPie
+        data={[
+          { x: 'success', y: success },
+          { x: 'running', y: running },
+          { x: 'pending', y: pending },
+          { x: 'failed', y: failed },
+        ]}
+        colorScale={['#28a745', '#17a2b8', '#6c757d', '#dc3545']}
+        labels={() => ''}
+        height={200}
+        width={200}
+        padding={{ top: 20, bottom: 15 }}
+        innerRadius={70}
+        radius={85}
+        style={{
+          labels: {
+            fontSize: 12,
+          },
+        }}
+      />
+      <div className="absolute">
+        <div style={{ fontSize: '30px' }}>{percentComplete}%</div>
+        <div>Complete</div>
+      </div>
+    </div>
   );
 };
 
