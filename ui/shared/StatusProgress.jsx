4b0ba60a3d72b7afaaf39dbe95986661d5a435f2
SuyashSalampuria <suyash546@gmail.com>
2020-07-16 12:25:21 +0530

    add tooltip to show area details

--- ui/shared/StatusProgress.jsx
+++ ui/shared/StatusProgress.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { VictoryPie } from 'victory';
+import { VictoryPie, VictoryTooltip } from 'victory';
 
 import { getPercentComplete } from '../helpers/display';
 
@@ -21,10 +21,13 @@ const StatusProgress = (props) => {
           { x: 'failed', y: failed },
         ]}
         colorScale={['#28a745', '#17a2b8', '#6c757d', '#dc3545']}
-        labels={() => ''}
+        labels={({ datum }) => (datum.y > 0 ? `${datum.x}: ${datum.y}` : '')}
+        labelComponent={
+          <VictoryTooltip pointerLength={0} flyoutComponent={<div />} />
+        }
         height={200}
         width={200}
-        padding={{ top: 20, bottom: 15 }}
+        padding={{ top: 35, bottom: 15 }}
         innerRadius={70}
         radius={85}
         style={{
