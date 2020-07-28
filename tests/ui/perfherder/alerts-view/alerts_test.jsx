e94b9e700e0a80a4dd90620e19a66337adcd74b8
Tarek Ziade <tarek@ziade.org>
2020-07-28 10:37:20 +0200

    added the xpcshell framework

--- tests/ui/perfherder/alerts-view/alerts_test.jsx
+++ tests/ui/perfherder/alerts-view/alerts_test.jsx
@@ -45,6 +45,7 @@ const frameworks = [
   { id: 12, name: 'devtools' },
   { id: 13, name: 'browsertime' },
   { id: 14, name: 'vcs' },
+  { id: 15, name: 'xpcshell' },
 ];
 
 const dummyFrameworkName = 'someTestFramework';
