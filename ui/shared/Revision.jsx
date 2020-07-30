fa962882a497d717c835df33ab98208807e8b2b8
Tin La <tinsila1@gmail.com>
2020-07-30 13:15:00 -0500

    removed commented space

--- ui/shared/Revision.jsx
+++ ui/shared/Revision.jsx
@@ -120,7 +120,6 @@ export class Revision extends React.PureComponent {
                 </div>
               );
             })}
-          {/* {<div style={{ visibility: 'hidden' }}>invisible space</div>} */}
           {<span className="mt-3">{`Commit: ${comment}`}</span>}
         </UncontrolledTooltip>
       </Row>
