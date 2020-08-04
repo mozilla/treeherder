6c47a71f886b25760f7ee9d89c4228e10474a7c2
SuyashSalampuria <suyash546@gmail.com>
2020-08-04 21:53:33 +0530

    add job_type to select related

--- treeherder/push_health/tests.py
+++ treeherder/push_health/tests.py
@@ -166,12 +166,12 @@ def get_test_failure_jobs(push):
     testfailed_jobs = (
         Job.objects.filter(push=push, tier__lte=2, result='testfailed',)
         .exclude(Q(machine_platform__platform='lint') | Q(job_type__symbol='mozlint'),)
-        .select_related('machine_platform', 'taskcluster_metadata')
+        .select_related('job_type', 'machine_platform', 'taskcluster_metadata')
     )
     failed_job_types = [job.job_type.name for job in testfailed_jobs]
     passing_jobs = Job.objects.filter(
         push=push, job_type__name__in=failed_job_types, result__in=['success', 'unknown']
-    ).select_related('machine_platform', 'taskcluster_metadata')
+    ).select_related('job_type', 'machine_platform', 'taskcluster_metadata')
 
     jobs = {}
 
