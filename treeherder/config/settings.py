c3cf77d1ce94e07c3100f456db73a6eafa0446c4
ionutgoldan <gold.ionut@gmail.com>
2020-09-07 11:55:51 +0300

    Bug 1660779 - Prepare feature flag in settings.py

--- treeherder/config/settings.py
+++ treeherder/config/settings.py
@@ -461,6 +461,11 @@ BUG_COOLDOWN_TIME = timedelta(weeks=2)  # time after bug is ready for assessment
 
 # Only generate alerts for data newer than this time in seconds in perfherder
 PERFHERDER_ALERTS_MAX_AGE = timedelta(weeks=2)
+# From the same job's log, ingest (or not) multiple PERFHERDER_DATA dumps
+# pertaining to the same performance signature
+PERFHERDER_ENABLE_MULTIDATA_INGESTION = env.bool(
+    'PERFHERDER_ENABLE_MULTIDATA_INGESTION', default=True
+)
 
 # Performance sheriff bot settings
 MAX_BACKFILLS_PER_PLATFORM = {
