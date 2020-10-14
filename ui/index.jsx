c70c14d55cfc68a964082f8a9eaa455b8cc01642
Sarah Clements <sclements313@gmail.com>
2020-10-14 16:22:49 -0700

    eslint fix

--- ui/index.jsx
+++ ui/index.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import { ConnectedRouter } from 'connected-react-router';
 import { Provider } from 'react-redux';
+
 import { configureStore, history } from './job-view/redux/configureStore';
+import App from './App';
 
 import './css/treeherder-custom-styles.css';
 import './css/treeherder-navbar.css';
 import './css/treeherder.css';
-
-import App from './App';
 
 render(
   <Provider store={configureStore()}>
