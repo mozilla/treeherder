d585b67b6a4987a35d888eab7844ca1d2bd142b6
Cameron Dawson <cdawson@mozilla.com>
2020-10-21 15:35:48 -0700

    Bug 1640024 - Make group_results endpoint match mozci format

--- treeherder/log_parser/failureline.py
+++ treeherder/log_parser/failureline.py
@@ -182,17 +182,12 @@ def get_group_results(push):
         'group_result__status',
         'name',
         'job_logs__job__taskcluster_metadata__task_id',
-        'job_logs__job__job_type__name',
     )
 
-    by_task_id = defaultdict(list)
+    by_task_id = defaultdict(dict)
     for group in groups:
-        by_task_id[group['job_logs__job__taskcluster_metadata__task_id']].append(
-            {
-                'group': group['name'],
-                'status': GroupStatus.STATUS_LOOKUP[group['group_result__status']],
-                'label': group['job_logs__job__job_type__name'],
-            }
-        ),
+        by_task_id[group['job_logs__job__taskcluster_metadata__task_id']][
+            group['name']
+        ] = bool(GroupStatus.STATUS_LOOKUP[group['group_result__status']] == "OK")
 
     return by_task_id
