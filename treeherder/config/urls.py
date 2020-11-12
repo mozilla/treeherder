eeb1d037b0367335423a12c84835a5c98d2e558d
Sparsh-Bansal <44178670+Sparsh-Bansal@users.noreply.github.com>
2020-11-13 03:32:06 +0530

    change url to re_path (#6844)

--- treeherder/config/urls.py
+++ treeherder/config/urls.py
@@ -1,13 +1,13 @@
 from django.conf import settings
-from django.conf.urls import include, url, re_path
+from django.urls import include, re_path
 from rest_framework.documentation import include_docs_urls
 
 from treeherder.webapp.api import urls as api_urls
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    url(r'^api/', include(api_urls)),
-    url(r'^docs/', include_docs_urls(title='REST API Docs')),
+    re_path(r'^api/', include(api_urls)),
+    re_path(r'^docs/', include_docs_urls(title='REST API Docs')),
     re_path(r'', TemplateView.as_view(template_name='index.html')),
 ]
 
@@ -15,5 +15,5 @@ if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns += [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
+        re_path(r'^__debug__/', include(debug_toolbar.urls)),
     ]
