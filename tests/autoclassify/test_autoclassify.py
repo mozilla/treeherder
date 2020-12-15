ed41015a48d46acf4c7ce5dafd23084761834d59
Cameron Dawson <cdawson@mozilla.com>
2020-12-15 11:08:07 -0800

    Bug 1594822 - Remove Autoclassify and Crossreference without migration (#6915)
    * Revert "Revert "Bug 1594822 - Remove Autoclassify and Crossreference code (#6897)""
    
    This reverts commit f671b32bdc3d7707f9445a520896232491950324.
    * remove migration changes

--- tests/autoclassify/test_autoclassify.py
+++
@@ -1,8 +0,0 @@
-from treeherder.autoclassify.autoclassify import get_matchers
-
-
-def test_get_matchers():
-    matchers = list(get_matchers())
-
-    assert len(matchers) == 2
-    assert all(m.__name__.endswith('_matcher') for m in matchers)
