5fd392ed76f33ed5a022e2764c86870809436f86
Armen Zambrano Gasparnian <armenzg@sentry.io>
2021-01-19 16:10:18 -0500

    Add Sentry to frontend

--- ui/index.jsx
+++ ui/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import * as Sentry from '@sentry/react';
 
 import App from './App';
 
 import './css/treeherder-custom-styles.css';
 import './css/treeherder-navbar.css';
 import './css/treeherder-base.css';
+
+Sentry.init({
+  dsn:
+    'https://55e8465b492c41fbb6af031de8b13e3e@o493645.ingest.sentry.io/5567629',
+});
 
 render(<App />, document.getElementById('root'));
