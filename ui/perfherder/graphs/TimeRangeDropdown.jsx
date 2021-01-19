7c3f508b1d5be282a42e604b688f67b08078cf34
Ionut Goldan <gold.ionut@gmail.com>
2021-01-19 11:15:32 +0200

    Bug 1604871 - Export dropdown as standalone component

---
+++ ui/perfherder/graphs/TimeRangeDropdown.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { DropdownToggle, UncontrolledDropdown } from 'reactstrap';
+import PropTypes from 'prop-types';
+
+import DropdownMenuItems from '../../shared/DropdownMenuItems';
+import { phTimeRanges } from '../constants';
+
+class TimeRangeDropdown extends React.PureComponent {
+  render() {
+    const { timeRangeText, updateTimeRange } = this.props;
+
+    return (
+      <UncontrolledDropdown
+        className="mr-0 text-nowrap"
+        title="Time range"
+        aria-label="Time range"
+      >
+        <DropdownToggle caret>{timeRangeText}</DropdownToggle>
+        <DropdownMenuItems
+          options={phTimeRanges.map((item) => item.text)}
+          selectedItem={timeRangeText}
+          updateData={(value) =>
+            updateTimeRange(phTimeRanges.find((item) => item.text === value))
+          }
+        />
+      </UncontrolledDropdown>
+    );
+  }
+}
+
+export default TimeRangeDropdown;
+
+TimeRangeDropdown.propTypes = {
+  timeRangeText: PropTypes.string.isRequired,
+  updateTimeRange: PropTypes.func.isRequired,
+};
