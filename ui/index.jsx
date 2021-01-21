c9a34a31248bed8a8c0d85b0533fd19b49f500e8
Armen Zambrano Gasparnian <armenzg@sentry.io>
2021-01-21 10:46:33 -0500

    Few more changes

--- ui/index.jsx
+++ ui/index.jsx
@@ -14,15 +14,19 @@ import './css/treeherder-base.css';
 // During development we want to report to the 'dev' release
 // rather than reporting commits from the local checkout, otherwise, let
 // it use the commit hash
-Sentry.init({
-  dsn:
-    'https://c6385f3aab0c4340b90ec2db9e9e3544@o493645.ingest.sentry.io/5567629',
-  environment:
-    process.env.NODE_ENV === 'production'
-      ? process.env.HEROKU_APP
-      : 'development',
-  release: process.env.NODE_ENV === 'dev' ? 'env' : null,
-});
-Sentry.captureMessage('Something went wrong');
+const extraOptions =
+  process.env.NODE_ENV === 'development' ? { release: 'dev' } : {};
+
+Sentry.init(
+  ...{
+    dsn:
+      'https://c6385f3aab0c4340b90ec2db9e9e3544@o493645.ingest.sentry.io/5567629',
+    environment:
+      process.env.NODE_ENV === 'production'
+        ? process.env.HEROKU_APP
+        : 'development',
+  },
+  ...extraOptions,
+);
 
 render(<App />, document.getElementById('root'));
