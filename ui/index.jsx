9db181124fcd89e9c9d9fc97294aab3f654aa184
Armen Zambrano Gasparnian <armenzg@sentry.io>
2021-01-21 09:25:04 -0500

    Public DSN is fine

--- ui/index.jsx
+++ ui/index.jsx
@@ -8,8 +8,21 @@ import './css/treeherder-custom-styles.css';
 import './css/treeherder-navbar.css';
 import './css/treeherder-base.css';
 
+// During production we want to report under which app the code
+// is deployed by using HEROKU_APP (e.g. treeherder-prod)
+//
+// During development we want to report to the 'dev' release
+// rather than reporting commits from the local checkout, otherwise, let
+// it use the commit hash
 Sentry.init({
-  dsn: process.env.SENTRY_DSN,
+  dsn:
+    'https://c6385f3aab0c4340b90ec2db9e9e3544@o493645.ingest.sentry.io/5567629',
+  environment:
+    process.env.NODE_ENV === 'production'
+      ? process.env.HEROKU_APP
+      : 'development',
+  release: process.env.NODE_ENV === 'dev' ? 'env' : null,
 });
+Sentry.captureMessage('Something went wrong');
 
 render(<App />, document.getElementById('root'));
