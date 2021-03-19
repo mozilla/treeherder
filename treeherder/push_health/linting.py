bf286969331ab5ac2db2147e793c55e685274673
Sarah Clements <sclements313@gmail.com>
2021-03-18 17:28:25 -0700

    refactor lints and builds to use same jobs

--- treeherder/push_health/linting.py
+++ treeherder/push_health/linting.py
@@ -1,16 +1,11 @@
-from django.db.models import Q
-
-from treeherder.model.models import Job
 from treeherder.push_health.utils import get_job_results
 
 
-def get_lint_failures(push):
-    lint_results = Job.objects.filter(
-        Q(machine_platform__platform='lint') | Q(job_type__symbol='mozlint'),
-        push=push,
-        tier__lte=2,
-    ).select_related('machine_platform', 'taskcluster_metadata', 'job_type', 'job_group')
-
+def get_lint_failures(push, jobs):
+    lint_results = [
+        job
+        for job in list(jobs)
+        if job.machine_platform.platform == 'lint' or job.job_type.symbol == 'mozlint'
+    ]
     result, failures, in_progress_count = get_job_results(lint_results, 'testfailed')
-
     return (result, failures, in_progress_count)
