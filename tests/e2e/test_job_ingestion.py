4a8401dae6db700258281124c55fcbb997ffaeac
Sarah Clements <sclements313@gmail.com>
2021-04-12 16:23:52 -0700

    Bug 1704311 - Re-enable caching of bug suggestions during ingestion

--- tests/e2e/test_job_ingestion.py
+++ tests/e2e/test_job_ingestion.py
@@ -1,9 +1,9 @@
-# from mock import MagicMock
+from mock import MagicMock
 
 from tests.test_utils import add_log_response
 from treeherder.etl.jobs import store_job_data
 
-# from treeherder.model.error_summary import get_error_summary
+from treeherder.model.error_summary import get_error_summary
 from treeherder.model.models import Job, JobLog, TextLogError
 
 
@@ -24,10 +24,10 @@ def test_store_job_with_unparsed_log(
 
     # create a wrapper around get_error_summary that records whether
     # it's been called
-    # mock_get_error_summary = MagicMock(name='get_error_summary', wraps=get_error_summary)
-    # import treeherder.model.error_summary
+    mock_get_error_summary = MagicMock(name='get_error_summary', wraps=get_error_summary)
+    import treeherder.model.error_summary
 
-    # monkeypatch.setattr(treeherder.model.error_summary, 'get_error_summary', mock_get_error_summary)
+    monkeypatch.setattr(treeherder.model.error_summary, 'get_error_summary', mock_get_error_summary)
     log_url = add_log_response("mozilla-central-macosx64-debug-bm65-build1-build15.txt.gz")
     errorsummary = add_log_response("mochitest-browser-chrome_errorsummary.log")
 
@@ -50,9 +50,9 @@ def test_store_job_with_unparsed_log(
     assert TextLogError.objects.count() == 3
     # verify that get_error_summary was called (to warm the bug suggestions
     # cache)
-    # assert mock_get_error_summary.called
-    # # should have 3 error summary lines
-    # assert len(get_error_summary(Job.objects.get(id=1))) == 3
+    assert mock_get_error_summary.called
+    # should have 3 error summary lines
+    assert len(get_error_summary(Job.objects.get(id=1))) == 3
 
 
 def test_store_job_pending_to_completed_with_unparsed_log(
@@ -70,7 +70,7 @@ def test_store_job_pending_to_completed_with_unparsed_log(
     store_job_data(test_repository, [job_data])
     # should have no text log errors or bug suggestions
     assert TextLogError.objects.count() == 0
-    # assert get_error_summary(Job.objects.get(guid=job_guid)) == []
+    assert get_error_summary(Job.objects.get(guid=job_guid)) == []
 
     # the second time, post a log that will get parsed
     log_url = add_log_response("mozilla-central-macosx64-debug-bm65-build1-build15.txt.gz")
@@ -89,7 +89,7 @@ def test_store_job_pending_to_completed_with_unparsed_log(
 
     # should have a full set of text log errors
     assert TextLogError.objects.count() == 3
-    # assert len(get_error_summary(Job.objects.get(guid=job_guid))) == 3
+    assert len(get_error_summary(Job.objects.get(guid=job_guid))) == 3
 
 
 def test_store_job_with_tier(test_repository, failure_classifications, push_stored):
