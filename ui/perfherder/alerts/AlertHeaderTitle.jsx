8c1675158ffb417f3bc0d0019824936bac24564e
esanuandra <andra.esanu@softvision.com>
2021-07-07 15:22:30 +0300

    Bug 1718601 - Fix icon position

--- ui/perfherder/alerts/AlertHeaderTitle.jsx
+++ ui/perfherder/alerts/AlertHeaderTitle.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from 'react-router-dom';
-import { Row, Badge } from 'reactstrap';
+import { Row, Col, Badge } from 'reactstrap';
 
 import Clipboard from '../../shared/Clipboard';
 import { getFrameworkName, getTitle } from '../perf-helpers/helpers';
@@ -25,34 +25,35 @@ export default class AlertHeaderTitle extends React.Component {
     const { clipboardVisible } = this.state;
     return (
       <Row
-        className="d-flex align-items-start"
         onMouseEnter={() => this.showClipboard(true)}
         onMouseLeave={() => this.showClipboard(false)}
       >
-        <Link
-          className="text-dark mr-1"
-          to={`./alerts?id=${alertSummary.id}&hideDwnToInv=0`}
-          id={`alert summary ${alertSummary.id.toString()} title`}
-          data-testid={`alert summary ${alertSummary.id.toString()} title`}
-        >
-          <h6 className="font-weight-bold align-middle">
-            <Badge className="mr-2">
-              {getFrameworkName(frameworks, alertSummary.framework)}
-            </Badge>
-            Alert #{alertSummary.id} - {alertSummary.repository} -{' '}
-            {getTitle(alertSummary)}{' '}
-            <FontAwesomeIcon
-              icon={faExternalLinkAlt}
-              className="icon-superscript"
-            />
-          </h6>
-        </Link>
-        <Clipboard
-          text={`${alertSummary.id}`}
-          description="Alert ID"
-          visible={clipboardVisible}
-          color="transparent"
-        />
+        <Col className="d-flex align-items-start p-0">
+          <Link
+            className="text-dark mr-1"
+            to={`./alerts?id=${alertSummary.id}&hideDwnToInv=0`}
+            id={`alert summary ${alertSummary.id.toString()} title`}
+            data-testid={`alert summary ${alertSummary.id.toString()} title`}
+          >
+            <h6 className="font-weight-bold align-middle">
+              <Badge className="mr-2">
+                {getFrameworkName(frameworks, alertSummary.framework)}
+              </Badge>
+              Alert #{alertSummary.id} - {alertSummary.repository} -{' '}
+              {getTitle(alertSummary)}{' '}
+              <FontAwesomeIcon
+                icon={faExternalLinkAlt}
+                className="icon-superscript"
+              />
+            </h6>
+          </Link>
+          <Clipboard
+            text={`${alertSummary.id}`}
+            description="Alert ID"
+            visible={clipboardVisible}
+            color="transparent"
+          />
+        </Col>
       </Row>
     );
   }
