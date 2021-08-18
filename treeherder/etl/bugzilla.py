619528f037e93697604fb3509f7e124b08048635
Sebastian Hengst <aryx.github@gmx-topmail.de>
2021-08-18 14:39:52 +0200

    Bug 1731631 - only ingest data about intermittent bugs changed since the last update of the bugscache

--- treeherder/etl/bugzilla.py
+++ treeherder/etl/bugzilla.py
@@ -1,7 +1,9 @@
 import logging
 
 import dateutil.parser
+from datetime import datetime, timedelta
 from django.conf import settings
+from django.db.models import Max
 
 from treeherder.model.models import Bugscache
 from treeherder.utils.github import fetch_json
@@ -9,15 +11,22 @@ from treeherder.utils.github import fetch_json
 logger = logging.getLogger(__name__)
 
 
-def fetch_intermittent_bugs(offset, limit):
+def fetch_intermittent_bugs(last_change_time, offset, limit):
+    last_change_time_string = last_change_time.strftime('%Y-%m-%dT%H:%M:%SZ')
     url = settings.BZ_API_URL + '/rest/bug'
     params = {
         'keywords': 'intermittent-failure',
-        'chfieldfrom': '-1y',
-        'include_fields': (
-            'id,summary,status,resolution,cf_crash_signature,'
-            'keywords,last_change_time, whiteboard'
-        ),
+        'last_change_time': last_change_time,
+        'include_fields': ','.join([
+            'id',
+            'summary',
+            'status',
+            'resolution',
+            'cf_crash_signature',
+            'keywords',
+            'last_change_time',
+            'whiteboard',
+        ]),
         'offset': offset,
         'limit': limit,
     }
@@ -29,6 +38,13 @@ class BzApiBugProcess:
     def run(self):
         bug_list = []
 
+        year_ago = datetime.utcnow() - timedelta(days = 365)
+        last_change_time_max = Bugscache.objects.all().aggregate(Max('modified'))['modified__max'] or None
+        if last_change_time_max:
+            last_change_time_max -= timedelta(minutes = 10)
+        else:
+            last_change_time_max = year_ago
+
         offset = 0
         limit = 500
         max_summary_length = Bugscache._meta.get_field('summary').max_length
@@ -36,16 +52,15 @@ class BzApiBugProcess:
 
         # Keep querying Bugzilla until there are no more results.
         while True:
-            bug_results_chunk = fetch_intermittent_bugs(offset, limit)
+            bug_results_chunk = fetch_intermittent_bugs(last_change_time_max, offset, limit)
             bug_list += bug_results_chunk
             if len(bug_results_chunk) < limit:
                 break
             offset += limit
 
+        insert_errors_observed = False
         if bug_list:
-            bugs_stored = set(Bugscache.objects.values_list('id', flat=True))
-            old_bugs = bugs_stored.difference(set(bug['id'] for bug in bug_list))
-            Bugscache.objects.filter(id__in=old_bugs).delete()
+            Bugscache.objects.filter(modified__lt=year_ago).delete()
 
             for bug in bug_list:
                 # we currently don't support timezones in treeherder, so
@@ -68,3 +83,10 @@ class BzApiBugProcess:
                     )
                 except Exception as e:
                     logger.error("error inserting bug '%s' into db: %s", bug, e)
+                    insert_errors_observed = True
+            if insert_errors_observed:
+                logger.error("error inserting some bugs, bugscache is incomplete, bugs updated during run will be ingested again during the next run")
+                # Move modification date of bugs inserted/updated during this
+                # run back to attempt to ingest bug data which failed during
+                # this insert/update in the next run.
+                Bugscache.objects.filter(modified__gt=last_change_time_max).update(modified=last_change_time_max)
