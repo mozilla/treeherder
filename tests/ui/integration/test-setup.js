f416cb802ded33b92215864a6d7c44dad632beb7
Joel Maher <joel.maher@gmail.com>
2021-10-12 16:14:49 -0700

    remove enzyme* and re-write bugfiler tests

--- tests/ui/integration/test-setup.js
+++ tests/ui/integration/test-setup.js
@@ -1,6 +1,2 @@
 // Entry point for Jest tests
-import { configure } from 'enzyme/build';
-import Adapter from 'enzyme-adapter-react-16/build';
 import '@testing-library/jest-dom/extend-expect';
-
-configure({ adapter: new Adapter() });
