86b2624623919723896ea069a15da1fbc7d36300
Sorin Mihai Toma <sorin.toma@softvision.com>
2021-12-06 17:02:40 +0200

    Bug 1744530 - Add user feedback on click for the copy to clipboard button

--- ui/shared/Clipboard.jsx
+++ ui/shared/Clipboard.jsx
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faClipboard } from '@fortawesome/free-regular-svg-icons';
+import {
+  faClipboard,
+  faCheckCircle,
+} from '@fortawesome/free-regular-svg-icons';
 import { Button } from 'reactstrap';
 
-const Clipboard = ({ description, text, outline, visible, color }) => {
+const Clipboard = ({ description, text, outline, color }) => {
+  const [copied, setCopied] = useState(false);
+
   if (!text) {
     return null;
   }
 
-  const copyToClipboard = () => navigator.clipboard.writeText(text);
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(text);
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 700);
+  };
 
   return (
     <Button
       type="button"
       title={`Copy ${description}`}
       onClick={copyToClipboard}
-      className={`py-0 px-1 ${visible ? '' : 'invisible'}`}
+      className="py-0 px-1"
       color={`${color || 'light'}`}
       outline={outline}
     >
-      <FontAwesomeIcon icon={faClipboard} />
+      {copied ? (
+        <FontAwesomeIcon icon={faCheckCircle} color="#2da745" />
+      ) : (
+        <FontAwesomeIcon icon={faClipboard} />
+      )}
     </Button>
   );
 };
@@ -29,13 +45,11 @@ Clipboard.propTypes = {
   description: PropTypes.string.isRequired,
   text: PropTypes.string,
   outline: PropTypes.bool,
-  visible: PropTypes.bool,
 };
 
 Clipboard.defaultProps = {
   text: null,
   outline: false,
-  visible: true,
 };
 
 export { Clipboard as default };
