eaf9bbf45dc97ef7e32e2dee190c095059f341d6
Sarah Clements <sclements313@gmail.com>
2022-02-14 17:25:20 +0000

    Taskcluster login fix on prototype and tc-staging (#7383)
    * Remove forward slash in tcClientIdMap const

--- ui/taskcluster-auth-callback/constants.js
+++ ui/taskcluster-auth-callback/constants.js
@@ -3,10 +3,10 @@ import { tcAuthCallbackUrl } from '../helpers/url';
 export const tcClientIdMap = {
   'https://treeherder.mozilla.org': 'production',
   'https://treeherder.allizom.org': 'stage',
-  'https://prototype.treeherder.nonprod.cloudops.mozgcp.net/': 'dev',
+  'https://prototype.treeherder.nonprod.cloudops.mozgcp.net': 'dev',
   'http://localhost:5000': 'localhost-5000',
   'http://localhost:8000': 'localhost-8000',
-  'https://tc-staging.treeherder.nonprod.cloudops.mozgcp.net/':
+  'https://tc-staging.treeherder.nonprod.cloudops.mozgcp.net':
     'taskcluster-staging',
 };
 
