f272922e657be9b7616962f44ad169b1d6f9e51f
Valentin Rigal <rigal@teklia.com>
2022-05-11 10:59:59 +0200

    Copy tables with no index nor constraint

--- treeherder/perf/migrations/0043_perf_datum_bigint_table.py
+++ treeherder/perf/migrations/0043_perf_datum_bigint_table.py
@@ -4,7 +4,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
     """Creates a copy of the current performance_datum table
     The primary key is updated from INT(11) to BIGINT(20) corresponding to BigAutoField
-    This migration is a preliminary work to https://bugzilla.mozilla.org/show_bug.cgi?id=1343328
+    perf_multicommitdatum temporarily flag a performance datum element
     """
 
     dependencies = [
@@ -14,12 +14,26 @@ class Migration(migrations.Migration):
     operations = [
         migrations.RunSQL(
             [
-                # Copy performance_datum table structure
-                'CREATE TABLE performance_datum_new LIKE performance_datum',
-                # Update the PK to bigint
-                'ALTER TABLE performance_datum_new CHANGE id id BIGINT(20) AUTO_INCREMENT',
+                # Copy performance_datum table structure with no index nor constraint
+                # ID column is updated to store a bigint, PK index and AUTO_INCREMENT will be added later on
+                (
+                    'CREATE TABLE `performance_datum_new` ('
+                    '    `id` bigint(20) NOT NULL,'
+                    '    `value` double NOT NULL,'
+                    '    `push_timestamp` datetime(6) NOT NULL,'
+                    '    `job_id` bigint(20) DEFAULT NULL,'
+                    '    `push_id` int(11) NOT NULL,'
+                    '    `repository_id` int(11) NOT NULL,'
+                    '    `signature_id` int(11) NOT NULL'
+                    ')'
+                ),
+                # Copy perf_multicommitdatum table as it has a FK to performance_datum
+                'CREATE TABLE `perf_multicommitdatum_new` (`perf_datum_id` bigint(20) NOT NULL)',
             ],
-            reverse_sql='DROP TABLE IF EXISTS performance_datum_new',
+            reverse_sql=[
+                'DROP TABLE IF EXISTS performance_datum_new',
+                'DROP TABLE IF EXISTS perf_multicommitdatum_new',
+            ],
             elidable=False,
         )
     ]
