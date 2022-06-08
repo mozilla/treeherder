7c21449e2d9978d46cdfb0636bbddce36b8e3ec9
Valentin Rigal <rigal@teklia.com>
2022-06-08 11:42:03 +0200

    Migration to drop multicommitdatum table

---
+++ treeherder/perf/migrations/0043_drop_multicommitdatum.py
@@ -0,0 +1,30 @@
+from django.db import migrations
+
+
+def check_no_multicommitdatum(apps, schema_editor):
+    MultiCommitDatum = apps.get_model('perf', 'MultiCommitDatum')
+    assert (
+        not MultiCommitDatum.objects.exists()
+    ), "MultiCommitDatum table must be empty to migrate PerformanceDatum PK"
+
+
+class Migration(migrations.Migration):
+    """This migration drops the perf_multicommitdatum table in order to perform
+    performance_datum PK migration from INT(11) to BIGINT(20) using an external
+    tool, percona toolkit's online schema change (pt-osc).
+    Any access to MultiCommitDatum may create errors before migration perf.0044 passed.
+    """
+
+    dependencies = [
+        ('perf', '0042_backfillrecord_new_fields'),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            check_no_multicommitdatum,
+            reverse_code=migrations.RunPython.noop,
+        ),
+        migrations.DeleteModel(
+            name='MultiCommitDatum',
+        ),
+    ]
