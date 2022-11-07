0b848bc4cfeaa5132b8b2dcb294e5cd299d2a1de
Joel Maher <joel.maher@gmail.com>
2022-11-07 13:36:35 -0800

    Adjust groupsummary to use group.status and be optimized

--- treeherder/webapp/api/serializers.py
+++ treeherder/webapp/api/serializers.py
@@ -322,9 +322,9 @@ class JobTypeNameField(serializers.Field):
 
 
 class GroupNameSerializer(serializers.ModelSerializer):
-    group_name = serializers.CharField(source="groups__name")
-    job_type_name = JobTypeNameField(source="job__job_type__name")
-    job_result = serializers.CharField(source="job__result")
+    group_name = serializers.CharField(source="job_log__groups__name")
+    job_type_name = JobTypeNameField(source="job_type__name")
+    group_status = serializers.CharField(source="job_log__group_result__status")
     job_count = serializers.IntegerField()
 
     class Meta:
@@ -332,7 +332,7 @@ class GroupNameSerializer(serializers.ModelSerializer):
         fields = (
             'group_name',
             'job_type_name',
-            'job_result',
+            'group_status',
             'job_count',
         )
 
