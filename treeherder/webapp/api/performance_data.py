5a4a2d2b32d8a9d6b80a6e642b1c0c572b369934
bacasandrei <beatrice.raluca.acasandrei@gmail.com>
2023-02-14 15:04:42 +0200

    Update the summary's status correctly before getting the data

--- treeherder/webapp/api/performance_data.py
+++ treeherder/webapp/api/performance_data.py
@@ -471,6 +471,19 @@ class PerformanceAlertSummaryViewSet(viewsets.ModelViewSet):
     ordering = ('-created', '-id')
     pagination_class = AlertSummaryPagination
 
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        for instance in queryset:
+            instance.update_status()
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
     def create(self, request, *args, **kwargs):
         data = request.data
 
