mozilla/treeherder

commit 0b856ec03fc25e6127981006f4c9042f3f078b95

Actually retrieve the latest classification, not the first (#7634)
we are not sure why CI is failing for such a small change.

ui/job-view/details/DetailsPanel.jsx
@@ -413,7 +413,7 @@ class DetailsPanel extends React.Component {
               logParseStatus={logParseStatus}
               jobDetailLoading={jobDetailLoading}
               latestClassification={
-                classifications.length ? classifications[0] : null
+                classifications.length ? classifications[classifications.length - 1] : null
               }
               logViewerUrl={logViewerUrl}
               logViewerFullUrl={logViewerFullUrl}

commit 0628f49ee54ea5cbaadf677e844d170147d157e6

Fix linting issue caused by 0b856ec03fc25e6127981006f4c9042f3f078b95

ui/job-view/details/DetailsPanel.jsx
@@ -413,7 +413,9 @@ class DetailsPanel extends React.Component {
               logParseStatus={logParseStatus}
               jobDetailLoading={jobDetailLoading}
               latestClassification={
-                classifications.length ? classifications[classifications.length - 1] : null
+                classifications.length
+                  ? classifications[classifications.length - 1]
+                  : null
               }
               logViewerUrl={logViewerUrl}
               logViewerFullUrl={logViewerFullUrl}