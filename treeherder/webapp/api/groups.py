873711b2ff9da7875593ede67b5086fb15d293ac
Eli Spicer <spicefe@gmail.com>
2023-06-10 13:19:08 -0400

    Fixed black to python 3.9

--- treeherder/webapp/api/groups.py
+++ treeherder/webapp/api/groups.py
@@ -35,8 +35,8 @@ class SummaryByGroupName(generics.ListAPIView):
 
         if 'enddate' in request.query_params:
             enddate = request.query_params['enddate']
-
-        if not enddate or not re.match(r'^[0-9]{4}-[0-9]{2}-[0-9]{2}$', enddate):
+        # Working on code here
+        if not enddate or not re.match(r'^[0-9]{4}-[0-9]{2}-[0-9]{2}$', enddate) or ((enddate - startdate).days > 1):
             enddate = startdate + datetime.timedelta(days=1)
         else:
             enddate = datetime.datetime.strptime(enddate, "%Y-%m-%d")
