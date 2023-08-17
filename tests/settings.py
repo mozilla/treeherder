ccae22a247df568ef9100a71d09fcb8274f986dd
Sebastian Hengst <aryx.github@gmx-topmail.de>
2023-08-18 00:23:41 +0200

    Bug 1849257 - disable django debug toolbar for test execution

--- tests/settings.py
+++ tests/settings.py
@@ -27,4 +27,11 @@ AUTHENTICATION_BACKENDS = ('treeherder.auth.backends.AuthBackend',)
 # For Push Health Usage dashboard
 NEW_RELIC_INSIGHTS_API_KEY = "123"
 
+# This controls whether the Django debug toolbar should be shown or not
+# https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#show-toolbar-callback
+# "You can provide your own function callback(request) which returns True or False."
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': lambda request: False,
+}
+
 INSTALLED_APPS.remove('django.contrib.staticfiles')  # noqa: F405
