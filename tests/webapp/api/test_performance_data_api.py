8a4cbfc7971ab40506031b36998d05704b514e91
Sebastian Hengst <aryx.github@gmx-topmail.de>
2023-08-29 22:47:17 +0200

    Skip test_alert_summary_tasks_get with PostgreSQL until fixed

--- tests/webapp/api/test_performance_data_api.py
+++ tests/webapp/api/test_performance_data_api.py
@@ -1,6 +1,7 @@
 import copy
 import pytest
 import datetime
+from django.conf import settings
 from django.urls import reverse
 from collections import defaultdict
 
@@ -481,6 +482,10 @@ def test_filter_data_by_signature(
             assert resp.data[signature.signature_hash][0]['value'] == float(i)
 
 
+@pytest.mark.skipif(
+    settings.DATABASES['default']['ENGINE'] != 'django.db.backends.mysql',
+    reason='Fails frequently with PostgreSQL',
+)
 def test_perf_summary(client, test_perf_signature, test_perf_data):
     query_params1 = (
         '?repository={}&framework={}&interval=172800&no_subtests=true&revision={}'.format(
@@ -528,6 +533,10 @@ def test_perf_summary(client, test_perf_signature, test_perf_data):
     assert resp2.json() == expected
 
 
+@pytest.mark.skipif(
+    settings.DATABASES['default']['ENGINE'] != 'django.db.backends.mysql',
+    reason='Fails frequently with PostgreSQL',
+)
 def test_data_points_from_same_push_are_ordered_chronologically(
     client, test_perf_signature, test_perf_data
 ):
@@ -711,6 +720,10 @@ def test_filter_out_retriggers():
     assert filtered_data == no_retriggers_data
 
 
+@pytest.mark.skipif(
+    settings.DATABASES['default']['ENGINE'] != 'django.db.backends.mysql',
+    reason='Fails with PostgreSQL',
+)
 def test_alert_summary_tasks_get(client, test_perf_alert_summary, test_perf_data):
     create_perf_alert(
         summary=test_perf_alert_summary,
