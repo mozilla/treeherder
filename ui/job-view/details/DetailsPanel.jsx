fc8010cf43fd793769ae8253470170cc057ac247
Gregory Mierzwinski <gmierz1@live.ca>
2023-12-11 07:29:14 -0500

    Include default replicates setting in DetailsPanel View url.

--- ui/job-view/details/DetailsPanel.jsx
+++ ui/job-view/details/DetailsPanel.jsx
@@ -304,7 +304,8 @@ class DetailsPanel extends React.Component {
                       d.signature_id,
                       1,
                       d.series.frameworkId,
-                    ]}&selected=${[d.signature_id, d.id]}`,
+                    ]}&selected=${[d.signature_id, d.id]}
+                    &replicates=0`,
                     shouldAlert: d.series.should_alert,
                     value: d.value,
                     measurementUnit: d.series.measurementUnit,
