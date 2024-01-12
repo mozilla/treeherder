7b5fa21720d7efe6ef5adab43f0ab931472f896f
Sebastian Hengst <aryx.github@gmx-topmail.de>
2024-01-12 22:28:10 +0100

    Bug 1890656 - use new Treestatus on non-production instances

--- ui/models/treeStatus.js
+++ ui/models/treeStatus.js
@@ -1,8 +1,29 @@
-const uri = 'https://treestatus.mozilla-releng.net/trees/';
+import { thHosts } from '../helpers/constants';
+
+let _treeStatusApiUrl;
+let _treeStatusUiUrl;
+for (const [hostPrettyName, config] of Object.entries(thHosts)) {
+  if (config.host === window.location.hostname) {
+    _treeStatusApiUrl = thHosts[hostPrettyName].treestatus.apiUrl;
+    _treeStatusUiUrl = thHosts[hostPrettyName].treestatus.uiUrl;
+  }
+}
+if (_treeStatusApiUrl === undefined) {
+  _treeStatusApiUrl = thHosts.default.treestatus.apiUrl;
+}
+if (_treeStatusUiUrl === undefined) {
+  _treeStatusUiUrl = thHosts.default.treestatus.uiUrl;
+}
+
+export function treeStatusUiUrl() {
+  return _treeStatusUiUrl;
+}
+
+const apiUrl = `${_treeStatusApiUrl}trees/`;
 
 export default class TreeStatusModel {
   static get(repoName) {
-    return fetch(`${uri}${repoName}`)
+    return fetch(`${apiUrl}${repoName}`)
       .then(async (resp) => {
         if (resp.ok) {
           return resp.json();
@@ -24,8 +45,7 @@ export default class TreeStatusModel {
         Promise.resolve({
           result: {
             status: 'error',
-            message_of_the_day:
-              'Unable to connect to the https://treestatus.mozilla-releng.net/trees/ API',
+            message_of_the_day: `Unable to connect to the ${apiUrl} API`,
             reason: reason.toString(),
             tree: repoName,
           },
