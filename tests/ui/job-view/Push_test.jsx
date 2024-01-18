a45dc9596faeb28d1c4575bec5a4ed0c9d03888c
Sebastian Hengst <aryx.github@gmx-topmail.de>
2024-01-18 19:18:45 +0100

    Bug 1875340 - remove incomplete test checking for test manifests of a job
    1. This was incomplete: existing code was broken and needed several fixes, and
       the second part was missing.
    2. The test paths are not a property of Job for performance reasons

--- tests/ui/job-view/Push_test.jsx
+++ tests/ui/job-view/Push_test.jsx
@@ -1,20 +1,7 @@
-import React from 'react';
-import fetchMock from 'fetch-mock';
-import { Provider } from 'react-redux';
-import { render, cleanup, waitFor } from '@testing-library/react';
-import { gzip } from 'pako';
-
-import { getProjectUrl, replaceLocation } from '../../../ui/helpers/location';
-import FilterModel from '../../../ui/models/filter';
-import pushListFixture from '../mock/push_list';
-import jobListFixture from '../mock/job_list/job_2';
-import configureStore from '../../../ui/job-view/redux/configureStore';
-import Push, {
+import {
   transformTestPath,
   transformedPaths,
 } from '../../../ui/job-view/pushes/Push';
-import { getApiUrl } from '../../../ui/helpers/url';
-import { findInstance } from '../../../ui/helpers/job';
 
 const manifestsByTask = {
   'test-linux1804-64/debug-mochitest-devtools-chrome-e10s-1': [
@@ -101,93 +88,5 @@ describe('Transformations', () => {
     tests.forEach(({ path, expected }) => {
       expect(transformTestPath(path)).toEqual(expected);
     });
-  });
-});
-
-describe('Push', () => {
-  const repoName = 'autoland';
-  const currentRepo = {
-    name: repoName,
-    getRevisionHref: () => 'foo',
-    getPushLogHref: () => 'foo',
-  };
-  const push = pushListFixture.results[1];
-  const revision = 'd5b037941b0ebabcc9b843f24d926e9d65961087';
-  const testPush = (store, filterModel) => (
-    <Provider store={store}>
-      <div id="th-global-content">
-        <Push
-          push={push}
-          isLoggedIn={false}
-          currentRepo={currentRepo}
-          filterModel={filterModel}
-          notificationSupported={false}
-          duplicateJobsVisible={false}
-          groupCountsExpanded={false}
-          isOnlyRevision={push.revision === revision}
-          pushHealthVisibility="None"
-          getAllShownJobs={() => {}}
-        />
-      </div>
-    </Provider>
-  );
-
-  beforeAll(async () => {
-    fetchMock.get(getProjectUrl('/push/?full=true&count=10', repoName), {
-      ...pushListFixture,
-      results: pushListFixture.results[1],
-    });
-    fetchMock.mock(
-      getApiUrl('/jobs/?push_id=511137', repoName),
-      jobListFixture,
-    );
-    const tcUrl =
-      'https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/gecko.v2.autoland.revision.d5b037941b0ebabcc9b843f24d926e9d65961087.taskgraph.decision/artifacts/public';
-    // XXX: Fix this to re-enable test
-    // I need to figure out the right options to get a gzip blob
-    fetchMock.get(`${tcUrl}/manifests-by-task.json.gz`, {
-      body: new Blob(await gzip(JSON.stringify(manifestsByTask)), {
-        type: 'application/gzip',
-      }),
-      sendAsJson: false,
-    });
-  });
-
-  afterAll(() => {
-    fetchMock.reset();
-  });
-
-  afterEach(() => {
-    cleanup();
-    replaceLocation({});
-  });
-
-  // eslint-disable-next-line jest/no-disabled-tests
-  test.skip('jobs should have test_path field to filter', async () => {
-    const { store } = configureStore();
-    const { getByText } = render(testPush(store, new FilterModel()));
-
-    const validateJob = async (name, testPaths) => {
-      const jobEl = await waitFor(() => getByText(name));
-      // Fetch the React instance of an object from a DOM element.
-      const { props } = findInstance(jobEl);
-      const { job } = props;
-      expect(job.test_paths).toStrictEqual(testPaths);
-    };
-
-    await validateJob('Jit8', []);
-    // XXX: It should be returning test paths instead of manifest paths
-    await validateJob('dt1', [
-      'devtools/client/framework/browser-toolbox/test/browser.ini',
-      'devtools/client/framework/test/browser.ini',
-      'devtools/client/framework/test/metrics/browser_metrics_inspector.ini',
-      'devtools/client/inspector/changes/test/browser.ini',
-      'devtools/client/inspector/extensions/test/browser.ini',
-      'devtools/client/inspector/markup/test/browser.ini',
-      'devtools/client/jsonview/test/browser.ini',
-      'devtools/client/shared/test/browser.ini',
-      'devtools/client/styleeditor/test/browser.ini',
-      'devtools/client/webconsole/test/node/fixtures/stubs/stubs.ini',
-    ]);
   });
 });
