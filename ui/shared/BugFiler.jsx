f4bc6841e276cce2bc4fd6401303dff4982049a6
Sebastian Hengst <aryx.github@gmx-topmail.de>
2024-04-24 11:46:36 +0200

    Bug 1866594 - fix bugfiler to use test name in summary if crash signature missing

--- ui/shared/BugFiler.jsx
+++ ui/shared/BugFiler.jsx
@@ -218,7 +218,7 @@ export class BugFilerClass extends React.Component {
         ].some((regexp) => regexp.test(summaryString));
       }
 
-      if (crashSignatures) {
+      if (crashSignatures.length > 0) {
         isTestPath = false;
         const parts = summaryString.split(' | ');
         summaryString = `${parts[0]} | single tracking bug`;
