38fae468d5c88325389d8d353afb208410e6c947
Sebastian Hengst <aryx.github@gmx-topmail.de>
2024-06-12 16:35:35 +0200

    Bug 1901721 - Add support for mozilla-esr128 and comm-esr128 repositories

--- ui/intermittent-failures/constants.js
+++ ui/intermittent-failures/constants.js
@@ -4,10 +4,12 @@ export const treeOptions = [
   'trunk',
   'mozilla-central',
   'mozilla-esr115',
+  'mozilla-esr128',
   'autoland',
   'firefox-releases',
   'comm-central',
   'comm-esr115',
+  'comm-esr128',
   'comm-releases',
   'fenix',
 ];
