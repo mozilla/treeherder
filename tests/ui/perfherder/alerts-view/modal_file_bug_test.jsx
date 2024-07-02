8962663af027de85b4690390bbd72ba52960909b
Valentin Rigal <rigal@teklia.com>
2024-07-02 16:22:00 +0200

    Lint fix

--- tests/ui/perfherder/alerts-view/modal_file_bug_test.jsx
+++ tests/ui/perfherder/alerts-view/modal_file_bug_test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, fireEvent, waitFor } from '@testing-library/react';
-// eslint-disable-next-line no-unused-vars,import/no-extraneous-dependencies
+// eslint-disable-next-line no-unused-vars
 import { getByText } from '@testing-library/dom';
 
 import FileBugModal from '../../../../ui/perfherder/alerts/FileBugModal';
