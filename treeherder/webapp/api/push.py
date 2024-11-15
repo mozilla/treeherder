dfa4b04f5569359b7b4bbe36ea4bec5cfabf3851
beatrice-acasandrei <69891317+beatrice-acasandrei@users.noreply.github.com>
2024-11-15 12:09:13 +0200

    Bug 1902029 - Search input criteria  of  12-40 characters is too restrictive (#8258)
    * Add new field author_contains to PushViewSet
    
    * Add iexact to the existing author param
    
    * Add test coverage

--- treeherder/webapp/api/push.py
+++ treeherder/webapp/api/push.py
@@ -155,9 +155,13 @@ class PushViewSet(viewsets.ViewSet):
         if author:
             if author.startswith("-"):
                 author = author[1::]
-                pushes = pushes.exclude(author=author)
+                pushes = pushes.exclude(author__iexact=author)
             else:
-                pushes = pushes.filter(author=author)
+                pushes = pushes.filter(author__iexact=author)
+
+        author_contains = filter_params.get("author_contains")
+        if author_contains:
+            pushes = pushes.filter(author__icontains=author_contains)
 
         if filter_params.get("hide_reviewbot_pushes") == "true":
             pushes = pushes.exclude(author="reviewbot")
