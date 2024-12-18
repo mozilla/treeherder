9606a5d6c4066968d6a02863c73bc75d16fe8019
Olivier Giorgis <ogiorgis@logilab.fr>
2024-12-18 09:25:59 +0100

    fix: remove useless accumulation of test variant

--- treeherder/intermittents_commenter/commenter.py
+++ treeherder/intermittents_commenter/commenter.py
@@ -65,7 +65,7 @@ class Commenter:
             change_whiteboard = None
             priority = 0
             rank = top_bugs.index(bug_id) + 1 if self.weekly_mode and bug_id in top_bugs else None
-            test_variants |= bug_stats[bug_id]["test_variants"]
+            test_variants = bug_stats[bug_id]["test_variants"]
 
             if bugs_info and bug_id in bugs_info:
                 if self.weekly_mode:
