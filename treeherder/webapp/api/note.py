3f682542218622744e74bb3d71fc414a097106a6
Joel Maher <joel.maher@gmail.com>
2025-03-11 07:25:20 -0700

    Revert "disable new failure lines cache for performance issues (#8526)"
    This reverts commit d7bb1b40aee891c707658177983d4921da6eacf3.

--- treeherder/webapp/api/note.py
+++ treeherder/webapp/api/note.py
@@ -1,12 +1,18 @@
 import logging
 
+from django.core.cache import caches
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.exceptions import ParseError
 from rest_framework.response import Response
 from rest_framework.status import HTTP_404_NOT_FOUND
 
-from treeherder.model.models import Job, JobNote, Push
+from treeherder.model.error_summary import (
+    LINE_CACHE_TIMEOUT,
+    cache_clean_error_line,
+    get_cleaned_line,
+)
+from treeherder.model.models import Job, JobNote, Push, TextLogError
 
 from .serializers import JobNoteDetailSerializer, JobNoteSerializer
 
@@ -73,9 +79,9 @@ class NoteViewSet(viewsets.ViewSet):
         """
         current_job = Job.objects.get(repository__name=project, id=int(request.data["job_id"]))
         fc_id = int(request.data["failure_classification_id"])
-        # revision = None
-        # if "text" in request.data:
-        #     revision = request.data["text"]
+        revision = None
+        if "text" in request.data:
+            revision = request.data["text"]
         JobNote.objects.create(
             job=current_job,
             failure_classification_id=fc_id,
@@ -83,33 +89,32 @@ class NoteViewSet(viewsets.ViewSet):
             text=request.data.get("text", ""),
         )
 
-        # disabled for performance issues with database
-        # if fc_id == 2:  # this is for fixed_by_commit (backout | follow_up_commit)
-        #     # remove cached failure line counts
-        #     db_cache = caches["db_cache"]
-        #     line_cache_key = "error_lines"
-        #     line_cache = db_cache.get(line_cache_key)
-        #     date = current_job.submit_time.date().isoformat()
-        #     if line_cache and date in line_cache.keys():
-        #         for err in TextLogError.objects.filter(job=current_job):
-        #             cache_clean_line = cache_clean_error_line(get_cleaned_line(err.line))
-        #             if cache_clean_line in line_cache[date].keys():
-        #                 line_cache[date][cache_clean_line] -= 1
-        #                 if (
-        #                     cache_clean_line in line_cache[date]["new_lines"].keys()
-        #                     and revision
-        #                     and line_cache[date]["new_lines"][cache_clean_line] == revision
-        #                 ):
-        #                     del line_cache[date]["new_lines"][cache_clean_line]
-        #                 try:
-        #                     db_cache.set(line_cache_key, line_cache, LINE_CACHE_TIMEOUT)
-        #                 except Exception as e:
-        #                     logger.error(
-        #                         "error caching error_lines for job %s: %s",
-        #                         current_job.id,
-        #                         e,
-        #                         exc_info=True,
-        #                     )
+        if fc_id == 2:  # this is for fixed_by_commit (backout | follow_up_commit)
+            # remove cached failure line counts
+            db_cache = caches["db_cache"]
+            line_cache_key = "error_lines"
+            line_cache = db_cache.get(line_cache_key)
+            date = current_job.submit_time.date().isoformat()
+            if line_cache and date in line_cache.keys():
+                for err in TextLogError.objects.filter(job=current_job):
+                    cache_clean_line = cache_clean_error_line(get_cleaned_line(err.line))
+                    if cache_clean_line in line_cache[date].keys():
+                        line_cache[date][cache_clean_line] -= 1
+                        if (
+                            cache_clean_line in line_cache[date]["new_lines"].keys()
+                            and revision
+                            and line_cache[date]["new_lines"][cache_clean_line] == revision
+                        ):
+                            del line_cache[date]["new_lines"][cache_clean_line]
+                        try:
+                            db_cache.set(line_cache_key, line_cache, LINE_CACHE_TIMEOUT)
+                        except Exception as e:
+                            logger.error(
+                                "error caching error_lines for job %s: %s",
+                                current_job.id,
+                                e,
+                                exc_info=True,
+                            )
 
         return Response({"message": "note stored for job {}".format(request.data["job_id"])})
 
