0cfa38e8446a8c2ac0a68909f6a9ec5fd67f58a3
Florin Bilt <fbilt@mozilla.com>
2025-03-13 16:44:26 +0200

    change label

--- ui/perfherder/graphs/GraphTooltip.jsx
+++ ui/perfherder/graphs/GraphTooltip.jsx
@@ -327,7 +327,7 @@ const GraphTooltip = ({
               {`Push time: ${toMercurialDateStr(dataPointDetails.x)}`}
             </p>
             <p className="small text-white pt-2">
-              {`Retrigger time: ${toMercurialDateStr(
+              {`Trigger date: ${toMercurialDateStr(
                 dataPointDetails.retrigger_time,
               )}`}
             </p>
