2424ddcdfdd19a98b14af4eeb979aad6dfbb6cc0
Sebastian Hengst <aryx.github@gmx-topmail.de>
2025-05-21 23:34:15 +0200

    Bug 1967851 - bug created for failure line must be used in task annotation

--- ui/shared/tabs/failureSummary/FailureSummaryTab.jsx
+++ ui/shared/tabs/failureSummary/FailureSummaryTab.jsx
@@ -84,10 +84,10 @@ class FailureSummaryTab extends React.Component {
     }));
   };
 
-  bugFilerCallback = (data) => {
+  bugFilerCallback = async (data) => {
     const { addBug } = this.props;
 
-    addBug({ id: data.id, newBug: data.id });
+    await addBug({ id: data.id, newBug: data.id });
     window.dispatchEvent(new CustomEvent(thEvents.saveClassification));
     // Open the newly filed bug in a new tab or window for further editing
     window.open(data.url);
