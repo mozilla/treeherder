09e685ab718798608a80951b00bcd0afbe3bff51
Bastien Orivel <eijebong@bananium.fr>
2025-06-05 21:57:12 +0200

    Bug 1970693 - Add support for mozilla-esr140 and comm-esr140 repositories (#8749)

--- ui/intermittent-failures/constants.js
+++ ui/intermittent-failures/constants.js
@@ -5,11 +5,13 @@ export const treeOptions = [
   'mozilla-central',
   'mozilla-esr115',
   'mozilla-esr128',
+  'mozilla-esr140',
   'autoland',
   'firefox-releases',
   'comm-central',
   'comm-esr115',
   'comm-esr128',
+  'comm-esr140',
   'comm-releases',
   'fenix',
 ];
