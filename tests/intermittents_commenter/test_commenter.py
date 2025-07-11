9bdcf9d9f03d9de022c2c7457c8fdeef8fda5924
Joel Maher <joel.maher@gmail.com>
2025-07-11 08:58:34 -0700

    Add descriptive text to intermittent comment when we have available runcount data. (#8835)

--- tests/intermittents_commenter/test_commenter.py
+++ tests/intermittents_commenter/test_commenter.py
@@ -92,6 +92,50 @@ def test_intermittents_commenter_with_failures(
     assert comment_params[0]["changes"]["comment"]["body"] == expected_comment
 
 
+@responses.activate
+def test_intermittents_commenter_with_no_runcount(
+    bug_data_with_5_failures,
+    mock_test_variants_firefoxci_request,
+    mock_test_manifests_firefoxci_request,
+    mock_testrun_matrix_firefoxci_request,
+    mock_summary_groups_request,
+):
+    startday = "2025-01-01"
+    endday = "2025-05-10"
+    alt_startday = startday
+    alt_endday = endday
+
+    process = Commenter(weekly_mode=True, dry_run=True)
+    params = {"include_fields": "product%2C+component%2C+priority%2C+whiteboard%2C+id"}
+    url = "{}/rest/bug?id={}&include_fields={}".format(
+        settings.BZ_API_URL, bug_data_with_5_failures["bug_id"], params["include_fields"]
+    )
+
+    content = {
+        "bugs": [
+            {
+                "component": "General",
+                "priority": "P3",
+                "product": "Testing",
+                "whiteboard": "[stockwell infra] [see summary at comment 92]",
+                "id": bug_data_with_5_failures["bug_id"],
+            }
+        ],
+        "faults": [],
+    }
+
+    responses.add(responses.Response(method="GET", url=url, json=content, status=200))
+
+    resp = process.fetch_bug_details(bug_data_with_5_failures["bug_id"])
+    assert resp == content["bugs"]
+
+    comment_params = process.generate_bug_changes(startday, endday, alt_startday, alt_endday)
+
+    with open("tests/intermittents_commenter/expected_comment_no_runcount.text") as comment:
+        expected_comment = comment.read()
+    assert comment_params[0]["changes"]["comment"]["body"] == expected_comment
+
+
 def test_get_test_variants(mock_test_variants_firefoxci_request):
     process = Commenter(weekly_mode=True, dry_run=True)
     test_suite = "mochitest-browser-chrome-spi-nw-10"
