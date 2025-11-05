77062ebda96435f0f3440701a48ea5c2775d5621
Gregory Mierzwinski <gmierz1@live.ca>
2025-11-05 10:38:47 -0500

    Use _emails_left for max number of emails to redo.

--- treeherder/perf/auto_perf_sheriffing/telemetry_alerting/alert_manager.py
+++ treeherder/perf/auto_perf_sheriffing/telemetry_alerting/alert_manager.py
@@ -213,7 +213,7 @@ class TelemetryAlertManager(AlertManager):
         # Limit to 50 emails due to fxci email rate limit
         alerts_no_emails = PerformanceTelemetryAlert.objects.filter(
             notified=False, bug_number__isnull=True
-        )[:50]
+        )[:self._emails_left()]
 
         for alert_row in alerts_no_emails:
             alert_no_email = TelemetryAlertFactory.construct_alert(alert_row)
