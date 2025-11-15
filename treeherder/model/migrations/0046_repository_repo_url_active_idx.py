e5b22aed4bf3586f338ba7dac777352491dfb091
Alexander Moses <moijes12@gmail.com>
2025-11-15 13:36:32 +0000

    Bug-1361570: Make migration update Django state
    Make migration update Django state and create index CONCURRENTLY (SeparateDatabaseAndState). This will fix the failing test.

--- treeherder/model/migrations/0046_repository_repo_url_active_idx.py
+++ treeherder/model/migrations/0046_repository_repo_url_active_idx.py
@@ -1,6 +1,8 @@
-from django.db import migrations
+from django.db import migrations, models
+
 
 class Migration(migrations.Migration):
+    # Use non-atomic so we can create the index CONCURRENTLY
     atomic = False
 
     dependencies = [
@@ -8,8 +10,18 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunSQL(
-            sql='CREATE INDEX CONCURRENTLY IF NOT EXISTS repo_url_active_idx ON repository (url, active_status);',
-            reverse_sql='DROP INDEX IF EXISTS repo_url_active_idx;',
+        migrations.SeparateDatabaseAndState(
+            database_operations=[
+                migrations.RunSQL(
+                    sql="CREATE INDEX CONCURRENTLY IF NOT EXISTS repo_url_active_idx ON repository (url, active_status);",
+                    reverse_sql="DROP INDEX IF EXISTS repo_url_active_idx;",
+                ),
+            ],
+            state_operations=[
+                migrations.AddIndex(
+                    model_name='repository',
+                    index=models.Index(fields=['url', 'active_status'], name='repo_url_active_idx'),
+                ),
+            ],
         ),
     ]
