5fad79113efcfc1bad5dc62c49c72f4f8109014c
Alex <moijes12@gmail.com>
2025-12-01 19:36:27 +0530

    Bug 1361570 - Add index on Repository (url, active_status) (#9070)
    * Bug-1361570: Add index on Repository (url, active_status)
    
    Add a composite index on Repository (url, active_status) to
    improve query performance for PushLoader.process().
    The index is create CONCURRENTLY to avoid locking table during
    production deployment.
    
    Resolves: Bug-1361570
    
    * Bug-1361570: Make migration update Django state
    
    Make migration update Django state and create index CONCURRENTLY (SeparateDatabaseAndState). This will fix the failing test.
    
    * Bug-1361570: Add new line to fix failing lint
    
    Add a new line at the end of the migration file to fix the failing linter.
    
    * Bug-1361570: Fix linting
    
    * Bug-1361570: Apply ruff to models.py

---
+++ treeherder/model/migrations/0046_repository_repo_url_active_idx.py
@@ -0,0 +1,27 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    # Use non-atomic so we can create the index CONCURRENTLY
+    atomic = False
+
+    dependencies = [
+        ("model", "0045_alter_failureline_expected_alter_failureline_level_and_more"),
+    ]
+
+    operations = [
+        migrations.SeparateDatabaseAndState(
+            database_operations=[
+                migrations.RunSQL(
+                    sql="CREATE INDEX CONCURRENTLY IF NOT EXISTS repo_url_active_idx ON repository (url, active_status);",
+                    reverse_sql="DROP INDEX IF EXISTS repo_url_active_idx;",
+                ),
+            ],
+            state_operations=[
+                migrations.AddIndex(
+                    model_name="repository",
+                    index=models.Index(fields=["url", "active_status"], name="repo_url_active_idx"),
+                ),
+            ],
+        ),
+    ]
