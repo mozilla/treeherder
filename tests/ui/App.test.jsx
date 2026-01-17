40ab1de6f98051c9752beb6910bf8ff245e1e2e1
Cameron Dawson <cameron@dawsoncode.com>
2026-01-17 13:12:10 -0800

    remove unnecessary unit tests

--- tests/ui/App.test.jsx
+++ tests/ui/App.test.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import fetchMock from 'fetch-mock';
-import { render, waitFor, screen } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import { Provider, ReactReduxContext } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 
 import App from '../../ui/App';
-import reposFixture from './mock/repositories';
-import pushListFixture from './mock/push_list';
 import { getApiUrl } from '../../ui/helpers/url';
 import { getProjectUrl } from '../../ui/helpers/location';
 import {
   configureStore,
   history,
 } from '../../ui/job-view/redux/configureStore';
+
+import reposFixture from './mock/repositories';
+import pushListFixture from './mock/push_list';
 
 /**
  * Tests for the main App component routing and URL transformation logic.
