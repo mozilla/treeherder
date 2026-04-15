mozilla/treeherder

commit a2c66add331fe774cd67f3bed01b2f8aa6d35084

Show "Open in Firefox Profiler" link for artifacts ending in .json.gz, too.

tests/ui/job-view/PerformanceTab_test.jsx
@@ -183,6 +183,35 @@ describe('PerformanceTab', () => {
     );
   });
 
+  test('perf test job with json.gz profile should show retrigger button and open button', async () => {
+    const { getByTestId } = render(
+      testPerformanceTab({
+        selectedJobFull: {
+          job_type_name:
+            'test-macosx1015-64-shippable-qr/opt-browsertime-something',
+          job_type_symbol: 'some',
+          job_group_name: 'Browsertime performance tests on Firefox',
+          hasSideBySide: false,
+        },
+        jobDetails: [
+          {
+            url: 'profile_something.json.gz',
+            value: 'profile_something.json.gz',
+          },
+        ],
+        perfJobDetail: [],
+      }),
+    );
+
+    const generateProfile = getByTestId('generate-profile');
+    expect(generateProfile.textContent).toBe('Re-trigger performance profile');
+
+    const openProfiler = getByTestId('open-profiler');
+    expect(openProfiler.href).toBe(
+      'https://profiler.firefox.com/from-url/profile_something.json.gz',
+    );
+  });
+
   test('perf test should use most relevant profile', async () => {
     const { getByTestId } = render(
       testPerformanceTab({

ui/job-view/details/tabs/PerformanceTab.jsx
@@ -98,7 +98,7 @@ class PerformanceTab extends React.PureComponent {
       return PROFILE_ZIP_RELEVANCE;
     }
 
-    if (!value.endsWith('.json')) {
+    if (!value.endsWith('.json') && !value.endsWith('.json.gz')) {
       return NO_PROFILE_RELEVANCE;
     }
 

commit 310586901f98283d31a3b8f38f72f7303c73804a

add PerfJobDetail mock data to new subtest in PerformanceTab
This fixes a regression from #9390.

tests/ui/job-view/PerformanceTab_test.jsx
@@ -199,7 +199,17 @@ describe('PerformanceTab', () => {
             value: 'profile_something.json.gz',
           },
         ],
-        perfJobDetail: [],
+        perfJobDetail: [
+          {
+            frameworkName: 'browsertime',
+            perfdocs: new Perfdocs(
+              'browsertime',
+              'outlook',
+              'macosx1015-64-shippable-qr',
+              'outlook ContentfulSpeedIndex opt cold fission webrender',
+            ),
+          },
+        ],
       }),
     );
 