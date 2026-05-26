62148b8b3fa0f42e52573a6eff02decbe67baeb0
Beatrice Acasandrei <bacasandrei@mozilla.com>
2026-05-26 21:03:48 +0300

    WIP

--- treeherder/webapp/api/performance_serializers.py
+++ treeherder/webapp/api/performance_serializers.py
@@ -461,6 +461,7 @@ class PerformanceQueryParamsSerializer(serializers.Serializer):
     all_data = OptionalBooleanField()
     replicates = OptionalBooleanField()
     no_retriggers = OptionalBooleanField()
+    include_missing_data = OptionalBooleanField()
 
     def validate(self, data):
         if (
@@ -526,6 +527,7 @@ class PerformanceSummarySerializer(serializers.ModelSerializer):
     job_ids = serializers.ListField(child=serializers.IntegerField(), default=[])
     data = PerformanceDatumSerializer(read_only=True, many=True, default=[])
     repository_name = serializers.CharField()
+    missing_data = serializers.ListField(child=serializers.DictField(), required=False)
 
     class Meta:
         model = PerformanceSignature
@@ -552,6 +554,7 @@ class PerformanceSummarySerializer(serializers.ModelSerializer):
             "alert_change_type",
             "alert_threshold",
             "submit_times",
+            "missing_data",
         ]
 
     def get_name(self, value):
