04eeb0e0da3a6bdbef4fb3075a9472122b788fe0
David Miculit <147625634+davidmiculit@users.noreply.github.com>
2026-05-29 11:28:15 +0300

    Bug 2038599 - Add bisection as a new tag option (#9561)
    * feat: add new bisection tag
    
    * refactor: more descriptive tooltip message

--- ui/perfherder/perf-helpers/constants.js
+++ ui/perfherder/perf-helpers/constants.js
@@ -152,6 +152,7 @@ export const tooltipMessages = {
   improvement: 'patch that generated an actual improvement',
   'regression-backedout': 'patch backed out due to causing regressions',
   'regression-fix': 'patch fixing a reported regression bug',
+  bisection: 'bisection identified the exact revision causing the performance shifts',
 };
 
 export const alertBackfillResultVisual = {
