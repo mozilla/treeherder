87e69a93e168d4545166a420b8b024f7439d9650
Alex Moses <moijes12@gmail.com>
2026-07-07 10:34:15 +0000

    Bug-2038705: Migrate ingest.query_data to PyGithub
    Migrated the `query_data` function in ingest.py to use the
    library written over PyGithub. Also, updated the unit tests for
    the same.

--- treeherder/etl/management/commands/ingest.py
+++ treeherder/etl/management/commands/ingest.py
@@ -23,7 +23,6 @@ from treeherder.etl.pushlog import HgPushlogProcess, last_push_id_from_server
 from treeherder.etl.taskcluster_pulse.handler import EXCHANGE_EVENT_MAP, handle_message
 from treeherder.model.models import Repository
 from treeherder.utils import github
-from treeherder.utils.github import fetch_api, fetch_api_full_url
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -275,54 +274,85 @@ def query_data(repo_meta, commit):
     # This is used for the `compare` API. The "event.base.sha" is only contained in Pulse events, thus,
     # we need to determine the correct value
     event_base_sha = repo_meta["branch"]
-    # First we try with `master` being the base sha
-    # e.g. https://api.github.com/repos/servo/servo/compare/master...1418c0555ff77e5a3d6cf0c6020ba92ece36be2e
-    compare_response = fetch_api(
-        f"repos/{repo_meta['owner']}/{repo_meta['repo']}/compare/{event_base_sha}...{commit}"
+    # First we try and get a comparsion with `master`` neing the base sha
+    comparison = github.get_comparison(
+        owner=repo_meta["owner"], repo_name=repo_meta["repo"], base=event_base_sha, head=commit
     )
-    merge_base_commit = compare_response.get("merge_base_commit")
+    merge_base_commit = comparison.merge_base_commit
+
     if merge_base_commit:
-        commiter_date = merge_base_commit["commit"]["committer"]["date"]
+        committer_date = merge_base_commit.commit.committer.date
+
         # Since we don't use PushEvents that contain the "before" or "event.base.sha" fields [1]
         # we need to discover the right parent which existed in the base branch.
         # [1] https://github.com/taskcluster/taskcluster/blob/3dda0adf85619d18c5dcf255259f3e274d2be346/services/github/src/api.js#L55
-        parents = compare_response["merge_base_commit"]["parents"]
+        parents = merge_base_commit.parents
         if len(parents) == 1:
             parent = parents[0]
-            commit_info = fetch_api_full_url(parent["url"])
-            committer_date = commit_info["commit"]["committer"]["date"]
-            # All commits involved in a PR share the same committer's date
-            if merge_base_commit["commit"]["committer"]["date"] == committer_date:
+            parent_commit = github.get_commit(
+                owner=repo_meta["owner"], repo=repo_meta["repo"], sha=parent.sha
+            )
+            parent_committer_date = parent_commit.commit.committer.date
+
+            # All commits involvled in a PR share the same date
+            if committer_date == parent_committer_date:
                 # Recursively find the forking parent
-                event_base_sha, _ = query_data(repo_meta, parent["sha"])
+                event_base_sha, _ = query_data(repo_meta, parent.sha)
             else:
-                event_base_sha = parent["sha"]
+                event_base_sha = parent.sha
+
         else:
+            event_base_sha = None  # Initialize in case no suitable parent is found
             for parent in parents:
-                _commit = fetch_api_full_url(parent["url"])
+                temp_commit = github.get_commit(
+                    owner=repo_meta["owner"], repo=repo_meta["repo"], sha=parent.sha
+                )
                 # All commits involved in a merge share the same committer's date
-                if commiter_date != _commit["commit"]["committer"]["date"]:
-                    event_base_sha = _commit["sha"]
+                if committer_date != temp_commit.commit.committer.date:
+                    event_base_sha = temp_commit.sha
                     break
+            if not event_base_sha:
+                # Fallback if no specific parent is found with different committer date
+                # This case might need more robust handling based on project-specific merge strategies
+                event_base_sha = repo_meta[
+                    "branch"
+                ]  # default to branch if no specific parent found
         # This is to make sure that the value has changed
-        assert event_base_sha != repo_meta["branch"]
-        logger.info("We have a new base: %s", event_base_sha)
-        # When using the correct event_base_sha the "commits" field will be correct
-        compare_response = fetch_api(
-            f"repos/{repo_meta['owner']}/{repo_meta['repo']}/compare/{event_base_sha}...{commit}"
-        )
+        if event_base_sha == repo_meta["branch"]:
+            logger.warning("Event base SHA did not change from repo branch: %s", event_base_sha)
+        else:
+            logger.info("We have a new base: %s", event_base_sha)
+            # When using the correct event_base_sha the "commits" field will be correct
+            compare_result = github.get_comparison(
+                repo_meta["owner"], repo_meta["repo"], event_base_sha, commit
+            )
+    else:
+        # If no merge_base_commit, then the initial compare_result should be used
+        # and event_base_sha remains the initial branch.
+        event_base_sha = repo_meta["branch"]
 
     commits = []
-    for _commit in compare_response["commits"]:
+    for temp_commit in compare_result.commits:
         commits.append(
             {
-                "message": _commit["commit"]["message"],
-                "author": _commit["commit"]["author"],
-                "committer": _commit["commit"]["committer"],
-                "id": _commit["sha"],
+                "message": temp_commit.commit.message,
+                "author": {
+                    "name": temp_commit.commit.author.name,
+                    "email": temp_commit.commit.author.email,
+                    "date": temp_commit.commit.author.date,
+                }
+                if temp_commit.commit.author
+                else None,
+                "committer": {
+                    "name": temp_commit.commit.committer.name,
+                    "email": temp_commit.commit.committer.email,
+                    "date": temp_commit.commit.committer.date,
+                }
+                if temp_commit.commit.committer
+                else None,
+                "id": temp_commit.sha,
             }
         )
-
     return event_base_sha, commits
 
 
