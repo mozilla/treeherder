ee74ae025dbfd756fa19e95b92bc8f308b425dfe
Florin Bilt <fbilt@mozilla.com>
2026-08-03 17:58:16 +0300

    Bug 2060249 - Shorten alert triage and bug due dates to 2 and 4 working days.

--- tests/model/test_time_to_triage.py
+++ tests/model/test_time_to_triage.py
@@ -11,8 +11,8 @@ def test_triage_due_alert_summary_created_monday(test_perf_alert_summary):
 
     test_perf_alert_summary.update_status()
 
-    # created monday isoweekday = 1 + OKR = 3 => 4
-    assert test_perf_alert_summary.triage_due_date.isoweekday() == THU
+    # created monday isoweekday = 1 + OKR = 2 => 3
+    assert test_perf_alert_summary.triage_due_date.isoweekday() == WED
 
 
 def test_triage_due_alert_summary_created_tuesday(test_perf_alert_summary):
@@ -23,8 +23,8 @@ def test_triage_due_alert_summary_created_tuesday(test_perf_alert_summary):
 
     test_perf_alert_summary.update_status()
 
-    # created tuesday isoweekday = 2 + OKR = 3 => 5
-    assert test_perf_alert_summary.triage_due_date.isoweekday() == FRI
+    # created tuesday isoweekday = 2 + OKR = 2 => 4
+    assert test_perf_alert_summary.triage_due_date.isoweekday() == THU
 
 
 def test_triage_due_alert_summary_created_wednesday(test_perf_alert_summary):
@@ -35,8 +35,8 @@ def test_triage_due_alert_summary_created_wednesday(test_perf_alert_summary):
 
     test_perf_alert_summary.update_status()
 
-    # created wednesday = 3 + OKR = 3 => 6 (saturday) => 1 (monday)
-    assert test_perf_alert_summary.triage_due_date.isoweekday() == MON
+    # created wednesday = 3 + OKR = 2 => 5
+    assert test_perf_alert_summary.triage_due_date.isoweekday() == FRI
 
 
 def test_triage_due_alert_summary_created_thursday(test_perf_alert_summary):
@@ -47,9 +47,8 @@ def test_triage_due_alert_summary_created_thursday(test_perf_alert_summary):
 
     test_perf_alert_summary.update_status()
 
-    # created thursday = 4 + OKR = 3 => 7 (sunday) => 1 (monday) + 1 day =2
-    # 1 (monday) + 1 day (1 day of OKR was saturday) = 2
-    assert test_perf_alert_summary.triage_due_date.isoweekday() == TUE
+    # created thursday = 4 + OKR = 2 => 6 (saturday) => 1 (monday)
+    assert test_perf_alert_summary.triage_due_date.isoweekday() == MON
 
 
 def test_triage_due_alert_summary_created_friday(test_perf_alert_summary):
@@ -60,9 +59,9 @@ def test_triage_due_alert_summary_created_friday(test_perf_alert_summary):
 
     test_perf_alert_summary.update_status()
 
-    # created friday = 5 + OKR = 3 => 8 => 1 (monday)
-    # 1 (monday) + 2 (2 days of OKR were weekend) = 3 (wednesday)
-    assert test_perf_alert_summary.triage_due_date.isoweekday() == WED
+    # created friday = 5 + OKR = 2 => 7 (sunday) => 1 (monday)
+    # 1 (monday) + 1 day (1 day of OKR was saturday) = 2 (tuesday)
+    assert test_perf_alert_summary.triage_due_date.isoweekday() == TUE
 
 
 def test_triage_due_alert_summary_created_saturday(test_perf_alert_summary):
@@ -73,8 +72,8 @@ def test_triage_due_alert_summary_created_saturday(test_perf_alert_summary):
 
     test_perf_alert_summary.update_status()
 
-    # created saturday = 6 => 1 (monday) + OKR = 3 => 4 (thursday)
-    assert test_perf_alert_summary.triage_due_date.isoweekday() == THU
+    # created saturday = 6 => 1 (monday) + OKR = 2 => 3 (wednesday)
+    assert test_perf_alert_summary.triage_due_date.isoweekday() == WED
 
 
 def test_triage_due_alert_summary_created_sunday(test_perf_alert_summary):
@@ -85,8 +84,8 @@ def test_triage_due_alert_summary_created_sunday(test_perf_alert_summary):
 
     test_perf_alert_summary.update_status()
 
-    # created sunday = 7 => 1 (monday) + OKR = 3 => 4 (thursday)
-    assert test_perf_alert_summary.triage_due_date.isoweekday() == THU
+    # created sunday = 7 => 1 (monday) + OKR = 2 => 3 (wednesday)
+    assert test_perf_alert_summary.triage_due_date.isoweekday() == WED
 
 
 def test_alert_summary_with_modified_created_date(test_perf_alert_summary):
@@ -97,16 +96,16 @@ def test_alert_summary_with_modified_created_date(test_perf_alert_summary):
 
     test_perf_alert_summary.update_status()
 
-    # created monday isoweekday = 1 + OKR = 3 => 4
-    assert test_perf_alert_summary.triage_due_date.isoweekday() == THU
+    # created monday isoweekday = 1 + OKR = 2 => 3
+    assert test_perf_alert_summary.triage_due_date.isoweekday() == WED
 
     test_perf_alert_summary.created = datetime.datetime.fromisoformat("2022-06-03")
 
     test_perf_alert_summary.update_status()
 
-    # created friday = 5 + OKR = 3 => 8 => 1 (monday)
-    # 1 (monday) + 2 (2 days of OKR were weekend) = 3 (wednesday)
-    assert test_perf_alert_summary.triage_due_date.isoweekday() == WED
+    # created friday = 5 + OKR = 2 => 7 (sunday) => 1 (monday)
+    # 1 (monday) + 1 day (1 day of OKR was saturday) = 2 (tuesday)
+    assert test_perf_alert_summary.triage_due_date.isoweekday() == TUE
 
 
 def test_bug_due_alert_summary_created_monday(test_perf_alert_summary):
@@ -117,8 +116,8 @@ def test_bug_due_alert_summary_created_monday(test_perf_alert_summary):
 
     test_perf_alert_summary.update_status()
 
-    # created monday isoweekday = 1 + OKR = 5 => 6 (saturday) => 1 (monday)
-    assert test_perf_alert_summary.bug_due_date.isoweekday() == MON
+    # created monday isoweekday = 1 + OKR = 4 => 5 (friday)
+    assert test_perf_alert_summary.bug_due_date.isoweekday() == FRI
 
 
 def test_bug_due_alert_summary_created_tuesday(test_perf_alert_summary):
@@ -129,9 +128,8 @@ def test_bug_due_alert_summary_created_tuesday(test_perf_alert_summary):
 
     test_perf_alert_summary.update_status()
 
-    # created tuesday isoweekday = 2 + OKR = 5 => 7 (saturday) => 1 (monday)
-    # 1 (monday) + 1 day (1 day of OKR was saturday) = 2 (tuesday)
-    assert test_perf_alert_summary.bug_due_date.isoweekday() == TUE
+    # created tuesday isoweekday = 2 + OKR = 4 => 6 (saturday) => 1 (monday)
+    assert test_perf_alert_summary.bug_due_date.isoweekday() == MON
 
 
 def test_bug_due_alert_summary_created_wednesday(test_perf_alert_summary):
@@ -142,9 +140,9 @@ def test_bug_due_alert_summary_created_wednesday(test_perf_alert_summary):
 
     test_perf_alert_summary.update_status()
 
-    # created wednesday = 3 + OKR = 5 => 1 (monday)
-    # 1 (monday) + 2 day (2 day of OKR was saturday) = 3 (wednesday)
-    assert test_perf_alert_summary.bug_due_date.isoweekday() == WED
+    # created wednesday = 3 + OKR = 4 => 7 (sunday) => 1 (monday)
+    # 1 (monday) + 1 day (1 day of OKR was saturday) = 2 (tuesday)
+    assert test_perf_alert_summary.bug_due_date.isoweekday() == TUE
 
 
 def test_bug_due_alert_summary_created_thursday(test_perf_alert_summary):
@@ -155,9 +153,9 @@ def test_bug_due_alert_summary_created_thursday(test_perf_alert_summary):
 
     test_perf_alert_summary.update_status()
 
-    # created thursday = 4 + OKR = 5 => 2 (tuesday)
-    # 2 (tuesday) + 2 day (2 day of OKR was saturday) = 4 (thursday)
-    assert test_perf_alert_summary.bug_due_date.isoweekday() == THU
+    # created thursday = 4 + OKR = 4 => 1 (monday)
+    # 1 (monday) + 2 days (2 days of OKR were weekend) = 3 (wednesday)
+    assert test_perf_alert_summary.bug_due_date.isoweekday() == WED
 
 
 def test_bug_due_alert_summary_created_friday(test_perf_alert_summary):
@@ -168,9 +166,9 @@ def test_bug_due_alert_summary_created_friday(test_perf_alert_summary):
 
     test_perf_alert_summary.update_status()
 
-    # created friday = 5 + OKR = 5 => 3 (wednesday)
-    # 3 (wednesday) + 2 (2 days of OKR were weekend) = 5 (friday)
-    assert test_perf_alert_summary.bug_due_date.isoweekday() == FRI
+    # created friday = 5 + OKR = 4 => 2 (tuesday)
+    # 2 (tuesday) + 2 days (2 days of OKR were weekend) = 4 (thursday)
+    assert test_perf_alert_summary.bug_due_date.isoweekday() == THU
 
 
 def test_bug_due_alert_summary_created_saturday(test_perf_alert_summary):
@@ -181,8 +179,8 @@ def test_bug_due_alert_summary_created_saturday(test_perf_alert_summary):
 
     test_perf_alert_summary.update_status()
 
-    # created saturday = 6 => 1 (monday) + OKR = 5 => 6 (saturday) => 1 (monday)
-    assert test_perf_alert_summary.bug_due_date.isoweekday() == MON
+    # created saturday = 6 => 1 (monday) + OKR = 4 => 5 (friday)
+    assert test_perf_alert_summary.bug_due_date.isoweekday() == FRI
 
 
 def test_bug_due_alert_summary_created_sunday(test_perf_alert_summary):
@@ -193,5 +191,5 @@ def test_bug_due_alert_summary_created_sunday(test_perf_alert_summary):
 
     test_perf_alert_summary.update_status()
 
-    # created sunday = 7 => 1 (monday) + OKR = 5 => 6 (saturday) => 1 (monday)
-    assert test_perf_alert_summary.bug_due_date.isoweekday() == MON
+    # created sunday = 7 => 1 (monday) + OKR = 4 => 5 (friday)
+    assert test_perf_alert_summary.bug_due_date.isoweekday() == FRI
