66f970341e3c43a9ab13b5f8a8d4b5765a014cea
Sebastian Hengst <aryx.github@gmx-topmail.de>
2026-09-24 02:11:56 +0200

    Revert "Upgrade Python 3.13 to 3.14 (#9324)" (#9889)
    This reverts commit f632ed475ffce975876337359be510c0f562b5c2.

--- treeherder/webapp/api/utils.py
+++ treeherder/webapp/api/utils.py
@@ -75,7 +75,8 @@ def get_artifact_list(root_url, task_id):
         artifacts = fetch_json(artifacts_url)
     except Exception as e:
         print(e)
-    return artifacts.get("artifacts", [])
+    finally:
+        return artifacts.get("artifacts", [])
 
 
 def get_profile_artifact_url(alert, metadata_key):
